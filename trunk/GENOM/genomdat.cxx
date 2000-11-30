cc49a003884b46b95091f48a025f0ee0578e99bf
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2000-11-30 15:09:34 +0000

    *** empty log message ***

--- trunk/GENOM/genomdat.cxx
+++ trunk/GENOM/genomdat.cxx
@@ -30,60 +30,65 @@ GB_ERROR save_Pool(const container_group_ptr& con,int generation) {
     else {
         GB_transaction dummy(save_main);
         
-        GBDATA *organisms_container = GB_create_container(save_main, "organisms");
+        GBDATA */*organisms_container = GB_create_container(save_main, "organisms");*/
+        organisms_container = GB_search(save_main, "organisms", GB_CREATE_CONTAINER);
          
        
-	if (organisms_container) {	
-	   GBDATA *field;
-	   field = GB_create(organisms_container, "number_of_entrees", GB_INT);//number of Genoms in database
-	   field = GB_create(organisms_container, "last_updated", GB_INT);
+        if (organisms_container) {	
+            GBDATA *field;
+            //field = GB_create(organisms_container, "number_of_entrees", GB_INT);//number of Genoms in database
+            field = /*GB_create(organisms_container, "last_update", GB_INT);*/ GB_search(organisms_container, "last_update", GB_CREATE); // typlos?
+            GB_write_int(field, ...........................);
 	   
-	   
-	   GBDATA *organism = GB_create(organisms_container, "organism");
-	   {
-	     if (organism) {
             
-                    GBDATA *field;
+            char acc_num = ...;
+            
+            // @@@ test ob organism mit acc_num bereits existiert!            
+            bool exist_acc = false;
+            
+            if (!exist_acc) {
+                GBDATA *organism = GB_create(organisms_container, "organism");
+                {
+                    if (organism) {
+            
+                        GBDATA *field;
                     
-                    field = GB_create(organism, "accession", GB_STRING);
-                    GB_write_string(field, g->get_gr()->get_inf().accession);
+                        field = GB_create(organism, "accession", GB_STRING);
+                        GB_write_string(field, /*string*/);                    
                     
-                    field = GB_create(organism, "entree_definition", GB_STRING); /*description of sequence entree*/
-                    GB_write_string(field, g->get_gr()->get_inf().entreedefinition);
+                        field = GB_create(organism, "entree_definition", GB_STRING); /*description of sequence entree*/
+                        GB_write_string(field, entreedefinition);
                     
-                    field = GB_create(organism, "species", GB_STRING);
-                    GB_write_string(field, g->get_gr()->get_inf().species);
-
-                    field = GB_create(organism, "strain", GB_STRING);
-                    GB_write_string(field, g->get_gr()->get_def().strain);
-		    
+                        field = GB_create(organism, "full_name", GB_STRING);
+                        GB_write_string(field, species);
+                    
+                        field = GB_create(organism,  "name",  GB_STRING);
+                        GB_write_string(field, ...........................);
+                    
+                        field = GB_create(organism, "strain", GB_STRING);
+                        GB_write_string(field, g->get_gr()->get_def().strain);
+                    
+                        field = GB_create(organism,  "sequence",  GB_STRING);
+                        GB_write_string(field,  ...........................);
+                    
+                    
                     
                    
-		    GBDATA *genes_container = GB_create_container(organism, "genes");
-                    if (genes_container) {
-                        const PG_species_container& sp = g->get_gr()->get_li();
-                        
-                        for (PG_genes_container::const_iterator s=sp.begin(); !error && s!=sp.end(); ++s) {
-                            GBDATA *species = GB_create(species_container, "name", GB_STRING);
-                            GB_write_string(species, s->get_name(error));
+                        GBDATA *genomfragments_container = GB_create_container(organism, "fragments"); 
+                        if (genomfragments_container) {                        
+                            while (fragment=...) {
+                                GBDATA *fragment_container = GB_create_container(organism, "frag"); 
+                            
+                                GBDATA *species = GB_create(species_container, "name", GB_STRING);
+                                GB_write_string(species, );
+                            }
                         }
-                    }
                     
-                    GBDATA *probe_container = GB_create_container(group, "probes");
-                    if (probe_container) {
-                        const PG_sequence_cont& pr = g->get_gr()->get_inf().seq_list;
-                        for (PG_sequence_cont::const_iterator p=pr.begin(); !error && p!=pr.end(); ++p) {
-                            GBDATA *probe = GB_create(probe_container, "data", GB_STRING);
-                            GB_write_string(probe, p->c_str());
-                        }
-                        GBDATA *probe = GB_create(probe_container, "history", GB_STRING);
-                        GB_write_string(probe, g->get_gr()->get_history().c_str());
-                    }                    
-                }
-                if (!error) error = GB_get_error();
-            }
+                        if (!error) error = GB_get_error();
+                    }
             
-        }
+                }
+            }
         
         if (!error) error = GB_get_error();    
         if (!error) error = GB_save(save_main, (char*)filename.c_str(), "a");
