1a16f4fdfa2bb463d278c10b1cab4f32e19c3272
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2000-12-14 17:41:48 +0000

    - generate_one_name uses acc if given

--- trunk/AWTC/awtc_rename.hxx
+++ trunk/AWTC/awtc_rename.hxx
@@ -13,6 +13,6 @@ void		awt_rename_cb(AW_window *aww,GBDATA *gb_main);
 AW_window	*create_awtc_names_admin_window(AW_root *aw_root);
 
 GB_ERROR	pars_names(GBDATA *gb_main, int update_status = 0);
-GB_ERROR    generate_one_name(GBDATA *gb_main, const char *full_name, char*& new_name);
+GB_ERROR    generate_one_name(GBDATA *gb_main, const char *full_name, const char *acc, char*& new_name);
 
 #endif
