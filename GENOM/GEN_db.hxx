22c43419cab771c644b17c603ed67ff03291d864
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2001-01-05 16:47:07 +0000

    GENOM DB Interface

---
+++ GENOM/GEN_db.hxx
@@ -0,0 +1,24 @@
+#ifndef GEN_DB_HXX
+#define GEN_DB_HXX
+
+// genes:
+
+GBDATA* GEN_find_gene(GBDATA *gb_species, const char *name); // find existing gene
+GBDATA* GEN_create_gene(GBDATA *gb_species, const char *name); // create or find existing gene
+
+GBDATA* GEN_first_gene(GBDATA *gb_species);
+GBDATA* GEN_next_gene(GBDATA *gb_gene);
+
+// annotations:
+
+GBDATA* GEN_find_annotation(GBDATA *gb_gene, const char *name); // find existing annotation
+GBDATA* GEN_create_annotation(GBDATA *gb_gene, const char *name); // create or find existing annotation
+
+GBDATA* GEN_first_annotation(GBDATA *gb_gene);
+GBDATA* GEN_next_annotation(GBDATA *gb_gene);
+
+
+
+#else
+#error GEN_db.hxx included twice
+#endif // GEN_DB_HXX
