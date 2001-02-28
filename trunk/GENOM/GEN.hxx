e0ae77278dcbcee9d82651f62f2d446c270c7b41
meierh <meierh@129cd134-2152-0410-ab65-f3f895bafe68>
2001-02-28 13:20:10 +0000

    *** empty log message ***

--- trunk/GENOM/GEN.hxx
+++ trunk/GENOM/GEN.hxx
@@ -2,11 +2,16 @@
 #define GEN_HXX
 
 #include <arbdb.h>
+#include <aw_window.hxx>
 
 #define GENOM_DB_TYPE "genom_db" // main flag (true=genom db, false/missing=normal db)
 
 GB_ERROR GEN_read(GBDATA *gb_main, const char *filename, const char *ali_name);
 
-#else
-#error GEN.hxx included twice
+AW_window *GEN_create_gene_window(AW_root *aw_root);
+AW_window *GEN_create_gene_query_window(AW_root *aw_root);
+AW_window *create_ad_list_reorder(AW_root *root);
+AW_window *create_ad_field_delete(AW_root *root);
+AW_window *create_ad_field_create(AW_root *root);
+void AD_map_species(AW_root *aw_root, AW_CL scannerid);
 #endif // GEN_HXX
