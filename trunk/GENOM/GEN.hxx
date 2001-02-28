3a8d8d9a3aa2ae4833c7fb4744054a54dfe636eb
meierh <meierh@129cd134-2152-0410-ab65-f3f895bafe68>
2001-02-28 15:15:37 +0000

    functions declared for geninfo-interface

--- trunk/GENOM/GEN.hxx
+++ trunk/GENOM/GEN.hxx
@@ -5,6 +5,9 @@
 #include <aw_window.hxx>
 
 #define GENOM_DB_TYPE "genom_db" // main flag (true=genom db, false/missing=normal db)
+#define AD_F_ALL (AW_active)(-1)
+#define AWAR_GENE_DEST "tmp/edit/gene_name_dest"
+
 
 GB_ERROR GEN_read(GBDATA *gb_main, const char *filename, const char *ali_name);
 
@@ -13,5 +16,19 @@ AW_window *GEN_create_gene_query_window(AW_root *aw_root);
 AW_window *create_ad_list_reorder(AW_root *root);
 AW_window *create_ad_field_delete(AW_root *root);
 AW_window *create_ad_field_create(AW_root *root);
+
+
+//functions in submenus of gen_info window
+void gene_delete_cb(AW_window *aww);
+void GEN_map_gene(AW_root *aw_root, AW_CL scannerid);
+void gene_rename_cb(AW_window *aww);
+void gene_copy_cb(AW_window *aww);
+void gene_create_cb(AW_window *aww);
+void GEN_create_field_items(AW_window *aws);
+void ad_list_reorder_cb(AW_window *aws);
 void AD_map_species(AW_root *aw_root, AW_CL scannerid);
+
+
+
+
 #endif // GEN_HXX
