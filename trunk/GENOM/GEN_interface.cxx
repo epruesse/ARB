b1a9e4c2af92d745cab771eceb3e1fcd3c176f1f
meierh <meierh@129cd134-2152-0410-ab65-f3f895bafe68>
2001-03-02 11:14:36 +0000

    function for search for next neighbors removed

--- trunk/GENOM/GEN_interface.cxx
+++ trunk/GENOM/GEN_interface.cxx
@@ -3,7 +3,7 @@
 #include <ntree.hxx>
 #include <aw_awars.hxx>
 #include <arbdbt.h>
-
+#include <probe_design.hxx>
 #include "GEN_db.hxx"
 
 
@@ -29,6 +29,8 @@ AW_CL ad_query_global_cbs = 0;
 
 //create_species_window
 //ad_create_query_window
+
+
 
 AW_window *create_gene_rename_window(AW_root *root)
 {
@@ -219,6 +221,131 @@ void GEN_create_field_items(AW_window *aws) {
     aws->insert_menu_topic("unhide_fields",		"Scan Database for all Hidden Fields","S","scandb.hlp",AD_F_ALL,(AW_CB)awt_selection_list_rescan_cb, (AW_CL)gb_main, AWT_NDS_FILTER );
 }
 
+
+//*****************************wird von 'GEN_gene_next_neighbours_create' aus aufgerufen******************************************
+
+void awtc_nn_search(AW_window *aww,AW_CL id ){
+  //   GB_transaction dummy(gb_main);
+//     int pts = aww->get_root()->awar(AWAR_PROBE_ADMIN_PT_SERVER)->read_int();
+//     int max_hits = aww->get_root()->awar("next_neighbours/max_hits")->read_int();
+//     char *sequence = 0;
+//     {
+//         char *sel_species = 	aww->get_root()->awar(AWAR_SPECIES_NAME)->read_string();
+//         GBDATA *gb_species =	GBT_find_species(gb_main,sel_species);
+
+//         if (!gb_species){
+//             delete sel_species;
+//             aw_message("Select a species first");
+//             return;
+//         }
+//         char *ali_name = aww->get_root()->awar(AWAR_DEFAULT_ALIGNMENT)->read_string();
+//         GBDATA *gb_data = GBT_read_sequence(gb_species,ali_name);
+//         if (!gb_data){
+//             aw_message(GBS_global_string("Species '%s' has no sequence '%s'",sel_species,ali_name));
+//         }
+//         delete sel_species;
+//         delete ali_name;
+//         if (!gb_data) return;
+//         sequence = GB_read_string(gb_data);
+//     }
+    
+//     AWTC_FIND_FAMILY ff(gb_main);
+//     GB_ERROR error = ff.go(pts,sequence,GB_TRUE,max_hits);
+//     delete sequence;
+//     AW_selection_list* sel = (AW_selection_list *)id;
+//     aww->clear_selection_list(sel);
+//     if (error) {
+//         aw_message(error);
+//         aww->insert_default_selection(sel,"No hits found","");
+//     }else{
+//         AWTC_FIND_FAMILY_MEMBER *fm;
+//         for (fm = ff.family_list; fm; fm = fm->next){
+//             const char *dis = GBS_global_string("%-20s Score:%4i",fm->name,fm->matches);
+//             aww->insert_selection(sel,(char *)dis,fm->name);
+//         }
+	
+//         aww->insert_default_selection(sel,"No more hits","");
+//     }
+//     aww->update_selection_list(sel);
+}
+
+void awtc_move_hits(AW_window *,AW_CL id, AW_CL cbs){
+    awt_copy_selection_list_2_queried_species((struct adaqbsstruct *)cbs,(AW_selection_list *)id);
+}
+
+void awtc_nn_search_all_listed(AW_window *aww,AW_CL _cbs  ){
+   //  struct adaqbsstruct *cbs = (struct adaqbsstruct *)_cbs;
+//     GB_begin_transaction(gb_main);
+//     int pts = aww->get_root()->awar(AWAR_PROBE_ADMIN_PT_SERVER)->read_int();
+//     char *dest_field = aww->get_root()->awar("next_neighbours/dest_field")->read_string();
+//     char *ali_name = aww->get_root()->awar(AWAR_DEFAULT_ALIGNMENT)->read_string();
+//     GB_ERROR error = 0;
+//     GB_TYPES dest_type = awt_get_type_of_changekey(gb_main,dest_field);
+//     if (!dest_type){
+//         error = GB_export_error("Please select a valid field");
+//     }
+//     long max = awt_count_queried_species(cbs);
+    
+//     if (strcmp(dest_field, "name")==0) {
+//         int answer = aw_message("CAUTION! This will destroy all name-fields of the listed species.\n",
+//                                 "Continue and destroy all name-fields,Abort");
+	
+//         if (answer==1) {
+//             error = GB_export_error("Aborted by user");
+//         }
+//     } 
+    
+//     aw_openstatus("Finding next neighbours");
+//     long count = 0;
+//     GBDATA *gb_species;
+//     for (gb_species = GBT_first_species(gb_main);
+//          !error && gb_species;
+//          gb_species = GBT_next_species(gb_species)){
+//         if (!IS_QUERIED(gb_species,cbs)) continue;
+//         count ++;
+//         GBDATA *gb_data = GBT_read_sequence(gb_species,ali_name);
+//         if (!gb_data)	continue;
+//         if (count %10 == 0){
+//             GBDATA *gb_name = GB_search(gb_species,"name",GB_STRING);
+//             aw_status(GBS_global_string("Species '%s' (%i:%i)",
+//                                         GB_read_char_pntr(gb_name),
+//                                         count, max));
+//         }
+//         if (aw_status(count/(double)max)){
+//             error = "operation aborted";
+//             break;
+//         }
+//         {
+//             char *sequence = GB_read_string(gb_data);
+//             AWTC_FIND_FAMILY ff(gb_main);
+//             error = ff.go(pts,sequence,GB_TRUE,1);	
+//             if (error) break;
+//             {
+//                 AWTC_FIND_FAMILY_MEMBER *fm = ff.family_list;
+//                 const char *value;
+//                 if (fm){
+//                     value = GBS_global_string("%i '%s'",fm->matches,fm->name);
+//                 }else{
+//                     value = "0";
+//                 }
+//                 GBDATA *gb_dest = GB_search(gb_species,dest_field,dest_type);
+//                 error = GB_write_as_string(gb_dest,value);
+//             }    
+//             delete sequence;
+//         }
+//     }
+//     aw_closestatus();
+//     if (error){
+//         GB_abort_transaction(gb_main);
+//         aw_message(error);
+//     }else{
+//         GB_commit_transaction(gb_main);
+//     }
+//     delete dest_field;
+//     delete ali_name;
+}
+//**********************************************************************************************************************************************************************************************
+
 AW_window *GEN_create_gene_window(AW_root *aw_root) {
 static AW_window_simple_menu *aws = 0;
     if (aws){
@@ -265,94 +392,6 @@ static AW_window_simple_menu *aws = 0;
     return (AW_window *)aws;
 }
 
-
-
-
-
-
-
-
-
-
-AW_window *GEN_gene_next_neighbours_create(AW_root *aw_root,AW_CL cbs){
-   //  static AW_window_simple *aws = 0;
-//     if (aws){
-//         return (AW_window *)aws;
-//     }
-//     aw_root->awar_int(AWAR_PROBE_ADMIN_PT_SERVER);
-//     aw_root->awar_int("next_neighbours/max_hits",20);
-	
-//     aws = new AW_window_simple;
-//     aws->init( aw_root, "SEARCH_NEXT_RELATIVE_OF_SELECTED", "Search Next Neighbours", 600, 0 );
-//     aws->load_xfig("ad_spec_nn.fig");
-	
-//     aws->at("close");
-//     aws->callback( (AW_CB0)AW_POPDOWN);
-//     aws->create_button("CLOSE","CLOSE","C");			   
-
-//     aws->at("help");
-//     aws->callback(AW_POPUP_HELP, (AW_CL)"next_neighbours.hlp");
-//     aws->create_button("HELP","HELP","H");			   
-
-//     aws->at("pt_server");
-//     probe_design_build_pt_server_choices(aws,AWAR_PROBE_ADMIN_PT_SERVER,AW_FALSE);
-
-//     aws->at("max_hit");
-//     aws->create_input_field("next_neighbours/max_hits",5);
-	
-//     aws->at("hits");
-//     AW_selection_list *id = aws->create_selection_list(AWAR_SPECIES_NAME);
-//     aws->insert_default_selection(id,"No hits found","");
-//     aws->update_selection_list(id);
-
-//     aws->at("go");
-//     aws->callback(awtc_nn_search,(AW_CL)id);
-//     aws->create_button("SEARCH","SEARCH");			   
-
-//     aws->at("move");
-//     aws->callback(awtc_move_hits,(AW_CL)id,cbs);
-//     aws->create_button("MOVE_TO_HITLIST","MOVE TO HITLIST");			   
-	
-//     return (AW_window *)aws;
-}
-
-AW_window *GEN_gene_next_neighbours_listed_create(AW_root *aw_root,AW_CL cbs){
-    // static AW_window_simple *aws = 0;
-//     if (aws){
-//         return (AW_window *)aws;
-//     }
-//     aw_root->awar_int(AWAR_PROBE_ADMIN_PT_SERVER);
-//     aw_root->awar_string("next_neighbours/dest_field","tmp");
-	
-//     aws = new AW_window_simple;
-//     aws->init( aw_root, "SEARCH_NEXT_RELATIVES_OF_LISTED", "Search Next Neighbours of Listed", 600, 0 );
-//     aws->load_xfig("ad_spec_nnm.fig");
-	
-//     aws->at("close");
-//     aws->callback( (AW_CB0)AW_POPDOWN);
-//     aws->create_button("CLOSE","CLOSE","C");			   
-
-//     aws->at("help");
-//     aws->callback(AW_POPUP_HELP, (AW_CL)"next_neighbours_marked.hlp");
-//     aws->create_button("HELP","HELP","H");			   
-
-//     aws->at("pt_server");
-//     probe_design_build_pt_server_choices(aws,AWAR_PROBE_ADMIN_PT_SERVER,AW_FALSE);
-
-
-	
-//     aws->at("field");
-//     awt_create_selection_list_on_scandb(gb_main,aws,"next_neighbours/dest_field",
-//                                         (1<<GB_INT) | (1<<GB_STRING), "field",0);
-
-
-//     aws->at("go");
-//     aws->callback(awtc_nn_search_all_listed,cbs);
-//     aws->create_button("SEARCH","SEARCH");			   
-	
-//     return (AW_window *)aws;
-}
-
 AW_window *GEN_create_gene_query_window(AW_root *aw_root) {
 
  static AW_window_simple_menu *aws = 0;
@@ -392,9 +431,7 @@ AW_window *GEN_create_gene_query_window(AW_root *aw_root) {
     aws->create_menu(       0,   "MORE_SEARCH",     "S" );
     aws->insert_menu_topic( "search_equal_fields_within_db","Search For Equal Fields and Mark Duplikates",			"E",0,	-1, (AW_CB)awt_search_equal_entries, cbs, 0 );
     aws->insert_menu_topic( "search_equal_words_within_db", "Search For Equal Words Between Fields and Mark Duplikates",	"W",0,	-1, (AW_CB)awt_search_equal_entries, cbs, 1 );
-    aws->insert_menu_topic( "search_next_relativ_of_sel",	"Search Next Relatives of SELECTED Gene in PT_Server ...",	"R",0,	-1, (AW_CB)AW_POPUP, 	(AW_CL)GEN_gene_next_neighbours_create, cbs );
-    aws->insert_menu_topic( "search_next_relativ_of_listed","Search Next Relatives of LISTED Gene in PT_Server ...",		"M",0,	-1, (AW_CB)AW_POPUP, 	(AW_CL)GEN_gene_next_neighbours_listed_create, cbs );
-
+    
     aws->button_length(7);
 
     aws->at("close");
