91eba8555cdd492e6e1221da279a0e363e7e1f01
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2001-11-29 22:31:07 +0000

    *** empty log message ***

--- trunk/AWT/AWT_nds.cxx
+++ trunk/AWT/AWT_nds.cxx
@@ -155,7 +155,7 @@ void AWT_create_select_nds_window(AW_window *aww,char *key_text,AW_CL cgb_main)
 		awt_create_selection_list_on_scandb((GBDATA *)cgb_main,
                                             (AW_window*)aws,"tmp/viewkey/key_text",
                                             AWT_NDS_FILTER,
-                                            "scandb","rescandb", &AWT_species_selector);
+                                            "scandb","rescandb", &AWT_species_selector, 20, 10);
 		//aw_root->awar(key_text)->add_callback((AW_RCB1)awt_pop_down_select_nds,(AW_CL)aws);
 
 		win =  (AW_window*)aws;
