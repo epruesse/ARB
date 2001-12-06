4c1f0231802434534831ce140b1fcfcb11d43874
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2001-12-06 16:12:27 +0000

    * db_server.cxx: - fixed command line usage and --help                  - shows sth at STDOUT

--- DBSERVER/db_server.cxx
+++ DBSERVER/db_server.cxx
@@ -18,56 +18,60 @@ int main(int argc,char **argv)
     int	last_update = 0;
     int	clock;
 
+    if (argc <=1 || strcmp(argv[1], "--help") == 0) {
+        printf("Syntax of the arb_db_server:\n");
+        printf("	arb_db_server [-D]database [-Tsocketid]\n");
+        return -1;
+    }
+
     params = arb_trace_argv(&argc,argv);
     if (!params->tcp) {
-	static char arg[] = ":";
-	params->tcp = arg;
+        static char arg[] = ":";
+        params->tcp = arg;
     }
 
     if (argc==2){
-	params->db_server = (char *)strdup(argv[1]);
-	argc--;
+        params->db_server = (char *)strdup(argv[1]);
+        argc--;
     }
 
-    if (argc !=1) {
-	printf("Syntax of the arb_db_server:\n");
-	printf("	arb_db_server [-D]database [-Tsocketid]\n");
-	return -1;
-    }
-    
-    // 
+    //
 
     gb_extern = GB_open(params->tcp,"rwc");
     if (gb_extern) {
-	GB_close(gb_extern);
-	return -1;
+        GB_close(gb_extern);
+        return -1;
     }
+    printf("Loading '%s'...\n", params->db_server);
     gb_main = GB_open(params->db_server,"rw");
     if (!gb_main) {
-	printf("Error: File %s not found\n",params->db_server);
-	return -1;
+        printf("Error: File %s not found\n",params->db_server);
+        return -1;
     }
     error = GBCMS_open(params->tcp, TIMEOUT,gb_main);
     if (error) {
-	printf("Cannot start your arb_db_server: %s\n",error);
-	return (-1);
+        printf("Cannot start your arb_db_server: %s\n",error);
+        return (-1);
     }
 
-    for (timer = LOOPS;timer>0;timer--) {	/* the server ends */
-	GB_begin_transaction(gb_main);
-	clock = GB_read_clock(gb_main);
-	if (GB_read_clients(gb_main) >0) {
-	    timer = LOOPS;
-	}
-	GB_commit_transaction(gb_main);
-	if (clock>last_update) {
-	    last_update = clock;
-	    GB_save(gb_main,0,"b");
-	    timer = LOOPS;
-	}
-	GBCMS_accept_calls(gb_main,GB_FALSE);
-}
-return(0);
+    printf("Database is saved after every transaction (one . one save)\n");
+
+    for (timer = LOOPS;timer>0;timer--) { /* the server ends */
+        GB_begin_transaction(gb_main);
+        clock = GB_read_clock(gb_main);
+        if (GB_read_clients(gb_main) >0) {
+            timer = LOOPS;
+        }
+        GB_commit_transaction(gb_main);
+        if (clock>last_update) {
+            last_update = clock;
+            printf("."); fflush(stdout);
+            GB_save(gb_main,0,"b");
+            timer = LOOPS;
+        }
+        GBCMS_accept_calls(gb_main,GB_FALSE);
+    }
+    return(0);
 }
 
 extern "C" {
