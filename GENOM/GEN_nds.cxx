eb415daac3d548b90f883921fbd3f58a00ee7b35
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2002-02-15 17:29:25 +0000

    *** empty log message ***

--- GENOM/GEN_nds.cxx
+++ GENOM/GEN_nds.cxx
@@ -1,8 +1,10 @@
 /*********************************************************************************
  *  Coded by Ralf Westram (coder@reallysoft.de) in 2001                          *
  *  Institute of Microbiology (Technical University Munich)                      *
- *  http://www.mikro.biologie.tu-muenchen.de/                                    *
+ *  http:                       //www.mikro.biologie.tu-muenchen.de/                                    *
  *********************************************************************************/
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
