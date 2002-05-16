0afdd43ec5fd09afb05db3be4908b575324dcd2d
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2002-05-16 17:40:34 +0000

    * ad_trees.cxx: - fixed automatic tree_name creation

--- NTREE/ad_trees.cxx
+++ NTREE/ad_trees.cxx
@@ -49,16 +49,25 @@ void tree_vars_callback(AW_root *aw_root)		// Map tree vars to display objects
 	char *fname = GBS_string_eval(treename,"*=*1.tree:tree_*=*1",0);
 	aw_root->awar(AWAR_TREE_EXPORT "/file_name")->write_string(fname);	// create default file name
 	delete fname;
-	GB_pop_transaction(gb_main);	
+	GB_pop_transaction(gb_main);
 	free(treename);
 }
 /*	update import tree name depending on file name */
 void tree_import_callback(AW_root *aw_root) {
-	GB_transaction dummy(gb_main);
-	char *treename = aw_root->awar(AWAR_TREE_IMPORT "/file_name")->read_string();
+	GB_transaction  dummy(gb_main);
+	char           *treename        = aw_root->awar(AWAR_TREE_IMPORT "/file_name")->read_string();
+    char           *treename_nopath = strrchr(treename, '/');
 
-	char *fname = GBS_string_eval(treename,"*.tree=tree_*1",0);
+    if (treename_nopath) {
+        ++treename_nopath;
+    }
+    else {
+        treename_nopath = treename;
+    }
+
+	char *fname = GBS_string_eval(treename_nopath,"*.tree=tree_*1",0);
 	aw_root->awar(AWAR_TREE_IMPORT "/tree_name")->write_string(fname);
+
 	delete fname;
 	delete treename;
 }
@@ -113,9 +122,9 @@ void create_trees_var(AW_root *aw_root, AW_default aw_def)
 	aw_root->awar(AWAR_TREE_IMPORT "/file_name")->add_callback( tree_import_callback);
 	aw_root->awar(AWAR_TREE_NAME)->add_callback( tree_vars_callback);
 	aw_root->awar(AWAR_TREE_SECURITY)->add_callback( ad_tree_set_security);
-    
+
 	aw_root->awar_int( AWAR_NODE_INFO_ONLY_MARKED, 0,	aw_def);
-    
+
 	tree_vars_callback(aw_root);
 }
 
@@ -231,11 +240,11 @@ AW_window *create_tree_export_window(AW_root *root)
 
 	aws->callback( (AW_CB0)AW_POPDOWN);
 	aws->at("close");
-	aws->create_button("CLOSE","CLOSE","C");			   
+	aws->create_button("CLOSE","CLOSE","C");
 
 	aws->at("help");
 	aws->callback(AW_POPUP_HELP,(AW_CL)"tr_export.hlp");
-	aws->create_button("HELP","HELP","H");			   
+	aws->create_button("HELP","HELP","H");
 
 	aws->at("user");
 	aws->create_option_menu(AWAR_TREE_EXPORT "/NDS",0,0);
@@ -248,11 +257,11 @@ AW_window *create_tree_export_window(AW_root *root)
 
 	aws->at("save2");
 	aws->callback(tree_save_cb);
-	aws->create_button("SAVE","SAVE","o");			   
+	aws->create_button("SAVE","SAVE","o");
 
 	aws->callback( (AW_CB0)AW_POPDOWN);
 	aws->at("cancel2");
-	aws->create_button("CANCEL","CANCEL","C");			   
+	aws->create_button("CANCEL","CANCEL","C");
 
 	return (AW_window *)aws;
 }
@@ -293,7 +302,7 @@ AW_window *create_tree_import_window(AW_root *root)
 
 	aws->callback( (AW_CB0)AW_POPDOWN);
 	aws->at("close");
-	aws->create_button("CLOSE","CLOSE","C");			   
+	aws->create_button("CLOSE","CLOSE","C");
 
 
 	aws->at("user");
@@ -304,11 +313,11 @@ AW_window *create_tree_import_window(AW_root *root)
 
 	aws->at("save2");
 	aws->callback(tree_load_cb);
-	aws->create_button("LOAD","LOAD","o");			   
+	aws->create_button("LOAD","LOAD","o");
 
 	aws->callback( (AW_CB0)AW_POPDOWN);
 	aws->at("cancel2");
-	aws->create_button("CANCEL","CANCEL","C");			   
+	aws->create_button("CANCEL","CANCEL","C");
 
 	return (AW_window *)aws;
 }
@@ -321,7 +330,7 @@ AW_window *create_tree_rename_window(AW_root *root)
 
 	aws->callback( (AW_CB0)AW_POPDOWN);
 	aws->at("close");
-	aws->create_button("CLOSE","CLOSE","C");			   
+	aws->create_button("CLOSE","CLOSE","C");
 
 	aws->at("label");
 	aws->create_button(0,"Please enter the new name\nof the tree");
@@ -331,7 +340,7 @@ AW_window *create_tree_rename_window(AW_root *root)
 
 	aws->at("ok");
 	aws->callback(tree_rename_cb);
-	aws->create_button("GO","GO","G");			   
+	aws->create_button("GO","GO","G");
 
 	return (AW_window *)aws;
 }
@@ -344,7 +353,7 @@ AW_window *create_tree_copy_window(AW_root *root)
 
 	aws->callback( (AW_CB0)AW_POPDOWN);
 	aws->at("close");
-	aws->create_button("CLOSE","CLOSE","C");			   
+	aws->create_button("CLOSE","CLOSE","C");
 
 	aws->at("label");
 	aws->create_button(0,"Please enter the name\nof the new tree");
@@ -354,7 +363,7 @@ AW_window *create_tree_copy_window(AW_root *root)
 
 	aws->at("ok");
 	aws->callback(tree_copy_cb);
-	aws->create_button("GO","GO","G");			   
+	aws->create_button("GO","GO","G");
 
 	return (AW_window *)aws;
 }
@@ -363,25 +372,25 @@ void ad_move_tree_info(AW_window *aww,AW_CL mode){
     /*
       mode == 0 -> move info (=overwrite info from source tree)
       mode == 1 -> compare info
-      mode == 2 -> add info 
+      mode == 2 -> add info
     */
     char *t1 = aww->get_root()->awar(AWAR_TREE_NAME)->read_string();
     char *t2 = aww->get_root()->awar(AWAR_TREE_DEST)->read_string();
     char *log_file = strdup(GBS_global_string("/tmp/arb_log_%s_%i",GB_getenvUSER(),GB_getuid()));
-    
+
     AW_BOOL compare_node_info = mode==1;
     AW_BOOL delete_old_nodes = mode==0;
     AW_BOOL nodes_with_marked_only = (mode==0 || mode==2) && aww->get_root()->awar(AWAR_NODE_INFO_ONLY_MARKED)->read_int();
-    
+
     // move or add node-info writes a log file (containing errors)
     // compare_node_info only sets remark branches
-    
+
     if ( compare_node_info ) { delete log_file;log_file = 0; } // no log if compare_node_info
-    
+
     AWT_move_info(gb_main, t1, t2, log_file, compare_node_info, delete_old_nodes, nodes_with_marked_only);
-    
+
     if (log_file) GB_edit(log_file);
-    
+
     delete log_file;
     delete t2;
     delete t1;
@@ -395,14 +404,14 @@ AW_window *create_tree_diff_window(AW_root *root){
     aws = new AW_window_simple;
     aws->init( root, "CMP_TOPOLOGY", "COMPARE TREE TOPOLOGIES", 200, 0 );
     aws->load_xfig("ad_tree_cmp.fig");
-    
+
     aws->callback( AW_POPDOWN);
     aws->at("close");
-    aws->create_button("CLOSE","CLOSE","C");		       
+    aws->create_button("CLOSE","CLOSE","C");
 
     aws->callback( AW_POPUP_HELP,(AW_CL)"tree_diff.hlp");
     aws->at("help");
-    aws->create_button("HELP","HELP","H");		       
+    aws->create_button("HELP","HELP","H");
 
     root->awar_string(AWAR_TREE_NAME);
     root->awar_string(AWAR_TREE_DEST);
@@ -411,12 +420,12 @@ AW_window *create_tree_diff_window(AW_root *root){
     awt_create_selection_list_on_trees(gb_main,(AW_window *)aws,AWAR_TREE_NAME);
     aws->at("tree2");
     awt_create_selection_list_on_trees(gb_main,(AW_window *)aws,AWAR_TREE_DEST);
-	
+
     aws->button_length(20);
-    
+
     aws->at("move");
     aws->callback(ad_move_tree_info,1); // compare
-    aws->create_button("CMP_TOPOLOGY","COMPARE TOPOLOGY");		       
+    aws->create_button("CMP_TOPOLOGY","COMPARE TOPOLOGY");
 
     return aws;
 }
@@ -427,35 +436,35 @@ AW_window *create_tree_cmp_window(AW_root *root){
     aws = new AW_window_simple;
     aws->init( root, "COPY_NODE_INFO_OF_TREE", "TREE COPY INFO", 200, 0 );
     aws->load_xfig("ad_tree_cmp.fig");
-    
+
     aws->callback( AW_POPDOWN);
     aws->at("close");
-    aws->create_button("CLOSE","CLOSE","C");		       
+    aws->create_button("CLOSE","CLOSE","C");
 
     aws->callback( AW_POPUP_HELP,(AW_CL)"tree_cmp.hlp");
     aws->at("help");
-    aws->create_button("HELP","HELP","H");		       
+    aws->create_button("HELP","HELP","H");
 
     root->awar_string(AWAR_TREE_NAME);
     root->awar_string(AWAR_TREE_DEST);
-    
+
     aws->at("tree1");
     awt_create_selection_list_on_trees(gb_main,(AW_window *)aws,AWAR_TREE_NAME);
     aws->at("tree2");
     awt_create_selection_list_on_trees(gb_main,(AW_window *)aws,AWAR_TREE_DEST);
-	
+
     aws->at("move");
     aws->callback(ad_move_tree_info,0); // move
-    aws->create_button("COPY_INFO","COPY INFO");		       
+    aws->create_button("COPY_INFO","COPY INFO");
 
     aws->at("cmp");
     aws->callback(ad_move_tree_info,2); // add
-    aws->create_button("ADD_INFO","ADD INFO");		       
-    
+    aws->create_button("ADD_INFO","ADD INFO");
+
     aws->at("only_marked");
     aws->label("only info containing marked species");
-    aws->create_toggle(AWAR_NODE_INFO_ONLY_MARKED);    
-    
+    aws->create_toggle(AWAR_NODE_INFO_ONLY_MARKED);
+
     return aws;
 }
 void ad_tr_delete_cb(AW_window *aww){
@@ -492,45 +501,45 @@ AW_window *create_trees_window(AW_root *aw_root)
 
 	aws->callback( AW_POPDOWN);
 	aws->at("close");
-	aws->create_button("CLOSE","CLOSE","C");			   
+	aws->create_button("CLOSE","CLOSE","C");
 
 	aws->callback( AW_POPUP_HELP,(AW_CL)"treeadm.hlp");
 	aws->at("help");
-	aws->create_button("HELP","HELP","H");			   
+	aws->create_button("HELP","HELP","H");
 
 	aws->button_length(20);
 
 	aws->at("delete");
 	aws->callback(ad_tr_delete_cb);
-	aws->create_button("DELETE","DELETE","D");			   
+	aws->create_button("DELETE","DELETE","D");
 
 	aws->at("rename");
 	aws->callback(AW_POPUP,(AW_CL)create_tree_rename_window,0);
-	aws->create_button("RENAME","RENAME","R");			   
+	aws->create_button("RENAME","RENAME","R");
 
 	aws->at("copy");
 	aws->callback(AW_POPUP,(AW_CL)create_tree_copy_window,0);
-	aws->create_button("COPY","COPY","C");			   
+	aws->create_button("COPY","COPY","C");
 
 	aws->at("move");
 	aws->callback(AW_POPUP,(AW_CL)create_tree_cmp_window,0);
-	aws->create_button("MOVE_NODE_INFO","MOVE NODE INFO","C");			   
-	
+	aws->create_button("MOVE_NODE_INFO","MOVE NODE INFO","C");
+
 	aws->at("cmp");
 	aws->callback(AW_POPUP,(AW_CL)create_tree_diff_window,0);
-	aws->create_button("CMP_TOPOLOGY","COMPARE TOPOLOGY","T");			   
-	
+	aws->create_button("CMP_TOPOLOGY","COMPARE TOPOLOGY","T");
+
 	aws->at("export");
 	aws->callback(AW_POPUP,(AW_CL)create_tree_export_window,0);
-	aws->create_button("EXPORT","EXPORT","E");			   
+	aws->create_button("EXPORT","EXPORT","E");
 
 	aws->at("import");
 	aws->callback(AW_POPUP,(AW_CL)create_tree_import_window,0);
-	aws->create_button("IMPORT","IMPORT","I");			   
+	aws->create_button("IMPORT","IMPORT","I");
 
 	aws->at("last");
 	aws->callback(create_tree_last_window);
-	aws->create_button("PUT_TO_END","PUT TO END","P");			   
+	aws->create_button("PUT_TO_END","PUT TO END","P");
 
 	aws->at("list");
 	awt_create_selection_list_on_trees(gb_main,(AW_window *)aws,AWAR_TREE_NAME);
