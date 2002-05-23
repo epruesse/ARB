12f97ab0fb68dd8732337ff16bc0dc2f656bc5d3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2002-05-23 07:26:08 +0000

    allows more arguments to control the input for fastDNAml

--- TOOLS/arb_export_rates.cxx
+++ TOOLS/arb_export_rates.cxx
@@ -23,9 +23,9 @@ int main(int argc, char **argv){
     if (argc<2 || strcmp(argv[1],"--help") == 0) {
         fprintf(stderr,
                 "\n"
-                "arb_export_rates:  Add a line to phylip format which\n"
-                "                   can be used by fastdnaml for rates\n"
-                "syntax: arb_export_rates SAI_NAME\n"
+                "arb_export_rates:  Add a line to phylip format which can be used by fastdnaml for rates\n"
+                "syntax: arb_export_rates SAI_NAME [other_fastdnaml_args]*\n"
+                "if other_fastdnaml_args are given they are inserted to the output\n"
                 "\n");
         return EXIT_FAILURE;
     }
@@ -35,43 +35,73 @@ int main(int argc, char **argv){
         GB_print_error();
         return EXIT_FAILURE;
     }
-    {
-        GB_transaction dummy(gb_main);
-        GBDATA *gb_sai = GBT_find_SAI(gb_main,argv[1]);
-        if (!gb_sai) return 0;		// No weights
 
+    char *firstline         = strdup("");
+    char *SAI_name          = argv[1];
 
-        char *ali_name = GBT_get_default_alignment(gb_main);
-        long	ali_len = GBT_get_alignment_len(gb_main,ali_name);
-        GBDATA *gb_data = GBT_read_sequence(gb_sai,ali_name);
-        if (!gb_data) return 0;
+    for (int arg = 2; arg<argc; ++arg) {
+        char        command_char  = argv[arg][0];
+        const char *not_in_header = "Y";
 
-        printf("C\nC 35 ");
-        double rate = 1.0;
-        int i;
-        for (i=0;i<35;i++){
-            printf("%f ",rate);
-            rate *= 0.71;
+        if (strchr(not_in_header, command_char) == 0) {
+            char *neu = GB_strdup(GBS_global_string("%s %c", firstline, command_char));
+            free(firstline);
+            firstline = neu;
         }
-        printf("\nCategories ");
+    }
 
-        char *seq = GB_read_string(gb_data);
+    {
+        GB_transaction  dummy(gb_main);
+        GBDATA         *gb_sai  = GBT_find_SAI(gb_main,SAI_name);
+        GBDATA         *gb_data = 0;
+        long            ali_len = 0;
 
-        char *filter = GBT_read_string(gb_main,AWAR_GDE_EXPORT_FILTER);
+        if (gb_sai)  {
+            char *ali_name = GBT_get_default_alignment(gb_main);
+            ali_len        = GBT_get_alignment_len(gb_main,ali_name);
+            gb_data        = GBT_read_sequence(gb_sai,ali_name);
+        }
 
-        int flen = strlen(filter);
-        int slen = strlen(seq);
-        if (slen < ali_len) slen = ali_len;
+        // print header
+        fputc(gb_data ? 'C' : 'F', stdout);
+        fputs(firstline, stdout);
+        fputc('\n', stdout);
 
-        for (i=0;i<slen;i++){
-            if (i>flen || filter[i] != '0') {
-                int c = seq[i];
-                if ( (c < '0' || c>'9' ) &&  (c < 'A' || c>'Z')) c = '1';
-                putchar(c);
+        for (int arg = 2; arg<argc; ++arg) { // print [other_fastdnaml_args]*
+            if (argv[arg][1]) { // dont print single character commands again on a own line
+                fputs(argv[arg], stdout);
+                fputc('\n', stdout);
             }
         }
-        for (;i<ali_len;i++){
-            putchar('1');
+
+        if (gb_data) { // if SAI was found
+            printf("C 35 ");
+            double rate = 1.0;
+            int    i;
+            for (i=0;i<35;i++){
+                printf("%f ",rate);
+                rate *= 0.71;
+            }
+            printf("\nCategories ");
+
+            char *seq = GB_read_string(gb_data);
+
+            char *filter = GBT_read_string(gb_main,AWAR_GDE_EXPORT_FILTER);
+
+            int flen = strlen(filter);
+            int slen = strlen(seq);
+            if (slen < ali_len) slen = ali_len;
+
+            for (i=0;i<slen;i++){
+                if (i>flen || filter[i] != '0') {
+                    int c = seq[i];
+                    if ( (c < '0' || c>'9' ) &&  (c < 'A' || c>'Z')) c = '1';
+                    putchar(c);
+                }
+            }
+            for (;i<ali_len;i++){
+                putchar('1');
+            }
         }
     }
     GB_close(gb_main);
