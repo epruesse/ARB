64c027ab85f9ea5689a362eeeaf63e967599cfe2
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2002-05-31 18:57:53 +0000

    * awt_dtree.hxx: - prototype for AWT_show_remark_branch

--- trunk/AWT/awt_dtree.hxx
+++ trunk/AWT/awt_dtree.hxx
@@ -117,8 +117,7 @@ class AWT_graphic_tree : public AWT_graphic {
 };
 
 AWT_graphic *NT_generate_tree( AW_root *root,GBDATA *gb_main );
-
-
+AW_BOOL      AWT_show_remark_branch(AW_device *device, const char *remark_branch, AW_pos x, AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL cd1, AW_CL cd2);
 
 
 #endif
