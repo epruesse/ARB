224142066cce4ae48187460ca5aefd7c82ee3250
richter <richter@129cd134-2152-0410-ab65-f3f895bafe68>
2002-06-27 10:19:29 +0000

    modified perl interface library files 27.06.2002

--- trunk/PERL2ARB/perlmain_source.c
+++ trunk/PERL2ARB/perlmain_source.c
@@ -8,9 +8,10 @@
 
 
 #include "EXTERN.h"
+#define PERL_IN_MINIPERLMAIN_C
 #include "perl.h"
 
-static void xs_init _((void));
+static void xs_init (pTHX);
 static PerlInterpreter *my_perl;
 
 #if defined (__MINT__) || defined (atarist)
@@ -26,47 +27,47 @@ main(int argc, char **argv, char **env)
 
 #ifdef PERL_GLOBAL_STRUCT
 #define PERLVAR(var,type) /**/
+#define PERLVARA(var,type) /**/
 #define PERLVARI(var,type,init) PL_Vars.var = init;
 #define PERLVARIC(var,type,init) PL_Vars.var = init;
 #include "perlvars.h"
 #undef PERLVAR
+#undef PERLVARA
 #undef PERLVARI
 #undef PERLVARIC
 #endif
 
-    PERL_SYS_INIT(&argc,&argv);
-
-    perl_init_i18nl10n(1);
+    PERL_SYS_INIT3(&argc,&argv,&env);
 
     if (!PL_do_undump) {
 	my_perl = perl_alloc();
 	if (!my_perl)
 	    exit(1);
-	perl_construct( my_perl );
+	perl_construct(my_perl);
 	PL_perl_destruct_level = 0;
     }
 
-    exitstatus = perl_parse( my_perl, xs_init, argc, argv, (char **) NULL );
+    exitstatus = perl_parse(my_perl, xs_init, argc, argv, (char **)NULL);
     if (!exitstatus) {
-	exitstatus = perl_run( my_perl );
+	exitstatus = perl_run(my_perl);
     }
 
-    perl_destruct( my_perl );
-    perl_free( my_perl );
+    perl_destruct(my_perl);
+    perl_free(my_perl);
 
     PERL_SYS_TERM();
 
-    exit( exitstatus );
+    exit(exitstatus);
     return exitstatus;
 }
 
 /* Register any extra external extensions */
 
-EXTERN_C void boot_ARB _((CV* cv));
-EXTERN_C void boot_DynaLoader _((CV* cv));
+EXTERN_C void boot_ARB (pTHX_ CV* cv);
+EXTERN_C void boot_DynaLoader (pTHX_ CV* cv);
 
 static void
-xs_init(void)
+xs_init(pTHX)
 {
 	char *file = __FILE__;
 	dXSUB_SYS;
