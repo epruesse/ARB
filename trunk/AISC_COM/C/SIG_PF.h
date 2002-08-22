79bcb480e4beb39beec0965e21ba344889c4006e
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2002-08-22 16:42:41 +0000

    solaris fix didnt work for linux

--- trunk/AISC_COM/C/SIG_PF.h
+++ trunk/AISC_COM/C/SIG_PF.h
@@ -1,20 +1,22 @@
 /*   File      : SIG_PF.h
  *   Purpose   : wrapper to include or define stuff needed for SIG_PF
- *   Time-stamp: <Tue Aug/13/2002 18:42 MET Coder@ReallySoft.de>
+ *   Time-stamp: <Thu Aug/22/2002 19:06 MET Coder@ReallySoft.de>
  */
 
-#if defined(SUN5_ECGS) || defined (LINUX)
+#if defined(SUN5_ECGS)
 # include <sys/iso/signal_iso.h>
+#elseif defined (LINUX)
+# include <sysent.h>
 #elseif defined(SUN4) || defined(SUN5)
-#  if defined(__cplusplus)
-#   include <sysent.h>	/* c++ only for sun (used for shutdown) */
-#  else
-#   define SIG_PF void (*)()
-#  endif
+# if defined(__cplusplus)
+#  include <sysent.h>	/* c++ only for sun (used for shutdown) */
 # else
-#  define SIG_PF void (*)(int)
+#  define SIG_PF void (*)()
 # endif
+#else
+# define SIG_PF void (*)(int)
 #endif
+
 
 
 
