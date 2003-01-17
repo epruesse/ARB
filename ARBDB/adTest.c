eee437fc06c7eeb015c6b7e4b2e8fd29f6a28e4c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-01-17 08:19:57 +0000

    * adTest.c: - added GB_dump_db_path, GB_get_db_path, GB_get_type_name and GB_fix_database

--- ARBDB/adTest.c
+++ ARBDB/adTest.c
@@ -20,13 +20,13 @@ void err_hook() {
 }
 
 #define err(gbd,quark,type,mess) \
-	do { \
-		fprintf(stderr, "(%s*)0x%p(=%s) %s (in (%s*)0x%p(=%s))\n",\
-			type,gbd,Main->keys[quark].key,mess,\
-			actType,actData,Main->keys[actQuark].key); \
-		err=1; \
+    do { \
+        fprintf(stderr, "(%s*)0x%p(=%s) %s (in (%s*)0x%p(=%s))\n",\
+            type,gbd,Main->keys[quark].key,mess,\
+            actType,actData,Main->keys[actQuark].key); \
+        err=1; \
         err_hook();\
-	} while (0)
+    } while (0)
 
 #define errGBD(gbd,quark,mess) err(gbd,quark,"GBDATA",mess)
 #define errGBC(gbc,quark,mess) err(gbc,quark,"GBCONTAINER",mess)
@@ -45,11 +45,11 @@ void testData(GB_MAIN_TYPE *Main, GBDATA *gbd, long server_id, int key_quark)
 void testContainer(GB_MAIN_TYPE *Main,GBCONTAINER *gbc, long server_id, int key_quark)
 {
     struct gb_header_list_struct *header;
-    int 	item,
+    int     item,
         err=0;
-    const char 	*oldType   = actType;
-    void 	*oldData  = actData;
-    int 	oldQuark  = actQuark;
+    const char  *oldType   = actType;
+    void    *oldData  = actData;
+    int     oldQuark  = actQuark;
 
 #if (VERBOSE>=1)
     printf("Teste (GBCONTAINER*)0x%p (=%s)\n", gbc, Main->keys[key_quark].key);
@@ -66,7 +66,7 @@ void testContainer(GB_MAIN_TYPE *Main,GBCONTAINER *gbc, long server_id, int key_
     for (item=0; item<gbc->d.nheader; item++) {
         GBDATA *gbd = GB_HEADER_LIST_GBD(header[item]);
         GBCONTAINER *father;
-        int 	type, quark = header[item].flags.key_quark;
+        int     type, quark = header[item].flags.key_quark;
 
         if (!gbd) continue;
 
@@ -81,7 +81,7 @@ void testContainer(GB_MAIN_TYPE *Main,GBCONTAINER *gbc, long server_id, int key_
         type = GB_TYPE(gbd);
 
         if (type==GB_DB) testContainer(Main,(GBCONTAINER*)gbd, server_id, quark);
-        else 		 testData(Main,gbd,server_id,quark);
+        else         testData(Main,gbd,server_id,quark);
 
         father = GB_FATHER(gbd);
         if (!father) {
@@ -105,10 +105,10 @@ void testContainer(GB_MAIN_TYPE *Main,GBCONTAINER *gbc, long server_id, int key_
 
 void gb_testDB(GBDATA *gbd)
 {
-    GB_MAIN_TYPE 	*Main = 0;
-    GBCONTAINER 	*gbc;
-    long 		server_id;
-    int 		err=0;
+    GB_MAIN_TYPE    *Main = 0;
+    GBCONTAINER     *gbc;
+    long        server_id;
+    int         err=0;
 
     actType = "GB_MAIN_TYPE";
     actData = gbd;
@@ -145,65 +145,81 @@ void gb_testDB(GBDATA *gbd)
     printf("testDB passed.\n");
 }
 
-void GB_dump(GBDATA *gbd) {
-    long type = GB_TYPE(gbd);
-    const char *type_name = 0;
-    static int indent;
-/*     int i; */
-    const char *content = 0;
+const char *GB_get_type_name(GBDATA *gbd) {
+    int         type = GB_TYPE(gbd);
+    const char *type_name;
 
     switch (type) {
-        case GB_INT: {
-            type_name = "GB_INT";
-            content = GBS_global_string("%i", GB_read_int(gbd));
-            break;
-        }
-        case GB_FLOAT: {
-            type_name = "GB_FLOAT";
-            content = GBS_global_string("%f", (float)GB_read_float(gbd));
-            break;
-        }
-        case GB_BYTE: {
-            type_name = "GB_BYTE";
-            content = GBS_global_string("%i", GB_read_byte(gbd));
-            break;
-        }
-        case GB_STRING: {
-            type_name = "GB_STRING";
-            content = GB_read_char_pntr(gbd);
-            break;
-        }
-        case GB_LINK: {
-            type_name = "GB_LINK";
-            content = GBS_global_string("link to %p", GB_follow_link(gbd));
-            break;
-        }
-        case GB_BITS: {
-            type_name = "GB_BITS";
-            break;
-        }
-        case GB_BYTES: {
-            type_name = "GB_BYTES";
-            break;
-        }
-        case GB_INTS: {
-            type_name = "GB_INTS";
-            break;
-        }
-        case GB_FLOATS: {
-            type_name = "GB_FLOATS";
-            break;
-        }
-        case GB_DB: {
-            type_name = "GB_DB";
-            content = "see below";
-            break;
-        }
+        case GB_INT:    { type_name = "GB_INT"; break; }
+        case GB_FLOAT:  { type_name = "GB_FLOAT"; break; }
+        case GB_BYTE:   { type_name = "GB_BYTE"; break; }
+        case GB_STRING: { type_name = "GB_STRING"; break; }
+        case GB_LINK:   { type_name = "GB_LINK"; break; }
+        case GB_BITS:   { type_name = "GB_BITS"; break; }
+        case GB_BYTES:  { type_name = "GB_BYTES"; break; }
+        case GB_INTS:   { type_name = "GB_INTS"; break; }
+        case GB_FLOATS: { type_name = "GB_FLOATS"; break; }
+        case GB_DB:     { type_name = "GB_DB"; break; }
         default: {
-            type_name = "<unknown>";
-            content = "";
+            static char *unknownType;
+            if (unknownType) free(unknownType);
+            unknownType = strdup(GBS_global_string("<unknown GB_TYPE=%i>", type));
+            type_name   = unknownType;
             break;
         }
+    }
+
+    return type_name;
+}
+
+const char *GB_get_db_path(GBDATA *gbd) {
+    GBDATA *gb_father = GB_get_father(gbd);
+
+    if (gb_father) {
+        static char *result;
+        char        *father_path = strdup(GB_get_db_path(gb_father));
+        int          key_quark   = GB_KEY_QUARK(gbd);
+
+        if (result) {
+            free(result);
+            result = 0;
+        }
+
+        if (key_quark == 0) { // illegal
+            result = strdup(GBS_global_string("%s/<illegal quark=0>", father_path));
+        }
+        else {
+            const char *key = GB_KEY(gbd);
+            result = strdup(GBS_global_string("%s/%s", father_path, key));
+        }
+
+        return result;
+    }
+    return "ROOT";
+}
+
+void GB_dump_db_path(GBDATA *gbd) {
+    printf("Path to GBDATA %p (type=%s) is '%s'\n", gbd, GB_get_type_name(gbd), GB_get_db_path(gbd));
+}
+
+void GB_dump(GBDATA *gbd) {
+    int         type      = GB_TYPE(gbd);
+    const char *type_name = GB_get_type_name(gbd);
+    static int  indent;
+    const char *content   = 0;
+
+    switch (type) {
+        case GB_INT:    { content = GBS_global_string("%i", GB_read_int(gbd)); break; }
+        case GB_FLOAT:  { content = GBS_global_string("%f", (float)GB_read_float(gbd)); break; }
+        case GB_BYTE:   { content = GBS_global_string("%i", GB_read_byte(gbd)); break; }
+        case GB_STRING: { content = GB_read_char_pntr(gbd); break; }
+        case GB_LINK:   { content = GBS_global_string("link to %p", GB_follow_link(gbd)); break; }
+        case GB_BITS:   { break; }
+        case GB_BYTES:  { break; }
+        case GB_INTS:   { break; }
+        case GB_FLOATS: { break; }
+        case GB_DB:     { content = "see below"; break; }
+        default:        { content = ""; break; }
     }
 
     if (content==0) content = "<unknown>";
@@ -223,6 +239,7 @@ void GB_dump(GBDATA *gbd) {
     }
 }
 
+#ifdef DEBUG
 char *GB_ralfs_test(GBDATA *gb_main)
 {
     GB_MAIN_TYPE *Main = GB_MAIN(gb_main);
@@ -235,13 +252,59 @@ char *GB_ralfs_test(GBDATA *gb_main)
     return NULL;
 }
 
-#ifndef NDEBUG
 char *GB_ralfs_menupoint(GBDATA *main_data)
 {
     return GB_ralfs_test(main_data);
 }
 #endif
 
+/* -------------------------------------------------------------------------------- */
+/* Fix database                                                                     */
+/* -------------------------------------------------------------------------------- */
 
+static GB_ERROR gb_fix_recursive(GBDATA *gbd) {
+    GBDATA *gbp;
+    int     type = GB_TYPE(gbd);
+
+    if (type == GB_DB) {
+        for (gbp = GB_find(gbd, 0, 0, down_level); gbp; gbp = GB_find(gbp, 0, 0, this_level|search_next)) {
+            gb_fix_recursive(gbp);
+        }
+    }
+    else {
+        int key_quark = GB_KEY_QUARK(gbd);
+        if (key_quark == 0) {
+            GB_MAIN_TYPE *Main         = GB_MAIN(gbd);
+            const char   *new_key_try  = GBS_global_string("illegal_zero_key_%s", GB_get_type_name(gbd));
+            char         *new_key_name = GBS_string_2_key(new_key_try);
+            GBQUARK       keyq         = gb_key_2_quark(Main, new_key_name);
+
+            ad_assert(keyq != 0);
+            {
+                long gbm_index    = GB_QUARK_2_GBMINDEX(Main, keyq);
+                GB_GBM_INDEX(gbd) = gbm_index; // set new index
+
+                /* @@@ FIXME: above command has no effect  */
+
+                printf("Fixed zero key_quark of ");
+                GB_dump_db_path(gbd);
+            }
+        }
+    }
+
+    return 0;
+}
+
+GB_ERROR GB_fix_database(GBDATA *gb_main) {
+    GB_ERROR err = 0;
+    GB_begin_transaction(gb_main);
+
+    err = gb_fix_recursive(gb_main);
+
+    if (err) GB_abort_transaction(gb_main);
+    else GB_commit_transaction(gb_main);
+
+    return err;
+}
 
 
