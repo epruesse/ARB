8a8c2f3ad5c5a68b8aa193a351ff63ae584bd882
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-01-24 17:50:19 +0000

    * aw_global_awars.hxx: - defines the names of the global awars

---
+++ trunk/WINDOW/aw_global_awars.hxx
@@ -0,0 +1,24 @@
+//  ==================================================================== //
+//                                                                       //
+//    File      : aw_global_awars.hxx                                    //
+//    Purpose   : define awars available in ALL arb applications         //
+//    Time-stamp: <Fri Jan/24/2003 17:41 MET Coder@ReallySoft.de>        //
+//                                                                       //
+//                                                                       //
+//  Coded by Ralf Westram (coder@reallysoft.de) in January 2003          //
+//  Copyright Department of Microbiology (Technical University Munich)   //
+//                                                                       //
+//  Visit our web site at: http://www.arb-home.de/                       //
+//                                                                       //
+//                                                                       //
+//  ==================================================================== //
+
+#ifndef AW_GLOBAL_AWARS_HXX
+#define AW_GLOBAL_AWARS_HXX
+
+#define AWAR_WWW_BROWSER "www/browser" // how to call the users browser
+
+#else
+#error aw_global_awars.hxx included twice
+#endif // AW_GLOBAL_AWARS_HXX
+
