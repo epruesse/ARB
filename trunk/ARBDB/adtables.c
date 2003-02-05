9d7e11afc26fde140e8f2785bed67c44efba7f1b
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-02-05 22:59:45 +0000

    * adtables.c: - untabified and formats fixed

--- trunk/ARBDB/adtables.c
+++ trunk/ARBDB/adtables.c
@@ -8,28 +8,28 @@
 
 /* *************** tables, for ARB BIO storage *******************
 
- *	hierarchical organisation:
+ *  hierarchical organisation:
 
 
- *	main:
- *		table_data:
- *			table:
- *				name	(indexed by default)
- *				entries
- *					entry
- *						name (which is an id)
- *						....
- *					entry
- *					...
- *				fields
- *					field
- *						name
- *						type
- *						description
- *					field
- *					....
- *		       }
- *		}
+ *  main:
+ *      table_data:
+ *          table:
+ *              name    (indexed by default)
+ *              entries
+ *                  entry
+ *                      name (which is an id)
+ *                      ....
+ *                  entry
+ *                  ...
+ *              fields
+ *                  field
+ *                      name
+ *                      type
+ *                      description
+ *                  field
+ *                  ....
+ *             }
+ *      }
  */
 
 GBDATA *gbt_table_link_follower(GBDATA *gb_main, GBDATA *gb_link, const char *link) {
@@ -40,14 +40,14 @@ GBDATA *gbt_table_link_follower(GBDATA *gb_main, GBDATA *gb_link, const char *li
     GBUSE(gb_link);
     sep = strchr(link,':');
     if (!sep){
-        GB_export_error("Link '%s' is missing second ':' tag");
+        GB_export_error("Link '%s' is missing second ':' tag", link);
         return 0;
     }
     save = *sep;
     *sep = 0;
     gb_table = GBT_open_table(gb_main,link,1);
     *sep = save;
-    
+
     if (!gb_table){
         GB_export_error("Table '%s' does not exist",link);
         return 0;
@@ -63,7 +63,7 @@ GB_ERROR GBT_install_table_link_follower(GBDATA *gb_main){
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
+
     static void g_bt_table_deleted(GBDATA *gb_table,int *clientdata, GB_CB_TYPE gbtype){
         GB_MAIN_TYPE *Main = gb_get_main_during_cb();
         GBUSE(gb_table);
@@ -72,11 +72,11 @@ extern "C" {
         GBS_free_hash(Main->table_hash);
         Main->table_hash = GBS_create_hash(256,0);
     }
-    
+
 #ifdef __cplusplus
 }
 #endif
-			
+
 GBDATA *GBT_open_table(GBDATA *gb_table_root,const char *table_name, GB_BOOL read_only){
     /** open a table. This routines is optimized to look for existing tables */
     GBDATA *gb_table;
@@ -97,14 +97,14 @@ GBDATA *GBT_open_table(GBDATA *gb_table_root,const char *table_name, GB_BOOL rea
     if (gb_table_name) return GB_get_father(gb_table_name);
     if (read_only) return 0;
 
-				/* now lets create the table */
-    gb_table = GB_create_container(gb_table_data,"table");  	GB_add_callback(gb_table,GB_CB_DELETE,g_bt_table_deleted,0);
+                /* now lets create the table */
+    gb_table = GB_create_container(gb_table_data,"table");      GB_add_callback(gb_table,GB_CB_DELETE,g_bt_table_deleted,0);
     gb_table_name = GB_create(gb_table,"name",GB_STRING);
     gb_table_description = GB_create(gb_table,"description",GB_STRING);
-    GB_write_string(gb_table_name,table_name);			GB_write_security_levels(gb_table_name,0,7,7); /* nether delete or change the name */
+    GB_write_string(gb_table_name,table_name);          GB_write_security_levels(gb_table_name,0,7,7); /* nether delete or change the name */
     GB_write_string(gb_table_description,"No description");
-    gb_table_entries = GB_create_container(gb_table,"entries");	GB_write_security_levels(gb_table_entries,0,0,7); /* nether delete this */
-    gb_table_fields = GB_create_container(gb_table,"fields");	GB_write_security_levels(gb_table_fields,0,0,7); /* nether intended to be deleted */
+    gb_table_entries = GB_create_container(gb_table,"entries"); GB_write_security_levels(gb_table_entries,0,0,7); /* nether delete this */
+    gb_table_fields = GB_create_container(gb_table,"fields");   GB_write_security_levels(gb_table_fields,0,0,7); /* nether intended to be deleted */
     gb_table_name_field = GBT_open_table_field(gb_table,"name",GB_STRING); /* for the id */
     GB_write_security_levels(gb_table_name_field,0,0,7); /* Never delete name field */
     return gb_table;
@@ -193,12 +193,12 @@ GB_TYPES GBT_get_type_of_table_entry_field(GBDATA *gb_table,const char *fieldnam
     gb_field_type = GB_find(gb_field_name,"type",0,down_level);
     return (GB_TYPES) GB_read_int(gb_field_type);
 }
-	
+
 GB_ERROR GBT_savely_write_table_entry_field(GBDATA *gb_table,GBDATA *gb_entry, const char *fieldname,const char *value_in_ascii_format){
     GBDATA *gb_entry_field;
     GB_TYPES type = GBT_get_type_of_table_entry_field(gb_table,fieldname);
     if (type == GB_NONE){
-        return GB_export_error("There is no field description '%s' for your table");
+        return GB_export_error("There is no field description '%s' for your table", fieldname);
     }
     gb_entry_field = GB_search(gb_entry,"fieldname",type);
     return GB_write_as_string(gb_entry_field,value_in_ascii_format);
@@ -211,13 +211,13 @@ GBDATA *GBT_open_table_field(GBDATA *gb_table, const char *fieldname, GB_TYPES t
     GBDATA *gb_table_field_description;
     GBDATA *gb_fields;
     if (gb_table_field) return gb_table_field;
-    
+
     gb_fields = GB_find(gb_table,"fields",0,down_level);
     gb_table_field = GB_create_container(gb_fields,"field");
     gb_table_field_name = GB_create(gb_table_field,"name",GB_STRING);
-    GB_write_string(gb_table_field_name,fieldname);	GB_write_security_levels(gb_table_field_name,0,7,7); /* never change this */
+    GB_write_string(gb_table_field_name,fieldname); GB_write_security_levels(gb_table_field_name,0,7,7); /* never change this */
     gb_table_field_type = GB_create(gb_table_field,"type",GB_INT);
-    GB_write_int(gb_table_field_type,type_of_field);	GB_write_security_levels(gb_table_field_type,0,7,7);
+    GB_write_int(gb_table_field_type,type_of_field);    GB_write_security_levels(gb_table_field_type,0,7,7);
     gb_table_field_description = GB_create(gb_table_field,"description",GB_STRING);
     GB_write_string(gb_table_field_description,"No description yet");
     return gb_table_field;
