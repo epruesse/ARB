907381fddffb89924bb3407f2dd2fddb4c29858d
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-02-06 22:09:33 +0000

    * careful.c: - untabified and indented              - const fixes

--- ALEIO/careful.c
+++ ALEIO/careful.c
@@ -5,50 +5,45 @@
 #include <errno.h>
 #include <stdio.h>
 
-static char *prog_name;
+static const char *prog_name;
 
 /* Use NAME as the name of the program in future error reports.
    If NAME is a filename containing slashes, only the last component
    of the path is used; this means that you can pass in argv[0], and
    still get clean error messages.
    Return the portion of NAME that will be used in error messages.  */
-char *
-careful_prog_name (char *name)
+const char * careful_prog_name (const char *name)
 {
-  prog_name = strrchr (name, '/');
-  if (prog_name)
-    prog_name++;
-  else
-    prog_name = name;
+    prog_name = strrchr (name, '/');
+    if (prog_name) prog_name++;
+    else prog_name = name;
 
-  return prog_name;
+    return prog_name;
 }
 
 
-void
-check_syscall (int call, char *filename, char *op)
+void check_syscall (int call, const char *filename, const char *op)
 {
-  if (call != -1)
-    return;
+    if (call != -1)
+        return;
 
-  fprintf (stderr, "%s: %s%s",
-	   prog_name,
-	   filename ? filename : "",
-	   filename ? ": " : "");
-  perror (op);
+    fprintf (stderr, "%s: %s%s",
+             prog_name,
+             filename ? filename : "",
+             filename ? ": " : "");
+    perror (op);
 
-  exit (2);
+    exit (2);
 }
 
 
 /* If an error has occured on F, print an error message mentioning
    FILENAME and OP, as with check_syscall, and exit.  If no error has
    occurred on F, simply return.  */
-void
-check_file (FILE *f, char *filename, char *op)
+void check_file (FILE *f, const char *filename, const char *op)
 {
-  if (ferror (f))
-    check_syscall (-1, filename, op);
+    if (ferror (f))
+        check_syscall (-1, filename, op);
 }
 
 
@@ -57,31 +52,31 @@ check_file (FILE *f, char *filename, char *op)
    and exit if this doesn't succeed.  */
 FILE * careful_open (const char *name, const char *mode, FILE *defalt)
 {
-  if (name)
+    if (name)
     {
-      FILE *result = fopen (name, mode);
+        FILE *result = fopen (name, mode);
 
-      if (! result)
-	{
-	  fprintf (stderr, "%s: ", prog_name);
-	  perror (name);
-	  exit (2);
-	}
+        if (! result)
+        {
+            fprintf (stderr, "%s: ", prog_name);
+            perror (name);
+            exit (2);
+        }
 
-      return result;
+        return result;
     }
-  else
-    return defalt;
+    else
+        return defalt;
 }
 
 
 /* If FILE is neither stdin nor stdout, close it.  */
 void
-careful_close (FILE *f, char *filename)
+careful_close (FILE *f, const char *filename)
 {
-  check_file (f, filename, "closing file");
+    check_file (f, filename, "closing file");
 
-  if (f != stdin && f != stdout)
-    if (fclose (f) == EOF)
-      check_syscall (-1, filename, "closing file");
+    if (f != stdin && f != stdout)
+        if (fclose (f) == EOF)
+            check_syscall (-1, filename, "closing file");
 }
