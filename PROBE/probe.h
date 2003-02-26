2accaf565c0b14412e23485c146a238f9f0ed1bc
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-02-26 18:22:43 +0000

    * probe.h: - untabified

--- PROBE/probe.h
+++ PROBE/probe.h
@@ -12,8 +12,8 @@ typedef unsigned long ulong;
 typedef unsigned int uint;
 typedef unsigned char uchar;
 
-#define	 min(a, b) ((a) < (b)) ? (a) : (b)
-#define	 max(a, b) ((a) > (b)) ? (a) : (b)
+#define  min(a, b) ((a) < (b)) ? (a) : (b)
+#define  max(a, b) ((a) > (b)) ? (a) : (b)
 #define PT_CORE *(int *)0=0;
 #define PT_MAX_MATCHES 10000
 #define PT_MAX_IDENTS 10000
@@ -30,46 +30,46 @@ enum PT_MATCH_TYPE {
 
 
 
-#define MATCHANSWER 50	/* private msg type: match result answer */
-#define CREATEANSWER 51	/* private msg type: create result answer */
-#define FINDANSWER 52	/* private msg type: find result answer */
+#define MATCHANSWER 50  /* private msg type: match result answer */
+#define CREATEANSWER 51 /* private msg type: create result answer */
+#define FINDANSWER 52   /* private msg type: find result answer */
 
 extern ulong physical_memory;
 struct Hs_struct;
 extern char *pt_error_buffer;
 
 typedef enum type_types_type {
-	t_int = 1,
-	t_string=0,
-	t_float=2
-	} type_types;
+    t_int = 1,
+    t_string=0,
+    t_float=2
+    } type_types;
 
-typedef enum PT_bases_enum	{
-	PT_QU = 0,
-	PT_N = 1,
-	PT_A, PT_C, PT_G, PT_T, PT_B_MAX } PT_BASES;
+typedef enum PT_bases_enum  {
+    PT_QU = 0,
+    PT_N = 1,
+    PT_A, PT_C, PT_G, PT_T, PT_B_MAX } PT_BASES;
 
 /*  POS TREE */
 typedef enum enum_PT_NODE_TYPE {
-	PT_NT_LEAF = 0,
-	PT_NT_CHAIN = 1,
-	PT_NT_NODE = 2,
-	PT_NT_SINGLE_NODE = 3,	/* stage 3 */
-	PT_NT_SAVED = 3,	/* stage 1+2 */
-	PT_NT_UNDEF = 4
-	} PT_NODE_TYPE;
+    PT_NT_LEAF = 0,
+    PT_NT_CHAIN = 1,
+    PT_NT_NODE = 2,
+    PT_NT_SINGLE_NODE = 3,  /* stage 3 */
+    PT_NT_SAVED = 3,    /* stage 1+2 */
+    PT_NT_UNDEF = 4
+    } PT_NODE_TYPE;
 
-typedef struct POS_TREE_struct	{
-	uchar		flags;
-	char		data;
-	} POS_TREE;
+typedef struct POS_TREE_struct  {
+    uchar       flags;
+    char        data;
+    } POS_TREE;
 
 typedef struct PTMM_struct {
-	char		*base;
-	int             stage1;
-	int             stage2;
-	int             stage3;
-	int		mode;
+    char        *base;
+    int             stage1;
+    int             stage2;
+    int             stage3;
+    int     mode;
 }               PTM2;
 
 
@@ -78,88 +78,88 @@ typedef struct PTMM_struct {
 
 
 struct probe_statistic {
-	int	match_count;		/* Counter for matches */
-	double	rel_match_count;	/* match_count / (seq_len - probe_len + 1) */
+    int match_count;        /* Counter for matches */
+    double  rel_match_count;    /* match_count / (seq_len - probe_len + 1) */
 };
 
 struct probe_input_data {    /* every taxas own data */
-				/******* name and sequence *********/
-     char 	*data;
+                /******* name and sequence *********/
+     char   *data;
      long       checksum;
-     int	size;
-     char	*name;
-     char	*fullname;
-     GBDATA	*gbd;
+     int    size;
+     char   *name;
+     char   *fullname;
+     GBDATA *gbd;
 
-				/********* probe design ************/
-     int	is_group;	/* -1:	nevermind
-				    0:  no group
-				    1:  group */
-				/* probe design (match) */
-     PT_probematch *match;	/* best hit for PT_new_design */
+                /********* probe design ************/
+     int    is_group;   /* -1:  nevermind
+                    0:  no group
+                    1:  group */
+                /* probe design (match) */
+     PT_probematch *match;  /* best hit for PT_new_design */
 
-  				/********* find family  ************/
+                /********* find family  ************/
      struct probe_statistic stat;
 
-  				/********* free pointer  ************/
-     int	next;
+                /********* free pointer  ************/
+     int    next;
 };
 
 struct probe_statistic_struct {
-	int	cut_offs;		/* statistic of chains */
-	int	single_node;
-	int	short_node;
-	int	long_node;
-	int	longs;
-	int	shorts;
-	int	shorts2;
-	int	chars;
+    int cut_offs;       /* statistic of chains */
+    int single_node;
+    int short_node;
+    int long_node;
+    int longs;
+    int shorts;
+    int shorts2;
+    int chars;
 
 };
 
-extern struct probe_struct_global	{
-	GBDATA	*gb_main;		/* ARBDB interface */
-	GBDATA	*gb_species_data;
-	GBDATA	*gb_extended_data;
-	char	*alignment_name;
-	GB_HASH *namehash;		/* name to int */
+extern struct probe_struct_global   {
+    GBDATA  *gb_main;       /* ARBDB interface */
+    GBDATA  *gb_species_data;
+    GBDATA  *gb_extended_data;
+    char    *alignment_name;
+    GB_HASH *namehash;      /* name to int */
 
-	struct probe_input_data *data;	/* the internal database */
-	char	*ecoli;			/* the ecoli sequenz */
-	void	*bi_ecoli;
+    struct probe_input_data *data;  /* the internal database */
+    char    *ecoli;         /* the ecoli sequenz */
+    void    *bi_ecoli;
 
-	int	data_count;
-	int	max_size;		/* maximum sequence len */
-	long	char_count;		/* number of all 'acgtuACGTU' */
+    int data_count;
+    int max_size;       /* maximum sequence len */
+    long    char_count;     /* number of all 'acgtuACGTU' */
 
-	int	mismatches;		/* chain handle in match */
-	double	wmismatches;
-	int	N_mismatches;
-	int	w_N_mismatches[PT_POS_TREE_HEIGHT+PT_POS_SECURITY+1];
+    int mismatches;     /* chain handle in match */
+    double  wmismatches;
+    int N_mismatches;
+    int w_N_mismatches[PT_POS_TREE_HEIGHT+PT_POS_SECURITY+1];
 
-	int	reversed;		/* tell the matcher whether probe is
-						reversed */
+    int reversed;       /* tell the matcher whether probe is
+                        reversed */
 
-    double	*pos_to_weight;			/* position to weight */
-    char complement[256];		/* complement */
+    double  *pos_to_weight;         /* position to weight */
+    char complement[256];       /* complement */
 
-    int	deep;			/* for probe matching */
-    int	height;
-    int	length;
-    int	apos;
+    int deep;           /* for probe matching */
+    int height;
+    int length;
+    int apos;
 
-    int	sort_by;
+    int sort_by;
 
-    char	*probe;			/* probe design + chains */
-    char	*main_probe;
+    char    *probe;         /* probe design + chains */
+    char    *main_probe;
 
-    char	*server_name;		/* name of this server */
+    char    *server_name;       /* name of this server */
     aisc_com *link;
     T_PT_MAIN main;
-    struct Hs_struct *com_so;			/* the communication socket */
+    struct Hs_struct *com_so;           /* the communication socket */
     POS_TREE *pt;
-    PTM2	*ptmain;
-    probe_statistic_struct	stat;
+    PTM2    *ptmain;
+    probe_statistic_struct  stat;
 } psg;
 
 
