57bb61ce16d6f2f43f0e6056b67aed4631d635de
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-03-25 16:08:24 +0000

    * di_matr.hxx: - untabified and indented

--- DIST/di_matr.hxx
+++ DIST/di_matr.hxx
@@ -1,59 +1,60 @@
 
 
 typedef enum {
-	PH_TRANSFORMATION_NONE,
-	PH_TRANSFORMATION_SIMILARITY,
-	PH_TRANSFORMATION_JUKES_CANTOR,
-	PH_TRANSFORMATION_FELSENSTEIN,
+    PH_TRANSFORMATION_NONE,
+    PH_TRANSFORMATION_SIMILARITY,
+    PH_TRANSFORMATION_JUKES_CANTOR,
+    PH_TRANSFORMATION_FELSENSTEIN,
 
-	PH_TRANSFORMATION_PAM,
-	PH_TRANSFORMATION_CATEGORIES_HALL,
-	PH_TRANSFORMATION_CATEGORIES_BARKER,
-	PH_TRANSFORMATION_CATEGORIES_CHEMICAL,
+    PH_TRANSFORMATION_PAM,
+    PH_TRANSFORMATION_CATEGORIES_HALL,
+    PH_TRANSFORMATION_CATEGORIES_BARKER,
+    PH_TRANSFORMATION_CATEGORIES_CHEMICAL,
 
-	PH_TRANSFORMATION_HAESCH,
-	PH_TRANSFORMATION_KIMURA,
-	PH_TRANSFORMATION_OLSEN,
-	PH_TRANSFORMATION_FELSENSTEIN_VOIGT,
-	PH_TRANSFORMATION_OLSEN_VOIGT,
-	PH_TRANSFORMATION_ML
+    PH_TRANSFORMATION_HAESCH,
+    PH_TRANSFORMATION_KIMURA,
+    PH_TRANSFORMATION_OLSEN,
+    PH_TRANSFORMATION_FELSENSTEIN_VOIGT,
+    PH_TRANSFORMATION_OLSEN_VOIGT,
+    PH_TRANSFORMATION_ML
 } PH_TRANSFORMATION;
 
 //extern const char *enum_trans_to_string[];
 
 enum PH_MATRIX_TYPE {
-	PH_MATRIX_FULL,
-	PH_MATRIX_COMPRESSED };
+    PH_MATRIX_FULL,
+    PH_MATRIX_COMPRESSED };
 
 class PHMATRIX;
 class AW_root;
 
 class ph_dummy {
-	PHMATRIX *dummy;
-	};
+    PHMATRIX *dummy;
+};
 
 class PHENTRY {
-	public:
-	PHENTRY(GBDATA *gbd,class PHMATRIX *phmatri);
-	PHENTRY(char *namei,class PHMATRIX *phmatri);
-	~PHENTRY(void);
-	PHMATRIX *phmatrix;
-	AP_sequence 		*sequence;
-	AP_sequence_parsimony 	*sequence_parsimony;	// if exist ok
-	AP_sequence_simple_protein *sequence_protein;
-	long	seq_len;
-	char	*name;
-	char	*full_name;
-	AP_FLOAT gc_bias;
-	int	group_nr;			// @@ OLIVER species belongs
-						// to group number xxxx
+public:
+    PHENTRY(GBDATA *gbd,class PHMATRIX *phmatri);
+    PHENTRY(char *namei,class PHMATRIX *phmatri);
+    ~PHENTRY();
+
+    PHMATRIX                   *phmatrix;
+    AP_sequence                *sequence;
+    AP_sequence_parsimony      *sequence_parsimony; // if exist ok
+    AP_sequence_simple_protein *sequence_protein;
+    long                        seq_len;
+    char                       *name;
+    char                       *full_name;
+    AP_FLOAT                    gc_bias;
+    int                         group_nr; // @@ OLIVER species belongs
+    // to group number xxxx
 };
 
 typedef long PH_MUT_MATR[AP_MAX][AP_MAX];
 
 enum PH_SAVE_TYPE {
-	PH_SAVE_PHYLIP_COMP,
-	PH_SAVE_READABLE,
+    PH_SAVE_PHYLIP_COMP,
+    PH_SAVE_READABLE,
     PH_SAVE_TABBED
 };
 
@@ -62,19 +63,19 @@ private:
     friend class PHENTRY;
     GBDATA *gb_main;
     GBDATA *gb_species_data;
-    char	*use;
-    long	seq_len;
-    char	cancel_columns[256];
+    char    *use;
+    long    seq_len;
+    char    cancel_columns[256];
     AP_tree_root *tree_root;
 
-    AW_root *aw_root;		// only link
+    AW_root *aw_root;       // only link
 
-    long	entries_mem_size;
+    long    entries_mem_size;
 
 public:
-    GB_BOOL	is_AA;
-    PHENTRY	**entries;
-    long	nentries;
+    GB_BOOL is_AA;
+    PHENTRY **entries;
+    long    nentries;
     static PHMATRIX *ROOT;
     AP_smatrix *matrix;
     enum PH_MATRIX_TYPE matrix_type;
@@ -86,20 +87,20 @@ public:
     char *unload(void);
     const char *save(char *filename,enum PH_SAVE_TYPE type);
 
-    void	clear(PH_MUT_MATR &hits);
-    void	make_sym(PH_MUT_MATR &hits);
-    void	rate_write(PH_MUT_MATR &hits,FILE *out);
-    long	*create_helix_filter(BI_helix *helix,AP_filter *filter);
-				// 0 non helix 1 helix; compressed filter
+    void    clear(PH_MUT_MATR &hits);
+    void    make_sym(PH_MUT_MATR &hits);
+    void    rate_write(PH_MUT_MATR &hits,FILE *out);
+    long    *create_helix_filter(BI_helix *helix,AP_filter *filter);
+    // 0 non helix 1 helix; compressed filter
     GB_ERROR calculate_rates(PH_MUT_MATR &hrates,PH_MUT_MATR &nrates,PH_MUT_MATR &pairs,long *filter);
     GB_ERROR haeschoe(const char *path);
-    double 	corr(double dist, double b, double & sigma);
+    double  corr(double dist, double b, double & sigma);
     GB_ERROR calculate(AW_root *awr, char *cancel, double alpha, PH_TRANSFORMATION transformation);
     char *calculate_overall_freqs(double rel_frequencies[AP_MAX],char *cancel_columns);
     GB_ERROR calculate_pro(PH_TRANSFORMATION transformation);
     char *analyse(AW_root *aw_root);
 
-    int search_group(GBT_TREE *node,GB_HASH *hash, long *groupcnt,char *groupname, PHENTRY **groups);		// @@ OLIVER
+    int search_group(GBT_TREE *node,GB_HASH *hash, long *groupcnt,char *groupname, PHENTRY **groups);       // @@ OLIVER
     char *compress(GBT_TREE *tree);
 };
 
