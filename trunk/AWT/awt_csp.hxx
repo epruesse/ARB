7585ce4bde7c30689109cbae4dd477655d03490e
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-03-26 16:45:21 +0000

    * awt_csp.hxx: - untabified and indented

--- trunk/AWT/awt_csp.hxx
+++ trunk/AWT/awt_csp.hxx
@@ -3,11 +3,11 @@
 
 
 /*
-	Create a window, that allows you to get weights from the sais
-		1.	create AWT_csp
-		2.	build button with callback create_csp_window
-		3.	call csp->go( second_filter)
-		4. 	use csp->weights ....
+  Create a window, that allows you to get weights from the sais
+  1.  create AWT_csp
+  2.  build button with callback create_csp_window
+  3.  call csp->go( second_filter)
+  4.  use csp->weights ....
 */
 
 class AW_root;
@@ -21,38 +21,38 @@ class AW_window;
 #define DIST_MIN_SEQ(seq_anz) (seq_anz / 10)
 
 class AWT_csp {
-	public:
-	GBDATA *gb_main;
-	AW_root *awr;
+public:
+    GBDATA *gb_main;
+    AW_root *awr;
 
-	char *awar_name;
-	char *awar_alignment;
-	char *alignment_name;
-	char *type_path;
-	char *awar_smooth;
-	char *awar_enable_helix;
-	void *sai_sel_box_id;
-/* real public */
-	size_t	seq_len;	// real length == 0 -> not valid
-	GB_UINT4 *weights;	// helix = 1, non helix == 2
-	float	*rates;
-	float	*ttratio;
-	float	*frequency[256];
-	GB_UINT4 *mut_sum;
-	GB_UINT4 *freq_sum;
-	char	*is_helix;	// == 1 -> helix; == 0 -> loop region
-	char	*desc;
+    char *awar_name;
+    char *awar_alignment;
+    char *alignment_name;
+    char *type_path;
+    char *awar_smooth;
+    char *awar_enable_helix;
+    void *sai_sel_box_id;
+    /* real public */
+    size_t  seq_len;    // real length == 0 -> not valid
+    GB_UINT4 *weights;  // helix = 1, non helix == 2
+    float   *rates;
+    float   *ttratio;
+    float   *frequency[256];
+    GB_UINT4 *mut_sum;
+    GB_UINT4 *freq_sum;
+    char    *is_helix;  // == 1 -> helix; == 0 -> loop region
+    char    *desc;
 
-	AWT_csp(GBDATA *gb_main, AW_root *awr, const char *awar_template);
-			/* awar_template == 	".../name" 
-			-> generated 		".../alignment"
-						".../smooth"
-						".../enable_helix"
-			*/
-	~AWT_csp(void);
-        void exit();
-	GB_ERROR go(AP_filter *filter = 0);
-	void print(void);
+    AWT_csp(GBDATA *gb_main, AW_root *awr, const char *awar_template);
+    /* awar_template ==     ".../name"
+       -> generated        ".../alignment"
+       ".../smooth"
+       ".../enable_helix"
+    */
+    ~AWT_csp(void);
+    void exit();
+    GB_ERROR go(AP_filter *filter = 0);
+    void print(void);
 };
 
 AW_window *create_csp_window(AW_root *root, AWT_csp *csp);
