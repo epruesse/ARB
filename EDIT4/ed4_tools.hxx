0bec65329c95d8c72fec5788b2ee9ad960986024
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-05-13 15:09:25 +0000

    - added ED4_propertyName()

--- EDIT4/ed4_tools.hxx
+++ EDIT4/ed4_tools.hxx
@@ -13,5 +13,6 @@ void ED4_set_clipping_rectangle(AW_rectangle *rect);
 
 void ED4_aws_init(AW_root *root, AW_window_simple *aws, GB_CSTR macro_format, GB_CSTR window_format, GB_CSTR typeId);
 
+const char *ED4_propertyName(int mode);
 
 
