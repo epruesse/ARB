eba6fe764e22738ec9e70bc455bcd500c60ab704
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-05-21 14:06:23 +0000

    - removed static variables - all stuff printed to console depends on #define - added HELP button - removed some casts - fixed some memory leaks

--- NTREE/NT_validManual.cxx
+++ NTREE/NT_validManual.cxx
@@ -29,9 +29,7 @@
 
 extern GBDATA* gb_main;
 //extern GBDATA* GB_selected_species;
-
-static AW_selection_list    *validNamesList;
-static struct selectValidNameStruct* svnp;
+// static AW_selection_list    *validNamesList;
 
 /*--------------------------creating and initializing AWARS----------------------------------------*/
 void NT_createValidNamesAwars(AW_root *aw_root, AW_default aw_def) {
@@ -80,7 +78,7 @@ void fillSelNamList(struct selectValidNameStruct* svnp){
             //                         ptr to list, item to display, item value (here: equal)
 
             if (strncmp (validName, searchstr, length) == 0){
-            svnp->aws->insert_selection(validNamesList,validName, validName);
+                svnp->aws->insert_selection(svnp->validNamesList,validName, validName);
             }
 
             free (validName);
@@ -99,9 +97,9 @@ void fillSelNamList(struct selectValidNameStruct* svnp){
     else {
         GB_commit_transaction(gb_main);
 
-        svnp->aws->insert_default_selection(validNamesList , "????", "????" );
-        svnp->aws->sort_selection_list(validNamesList , 0);
-        svnp->aws->update_selection_list(validNamesList);
+        svnp->aws->insert_default_selection(svnp->validNamesList , "????", "????" );
+        svnp->aws->sort_selection_list(svnp->validNamesList , 0);
+        svnp->aws->update_selection_list(svnp->validNamesList);
     }
 
 }
@@ -118,16 +116,16 @@ void fillSelNamList(struct selectValidNameStruct* svnp){
 
 // }
 
-void updateValNameList(AW_root *awr){
-
+void updateValNameList(AW_root *awr, AW_CL cl_svnp) {
+    selectValidNameStruct *svnp = reinterpret_cast<selectValidNameStruct*>(cl_svnp);
     const char* selectedName = awr->awar(AWAR_INPUT_INITIALS)->read_string();
 
-#ifdef DEBUG
+#ifdef DUMP
     aw_message(GBS_global_string("now selected: %s\n",selectedName));
 #endif
     svnp->initials = selectedName;
     fillSelNamList(svnp);
-#ifdef DEBUG
+#ifdef DUMP
     aw_message(GBS_global_string("SelectionList updated"));
 #endif
 }
@@ -135,16 +133,18 @@ void updateValNameList(AW_root *awr){
 
 
 struct selectValidNameStruct* createValNameList(GBDATA *gb_main,AW_window *aws, const char *awarName){
+#if defined(DUMP)
     aw_message("ValidNameSelectionList was created");
-//     GBDATA *gb_presets;
-//     AW_root *aw_root  = aws->get_root();
-    validNamesList = aws->create_selection_list(awarName,0,"",10,20);
+#endif // DUMP
+    //     GBDATA *gb_presets;
+    //     AW_root *aw_root  = aws->get_root();
 
-    svnp = new struct selectValidNameStruct; // declared static
-    svnp->aws = aws;
-    svnp->gb_main = gb_main;
-    svnp->validNamesList = validNamesList;
-    svnp->initials = "";
+    static struct selectValidNameStruct* svnp = new struct selectValidNameStruct; // declared static
+
+    svnp->aws            = aws;
+    svnp->gb_main        = gb_main;
+    svnp->validNamesList = aws->create_selection_list(awarName,0,"",10,20);;
+    svnp->initials       = "";
 
     fillSelNamList(svnp);
     return svnp;
@@ -157,65 +157,71 @@ struct selectValidNameStruct* createValNameList(GBDATA *gb_main,AW_window *aws, 
 void selectValidNameFromList(AW_window* selManWindowRoot, AW_CL, AW_CL)
 
 {
-    // working
-    const char* selectedValName = selManWindowRoot->get_root()->awar(AWAR_SELECTED_VALNAME)->read_string();
+    char *selectedValName     = selManWindowRoot->get_root()->awar(AWAR_SELECTED_VALNAME)->read_string();
+    char *selectedSpeciesName = selManWindowRoot->get_root()->awar(AWAR_SPECIES_NAME)->read_string();
 
-    const char * selectedSpeciesName = selManWindowRoot->get_root()->awar(AWAR_SPECIES_NAME)->read_string();
     //    aw_message(GBS_global_string("actual marked species is %s", selectedSpeciesName));
 
-    if(strncmp(selectedSpeciesName,"",strlen(selectedSpeciesName))==0){
+//     if (strncmp(selectedSpeciesName,"",strlen(selectedSpeciesName))==0) {
+    if (strcmp(selectedSpeciesName,"")==0) {
         aw_message("No species selected!");
-        return;
-    }
-#ifdef DEBUG
-    else{
-        aw_message(GBS_global_string("actual marked species is %s", selectedSpeciesName));
-    }
-#endif
-
-    GB_begin_transaction(gb_main);
-    GB_ERROR err = 0;
-
-
-    // begin own code
-    GBDATA* GB_selectedSpecies = GBT_find_species(gb_main, selectedSpeciesName);
-#ifdef DEBUG
-    const char * test = GB_read_string(GB_find(GB_selectedSpecies,"full_name",0,down_level));
-    aw_message(GBS_global_string("species %s in database found", test ));
-    aw_message("----test----");
-#endif
-
-    if(! GB_selectedSpecies)
-        {err = "species not found in database";
-    }else{ GBDATA* GB_nameCont = GB_search(GB_selectedSpecies,"Valid_Name",GB_CREATE_CONTAINER);
-
-       if(! GB_nameCont)
-           {err = "could not create Valid Name container in database";}
-       else{
-           GBDATA* GB_valName = GB_find(GB_nameCont,"NameString",0,down_level);
-           if(! GB_valName){GB_valName = GB_create(GB_nameCont, "NameString", GB_STRING);}
-
-           GBDATA* GB_valType = GB_find(GB_nameCont,"DescType",0,down_level);
-           if(! GB_valType){GB_valType = GB_create(GB_nameCont, "DescType", GB_STRING);}
-
-           GB_write_string(GB_valName, selectedValName);
-           GB_write_string(GB_valType, "MAN");
-       }
-    }
-  // end own code
-    if (err) {
-      GB_abort_transaction(gb_main);
-        aw_message(err);
     }
     else {
-        GB_commit_transaction(gb_main);
-
-    }
 #ifdef DEBUG
-    aw_message(GBS_global_string("Selection confirmed: %s", selectedValName));
-    printf("Selection confirmed: %s\n", selectedValName);
+        aw_message(GBS_global_string("current marked species is %s", selectedSpeciesName));
+#endif
+
+        GB_begin_transaction(gb_main);
+        GB_ERROR err = 0;
+
+
+        // begin own code
+        GBDATA* GB_selectedSpecies = GBT_find_species(gb_main, selectedSpeciesName);
+
+#if 0
+        const char * test = GB_read_string(GB_find(GB_selectedSpecies,"full_name",0,down_level));
+        aw_message(GBS_global_string("species %s in database found", test ));
+        aw_message("----test----");
+#endif
+
+        if(! GB_selectedSpecies) {
+            err = "species not found in database";
+        }
+        else {
+            GBDATA* GB_nameCont = GB_search(GB_selectedSpecies,"Valid_Name",GB_CREATE_CONTAINER);
+
+            if (! GB_nameCont) {
+                err = "could not create Valid Name container in database";
+            }
+            else {
+                GBDATA* GB_valName = GB_find(GB_nameCont,"NameString",0,down_level);
+                if(! GB_valName){GB_valName = GB_create(GB_nameCont, "NameString", GB_STRING);}
+
+                GBDATA* GB_valType = GB_find(GB_nameCont,"DescType",0,down_level);
+                if(! GB_valType){GB_valType = GB_create(GB_nameCont, "DescType", GB_STRING);}
+
+                GB_write_string(GB_valName, selectedValName);
+                GB_write_string(GB_valType, "MAN");
+            }
+        }
+        // end own code
+        if (err) {
+            GB_abort_transaction(gb_main);
+            aw_message(err);
+        }
+        else {
+            GB_commit_transaction(gb_main);
+
+        }
+#ifdef DEBUG
+        aw_message(GBS_global_string("Selection confirmed: %s", selectedValName));
+        printf("Selection confirmed: %s\n", selectedValName);
 
 #endif
+    }
+
+    free(selectedSpeciesName);
+    free(selectedValName);
 }
 
 
@@ -229,13 +235,17 @@ AW_window *NT_searchManuallyNames(AW_root *aw_root /*, AW_CL*/ )
     aws->load_xfig("ad_selManNam.fig");
 
     aws->at("close");
-    aws->callback( (AW_CB0)AW_POPDOWN); // arb specific close callback, like a constant
+    aws->callback(AW_POPDOWN); // arb specific close callback, like a constant
     aws->create_button("CLOSE","CLOSE","C");
+
+    aws->at("help");
+    aws->callback(AW_POPUP_HELP,(AW_CL)"vn_search.hlp");
+    aws->create_button("HELP","HELP","H");
 
     aws->at("nameList");
     // creates the selection list and asign AWAR_SELECTED_VALNAME
 
-    struct selectValidNameStruct *vns = createValNameList(gb_main, (AW_window *)aws, AWAR_SELECTED_VALNAME);
+    struct selectValidNameStruct *vns = createValNameList(gb_main, aws, AWAR_SELECTED_VALNAME);
 
     aws->at("select");
     aws->callback( selectValidNameFromList,0,0); // (... 0,0)
@@ -244,12 +254,9 @@ AW_window *NT_searchManuallyNames(AW_root *aw_root /*, AW_CL*/ )
     aws->at("enterInitial");
     aws->create_input_field(AWAR_INPUT_INITIALS , 30 );
 
-    //Awar_CB1 f, AW_CL cd1
-    //    aw_root->awar(AWAR_INPUT_INITIALS)->add_callback((Awar_CB1)fillSelNamList,(AW_CL)vns);//
-    //    aw_root->awar(AWAR_INPUT_INITIALS)->add_callback((Awar_CB1)awarTestFunction,(AW_CL)vns);
-    aw_root->awar(AWAR_INPUT_INITIALS)->add_callback(updateValNameList); // awr seems to be given automatically
+    aw_root->awar(AWAR_INPUT_INITIALS)->add_callback(updateValNameList, (AW_CL)vns);
 
 
-    return (AW_window *)aws;
+    return aws;
 
 }
