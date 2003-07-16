0263801dfcad22b3617e250a814a1707f4101060
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-07-16 08:59:37 +0000

    - new member partial_match()

--- trunk/AWT/awt_seq_dna.hxx
+++ trunk/AWT/awt_seq_dna.hxx
@@ -3,17 +3,18 @@
 
 class AP_sequence_parsimony :  public  AP_sequence {
 private:
-	void	build_table(void);
+    void    build_table(void);
 
 public:
-	char		*sequence;		// AP_BASES
-	static char	*table;
-	AP_sequence_parsimony(AP_tree_root *rooti);
-	~AP_sequence_parsimony(void);
-	AP_sequence 	*dup(void);		// used to get the real new element
-	void set(	char *sequence);
-	AP_FLOAT combine(	const AP_sequence * lefts, const	AP_sequence *rights) ;
-	AP_FLOAT real_len(void);
+    char        *sequence;      // AP_BASES
+    static char *table;
+    AP_sequence_parsimony(AP_tree_root *rooti);
+    ~AP_sequence_parsimony(void);
+    AP_sequence     *dup(void);     // used to get the real new element
+    void set(   char *sequence);
+    AP_FLOAT combine(   const AP_sequence * lefts, const    AP_sequence *rights) ;
+    AP_FLOAT real_len(void);
+    void partial_match(const AP_sequence* part, long *overlap, long *penalty) const;
 };
 
 
