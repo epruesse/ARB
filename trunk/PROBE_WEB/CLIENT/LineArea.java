7e78610078e4c01eb312e76d2efac9b59337d1b1
richter <richter@129cd134-2152-0410-ab65-f3f895bafe68>
2003-07-23 11:39:44 +0000

    core classes for WEB-ARB / ALLPROBES for the local java client contains: - TreeParser           - TreeLoader           - GUI frame           - TreeLayout and Display           - TreeNode definition           - supplementary helper and debug classes

---
+++ trunk/PROBE_WEB/CLIENT/LineArea.java
@@ -0,0 +1,38 @@
+public class LineArea
+{
+private int[] corners;
+
+public LineArea(int x1, int y1, int x2, int y2)
+    {
+        corners = new int[4];
+        corners[0] = x1;
+        corners[1] = y1;
+        corners[2] = x2;
+        corners[3] = y2;
+    }
+
+public LineArea(int [] coordinates)
+    {
+        corners = coordinates;
+    }
+
+public boolean isInside(int x, int y)
+    {
+        if (x >= corners[0] && x <= corners[2] && y >= corners[1] && y <= corners[3])
+            {
+                return true;
+            }
+        else
+            {
+                return false;
+            }
+    }
+
+
+public void getBorder()
+    {
+        System.out.println("Click found in area: " + corners[0] + "/" + corners[1] + "\t"
+                           + corners[2] + "/" + corners[3]);
+    }
+
+}
---
+++ trunk/PROBE_WEB/CLIENT/TreeDisplay.java
@@ -0,0 +1,391 @@
+//package de.arbhome.allProbes;
+
+// TreeDisplay.java
+
+import java.lang.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+//import java.Thread.*;
+
+public class TreeDisplay
+extends Canvas
+{
+private long displayCounter;
+private String treeString;
+private TreeNode root;
+private TreeNode vs; // visible subtree
+
+// private int xSize = 800;
+// private int ySize = 1000;
+private int xSize = 0;
+private int ySize = 0;
+
+private int leafNumber;
+
+private HashMap branchLines; // vertical lines
+private HashMap rootLines;  // horizontal lines
+private int yPosition = 10;
+private int leafSpace  = 15 ;// ySpan / leafNumber;
+private int yPointer = 0; // serves as pointer to the y-space already used up
+private int xSpreading = 1000;
+private int treeLevels;
+private boolean newLayout = true;
+private LineAreaFactory laf;
+private Set rootSet;
+private Set branchSet;
+public float maxDist = 0;
+
+
+
+
+
+
+public TreeDisplay(TreeNode root, int treeLevels)
+    {
+
+        setBackground(Color.lightGray);
+        // postpone this later until tree is parsed to determined the needed space
+
+        //        setVisible(true);
+
+        displayCounter = 0;
+
+        rootLines = new HashMap();
+        branchLines = new HashMap();
+        rootSet = new HashSet();
+        branchSet = new HashSet();
+        laf = new LineAreaFactory(3);
+
+        this.treeLevels = treeLevels;
+        this.root = root;
+        if (root == null)
+            {
+                System.out.println("TreeDisplay(): no root node obtained from caller"); System.exit(1);
+            }
+//         if (root == null)
+//             {
+//                 System.out.println("tree parsing failed, no root node returned");
+//                 System.exit(1);
+//             }
+
+//        vs =  clipTree(root, treeLevels);
+        vs = root;
+            //        root.setDistance((float)0.01);
+        vs.setDistance((float)0.01);
+
+        System.out.println("root name: " + vs.getNodeName());
+        System.out.println(">>>"+vs.getNodeName()+"<<<");
+
+        System.out.println("tree has " + vs.getNoOfLeaves() + " leaves");
+        leafNumber = vs.getNoOfLeaves();
+        //        root.calculateTotalDist(0);
+        vs.calculateTotalDist(0);
+        calculateYValues4Leaves(vs, treeLevels);
+        calculateYValues4internal(vs, treeLevels);
+
+        //        setSize(xSize,ySize);
+        //        xSize = (int)(root.getMaxDepth()*xSpreading +100);
+        xSize = 1000;
+        ySize = yPointer + 50;
+        setSize(xSize,ySize);
+        System.out.println("tree area: x: " + xSize);
+        System.out.println("maxDist  : x: " + maxDist);
+        System.out.println("tree area: y: " + ySize);
+        addMouseListener(new MouseAdapter()
+        {
+           public void mouseClicked(MouseEvent event)
+                 {
+                     System.out.print("left mouse button clicked at ");
+                     System.out.print("x pos:" + event.getX());
+                     System.out.println("y pos:" + event.getY());
+                     handleMouseClick(event.getX(), event.getY()); 
+                };
+ 
+        }
+                         );
+        
+
+
+        
+        
+        //        setVisible(true);
+    }
+
+
+public void paint(Graphics g)
+    {
+
+        // rooot changed, layout new tree 
+
+
+        // do some cleanup or reset of datastructures before a new layout is generated
+        if (newLayout == true)
+            {
+
+                yPointer = 0;
+                rootLines.clear();
+                branchLines.clear();
+                rootSet.clear();
+                branchSet.clear();
+                vs.calculateTotalDist(0);
+                calculateYValues4Leaves(vs, treeLevels);
+                calculateYValues4internal(vs, treeLevels);
+                setSize(xSize,yPointer + 50);
+
+            }
+
+        if (root == null) 
+            {
+                System.out.println("in paint(): no valid node given to display");
+                System.exit(1);
+            }
+
+
+        displayTreeGraph(g, vs, treeLevels);
+        // new lines are registered in displayTreeGraph()
+ 
+        if (rootSet.isEmpty() || branchSet.isEmpty()) // equivalent to newLayout == true except first invocation
+            {
+                rootSet = rootLines.keySet();
+                branchSet = branchLines.keySet();
+            }
+       newLayout = false;
+
+        System.out.println("method display tree was called "+ ++displayCounter+" times");
+    }
+
+
+
+public void calculateYValues4Leaves(TreeNode node, int depth)
+    {
+
+        if ((node.testLeaf() == true) || (depth == 0))
+            {
+
+                 node.setYOffset( yPointer + yPosition);
+                 yPointer = yPointer + leafSpace;
+                 if (node.testLeaf() == false && depth == 0 )
+                     {
+                         node.setGrouped(true);
+                     }
+
+
+            }
+        else
+            {
+                node.setGrouped(false);
+                calculateYValues4Leaves((TreeNode)(node.getChilds().elementAt(0)), depth -1 ); 
+                calculateYValues4Leaves((TreeNode)(node.getChilds().elementAt(1)), depth -1 );
+            }
+
+    }
+
+
+
+public int calculateYValues4internal(TreeNode node, int depth)
+{
+  return  (node.testLeaf() || depth == 0 )?  node.getYOffset()
+        :  node.setYOffset((calculateYValues4internal((TreeNode)(node.getChilds().elementAt(0) ), depth -1 ) 
+                            + calculateYValues4internal((TreeNode)(node.getChilds().elementAt(1) ), depth -1 )) /2 );
+
+}
+
+
+
+
+public void displayTreeGraph(Graphics g, TreeNode node, int depth)
+    {
+
+        if (node == null) 
+            {
+                System.out.println("no valid node given to display");
+                System.exit(1);
+            }
+        if ((node.testLeaf() == true) || (depth == 0))
+             {
+                 int[] line = new int[4];
+                 line[0] = (int)((node.getTotalDist() - node.getDistance())* xSpreading);
+                 line[1] = (int) node.getYOffset();
+                 line[2] = (int)(node.getTotalDist()*xSpreading);
+                 line[3] = (int) node.getYOffset();
+                 g.drawLine( line[0],
+                             line[1],
+                             line[2],
+                             line[3]
+                             );
+                 if (newLayout == true)
+                     {
+//                          rootLines.put(new Integer((int) node.getYOffset()), new Pair(
+//                                                                                 (int)((node.getTotalDist() - node.getDistance())* xSpreading),
+//                                                                                 (int)(node.getTotalDist()*xSpreading) 
+//                                                                                 )
+//                                          ); 
+
+                         rootLines.put(laf.getLAObject(line), node); 
+
+                         node.setXOffset(
+                                         line[2]
+                                         //  (int)(node.getTotalDist()*xSpreading)
+                                         );
+                     };
+
+
+
+                 if (node.testGrouped() == false) // add the node name if node is leaf
+                     {
+                         g.drawString(node.getNodeName(), (int)( node.getTotalDist() * xSpreading) + 5, node.getYOffset() +3 ); 
+                     }
+                 else // draws a box with number of child if node is internal
+                     {
+                         g.drawRect((int)( node.getTotalDist() * xSpreading),
+                                    node.getYOffset() - ( leafSpace/2 - 1 ), 
+                                    leafSpace * 3 , 
+                                    (int)(leafSpace * 0.8) );
+                         g.drawString(Integer.toString(node.getNoOfLeaves()), (int)( node.getTotalDist() * xSpreading) + 5, node.getYOffset() +4 ); 
+                     }
+ 
+
+             } // end of scope of old line object
+         else
+             {
+                 if(depth > 0){ // rather obsolete
+
+
+                 int[] line = new int[4];
+                 line[0] = (int)((node.getTotalDist() - node.getDistance())* xSpreading);
+                 line[1] = node.getYOffset();
+                 line[2] = (int)(node.getTotalDist()*xSpreading);
+                 line[3] = node.getYOffset();
+                 g.drawLine( line[0],
+                             line[1],
+                             line[2],
+                             line[3]
+                             );
+
+
+                 if (newLayout == true)
+                     {
+//                          branchLines.put(new Integer((int)(node.getYOffset())), new Pair(
+//                                                                                 (int)((node.getTotalDist() - node.getDistance())* xSpreading),
+//                                                                                 (int)(node.getTotalDist()*xSpreading) 
+//                                                                                 )
+//                                          ); 
+
+
+                         rootLines.put(laf.getLAObject(line), node); 
+
+                         // node.setXOffset((int)(node.getTotalDist()*xSpreading));
+                         node.setXOffset(line[2]);
+                     };
+
+                 // vertical branching lines
+
+                 //                 int[] 
+                 line = new int[4];
+                 line[0] = (int)((node.getTotalDist())* xSpreading);
+                 line[1] = ((TreeNode)(node.getChilds().elementAt(0))).getYOffset();
+                 line[2] = (int)((node.getTotalDist())* xSpreading);
+                 line[3] = ((TreeNode)(node.getChilds().elementAt(1))).getYOffset();
+                 g.drawLine( line[0],
+                             line[1],
+                             line[2],
+                             line[3]
+                             );
+
+                 if (newLayout == true)
+                     {
+//                          branchLines.put(new Integer((int)((node.getTotalDist())* xSpreading)), 
+//                                          new Pair(
+//                                                   ((TreeNode)(node.getChilds().elementAt(0))).getYOffset(),
+//                                                   ((TreeNode)(node.getChilds().elementAt(1))).getYOffset()
+//                                                   )
+//                                          ); 
+
+
+                         branchLines.put(laf.getLAObject(line), node); 
+
+
+                     };
+
+
+
+                 displayTreeGraph (g, (TreeNode)(node.getChilds().elementAt(0)), depth -1);
+                 displayTreeGraph (g, (TreeNode)(node.getChilds().elementAt(1)), depth -1);
+                 }
+                 else
+                     {
+//                          g.drawLine( (int)((node.getTotalDist() - node.getDistance())* xSpreading),
+//                                      node.getYOffset(),
+//                                      (int)(node.getTotalDist()*xSpreading),
+//                                      node.getYOffset()
+//                                      );
+                         
+                         System.out.println("I don't know why I was reached");
+                         System.exit(0);
+                     }
+
+             }
+
+
+    }
+
+    // yet to be implemented 
+private void recalculateLayout(TreeNode newRoot)
+    {
+
+
+    }
+
+
+public void handleMouseClick(int x, int y)
+    {
+        System.out.println("handleMouseCLick() was called");
+        System.out.println("coordinates " + x + "\t" + y );
+        for (Iterator it = rootSet.iterator(); it.hasNext();)
+            {
+                LineArea la = (LineArea) it.next();
+                if (la.isInside(x,y))
+                    {
+                        if(((TreeNode) rootLines.get(la)).equals(vs))
+                            {
+                                vs = ((TreeNode) rootLines.get(la)).getFather();
+                            }
+                        else
+                            {
+                                vs = (TreeNode) rootLines.get(la);
+                            }
+
+                        newLayout = true;
+                        la.getBorder(); 
+                        repaint();
+                    }
+
+            }
+
+        for (Iterator it = branchSet.iterator(); it.hasNext();)
+            {
+                LineArea la = (LineArea) it.next();
+                if (la.isInside(x,y))
+                    {
+                        vs = (TreeNode) branchLines.get(la);
+                        newLayout = true;
+                        la.getBorder();
+                        repaint();
+                    }
+
+            }
+
+    }
+
+// private Pair getLine(int x, int y)
+//     {
+
+
+
+
+//         return new Pair(0,9);
+//     }
+    
+    
+}
