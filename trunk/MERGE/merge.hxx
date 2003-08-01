354b1a681a604400831f931c266d793fd731bd40
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-01 13:05:36 +0000

    - added MG_select_preserves_cb

--- trunk/MERGE/merge.hxx
+++ trunk/MERGE/merge.hxx
@@ -2,7 +2,7 @@
 //                                                                       //
 //    File      : merge.hxx                                              //
 //    Purpose   : Local header for usage inside directory MERGE          //
-//    Time-stamp: <Wed Jul/23/2003 11:01 MET Coder@ReallySoft.de>        //
+//    Time-stamp: <Fri Jul/25/2003 10:00 MET Coder@ReallySoft.de>        //
 //                                                                       //
 //                                                                       //
 //  Copyright Department of Microbiology (Technical University Munich)   //
@@ -19,12 +19,13 @@
 #define AWAR_MERGE_DB "tmp/merge1/db"
 #define AWAR_MAIN_DB  "tmp/merge2/db"
 
-AW_window * MG_merge_alignment_cb(AW_root *awr);
-AW_window * MG_merge_names_cb(AW_root *awr);
-AW_window * MG_merge_species_cb(AW_root *awr);
-AW_window * MG_merge_extendeds_cb(AW_root *awr);
-AW_window * MG_merge_trees_cb(AW_root *awr);
-AW_window * MG_merge_configs_cb(AW_root *awr);
+AW_window *MG_merge_alignment_cb(AW_root *awr);
+AW_window *MG_merge_names_cb(AW_root *awr);
+AW_window *MG_merge_species_cb(AW_root *awr);
+AW_window *MG_select_preserves_cb(AW_root *awr);
+AW_window *MG_merge_extendeds_cb(AW_root *awr);
+AW_window *MG_merge_trees_cb(AW_root *awr);
+AW_window *MG_merge_configs_cb(AW_root *awr);
 AW_window *create_mg_check_fields(AW_root *aw_root);
 
 void MG_create_config_awar(AW_root *aw_root, AW_default aw_def);
