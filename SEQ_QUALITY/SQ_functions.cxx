fc23fc28a458cfa1dc79c8662b083032c92ea28f
huberj <huberj@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-04 13:50:27 +0000

    *** empty log message ***

--- SEQ_QUALITY/SQ_functions.cxx
+++ SEQ_QUALITY/SQ_functions.cxx
@@ -4,33 +4,93 @@
 
 
 
-char *  SQ_get_raw_sequence(GBDATA *gb_main) {
+int SQ_get_raw_sequence(GBDATA *gb_main) {
 
 
+    int j = 0;
+    int k;
     char *alignment_name;
     char *rawSequence = 0;
+    int result = 0;
+    int abw = 0; //Standardabweichung
+    int avr = 0; //average length of Sequence
+    char filename[64];
+    char working_on_sequence[10000];
+    int sequenceLength = 0;
+    int sequenceQuality;
+    int qualities[100][1];
     GBDATA *read_sequence = 0;
     GBDATA *gb_species;
     GBDATA *gb_species_data;
+    GBDATA *gb_name;
+    FILE *file1;
 
 
+    strcpy (filename, "rawsequence.txt");
     GB_push_transaction(gb_main);
 
 
-    gb_species_data = GB_search(gb_main,"species_data",GB_FIND);
-    gb_species = GBT_first_species_rel_species_data(gb_species_data);
-    alignment_name = GBT_get_default_alignment(gb_main);
-
-
-    if (gb_species) {
-        read_sequence = GBT_read_sequence(gb_species, alignment_name);
-    
-	if (read_sequence) {
-	    rawSequence = GB_read_string(read_sequence);
-	}
+    //qualities init
+    for (int i = 0; i <=99; i++) {
+	qualities [i][0] = 0;
     }
 
 
-    return rawSequence;
+    gb_species_data = GB_search(gb_main,"species_data",GB_FIND);
+    alignment_name = GBT_get_default_alignment(gb_main);
+
+
+
+    for (gb_species = GBT_first_marked_species(gb_main); gb_species; gb_species = GBT_next_marked_species(gb_species) ){
+
+
+        gb_name = GB_find(gb_species, "name", 0, down_level);
+	if (gb_name) {
+
+	    read_sequence = GBT_read_sequence(gb_species, alignment_name);
+
+	    if (read_sequence) {
+
+		rawSequence = GB_read_string(read_sequence);
+
+		//debug
+		sequenceLength = 100;
+		sequenceQuality = 101;
+		strcpy (working_on_sequence, rawSequence);
+
+		for (int i = 1; i <= sequenceLength; i++) {
+		    if (working_on_sequence[i] == '-') {
+			sequenceQuality--;
+		    }
+		}
+
+		qualities[j][0] = sequenceQuality;
+		j++;
+
+	    }
+	}
+
+    }
+    //calculate average
+    k = j;
+    for (int i = 0; i <= k-1; i++) {
+	avr = avr + qualities[i][0];
+	if (qualities[i][0] == 0) {
+	    k--;
+	}
+    }
+    avr = avr / k;
+
+    //calculate Standardabweichung
+    for (int i = 0; i <= j-1; i++) {
+	abw = abw + (avr - qualities[i][0]);
+	if (qualities[i][0] == 0) {
+	    j--;
+	}
+    }
+    abw = abw / j;
+
+    result = avr;
+    return result;
 
 } 
