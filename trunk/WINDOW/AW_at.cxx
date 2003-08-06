a4e046a964f4ab8b89b74eddbf05d493904e8173
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-06 15:37:42 +0000

    - untabified/indented - simplified at_ifdef - added at_(un)set_to()

--- trunk/WINDOW/AW_at.cxx
+++ trunk/WINDOW/AW_at.cxx
@@ -14,9 +14,9 @@
 /*******************************************************************************************************/
 
 AW_at::AW_at(void) {
-	memset((char*)this,0,sizeof(AW_at));
-	length_of_buttons = 10;
-	shadow_thickness = 2;
+    memset((char*)this,0,sizeof(AW_at));
+    length_of_buttons = 10;
+    shadow_thickness = 2;
 }
 
 
@@ -30,28 +30,28 @@ int  AW_window::get_button_length() const { return _at->length_of_buttons; }
 void AW_window::highlight( void ) { _at->highlight = AW_TRUE; }
 
 void AW_window::auto_increment( int x, int y ) {
-	_at->auto_increment_x = x;
-	_at->auto_increment_y = y;
-	_at->x_for_newline = _at->x_for_next_button;
-	_at->do_auto_space = AW_FALSE;
-	_at->do_auto_increment = AW_TRUE;
-	_at->biggest_height_of_buttons = 0;
+    _at->auto_increment_x = x;
+    _at->auto_increment_y = y;
+    _at->x_for_newline = _at->x_for_next_button;
+    _at->do_auto_space = AW_FALSE;
+    _at->do_auto_increment = AW_TRUE;
+    _at->biggest_height_of_buttons = 0;
 }
 
 
 void AW_window::auto_space( int x, int y ) {
-	_at->auto_space_x = x;
-	_at->auto_space_y = y;
-	_at->x_for_newline = _at->x_for_next_button;
-	_at->do_auto_space = AW_TRUE;
-	_at->do_auto_increment = AW_FALSE;
-	_at->biggest_height_of_buttons = 0;
+    _at->auto_space_x = x;
+    _at->auto_space_y = y;
+    _at->x_for_newline = _at->x_for_next_button;
+    _at->do_auto_space = AW_TRUE;
+    _at->do_auto_increment = AW_FALSE;
+    _at->biggest_height_of_buttons = 0;
 }
 
 
 void AW_window::auto_off( void ) {
-	_at->do_auto_space = AW_FALSE;
-	_at->do_auto_increment = AW_FALSE;
+    _at->do_auto_space = AW_FALSE;
+    _at->do_auto_increment = AW_FALSE;
 }
 
 
@@ -60,50 +60,50 @@ void AW_window::auto_off( void ) {
 /*******************************************************************************************************/
 
 void AW_window::id( const char *id ) {
-	delete _at->id_for_next_button;
-	_at->id_for_next_button		= strdup( id );
+    delete _at->id_for_next_button;
+    _at->id_for_next_button     = strdup( id );
 }
 
 
 void AW_window::help_text(const char *id ) {
-	delete _at->helptext_for_next_button;
-	_at->helptext_for_next_button	= strdup( id );
+    delete _at->helptext_for_next_button;
+    _at->helptext_for_next_button   = strdup( id );
 }
 
 
 void AW_window::mask( AW_active mask ) {
-	_at->mask_for_next_button		= mask;
+    _at->mask_for_next_button       = mask;
 }
 
 
 void AW_window::callback( void (*f)(AW_window*,AW_CL,AW_CL), AW_CL cd1, AW_CL cd2 ) {
-	_callback = new AW_cb_struct(this,(AW_CB)f,cd1,cd2);
+    _callback = new AW_cb_struct(this,(AW_CB)f,cd1,cd2);
 }
 
 void AW_window::callback( void (*f)(AW_window*,AW_CL), AW_CL cd1 ) {
-	_callback = new AW_cb_struct(this,(AW_CB)f,cd1);
+    _callback = new AW_cb_struct(this,(AW_CB)f,cd1);
 }
 
 void AW_window::callback( void (*f)(AW_window*) ) {
-	_callback = new AW_cb_struct(this,(AW_CB)f);
+    _callback = new AW_cb_struct(this,(AW_CB)f);
 }
 void AW_window::callback( AW_cb_struct * /*owner*/awcbs ) {
-	_callback = awcbs;
+    _callback = awcbs;
 }
 
 void AW_window::d_callback( void (*f)(AW_window*,AW_CL,AW_CL), AW_CL cd1, AW_CL cd2 ) {
-	_d_callback = new AW_cb_struct(this,(AW_CB)f,cd1,cd2);
+    _d_callback = new AW_cb_struct(this,(AW_CB)f,cd1,cd2);
 }
 
 void AW_window::d_callback( void (*f)(AW_window*,AW_CL), AW_CL cd1 ) {
-	_d_callback = new AW_cb_struct(this,(AW_CB)f,cd1);
+    _d_callback = new AW_cb_struct(this,(AW_CB)f,cd1);
 }
 
 void AW_window::d_callback( void (*f)(AW_window*) ) {
-	_d_callback = new AW_cb_struct(this,(AW_CB)f);
+    _d_callback = new AW_cb_struct(this,(AW_CB)f);
 }
 void AW_window::d_callback( AW_cb_struct * /*owner*/awcbs ) {
-	_d_callback = awcbs;
+    _d_callback = awcbs;
 }
 
 void AW_window::set_background(const char *colorname){
@@ -112,145 +112,187 @@ void AW_window::set_background(const char *colorname){
 
 
 void AW_window::label( const char *label ) {
-	delete _at->label_for_inputfield;
-	_at->label_for_inputfield = strdup( label );
+    delete _at->label_for_inputfield;
+    _at->label_for_inputfield = strdup( label );
 }
 
 
 void AW_window::at( int x, int y ) {
-		at_x(x);
-		at_y(y);
+    at_x(x);
+    at_y(y);
 }
 
 void AW_window::at_x( int x ) {
-	if (_at->x_for_next_button > _at->max_x_size) _at->max_x_size = _at->x_for_next_button;
-	_at->x_for_next_button = x;
-	if (_at->x_for_next_button > _at->max_x_size) _at->max_x_size = _at->x_for_next_button;
+    if (_at->x_for_next_button > _at->max_x_size) _at->max_x_size = _at->x_for_next_button;
+    _at->x_for_next_button = x;
+    if (_at->x_for_next_button > _at->max_x_size) _at->max_x_size = _at->x_for_next_button;
 }
 
 
 void AW_window::at_y( int y ) {
-	if (_at->y_for_next_button + _at->biggest_height_of_buttons > _at->max_y_size)
-			_at->max_y_size = _at->y_for_next_button + _at->biggest_height_of_buttons;
-	_at->biggest_height_of_buttons = _at->biggest_height_of_buttons + _at->y_for_next_button - y;
-	if (_at->biggest_height_of_buttons<0){
-		_at->biggest_height_of_buttons = 0;
-		if (_at->max_y_size < y) 	_at->max_y_size = y;
-	}
-	_at->y_for_next_button = y;
+    if (_at->y_for_next_button + _at->biggest_height_of_buttons > _at->max_y_size)
+        _at->max_y_size = _at->y_for_next_button + _at->biggest_height_of_buttons;
+    _at->biggest_height_of_buttons = _at->biggest_height_of_buttons + _at->y_for_next_button - y;
+    if (_at->biggest_height_of_buttons<0){
+        _at->biggest_height_of_buttons = 0;
+        if (_at->max_y_size < y)    _at->max_y_size = y;
+    }
+    _at->y_for_next_button = y;
 }
 
 
 void AW_window::at_shift( int x, int y ) {
-	at(x+_at->x_for_next_button,y+_at->y_for_next_button);
+    at(x+_at->x_for_next_button,y+_at->y_for_next_button);
 }
 
 
 void AW_window::at_newline( void ) {
 
-	if ( _at->do_auto_increment ) {
-		at_y(_at->auto_increment_y + _at->y_for_next_button);
-	}
-	else {
-		if ( _at->do_auto_space ) {
-			at_y(_at->y_for_next_button + _at->auto_space_y + _at->biggest_height_of_buttons);
-		}
-		else {
-			AW_ERROR("neither auto_space nor auto_increment activated while using at_newline");
-		}
-	}
-	at_x(_at->x_for_newline);
+    if ( _at->do_auto_increment ) {
+        at_y(_at->auto_increment_y + _at->y_for_next_button);
+    }
+    else {
+        if ( _at->do_auto_space ) {
+            at_y(_at->y_for_next_button + _at->auto_space_y + _at->biggest_height_of_buttons);
+        }
+        else {
+            AW_ERROR("neither auto_space nor auto_increment activated while using at_newline");
+        }
+    }
+    at_x(_at->x_for_newline);
 }
 
 
 void AW_window::at( const char *id ) {
-AWUSE(id);
-char to_position[100];memset(to_position,0,sizeof(to_position));
-	_at->attach_y = _at->attach_x = AW_FALSE;
-	_at->attach_ly = _at->attach_lx = AW_FALSE;
-	_at->attach_any = AW_FALSE;
-	_at->correct_for_at_string = AW_TRUE;
+    AWUSE(id);
+    char to_position[100];memset(to_position,0,sizeof(to_position));
+    _at->attach_y = _at->attach_x = AW_FALSE;
+    _at->attach_ly = _at->attach_lx = AW_FALSE;
+    _at->attach_any = AW_FALSE;
+    _at->correct_for_at_string = AW_TRUE;
 
-	if ( !xfig_data ) {
-		AW_ERROR( "no xfig file loaded " );
-		return;
-	}
-	AW_xfig *xfig = (AW_xfig *)xfig_data;
-	AW_xfig_pos	*pos;
-	if (1){
-	    pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,id);
-	}
-	if (!pos){
-	    sprintf( to_position, "X:%s", id );	pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
-	    if (pos) _at->attach_any = _at->attach_lx = AW_TRUE;
-	}
-	if (!pos){
-	    sprintf( to_position, "Y:%s", id );	pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
-	    if (pos) _at->attach_any = _at->attach_ly = AW_TRUE;
-	}
-	if (!pos){
-	    sprintf( to_position, "XY:%s", id );	pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
-	    if (pos) _at->attach_any = _at->attach_lx = _at->attach_ly = AW_TRUE;
-	}
-	if( !pos ) {
-		AW_ERROR(" ID '%s' does not exist in xfig file", id);
-		return;
-	}
-	at(	(pos->x - xfig->minx),
-		(pos->y - xfig->miny - this->get_root()->font_height - 9));
+    if ( !xfig_data ) {
+        AW_ERROR( "no xfig file loaded " );
+        return;
+    }
+    AW_xfig *xfig = (AW_xfig *)xfig_data;
+    AW_xfig_pos *pos;
+    if (1){
+        pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,id);
+    }
+    if (!pos){
+        sprintf( to_position, "X:%s", id ); pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
+        if (pos) _at->attach_any = _at->attach_lx = AW_TRUE;
+    }
+    if (!pos){
+        sprintf( to_position, "Y:%s", id ); pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
+        if (pos) _at->attach_any = _at->attach_ly = AW_TRUE;
+    }
+    if (!pos){
+        sprintf( to_position, "XY:%s", id );    pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
+        if (pos) _at->attach_any = _at->attach_lx = _at->attach_ly = AW_TRUE;
+    }
+    if( !pos ) {
+        AW_ERROR(" ID '%s' does not exist in xfig file", id);
+        return;
+    }
+    at( (pos->x - xfig->minx),
+        (pos->y - xfig->miny - this->get_root()->font_height - 9));
 
-	if ( pos->center ) {
-		_at->correct_for_at_center = pos->center;
-	}
+    if ( pos->center ) {
+        _at->correct_for_at_center = pos->center;
+    }
 
 
-	if (1){
-	    sprintf( to_position, "to:%s", id );	pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
-	}
-	if (!pos) {
-	    sprintf( to_position, "to:X:%s", id );	pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
-	    if (pos) _at->attach_any = _at->attach_x = AW_TRUE;
-	}
-	if (!pos) {
-	    sprintf( to_position, "to:Y:%s", id );	pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
-	    if (pos) _at->attach_any = _at->attach_y = AW_TRUE;
-	}
-	if (!pos) {
-	    sprintf( to_position, "to:XY:%s", id );	pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
-	    if (pos) _at->attach_any = _at->attach_x = _at->attach_y = AW_TRUE;
-	}
+    if (1){
+        sprintf( to_position, "to:%s", id );    pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
+    }
+    if (!pos) {
+        sprintf( to_position, "to:X:%s", id );  pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
+        if (pos) _at->attach_any = _at->attach_x = AW_TRUE;
+    }
+    if (!pos) {
+        sprintf( to_position, "to:Y:%s", id );  pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
+        if (pos) _at->attach_any = _at->attach_y = AW_TRUE;
+    }
+    if (!pos) {
+        sprintf( to_position, "to:XY:%s", id ); pos = (AW_xfig_pos*)GBS_read_hash(xfig->hash,to_position);
+        if (pos) _at->attach_any = _at->attach_x = _at->attach_y = AW_TRUE;
+    }
 
-	if( pos ) {
-		_at->to_position_exists = AW_TRUE;
-		_at->to_position_x = (pos->x - xfig->minx);
-		_at->to_position_y = (pos->y - xfig->miny);
-		_at->correct_for_at_center = 0;	// left justified
-	}else {
-		_at->to_position_exists = AW_FALSE;
-	}
+    if( pos ) {
+        _at->to_position_exists = AW_TRUE;
+        _at->to_position_x = (pos->x - xfig->minx);
+        _at->to_position_y = (pos->y - xfig->miny);
+        _at->correct_for_at_center = 0; // left justified
+    }else {
+        _at->to_position_exists = AW_FALSE;
+    }
+}
+
+// set "to:XY:id" manually
+// use negative offsets to set offset from right/lower border to to-position
+
+void AW_window::at_set_to(AW_BOOL attach_x, AW_BOOL attach_y, int xoff, int yoff) {
+    aw_assert(attach_x || attach_y); // use at_unset_to() to un-attach
+
+//     if ( !xfig_data ) {
+//         AW_ERROR( "no xfig file loaded " );
+//         return;
+//     }
+//     AW_xfig *xfig = (AW_xfig *)xfig_data;
+
+    _at->attach_any = AW_TRUE;
+    _at->attach_x   = attach_x;
+    _at->attach_y   = attach_y;
+
+    _at->to_position_exists = AW_TRUE;
+    _at->to_position_x      = xoff >= 0 ? _at->x_for_next_button + xoff : _at->max_x_size+xoff;
+    _at->to_position_y      = yoff >= 0 ? _at->y_for_next_button + yoff : _at->max_y_size+yoff;
+
+    if (_at->to_position_x > _at->max_x_size) _at->max_x_size = _at->to_position_x;
+    if (_at->to_position_y > _at->max_y_size) _at->max_y_size = _at->to_position_y;
+
+    _at->correct_for_at_center = xoff >= 0 ? 0 : 2; // left or right justified
+}
+
+void AW_window::at_unset_to() {
+    _at->attach_any = _at->attach_x = _at->attach_y = _at->to_position_exists = AW_FALSE;
 }
 
 AW_BOOL AW_window::at_ifdef(const  char *id) {
-	if (!xfig_data) return AW_FALSE;
-	AW_xfig *xfig = (AW_xfig *)xfig_data;
-	char buffer[1000];
-	sprintf(buffer,"%s",id);	if (GBS_read_hash(xfig->hash,buffer))	return AW_TRUE;
-	sprintf(buffer,"X:%s",id);	if (GBS_read_hash(xfig->hash,buffer))	return AW_TRUE;
-	sprintf(buffer,"Y:%s",id);	if (GBS_read_hash(xfig->hash,buffer))	return AW_TRUE;
-	sprintf(buffer,"XY:%s",id);	if (GBS_read_hash(xfig->hash,buffer))	return AW_TRUE;
-	return AW_FALSE;
+    if (!xfig_data) return AW_FALSE;
+    AW_xfig *xfig = (AW_xfig *)xfig_data;
+    char     buffer[100];
+
+#if defined(DEBUG)
+    int printed =
+#endif // DEBUG
+        sprintf(buffer,"XY:%s",id);
+#if defined(DEBUG)
+    aw_assert(printed<100);
+#endif // DEBUG
+
+    if (GBS_read_hash(xfig->hash,buffer+3)) return AW_TRUE; // "tag"
+    if (GBS_read_hash(xfig->hash,buffer+1)) return AW_TRUE; // "Y:tag"
+    if (GBS_read_hash(xfig->hash,buffer)) return AW_TRUE; // "XY:tag"
+    buffer[1] = 'X';
+    if (GBS_read_hash(xfig->hash,buffer+1)) return AW_TRUE; // "X:tag"
+
+    return AW_FALSE;
 }
 
 void AW_window::check_at_pos( void ) {
-	if (_at->x_for_next_button<10){
-	//	printf("X Position should be greater 10\n");
-	}
+    if (_at->x_for_next_button<10){
+        //  printf("X Position should be greater 10\n");
+    }
 }
 
 void AW_window::get_at_position( int *x, int *y ) {
-	*x = _at->x_for_next_button;
-	*y = _at->y_for_next_button;
+    *x = _at->x_for_next_button;
+    *y = _at->y_for_next_button;
 }
+
 
 void AW_window::store_at_size_and_attach( AW_at_size *at_size ) {
     at_size->store(_at);
@@ -262,53 +304,53 @@ void AW_window::restore_at_size_and_attach( const AW_at_size *at_size ) {
 
 void AW_window::unset_at_commands( void ) {
 
-	delete _at->id_for_next_button;
-	_at->id_for_next_button = NULL;
+    delete _at->id_for_next_button;
+    _at->id_for_next_button = NULL;
 
-	delete _at->helptext_for_next_button;
-	_at->helptext_for_next_button 	= NULL;
+    delete _at->helptext_for_next_button;
+    _at->helptext_for_next_button   = NULL;
 
-	_at->mask_for_next_button			= AWM_ALL;
+    _at->mask_for_next_button           = AWM_ALL;
 
-	_callback				= NULL;
-	_d_callback				= NULL;
+    _callback               = NULL;
+    _d_callback             = NULL;
 
-	free(_at->label_for_inputfield);
-	_at->label_for_inputfield = NULL;
+    free(_at->label_for_inputfield);
+    _at->label_for_inputfield = NULL;
 
-	_at->correct_for_at_string = AW_FALSE;
-	_at->correct_for_at_center = 0;
+    _at->correct_for_at_string = AW_FALSE;
+    _at->correct_for_at_center = 0;
 
-	_at->to_position_exists	= AW_FALSE;
+    _at->to_position_exists = AW_FALSE;
 
-	_at->highlight = AW_FALSE;
-	free(_at->background_colorname);
-	_at->background_colorname = 0;
+    _at->highlight = AW_FALSE;
+    free(_at->background_colorname);
+    _at->background_colorname = 0;
 }
 
 void AW_window::increment_at_commands( int width, int height ) {
 
-	at_shift(width,0);
-    at_shift(-width,0);		    // set bounding box
+    at_shift(width,0);
+    at_shift(-width,0);         // set bounding box
 
-	if ( _at->do_auto_increment ) {
-		at_shift(_at->auto_increment_x,0);
-	}
-	if ( _at->do_auto_space ) {
-		at_shift(_at->auto_space_x + width,0);
-	}
+    if ( _at->do_auto_increment ) {
+        at_shift(_at->auto_increment_x,0);
+    }
+    if ( _at->do_auto_space ) {
+        at_shift(_at->auto_space_x + width,0);
+    }
 
-	if ( _at->biggest_height_of_buttons < height ) {
-		_at->biggest_height_of_buttons = height;
-	}
+    if ( _at->biggest_height_of_buttons < height ) {
+        _at->biggest_height_of_buttons = height;
+    }
 
-	if ( _at->max_y_size < (_at->y_for_next_button +_at->biggest_height_of_buttons +3.0)) {
-		_at->max_y_size = _at->y_for_next_button +_at->biggest_height_of_buttons +3;
-	}
+    if ( _at->max_y_size < (_at->y_for_next_button +_at->biggest_height_of_buttons +3.0)) {
+        _at->max_y_size = _at->y_for_next_button +_at->biggest_height_of_buttons +3;
+    }
 
-	if ( _at->max_x_size < (_at->x_for_next_button + this->get_root()->font_width)) {
-		_at->max_x_size = _at->x_for_next_button + this->get_root()->font_width;
-	}
+    if ( _at->max_x_size < (_at->x_for_next_button + this->get_root()->font_width)) {
+        _at->max_x_size = _at->x_for_next_button + this->get_root()->font_width;
+    }
 }
 
 /*******************************************************************************************************/
@@ -316,42 +358,42 @@ void AW_window::increment_at_commands( int width, int height ) {
 /*******************************************************************************************************/
 
 int AW_window::calculate_string_width( int columns ) {
-	if ( xfig_data ) {
-		AW_xfig *xfig = (AW_xfig *)xfig_data;
-		//return columns * this->get_root()->font_width;
-		return (int)(columns * xfig->font_scale * 8);	/* stdfont 8x13 */
-	}else {
-		return columns * 8;	/* stdfont 8x13 */
-	}
+    if ( xfig_data ) {
+        AW_xfig *xfig = (AW_xfig *)xfig_data;
+        //return columns * this->get_root()->font_width;
+        return (int)(columns * xfig->font_scale * 8);   /* stdfont 8x13 */
+    }else {
+        return columns * 8; /* stdfont 8x13 */
+    }
 }
 
 int AW_window::calculate_label_length()
-	{
-	if (_at->length_of_label_for_inputfield) return _at->length_of_label_for_inputfield;
-	if (_at->label_for_inputfield) return strlen(_at->label_for_inputfield);
-	return 0;
+{
+    if (_at->length_of_label_for_inputfield) return _at->length_of_label_for_inputfield;
+    if (_at->label_for_inputfield) return strlen(_at->label_for_inputfield);
+    return 0;
 }
 
 int AW_window::calculate_string_height( int rows , int offset ) {
-	if ( xfig_data ) {
-		AW_xfig *xfig = (AW_xfig *)xfig_data;
-		return (int)((rows * 13 + offset ) * xfig->font_scale);	/* stdfont 8x13 */
-	}
-	else {
-		return (rows * 13 + offset );	/* stdfont 8x13 */
-	}
+    if ( xfig_data ) {
+        AW_xfig *xfig = (AW_xfig *)xfig_data;
+        return (int)((rows * 13 + offset ) * xfig->font_scale); /* stdfont 8x13 */
+    }
+    else {
+        return (rows * 13 + offset );   /* stdfont 8x13 */
+    }
 }
 
 
 char *AW_window::align_string( const char *string, int columns ) {
-char *help_label;
+    char *help_label;
 
-	help_label = new char[200];
-	strcpy( help_label, string );
-	strcat( help_label,"                                                                               " );
-	help_label[columns] = '\0';
+    help_label = new char[200];
+    strcpy( help_label, string );
+    strcat( help_label,"                                                                               " );
+    help_label[columns] = '\0';
 
-	return help_label;
+    return help_label;
 }
 
 /*******************************************************************************************************/
