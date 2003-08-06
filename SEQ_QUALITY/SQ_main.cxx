13d488bda7eaca24f7c25f4ee27bcbc68a58ff01
huberj <huberj@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-06 14:28:33 +0000

    *** empty log message ***

--- SEQ_QUALITY/SQ_main.cxx
+++ SEQ_QUALITY/SQ_main.cxx
@@ -10,12 +10,12 @@
 GB_ERROR SQ_calc_seq_quality(GBDATA *gb_main, const char *tree_name) {
     
 
-    int quality = SQ_get_raw_sequence(gb_main); 
+    int percent = SQ_calc_sequence_diff(gb_main); 
     //debug
     tree_name = 0;
 
     GB_ERROR error = 0;
-    error = GB_export_error("Alignment quality: %i ", quality);
+    error = GB_export_error("Difference of sequence from average in percent: %i ", percent);
     return error;
 
 
