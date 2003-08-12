1af0fdd96c97f0b374785ba020419a61306da23e
huberj <huberj@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-12 14:56:13 +0000

    by Ralf

--- trunk/SEQ_QUALITY/seq_quality.cxx
+++ trunk/SEQ_QUALITY/seq_quality.cxx
@@ -5,10 +5,10 @@
 
 GB_ERROR SQ_calc_seq_quality(GBDATA *gbmain, const char *tree_name) {
 
-
-    if (!tree) return GB_get_error();
-    GB_ERROR error = GBT_link_tree(tree,gbmain,GB_FALSE);
-    if (error) return error;
+  GBT_TREE *tree = GBT_read_tree(gbmain, tree_name, sizeof(GBT_TREE));
+  if (!tree) /*...*/
+  GB_ERROR error = GBT_link_tree(tree,gbmain,GB_FALSE);
+  if (error) /*...*/
 
 
 }
