a76fb13b9b48dba60fbd305cb36acdca87102de3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-12 08:35:41 +0000

    - saves AWAR_SAI_ENABLE, AWAR_SAI_ALL_SPECIES and AWAR_SAI_CLR_TRANS_TABLE to props - removed all AWAR definitions from header - added ED4_createVisualizeSAI_Awars - AW_root passed to getSaiColorString() - removed unneeded function checkSai()

--- trunk/EDIT4/ed4_visualizeSAI.hxx
+++ trunk/EDIT4/ed4_visualizeSAI.hxx
@@ -10,33 +10,7 @@
 /*                                                                                       */
 /*=======================================================================================*/
 
-#define AWAR_SAI_CLR_TAB             "saicolors/"
-#define AWAR_SAI_SELECT              AWAR_SAI_CLR_TAB "select"
-#define AWAR_SAI_CLR_DEF             AWAR_SAI_CLR_TAB "clr_trans_tab/"
-#define AWAR_SAI_CLR_TRANS_TAB_NAMES AWAR_SAI_CLR_TAB "clr_trans_tab_names"
-
-#define AWAR_SAI_ENABLE                "tmp/sai/enable"
-#define AWAR_SAI_ALL_SPECIES           "tmp/sai/all_species"
-#define AWAR_SAI_CLR_TRANS_TABLE       "tmp/sai/clr_trans_table"
-#define AWAR_SAI_CLR_TRANS_TAB_CREATE  "tmp/sai/clr_trans_tab_create"
-#define AWAR_SAI_CLR_TRANS_TAB_COPY    "tmp/sai/clr_trans_tab_copy"
-
-#define AWAR_SAI_CLR    "tmp/sai/color_"
-#define AWAR_SAI_CLR_0  AWAR_SAI_CLR "0"
-#define AWAR_SAI_CLR_1  AWAR_SAI_CLR "1"
-#define AWAR_SAI_CLR_2  AWAR_SAI_CLR "2"
-#define AWAR_SAI_CLR_3  AWAR_SAI_CLR "3"
-#define AWAR_SAI_CLR_4  AWAR_SAI_CLR "4"
-#define AWAR_SAI_CLR_5  AWAR_SAI_CLR "5"
-#define AWAR_SAI_CLR_6  AWAR_SAI_CLR "6"
-#define AWAR_SAI_CLR_7  AWAR_SAI_CLR "7"
-#define AWAR_SAI_CLR_8  AWAR_SAI_CLR "8"
-#define AWAR_SAI_CLR_9  AWAR_SAI_CLR "9"
-
-#define CREATE_CLR_TR_TABLE  1
-#define COPY_CLR_TR_TABLE    0
-
+void        ED4_createVisualizeSAI_Awars(AW_root *aw_root, AW_default aw_def); // create awars
 AW_window  *ED4_createVisualizeSAI_window(AW_root *aw_root);
-const char *getSaiColorString(int start, int end); //used in ED4_text_terminal.cxx & SEC_paint.cxx to paint the SAI in the background
-int         checkSai(const char *species_name); //used in ED4_text_terminal.cxx  to validate SAI with respect to species
+const char *getSaiColorString(AW_root *aw_root, int start, int end); // used in ED4_text_terminal.cxx & SEC_paint.cxx to paint the SAI in the background
 
