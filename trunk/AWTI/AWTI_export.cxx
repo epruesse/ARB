bc4dac3356d5b71d3c79ec51d1a3aaceec1a7d4c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-14 23:31:50 +0000

    - fixed calls to init() - beautified

--- trunk/AWTI/AWTI_export.cxx
+++ trunk/AWTI/AWTI_export.cxx
@@ -20,47 +20,47 @@
 using std::string;
 
 export_format_struct::export_format_struct(void){
-	memset((char *)this,0,sizeof(export_format_struct));
+    memset((char *)this,0,sizeof(export_format_struct));
 }
 
 export_format_struct::~export_format_struct(void)
-	{
-	struct export_format_struct *efo= this;
-	free(efo->system);
-	free(efo->new_format);
-	free(efo->suffix);
+{
+    struct export_format_struct *efo= this;
+    free(efo->system);
+    free(efo->new_format);
+    free(efo->suffix);
 }
 
 
 char *awtc_read_export_format(export_format_struct * efo,char *file){
-	char *fullfile = AWT_unfold_path(file,"ARBHOME");
+    char *fullfile = AWT_unfold_path(file,"ARBHOME");
 
-	FILE *in = fopen(fullfile,"r");
-	free(fullfile);
-	sprintf(AW_ERROR_BUFFER,"Form %s not readable (select a form or check permissions)", file);
-	if (!in) return AW_ERROR_BUFFER;
-	char *s1=0,*s2=0;
+    FILE *in = fopen(fullfile,"r");
+    free(fullfile);
+    sprintf(AW_ERROR_BUFFER,"Form %s not readable (select a form or check permissions)", file);
+    if (!in) return AW_ERROR_BUFFER;
+    char *s1=0,*s2=0;
 
-	while (!awtc_read_string_pair(in,s1,s2)){
-		if (!s1) {
-			continue;
-		}else if (!strcmp(s1,"SYSTEM")) {
-			efo->system = s2; s2 = 0;
-		}else if (!strcmp(s1,"INTERNAL")) {
-			efo->internal_command = s2; s2 = 0;
-		}else if (!strcmp(s1,"PRE_FORMAT")) {
-			efo->new_format = s2; s2 = 0;
-		}else if (!strcmp(s1,"SUFFIX")) {
-			efo->suffix = s2; s2 = 0;
-		}else if (!strcmp(s1,"BEGIN")) {
-			break;
-		}else{
-			fprintf(stderr,"Unknown command in import format file: %s\n",s1);
-		}
-	}
+    while (!awtc_read_string_pair(in,s1,s2)){
+        if (!s1) {
+            continue;
+        }else if (!strcmp(s1,"SYSTEM")) {
+            efo->system = s2; s2 = 0;
+        }else if (!strcmp(s1,"INTERNAL")) {
+            efo->internal_command = s2; s2 = 0;
+        }else if (!strcmp(s1,"PRE_FORMAT")) {
+            efo->new_format = s2; s2 = 0;
+        }else if (!strcmp(s1,"SUFFIX")) {
+            efo->suffix = s2; s2 = 0;
+        }else if (!strcmp(s1,"BEGIN")) {
+            break;
+        }else{
+            fprintf(stderr,"Unknown command in import format file: %s\n",s1);
+        }
+    }
 
-	fclose(in);
-	return 0;
+    fclose(in);
+    return 0;
 }
 
 //  -----------------------------
@@ -142,14 +142,14 @@ GB_ERROR AWTI_export_format(AW_root *aw_root, GBDATA *gb_main, char *formname, c
     if (!form || form[0] == 0) {
         if (!formname || formname[0] == 0) error = GB_export_error("Please select a form");
         else error                               = GB_export_IO_error("loading export form", fullformname);
-	}else{
-		char *form2 = GBS_string_eval(form,"*\nBEGIN*\n*=*3:\\==\\\\\\=:*=\\*\\=*1:\\:=\\\\\\:",0);
-		if (!form2) error = (char *)GB_get_error();
-		free(form);
-		form = form2;
-	}
+    }else{
+        char *form2 = GBS_string_eval(form,"*\nBEGIN*\n*=*3:\\==\\\\\\=:*=\\*\\=*1:\\:=\\\\\\:",0);
+        if (!form2) error = (char *)GB_get_error();
+        free(form);
+        form = form2;
+    }
 
-	if (openstatus) aw_openstatus("Exporting Data");
+    if (openstatus) aw_openstatus("Exporting Data");
 
     export_format_struct *efo = 0;
     if (!error) {
@@ -157,11 +157,11 @@ GB_ERROR AWTI_export_format(AW_root *aw_root, GBDATA *gb_main, char *formname, c
         error = awtc_read_export_format(efo,formname);
     }
 
-	if (!error) {
-		if (efo->system && !efo->new_format) {
-			error = "Format File Error: You can only use the command SYSTEM "
-				"when you use the command PRE_FORMAT";
-		}
+    if (!error) {
+        if (efo->system && !efo->new_format) {
+            error = "Format File Error: You can only use the command SYSTEM "
+                "when you use the command PRE_FORMAT";
+        }
         else if (efo->internal_command) {
             if (efo->system) {
                 error = "Format File Error: You can't use SYSTEM together with INTERNAL";
@@ -183,9 +183,9 @@ GB_ERROR AWTI_export_format(AW_root *aw_root, GBDATA *gb_main, char *formname, c
             }
         }
         else if (efo->new_format) {
-			if (efo->system) {
-				char intermediate[1024];
-				char srt[1024];
+            if (efo->system) {
+                char intermediate[1024];
+                char srt[1024];
 
                 {
                     const char *out_nameonly        = strrchr(outname, '/');
@@ -194,31 +194,31 @@ GB_ERROR AWTI_export_format(AW_root *aw_root, GBDATA *gb_main, char *formname, c
                 }
 
                 char *intermediate_resulting = 0;
-				error = AWTI_export_format(aw_root, gb_main, efo->new_format, intermediate, 0, 0, &intermediate_resulting);
+                error = AWTI_export_format(aw_root, gb_main, efo->new_format, intermediate, 0, 0, &intermediate_resulting);
 
-				if (!error) {
-					sprintf(srt,"$<=%s:$>=%s",intermediate_resulting, outname);
-					char *sys = GBS_string_eval(efo->system,srt,0);
-					sprintf(AW_ERROR_BUFFER,"exec '%s'",efo->system);
-					aw_status(AW_ERROR_BUFFER);
-					if (system(sys)) {
-						sprintf(AW_ERROR_BUFFER,"Error in '%s'",sys);
-						delete sys; error = AW_ERROR_BUFFER;
-					}
-					free(sys);
-				}
+                if (!error) {
+                    sprintf(srt,"$<=%s:$>=%s",intermediate_resulting, outname);
+                    char *sys = GBS_string_eval(efo->system,srt,0);
+                    sprintf(AW_ERROR_BUFFER,"exec '%s'",efo->system);
+                    aw_status(AW_ERROR_BUFFER);
+                    if (system(sys)) {
+                        sprintf(AW_ERROR_BUFFER,"Error in '%s'",sys);
+                        delete sys; error = AW_ERROR_BUFFER;
+                    }
+                    free(sys);
+                }
                 free(intermediate_resulting);
-			}else{
-				error = AWTI_export_format(aw_root, gb_main, efo->new_format, outname, multiple, 0, 0);
-			}
-			goto end_of_AWTI_export_format;
-		}
-	}
+            }else{
+                error = AWTI_export_format(aw_root, gb_main, efo->new_format, outname, multiple, 0, 0);
+            }
+            goto end_of_AWTI_export_format;
+        }
+    }
 
     if (!error) {
         try {
             FILE         *out   = 0;
-            int	          count = 0;
+            int           count = 0;
             char          buffer[1024];
             XML_Document *xml   = 0;
 
@@ -332,79 +332,79 @@ GB_ERROR AWTI_export_format(AW_root *aw_root, GBDATA *gb_main, char *formname, c
     }
 
  end_of_AWTI_export_format:
-	if (openstatus) aw_closestatus();
+    if (openstatus) aw_closestatus();
 
-	free(fullformname);
-	free(form);
-	delete efo;
+    free(fullformname);
+    free(form);
+    delete efo;
 
-	return error;
+    return error;
 }
 
 
 void AWTC_export_go_cb(AW_window *aww,GBDATA *gb_main){
 
 
-	GB_transaction dummy(gb_main);
+    GB_transaction dummy(gb_main);
 
-	AW_root *awr = aww->get_root();
-	char	*formname = awr->awar(AWAR_EXPORT_FORM"/file_name")->read_string();
-	int	multiple = (int)awr->awar(AWAR_EXPORT_MULTIPLE_FILES)->read_int();
-	GB_ERROR error = 0;
+    AW_root *awr = aww->get_root();
+    char    *formname = awr->awar(AWAR_EXPORT_FORM"/file_name")->read_string();
+    int multiple = (int)awr->awar(AWAR_EXPORT_MULTIPLE_FILES)->read_int();
+    GB_ERROR error = 0;
 
-	char *outname      = awr->awar(AWAR_EXPORT_FILE"/file_name")->read_string();
+    char *outname      = awr->awar(AWAR_EXPORT_FILE"/file_name")->read_string();
     char *real_outname = 0;     // with suffix
 
-	error = AWTI_export_format(awr, gb_main, formname, outname, multiple,1, &real_outname);
+    error = AWTI_export_format(awr, gb_main, formname, outname, multiple,1, &real_outname);
     if (real_outname) awr->awar(AWAR_EXPORT_FILE"/file_name")->write_string(real_outname);
 
     awt_refresh_selection_box(awr, AWAR_EXPORT_FILE);
 
-	free(outname);
-	free(formname);
+    free(outname);
+    free(formname);
 
-	if (error) aw_message(error);
+    if (error) aw_message(error);
 }
 
 AW_window *open_AWTC_export_window(AW_root *awr,GBDATA *gb_main)
-	{
-	static AW_window_simple *aws = 0;
-	if (aws) return (AW_window *) aws;
+{
+    static AW_window_simple *aws = 0;
+    if (aws) return (AW_window *) aws;
 
 
-	awr->awar_string(AWAR_EXPORT_FORM"/directory","lib/export",AW_ROOT_DEFAULT);
-	awr->awar_string(AWAR_EXPORT_FORM"/filter",".eft",AW_ROOT_DEFAULT);
-	awr->awar_string(AWAR_EXPORT_FORM"/file_name","",AW_ROOT_DEFAULT);
-	awr->awar_string(AWAR_EXPORT_FILE"/directory","",AW_ROOT_DEFAULT);
-	awr->awar_string(AWAR_EXPORT_FILE"/filter","",AW_ROOT_DEFAULT);
-	awr->awar_string(AWAR_EXPORT_FILE"/file_name","noname",AW_ROOT_DEFAULT);
-	awr->awar_string(AWAR_EXPORT_ALI,"16s",AW_ROOT_DEFAULT);
-	awr->awar_int(AWAR_EXPORT_MULTIPLE_FILES,0,AW_ROOT_DEFAULT);
+    awr->awar_string(AWAR_EXPORT_FORM"/directory","lib/export",AW_ROOT_DEFAULT);
+    awr->awar_string(AWAR_EXPORT_FORM"/filter",".eft",AW_ROOT_DEFAULT);
+    awr->awar_string(AWAR_EXPORT_FORM"/file_name","",AW_ROOT_DEFAULT);
+    awr->awar_string(AWAR_EXPORT_FILE"/directory","",AW_ROOT_DEFAULT);
+    awr->awar_string(AWAR_EXPORT_FILE"/filter","",AW_ROOT_DEFAULT);
+    awr->awar_string(AWAR_EXPORT_FILE"/file_name","noname",AW_ROOT_DEFAULT);
+    awr->awar_string(AWAR_EXPORT_ALI,"16s",AW_ROOT_DEFAULT);
+    awr->awar_int(AWAR_EXPORT_MULTIPLE_FILES,0,AW_ROOT_DEFAULT);
 
-	aws = new AW_window_simple;
+    aws = new AW_window_simple;
 
-	aws->init( awr, "ARB_EXPORT", "ARB EXPORT", 400, 100 );
-	aws->load_xfig("awt/export_db.fig");
+    aws->init( awr, "ARB_EXPORT", "ARB EXPORT");
+    aws->load_xfig("awt/export_db.fig");
 
-	aws->at("close");
-	aws->callback(AW_POPDOWN);
-	aws->create_button("CLOSE", "CLOSE","C");
+    aws->at("close");
+    aws->callback(AW_POPDOWN);
+    aws->create_button("CLOSE", "CLOSE","C");
 
-	aws->at("help");
-	aws->callback(AW_POPUP_HELP,(AW_CL)"arb_export.hlp");
-	aws->create_button("HELP", "HELP","H");
+    aws->at("help");
+    aws->callback(AW_POPUP_HELP,(AW_CL)"arb_export.hlp");
+    aws->create_button("HELP", "HELP","H");
 
-	awt_create_selection_box(aws,AWAR_EXPORT_FILE,"f" );
+    awt_create_selection_box(aws,AWAR_EXPORT_FILE,"f" );
 
-	awt_create_selection_box(aws,AWAR_EXPORT_FORM,"","ARBHOME", AW_FALSE );
+    awt_create_selection_box(aws,AWAR_EXPORT_FORM,"","ARBHOME", AW_FALSE );
 
-	aws->at("multiple");
-	aws->create_toggle(AWAR_EXPORT_MULTIPLE_FILES);
+    aws->at("multiple");
+    aws->create_toggle(AWAR_EXPORT_MULTIPLE_FILES);
 
-	aws->at("go");
-	aws->highlight();
-	aws->callback((AW_CB1)AWTC_export_go_cb,(AW_CL)gb_main);
-	aws->create_button("GO", "GO","G");
+    aws->at("go");
+    aws->highlight();
+    aws->callback((AW_CB1)AWTC_export_go_cb,(AW_CL)gb_main);
+    aws->create_button("GO", "GO","G");
 
-	return (AW_window *) aws;
+    return (AW_window *) aws;
 }
