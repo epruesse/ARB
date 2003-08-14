5009d99749be1c14074793e4230bd2f6f3ba8914
huberj <huberj@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-14 14:00:36 +0000

    *** empty log message ***

--- SEQ_QUALITY/SQ_functions.cxx
+++ SEQ_QUALITY/SQ_functions.cxx
@@ -10,36 +10,24 @@
 #endif
 #define seq_assert(bed) arb_assert(bed)
 
-int SQ_calc_sequence_diff(GBDATA *gb_main) {
 
 
-    int j = 0;
+void SQ_calc_sequence_structure(GBDATA *gb_main) {
+
+
     char *alignment_name;
-    const char *rawSequence = 0;
-    int result = 0;
-    int avr = 0; //average length of Sequence
-    char working_on_sequence[100000];
-    int sequenceLength = 0;
-    int qualities[100][3];
+
     GBDATA *read_sequence = 0;
     GBDATA *gb_species;
     GBDATA *gb_species_data;
     GBDATA *gb_name;
+    GBDATA *(*getFirst)(GBDATA*) = 0;
+    GBDATA *(*getNext)(GBDATA*) = 0;
 
 
     GB_push_transaction(gb_main);
-
-    //qualities init
-    for (int i = 0; i <=99; i++) {
-	qualities [i][0] = 0;
-	qualities [i][1] = 0;
-	qualities [i][2] = 0;
-    }
-
-
     gb_species_data = GB_search(gb_main,"species_data",GB_CREATE_CONTAINER);
     alignment_name = GBT_get_default_alignment(gb_main);
-
     seq_assert(alignment_name);
 
     {
@@ -49,8 +37,108 @@ int SQ_calc_sequence_diff(GBDATA *gb_main) {
     }
 
 
+    if (true /*marked_only*/) {
+      getFirst = GBT_first_marked_species;
+      getNext = GBT_next_marked_species;
+    }
+    else {
+
+    }
+
+
+    for (gb_species = getFirst(gb_main); gb_species; gb_species = getNext(gb_species) ){
+     
+      gb_name = GB_find(gb_species, "name", 0, down_level);
+
+      if (gb_name) {
+
+	GBDATA *gb_ali = GB_find(gb_species,alignment_name,0,down_level);
+	
+	if (gb_ali) {   //!gb_ali
+	  GBDATA *gb_quality = GB_search(gb_ali, "quality", GB_CREATE_CONTAINER);	  
+	  read_sequence = GB_find(gb_ali,"data",0,down_level);
+
+	  if (read_sequence) {
+	    int count_bases = 0;
+	    int count_scores = 0;
+	    int count_dots = 0;
+	    int percent_scores = 0;
+	    int percent_dots = 0;
+	    int percent_bases = 0;
+	    int sequenceLength = 0;
+	    const char *rawSequence = 0;
+
+	    rawSequence = GB_read_char_pntr(read_sequence);
+	    sequenceLength = GB_read_count(read_sequence);
+	    count_bases = sequenceLength;
+
+	    //claculate physical layout of sequence
+	    for (int i = 0; i < sequenceLength; i++) {
+	      if (rawSequence[i] == '-') {
+		count_bases--;
+		count_scores++;
+	      }
+	      if (rawSequence[i] == '.') {
+		count_bases--;
+		count_dots++;
+	      }
+	    }
+
+	    //calculate layout in percent
+	    percent_scores = (100 * count_scores) / sequenceLength;
+	    percent_dots = (100 * count_dots) / sequenceLength;
+	    percent_bases = 100 - (percent_scores + percent_dots);
+
+
+	    GBDATA *gb_result1 = GB_search(gb_quality, "number_of_bases", GB_INT);
+	    seq_assert(gb_result1);
+	    GB_write_int(gb_result1, count_bases);
+
+	    GBDATA *gb_result2 = GB_search(gb_quality, "number_of_scores", GB_INT);
+	    seq_assert(gb_result2);
+	    GB_write_int(gb_result2, count_scores);
+
+	    GBDATA *gb_result3 = GB_search(gb_quality, "number_of_dots", GB_INT);
+	    seq_assert(gb_result3);
+	    GB_write_int(gb_result3, count_dots);
+
+	    GBDATA *gb_result4 = GB_search(gb_quality, "percent_of_bases", GB_INT);
+	    seq_assert(gb_result4);
+	    GB_write_int(gb_result4, percent_bases);
+
+	    GBDATA *gb_result5 = GB_search(gb_quality, "percent_of_scores", GB_INT);
+	    seq_assert(gb_result5);
+	    GB_write_int(gb_result5, percent_scores);
+
+	    GBDATA *gb_result6 = GB_search(gb_quality, "percent_of_dots", GB_INT);
+	    seq_assert(gb_result6);
+	    GB_write_int(gb_result6, percent_dots);
+
+	  }
+	}
+      }
+    }
+}
+
+
+
+int SQ_calc_average_structure(GBDATA *gb_main) {
+
+    char *alignment_name;
+    int avg_bases = 0;
+    int j = 0;
+
+    GBDATA *gb_species;
+    GBDATA *gb_species_data;
+    GBDATA *gb_name;
     GBDATA *(*getFirst)(GBDATA*) = 0;
     GBDATA *(*getNext)(GBDATA*) = 0;
+
+
+    GB_push_transaction(gb_main);
+    gb_species_data = GB_search(gb_main,"species_data",GB_CREATE_CONTAINER);
+    alignment_name = GBT_get_default_alignment(gb_main);
+    seq_assert(alignment_name);
 
     if (true /*marked_only*/) {
       getFirst = GBT_first_marked_species;
@@ -60,76 +148,26 @@ int SQ_calc_sequence_diff(GBDATA *gb_main) {
 
     }
 
+
     for (gb_species = getFirst(gb_main); gb_species; gb_species = getNext(gb_species) ){
+     
       gb_name = GB_find(gb_species, "name", 0, down_level);
+
       if (gb_name) {
 
 	GBDATA *gb_ali = GB_find(gb_species,alignment_name,0,down_level);
-	if (!gb_ali) {
-	  GBDATA *gb_quality = GB_search(gb_ali, "quality", GB_CREATE_CONTAINER);	 
-	  
-	  read_sequence = GB_find(gb_ali,"data",0,down_level);
-
-	  if (read_sequence) {
-	    int sequenceQuality;
-
-	    rawSequence = GB_read_char_pntr(read_sequence);
-	    sequenceLength = GB_read_count(read_sequence);
-	    sequenceQuality = sequenceLength;
-	    strcpy (working_on_sequence, rawSequence);
-
-	    for (int i = 0; i < sequenceLength; i++) {
-	      if (working_on_sequence[i] == '-') {
-		sequenceQuality--;
-	      }
-	      if (working_on_sequence[i] == '.') {
-		sequenceQuality--;
-	      }
-	    }
-
-	    qualities[j][0] = sequenceQuality;
+	
+	if (gb_ali) {
+	    GBDATA *gb_quality = GB_search(gb_ali, "quality", GB_FIND);	  
+	    GBDATA *gb_result1 = GB_search(gb_quality, "number_of_bases", GB_INT);	    
+	    avg_bases = avg_bases + GB_read_int(gb_result1);
 	    j++;
 
-	    
-	    {
-	      GBDATA *gb_example_result = GB_search(gb_quality, "example_result",GB_INT);
-	      seq_assert(gb_example_result);
-
-	      GB_write_int(gb_example_result, 4711);
-	    }
-	  }
 	}
       }
-
     }
-
-
-    //calculate average
-
-    for (int i = 0; i < j; i++) {
-	avr = avr + qualities[i][0];
-    }
-
-    avr = avr / j;
-
-
-    //calculate differences
-
-    for (int i = 0; i < j; i++) {
-	    qualities[i][1] = avr - qualities[i][0];
-    }
-
-
-    //calculate difference from average in %
-
-    for (int i = 0; i < j; i++) {
-	    qualities[i][2] = ((100*qualities[i][1]) / avr);
-    }
-
-    free(alignment_name);
-
-    result = qualities[0][2];
-    return result;
+    avg_bases = avg_bases / j ;
+    return avg_bases;
 
 }
 
