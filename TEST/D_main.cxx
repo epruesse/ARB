67bc6eae27f96727260cf2d0eeec930e39f7827e
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-14 23:53:32 +0000

    - uses AWAR_FOOTER - test mode help for max allowed length (=AWAR_FOOTER_MAX_LEN)

--- TEST/D_main.cxx
+++ TEST/D_main.cxx
@@ -16,6 +16,10 @@
 #include "d_classes.hxx"
 #include "d_main.hxx"
 #include "d_awt_graphic_designer.hxx"
+
+#include "arb_assert.hxx"
+#define test_assert(cond) arb_assert(cond)
+
 AW_HEADER_MAIN;
 
 void AD_map_viewer(GBDATA* gbd){return;};
@@ -24,98 +28,98 @@ GBDATA *gb_main;
 
 AW_window *create_main_window(AW_root *aw_root) {
 
-	AW_gc_manager aw_gc_manager = 0;
-	AW_window_menu_modes *awm = new AW_window_menu_modes();
-	awm->init(aw_root,"DESIGNER", 800,600,0,0);
-	AWT_canvas *ntw = new AWT_canvas(gb_main,awm,(AWT_graphic *)new AWT_graphic_designer(), aw_gc_manager);
-	awm->create_menu(       0,	"File",	"F",	"xxx.hlp", 	0 );
-	awm->insert_menu_topic(	0,	"Quit",	"Q",	"quit.hlp",	0, (AW_CB)quit_cb, 0, 0);
-	awm->insert_separator();
-	awm->insert_menu_topic(0,"Colors and Fonts ...", "C","colors.hlp",0,
-				AW_POPUP, (AW_CL)AW_create_gc_window, (AW_CL)aw_gc_manager );
-	awm->insert_menu_topic(0, "Print View to Printer ...",	"P","print.hlp",0,(AW_CB)AWT_create_print_window, (AW_CL)ntw,	0 );
+    AW_gc_manager aw_gc_manager = 0;
+    AW_window_menu_modes *awm = new AW_window_menu_modes();
+    awm->init(aw_root,"DESIGNER", 800,600,0,0);
+    AWT_canvas *ntw = new AWT_canvas(gb_main,awm,(AWT_graphic *)new AWT_graphic_designer(), aw_gc_manager);
+    awm->create_menu(       0,  "File", "F",    "xxx.hlp",  0 );
+    awm->insert_menu_topic( 0,  "Quit", "Q",    "quit.hlp", 0, (AW_CB)quit_cb, 0, 0);
+    awm->insert_separator();
+    awm->insert_menu_topic(0,"Colors and Fonts ...", "C","colors.hlp",0,
+                           AW_POPUP, (AW_CL)AW_create_gc_window, (AW_CL)aw_gc_manager );
+    awm->insert_menu_topic(0, "Print View to Printer ...",  "P","print.hlp",0,(AW_CB)AWT_create_print_window, (AW_CL)ntw,   0 );
 
-	awm->create_mode( 0, "zoom.bitmap", "mode_pzoom.hlp", 0, (AW_CB)nt_mode_event,
-			(AW_CL)ntw,
-			(AW_CL)AWT_MODE_ZOOM
-			);
-	awm->create_mode( 0, "lzoom.bitmap", "mode_lzoom.hlp", 0, (AW_CB)nt_mode_event,
-			(AW_CL)ntw,
-			(AW_CL)AWT_MODE_LZOOM
-			);
+    awm->create_mode( 0, "zoom.bitmap", "mode_pzoom.hlp", 0, (AW_CB)nt_mode_event,
+                      (AW_CL)ntw,
+                      (AW_CL)AWT_MODE_ZOOM
+                      );
+    awm->create_mode( 0, "lzoom.bitmap", "mode_lzoom.hlp", 0, (AW_CB)nt_mode_event,
+                      (AW_CL)ntw,
+                      (AW_CL)AWT_MODE_LZOOM
+                      );
 
-	awm->set_info_area_height( 0 );
-	awm->set_bottom_area_height( 0 );
+    awm->set_info_area_height( 0 );
+    awm->set_bottom_area_height( 0 );
 
-	return (AW_window *)awm;
+    return (AW_window *)awm;
 }
 
 void quit_cb(AW_window *aww) {
-	exit(0);
+    exit(0);
 }
 
 
 
 int main(int argc, char **argv) {
-class Element *root;
-class Element *tmp_elem;
-int	number = 5;
+    class Element *root;
+    class Element *tmp_elem;
+    int number = 5;
 
 
-	AW_root *aw_root;
-	AW_default aw_default;
+    AW_root *aw_root;
+    AW_default aw_default;
 
-	char *db_server =":";
+    char *db_server =":";
 
-//	aw_initstatus();
+    //  aw_initstatus();
 
-	aw_root = new AW_root;
-	aw_default = aw_root->open_default(".designer");
-	aw_root->init_variables(aw_default);
-	aw_root->init("DESIGNER");
+    aw_root = new AW_root;
+    aw_default = aw_root->open_default(".designer");
+    aw_root->init_variables(aw_default);
+    aw_root->init("DESIGNER");
 
-//***********************************************************
+    //***********************************************************
 
-	root = new Element;
+        root = new Element;
 
-	printf("start\n");
+        printf("start\n");
 
-	root->set_contents(1);
-	root->print_contents();
+        root->set_contents(1);
+        root->print_contents();
 
-	tmp_elem = root->add_son()->set_contents(11)->print_contents();
-	tmp_elem->add_son()->set_contents(111)->print_contents();
-	tmp_elem->add_son()->set_contents(112)->print_contents();
-	tmp_elem->add_son()->set_contents(113)->print_contents();
+        tmp_elem = root->add_son()->set_contents(11)->print_contents();
+        tmp_elem->add_son()->set_contents(111)->print_contents();
+        tmp_elem->add_son()->set_contents(112)->print_contents();
+        tmp_elem->add_son()->set_contents(113)->print_contents();
 
-	tmp_elem = root->add_son()->set_contents(2)->print_contents();
-	tmp_elem->add_son()->set_contents(121)->print_contents();
-	tmp_elem->add_son()->set_contents(122)->print_contents()->add_son()->set_contents(1221)->print_contents();
+        tmp_elem = root->add_son()->set_contents(2)->print_contents();
+        tmp_elem->add_son()->set_contents(121)->print_contents();
+        tmp_elem->add_son()->set_contents(122)->print_contents()->add_son()->set_contents(1221)->print_contents();
 
-	tmp_elem = root->add_son()->set_contents(3)->print_contents();
-	tmp_elem->add_son()->set_contents(131)->print_contents();
-	tmp_elem->add_son()->set_contents(132)->print_contents();
-	tmp_elem->add_son()->set_contents(133)->print_contents();
+        tmp_elem = root->add_son()->set_contents(3)->print_contents();
+        tmp_elem->add_son()->set_contents(131)->print_contents();
+        tmp_elem->add_son()->set_contents(132)->print_contents();
+        tmp_elem->add_son()->set_contents(133)->print_contents();
 
-	root->get_number_of_sons(number);
-	printf("Number of sons:%i\n",number);
+        root->get_number_of_sons(number);
+        printf("Number of sons:%i\n",number);
 
-//	root->print_contents_recursive(0);
+        //  root->print_contents_recursive(0);
 
-	root->calculate_sizes();
+        root->calculate_sizes();
 
-	root->dump_recursive();
+        root->dump_recursive();
 
-	printf("ende\n");
+        printf("ende\n");
 
-//***********************************************************
+        //***********************************************************
 
-	AW_window_menu_modes *awm = (AW_window_menu_modes*)create_main_window(aw_root);
-	awm->show();
+            AW_window_menu_modes *awm = (AW_window_menu_modes*)create_main_window(aw_root);
+            awm->show();
 
-	aw_root->awar_string( "tmp/LeftFooter", "");
+            aw_root->awar_string( AWAR_FOOTER, "");
 
-	aw_root->main_loop();
+            aw_root->main_loop();
 
 
 }
@@ -124,11 +128,11 @@ int	number = 5;
 void
 nt_mode_event( AW_window *aws, AWT_canvas *ntw, AWT_COMMAND_MODE mode)
 {
-	AWUSE(aws);
-	char *text;
-	static int story = 0;
+    AWUSE(aws);
+    char *text;
+    static int story = 0;
 
-	switch(mode){
+    switch(mode){
         case AWT_MODE_MARK:
             text="MARK MODE    LEFT: mark subtree   RIGHT: unmark subtree";
             break;
@@ -181,8 +185,10 @@ nt_mode_event( AW_window *aws, AWT_canvas *ntw, AWT_COMMAND_MODE mode)
         default:
             text="No help for this mode available";
             break;
-	}
+    }
 
-	ntw->awr->awar("tmp/LeftFooter")->write_string( text);
-	ntw->set_mode(mode);
+    test_assert(strlen(text) < AWAR_FOOTER_MAX_LEN); // text too long!
+
+    ntw->awr->awar(AWAR_FOOTER)->write_string( text);
+    ntw->set_mode(mode);
 }
