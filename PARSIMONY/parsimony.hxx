142b69cf469d623da3f84594534318245b22077c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-14 23:44:42 +0000

    - beautified

--- PARSIMONY/parsimony.hxx
+++ PARSIMONY/parsimony.hxx
@@ -1,43 +1,42 @@
-#define AWAR_FOOTER		"tmp/LeftFooter"
-#define AWAR_ALIGNMENT		"tmp/pars/alignment"
-#define AWAR_FILTER_NAME	"tmp/pars/filter/name"
-#define AWAR_FILTER_FILTER	"tmp/pars/filter/filter"
-#define AWAR_FILTER_ALIGNMENT	"tmp/pars/filter/alignment"
-#define AWAR_PARSIMONY		"tmp/pars/parsimony"
-#define AWAR_BEST_PARSIMONY	"tmp/pars/best_parsimony"
-#define AWAR_STACKPOINTER	"tmp/pars/stackpointer"
+#define AWAR_ALIGNMENT      "tmp/pars/alignment"
+#define AWAR_FILTER_NAME    "tmp/pars/filter/name"
+#define AWAR_FILTER_FILTER  "tmp/pars/filter/filter"
+#define AWAR_FILTER_ALIGNMENT   "tmp/pars/filter/alignment"
+#define AWAR_PARSIMONY      "tmp/pars/parsimony"
+#define AWAR_BEST_PARSIMONY "tmp/pars/best_parsimony"
+#define AWAR_STACKPOINTER   "tmp/pars/stackpointer"
 
 #define NNI_MODES // uncomment to hide NNI/K.L. mode buttons
 
 //#define AWAR_TREE AWAR_TREE
 
-#define AWAR_PARS_TYPE		"pars/pars_type"
+#define AWAR_PARS_TYPE      "pars/pars_type"
 
 enum PARS_pars_type {
-	PARS_WAGNER,
-	PARS_TRANSVERSION
+    PARS_WAGNER,
+    PARS_TRANSVERSION
 };
 
 extern class AP_main {
 private:
-    class AP_main_stack	*stack;
-    class AP_main_list	list;
-    unsigned long		stack_level;
+    class AP_main_stack *stack;
+    class AP_main_list  list;
+    unsigned long       stack_level;
 
 public:
     // *************** read only
-    char	*use;
-    unsigned long		user_push_counter;
+    char    *use;
+    unsigned long       user_push_counter;
     // ************** real public
     struct {
-	  unsigned  int	add_marked:1;
-	  unsigned  int	add_selected:1;
-	  unsigned  int	calc_branch_lenths:1;
-	  unsigned  int	calc_bootstrap:1;
-	  unsigned  int	quit:1;
+        unsigned  int add_marked:1;
+        unsigned  int add_selected:1;
+        unsigned  int calc_branch_lenths:1;
+        unsigned  int calc_bootstrap:1;
+        unsigned  int quit:1;
     } commands;
 
-    unsigned long      	combineCount;
+    unsigned long       combineCount;
     AP_main(void);
     ~AP_main(void);
     class AP_tree **tree_root;
@@ -52,7 +51,7 @@ public:
     void push(void);
     void pop(void);
     void push_node(AP_tree * node,AP_STACK_MODE);
-    void clear(void);				// clears all buffers
+    void clear(void);               // clears all buffers
 
 
 
