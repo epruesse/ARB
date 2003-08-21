120dae53ab8f7d8b973692c115067c2b8d7e8320
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-21 13:30:09 +0000

    - only exports window interface

--- trunk/SEQ_QUALITY/seq_quality.h
+++ trunk/SEQ_QUALITY/seq_quality.h
@@ -1,7 +1,9 @@
 #ifndef SEQ_QUALITY_H
 #define SEQ_QUALITY_H
 
-GB_ERROR SQ_calc_seq_quality(GBDATA *gbmain, const char *tree_name);
+
+void       SQ_create_awars(AW_root *awr, AW_default aw_def);
+AW_window *SQ_create_seq_quality_window(AW_root *aw_root, AW_CL);
 
 #else
 #error seq_quality.h included twice
