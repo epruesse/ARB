97c95eee7fe6b7ab359aeee1be843fbed1a995da
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-22 19:52:43 +0000

    - shows reason why SECEDIT could not be started

--- trunk/EDIT4/ED4_secedit.cxx
+++ trunk/EDIT4/ED4_secedit.cxx
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include <aw_root.hxx>
+#include <arbdb.h>
 #include <aw_device.hxx>
 #include <aw_window.hxx>
 
@@ -14,11 +15,12 @@
 void ED4_SECEDIT_start(AW_window *aw, AW_CL, AW_CL)
 {
     static AW_window *aw_sec = 0;
-    
+
     if (!aw_sec) { // do not open window twice
         aw_sec = SEC_create_main_window(aw->get_root());
         if (!aw_sec) {
-            aw_message("Couldn't start secondary structure editor");
+            GB_ERROR err = GB_get_error();
+            aw_message(GBS_global_string("Couldn't start secondary structure editor.\nReason: %s", err));
             return;
         }
     }
