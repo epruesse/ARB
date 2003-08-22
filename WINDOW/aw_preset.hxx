7d5ca9fee29dd75b1b4e085575190058e51b6da4
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-08-22 19:55:16 +0000

    - added AW_copy_GCs()

--- WINDOW/aw_preset.hxx
+++ WINDOW/aw_preset.hxx
@@ -11,8 +11,8 @@ void AW_save_specific_defaults( AW_window *aw, const char *filename);
 AW_window *AW_preset_window( AW_root *root );
 
 typedef enum {
-	AW_GCM_DATA_AREA,
-	AW_GCM_WINDOW_AREA
+    AW_GCM_DATA_AREA,
+    AW_GCM_WINDOW_AREA
 } AW_GCM_AREA;
 
 AW_gc_manager AW_manage_GC(AW_window                                       *aww,
@@ -22,30 +22,29 @@ AW_gc_manager AW_manage_GC(AW_window                                       *aww,
                            const char                                      *default_background_color,
                            ...);
 /* creates some GC pairs: one for normal operation,
-					the other for drag mode
-		eg.
-		AW_manage_GC(aww,device,10,20,AW_GCM_DATA_AREA,
-				my_expose_cb, cd1 ,cd2,
-				"name","#sequence",0);
+                    the other for drag mode
+        eg.
+        AW_manage_GC(aww,device,10,20,AW_GCM_DATA_AREA, my_expose_cb, cd1 ,cd2, "name","#sequence",0);
 
                 (see implementation for more details on parameter strings)
 
-		will create 4 GCs:
-			GC 10 (normal) and 20 (drag)
-			GC 11 (normal and monospaced (indicated by '#')
-			   21 drag and monospaced
-			don't forget the 0 at the end of the fontname field
+        will create 4 GCs:
+            GC 10 (normal) and 20 (drag)
+            GC 11 (normal and monospaced (indicated by '#')
+               21 drag and monospaced
+            don't forget the 0 at the end of the fontname field
 
-			When the GCs are modified the 'changecb' is called
-		*/
+            When the GCs are modified the 'changecb' is called
+        */
 
 AW_window *AW_create_gc_window(AW_root *aw_root, AW_gc_manager id);
-		/* opens the properties Window */
+        /* opens the properties Window */
 
 
 void AW_preset_create_font_chooser(AW_window *aws, const char *awar, const char *label,bool message_reload = false);
 void AW_preset_create_scale_chooser(AW_window *aws, const char *awar, const char *label);
 void AW_preset_create_color_chooser(AW_window *aws, const char *awar, const char *label,bool message_reload = false, bool show_label = false);
 
+void AW_copy_GCs(AW_root *aw_root, const char *source_window, const char *dest_window, AW_BOOL has_font_info, const char *id0, ...);
 
 #endif
