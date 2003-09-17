3eb0c4f3eacd14a81ba845be59e7c9fcaaa130dd
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-09-17 08:57:10 +0000

    - provides code to use existing species_mapping

---
+++ trunk/PROBE_SERVER/mapping.h
@@ -0,0 +1,70 @@
+//  ==================================================================== //
+//                                                                       //
+//    File      : mapping.cxx                                            //
+//    Purpose   : simple species mapping                                 //
+//    Time-stamp: <Wed Sep/17/2003 10:15 MET Coder@ReallySoft.de>        //
+//                                                                       //
+//                                                                       //
+//  Coded by Ralf Westram (coder@reallysoft.de) in September 2003        //
+//  Copyright Department of Microbiology (Technical University Munich)   //
+//                                                                       //
+//  Visit our web site at: http://www.arb-home.de/                       //
+//                                                                       //
+//                                                                       //
+//  ==================================================================== //
+
+#ifndef __MAP__
+#include <map>
+#endif
+
+using namespace std;
+
+typedef int SpeciesID;
+
+static map<string, SpeciesID> PM_species2num_map;
+static map<SpeciesID, string> PM_num2species_map;
+static bool                   PM_species_maps_initialized = false;
+
+GB_ERROR PM_initSpeciesMaps(GBDATA *pb_main) {
+    GB_transaction dummy(pb_main);
+    GB_ERROR       error = 0;
+
+    if (PM_species_maps_initialized) {
+        error = "initSpeciesMaps called twice";
+    }
+    else {
+        GBDATA *pb_mapping = GB_find(pb_main, "species_mapping", 0, down_level);
+
+        if (!pb_mapping) {
+            error = GB_get_error();
+        }
+        else {
+            const char *mapping = GB_read_char_pntr(pb_mapping);
+            if (!mapping) return "Can't read mapping";
+
+            while (mapping[0]) {
+                const char *komma     = strchr(mapping, ',');   if (!komma) break;
+                const char *semicolon = strchr(komma, ';');     if (!semicolon) break;
+
+                string name(mapping, komma-mapping);
+                komma += 1;
+
+                string    idnum(komma,semicolon-komma);
+                SpeciesID id = atoi(idnum.c_str());
+
+                PM_species2num_map[name] = id;
+                PM_num2species_map[id]   = name;
+
+                mapping = semicolon+1;
+            }
+
+            PM_species_maps_initialized = true;
+        }
+    }
+
+    return error;
+}
+
+SpeciesID PM_name2ID(const string& name) { return PM_species2num_map[name]; }
+const string& PM_ID2name(SpeciesID id) { return PM_num2species_map[id]; }
+
