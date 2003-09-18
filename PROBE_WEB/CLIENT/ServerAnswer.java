dd225f06c75c9d8d66a042cb3a507b79019af1ee
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-09-18 13:40:59 +0000

    - makes HTTP errors readable - error handling

--- PROBE_WEB/CLIENT/ServerAnswer.java
+++ PROBE_WEB/CLIENT/ServerAnswer.java
@@ -4,70 +4,129 @@ public class ServerAnswer
 {
 private HashMap map;
 private String  error_message;
+private boolean is_server_problem = false;
 
-public ServerAnswer(String answer) {
-    map                  = new HashMap();
-    error_message        = "";
-    boolean format_error = false;
+private String removeLF(String str)
+    {
+        int lf = str.indexOf("\n");
+        if (lf == -1) return str;
+        if (lf == 0) return removeLF(str.substring(1));
+        return str.substring(0, lf) + removeLF(str.substring(lf+1));
+    }
+private String removeTags(String str)
+    {
+        String  res   = "";
+        int     start = 0;
+        boolean done  = false;
 
-    int start = 0;
-    int nl    = answer.indexOf('\n');
+        while (!done) {
+            int open  = str.indexOf('<', start);
+            int close = -1;
 
-    while (nl != -1) {
-        String line = answer.substring(start, nl);
-        int    eq   = line.indexOf('=');
-        if (eq !=  -1) {
-            String key   = line.substring(0, eq);
-            String value = line.substring(eq+1);
-            map.put(key, value);
-            System.out.println("key='"+key+"' value='"+value+"'");
+            if (open != -1) {
+                close = str.indexOf('>', open+1);
+            }
+
+            if (close == -1) {
+                if (!res.equals("")) res += " ";
+                res  += removeLF(str.substring(start));
+                done  = true;
+            }
+            else {
+                if (start != open) {
+                    if (!res.equals("")) res += " ";
+                    res += removeLF(str.substring(start, open));
+                }
+                start = close+1;
+            }
         }
-        else {
-            format_error = true;
-        }
 
-        start = nl+1;
-        nl    = answer.indexOf('\n', start);
+        return res;
     }
 
-    if (format_error) {
-        if (hasKey("result") && !getValue("result").equals("ok")) {
-            format_error   = false;
-            error_message  = getValue("message");
+public ServerAnswer(String answer)
+    {
+        map                  = new HashMap();
+        error_message        = null;
+        boolean format_error = false;
+
+        int start = 0;
+        int nl    = answer.indexOf('\n');
+
+        while (nl != -1) {
+            String line = answer.substring(start, nl);
+            int    eq   = line.indexOf('=');
+            if (eq !=  -1) {
+                String key   = line.substring(0, eq);
+                String value = line.substring(eq+1);
+                map.put(key, value);
+                System.out.println("    ANSWER: key='"+key+"' value='"+value+"'");
+            }
+            else {
+                format_error = true;
+            }
+
+            start = nl+1;
+            nl    = answer.indexOf('\n', start);
         }
-        else {
-            error_message = "Server problem ("+answer+")";
-        }
-    }
-    else {
-        if (hasKey("result")) {
-            String result = getValue("result");
-            if (!result.equals("ok")) {
+
+        if (format_error) {
+            if (hasKey("result") && !getValue("result").equals("ok")) {
                 error_message  = getValue("message");
+            }
+            else {
+                error_message     = removeTags(answer);
+                is_server_problem = true;
             }
         }
         else {
-            error_message = "Server problem (no result found)";
+            if (hasKey("result")) {
+                String result = getValue("result");
+                if (!result.equals("ok")) {
+                    error_message  = getValue("message");
+                }
+            }
+            else {
+                error_message = "no 'result' found";
+                is_server_problem = true;
+            }
         }
     }
-}
 
-public boolean hasError() {
-    return !error_message.equals("");
-}
+public boolean hasKey(String key)
+    {
+        return !hasError() && map.containsKey(key);
+    }
 
-public String getError() {
-    if (!hasError()) return "";
-    return error_message;
-}
+public String getValue(String key)
+    {
+        if (!hasKey(key)) return null;
+        return (String)map.get(key);
+    }
 
-public boolean hasKey(String key) {
-    return map.containsKey(key);
-}
+    // error handling
 
-public String getValue(String key) {
-    if (!hasKey(key)) return "";
-    return (String)map.get(key);
-}
+public boolean hasError()
+    {
+        return error_message != null;
+    }
+
+public String getError()
+    {
+        if (!hasError()) return "";
+        return error_message;
+    }
+
+public void handleError()
+    {
+        if (hasError()) {
+            if (is_server_problem) {
+                Toolkit.AbortWithServerProblem("[ServerAnswer] "+getError());
+            }
+            else {
+                Toolkit.AbortWithError("[ServerAnswer] "+getError());
+            }
+        }
+    }
 
 }
