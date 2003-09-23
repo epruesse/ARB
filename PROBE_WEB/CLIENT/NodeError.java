b8cf13e14912919c7fada696d9ac84394ac93e99
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-09-23 08:52:08 +0000

    - dumps changed TreeNode variables

--- PROBE_WEB/CLIENT/NodeError.java
+++ PROBE_WEB/CLIENT/NodeError.java
@@ -6,20 +6,18 @@ public class NodeError
 {
 public NodeError(TreeNode tn)
     {
-        System.out.println("NodeError:node name: >>>" + tn.getNodeName() + "<<<");
-        System.out.println("NodeError:node distance: >>>" + tn.getDistance() + "<<<");
-        System.out.println("NodeError:father node: >>>" + tn.getFather() + "<<<");
-        System.out.println("NodeError:child node container: >>>" + tn.getChilds() + "<<<");
-        System.out.println("NodeError:leaf status: >>>" + tn.testLeaf() + "<<<");
-        System.out.println("NodeError:level: >>>" + tn.level + "<<<");
-        System.out.println("NodeError:node serial: >>>" + tn.nodeSerial + "<<<");
-        //        System.out.println("maxDepth: >>>" + maxDepth + "<<<");
-//         System.out.println(": >>>" + tn.getFather() + "<<<");
-//         System.out.println(": >>>" + tn.getFather() + "<<<");
-//         System.out.println(": >>>" + tn.getFather() + "<<<");
-
-
-
-
+        System.out.println("NodeError:node information    : '" + tn.getNodeInformation() + "'");
+        System.out.println("NodeError:short name          : '" + tn.getShortName() + "'");
+        System.out.println("NodeError:full name           : '" + tn.getFullName() + "'");
+        System.out.println("NodeError:acc                 : '" + tn.getAccessionNumber() + "'");
+        System.out.println("NodeError:exact matches       : " + tn.getExactMatches() );
+        System.out.println("NodeError:min. non-group-hits : " + tn.getMinNonGroupHits() );
+        System.out.println("NodeError:max. coverage       : " + tn.getMaxCoverage() );
+        System.out.println("NodeError:node distance       : " + tn.getDistance() );
+        System.out.println("NodeError:father node         : " + tn.getFather() );
+        System.out.println("NodeError:child node container: " + tn.getChilds() );
+        System.out.println("NodeError:leaf status         : " + tn.testLeaf() );
+        System.out.println("NodeError:level               : " + tn.level );
+        System.out.println("NodeError:node serial         : " + tn.nodeSerial );
     }
 }
