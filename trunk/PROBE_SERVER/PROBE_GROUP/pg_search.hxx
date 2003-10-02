0e79b5f02e1eec16f5900b32c7067a1b5e4f1041
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-10-02 15:55:42 +0000

    - added PG_find_probe_group_for_species, removed PG_find_probe_for_subtree + PG_get_id

--- trunk/PROBE_SERVER/PROBE_GROUP/pg_search.hxx
+++ trunk/PROBE_SERVER/PROBE_GROUP/pg_search.hxx
@@ -7,9 +7,10 @@
 #ifndef __PG_DB_HXX__
 #include "pg_db.hxx"
 #endif
-#ifndef DEQUE
-#include <deque>
+#ifndef SET
+#include <set>
 #endif
+
 
 // initialization (needed for all functions below):
 GB_ERROR 	PG_init_pt_server(GBDATA *gb_main, const char *servername, void (*print_function)(const char *format, ...));
@@ -33,8 +34,10 @@ struct PG_probe_match_para {
 GB_ERROR 	PG_probe_match(PG_Group& g, const PG_probe_match_para& para, const char *for_probe);
 
 GBDATA *PG_find_species(GBDATA *node,int id,long gbs);
-void    PG_find_probe_for_subtree(GBDATA *node,const std::deque<SpeciesID>& species, std::deque<std::string>& probe);
-string  PG_get_id(GBDATA *node);
+GBDATA *PG_find_probe_group_for_species(GBDATA *node, const std::set<SpeciesID>& species);
+
+// void       PG_find_probe_for_subtree(GBDATA *node,const std::deque<SpeciesID>& species, std::deque<std::string>& probe);
+// SpeciesID  PG_get_id(GBDATA *node);
 
 #else
 #error pg_search.hxx included twice
