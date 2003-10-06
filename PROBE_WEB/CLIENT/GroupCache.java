4f9714472a1ca48e525670de79cccf5cd403927b
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-10-06 17:56:39 +0000

    - retrieves and caches group member information

---
+++ PROBE_WEB/CLIENT/GroupCache.java
@@ -0,0 +1,82 @@
+
+import java.util.*;
+
+class GroupCache
+{
+private HashMap groups;
+private String  error;
+
+public GroupCache()
+    {
+        groups = new HashMap();
+        error  = null;
+    }
+
+private String retrieveMemberList(HttpSubsystem webAccess, String groupId, int probe_length)
+    {
+        String       answer = webAccess.retrieveGroupMembers(groupId, probe_length);
+        ServerAnswer parsed = new ServerAnswer(answer, true, true);
+
+        if (parsed.hasError()) {
+            error = parsed.getError();
+            return null;
+        }
+
+        if (!parsed.hasKey("membercount")) {
+            error = "'membercount' expected in server answer";
+            return null;
+        }
+
+        String memberList  = null;
+        int    membercount = Integer.parseInt(parsed.getValue("membercount"));
+
+        for (int m = 1; m <= membercount; m++) {
+            String keyName = "m"+m;
+            if (!parsed.hasKey(keyName)) {
+                error = "'"+keyName+"' expected in server answer";
+                return null;
+            }
+
+            String speciesName = parsed.getValue(keyName);
+            if (memberList == null) {
+                memberList = speciesName;
+            }
+            else {
+                memberList = memberList+","+speciesName;
+            }
+        }
+
+        if (memberList == null) {
+            error = "No members found (shouldn't occur!)";
+        }
+
+        return memberList;
+    }
+
+public String getGroupMembers(HttpSubsystem webAccess, String groupId, int probe_length)
+    {
+        error           = null;
+        String hash_key = groupId+"_"+probe_length;
+
+        // System.out.println("hash_key='"+hash_key+"'");
+
+        String members = null;
+        if (groups.containsKey(hash_key)) {
+            members = (String)groups.get(hash_key);
+            // System.out.println("cached: '"+members+"'");
+        }
+        else {
+            members = retrieveMemberList(webAccess, groupId, probe_length);
+            // System.out.println("retrieved: '"+members+"'");
+            if (error == null) {
+                groups.put(hash_key, members);
+            }
+        }
+        return members;
+    }
+
+public String getError()
+    {
+        return error;
+    }
+}
