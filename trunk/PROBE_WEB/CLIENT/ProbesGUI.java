7271d08dcf7b363fe681ae9e1b832250db99bbd9
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-10-08 13:12:49 +0000

    - added getScrollPaneSize - display modifications (size/pos) done here

--- trunk/PROBE_WEB/CLIENT/ProbesGUI.java
+++ trunk/PROBE_WEB/CLIENT/ProbesGUI.java
@@ -20,54 +20,59 @@ private ProbesGUIActionListener al;
 private ProbeListActionListener ll;
 private Client                  boss;
 
-public ProbesGUI( TreeNode root, int levels, String title, Client b)
-  {
-    super(title);
-    setBackground(Color.lightGray);
+public ProbesGUI(TreeNode root, int levels, String title, Client b)
+    {
+        super(title);
+        setVisible(true);
+        setBackground(Color.lightGray);
 
-    setLayout(new BorderLayout());
-    // System.out.println("reference to ProbesGUI: " + this);
-    boss = b;
-    td = new TreeDisplay( root, levels);
+        setLayout(new BorderLayout());
+        setLocation(200, 200);
 
-    // debug code
-    if (td == null) {
-        System.out.println("constructor/ProbesGUI: no TreeDisplay received");
+        boss = b;
+
+        td = new TreeDisplay( root, levels);
+        if (td == null) Toolkit.AbortWithError("constructor/ProbesGUI: no TreeDisplay received");
+        td.setBoss(b);
+
+        int tree_height = 400;
+
+        al = new ProbesGUIActionListener(this);
+        setMenuBar(pm = new ProbeMenu(al));
+
+        details = new TextArea("Display detail information", 10, 40, TextArea.SCROLLBARS_BOTH);
+        add(details, BorderLayout.SOUTH);
+
+        probe_list = new ProbeList(probeListWidth, tree_height);
+        ll = new ProbeListActionListener(this);
+        probe_list.addItemListener(ll);
+        probe_list.add("Right click a node");
+        probe_list.add("to display probes.");
+        add(probe_list, BorderLayout.EAST);
+
+        sc = new ScrollPane();
+        if (root == null) {
+            Toolkit.InternalError("in ProbesGUI: can't display invalid tree node");
+        }
+
+        sc.add(td);
+        sc.getVAdjustable().setUnitIncrement(1);
+        sc.getHAdjustable().setUnitIncrement(1);
+        sc.setSize(600,tree_height);
+        sc.setBackground(Color.red);
+        add(sc, BorderLayout.CENTER);
+
+        //Window-Listener
+        addWindowListener(new WindowClosingAdapter(true));
+
+        //Dialogelement anordnen
+        pack();
     }
 
-    int tree_height = 400;
-
-    al = new ProbesGUIActionListener(this);
-    setMenuBar(pm = new ProbeMenu(al));
-
-    details = new TextArea("Display detail information", 10, 40, TextArea.SCROLLBARS_BOTH);
-    add(details, BorderLayout.SOUTH);
-
-    probe_list = new ProbeList(probeListWidth, tree_height);
-    ll = new ProbeListActionListener(this);
-    probe_list.addItemListener(ll);
-    probe_list.add("Right click a node");
-    probe_list.add("to display probes.");
-    add(probe_list, BorderLayout.EAST);
-
-    sc = new ScrollPane();
-    if (root == null) {
-        Toolkit.InternalError("in ProbesGUI: can't display invalid tree node");
+public Dimension getScrollPaneSize()
+    {
+        return sc.getViewportSize();
     }
-
-    sc.add(td);
-    sc.getVAdjustable().setUnitIncrement(1);
-    sc.getHAdjustable().setUnitIncrement(1);
-    sc.setSize(600,tree_height);
-    sc.setBackground(Color.red);
-    add(sc, BorderLayout.CENTER);
-
-    //Window-Listener
-    addWindowListener(new WindowClosingAdapter(true));
-
-    //Dialogelement anordnen
-    pack();
-  }
 
 public TreeDisplay getTreeDisplay()
     {
