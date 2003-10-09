3e8416827e3af7d670c45cc3de2b408f8183f9a5
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-10-09 10:37:31 +0000

    - PG_probe_match_para -> probe_config_data

--- trunk/PROBE_SERVER/PROBE_GROUP/pg_search.hxx
+++ trunk/PROBE_SERVER/PROBE_GROUP/pg_search.hxx
@@ -1,45 +1,47 @@
 #ifndef PG_SEARCH_HXX
 #define PG_SEARCH_HXX
 
+#ifndef SET
+#include <set>
+#endif
+
 #ifndef PG_DEF_HXX
 #include "pg_def.hxx"
 #endif
 #ifndef __PG_DB_HXX__
 #include "pg_db.hxx"
 #endif
-#ifndef SET
-#include <set>
+#ifndef GLOBAL_DEFS_H
+#include "../global_defs.h"
 #endif
+
 
 typedef std::set<SpeciesID> SpeciesBag;
 
 // initialization (needed for all functions below):
-GB_ERROR 	PG_init_pt_server(GBDATA *gb_main, const char *servername, void (*print_function)(const char *format, ...));
-void 		PG_exit_pt_server(void);
+GB_ERROR PG_init_pt_server(GBDATA *gb_main, const char *servername, void (*print_function)(const char *format, ...));
+void     PG_exit_pt_server(void);
 
 // iterate through all probes:
-bool        	 PG_init_find_probes(int length, GB_ERROR& error);
-bool        	 PG_exit_find_probes();
-const char 	*PG_find_next_probe(GB_ERROR& error);
+bool        PG_init_find_probes(int length, GB_ERROR& error);
+bool        PG_exit_find_probes();
+const char *PG_find_next_probe(GB_ERROR& error);
 
 // probe match:
 
-struct PG_probe_match_para {
-    // expert window
-    double	bondval[16];
-    double	split;		// should be 0.5
-    double 	dtedge;		// should be 0.5
-    double 	dt;		// should be 0.5
-};
+// struct PG_probe_match_para {
+//     // expert window
+//     double bondval[16];
+//     double split;               // should be 0.5
+//     double dtedge;              // should be 0.5
+//     double dt;                  // should be 0.5
+// };
 
-GB_ERROR 	PG_probe_match(PG_Group& g, const PG_probe_match_para& para, const char *for_probe);
+GB_ERROR PG_probe_match(PG_Group& g, const probe_config_data& para, const char *for_probe);
 
 GBDATA *PG_find_species(GBDATA *node,int id,long gbs);
 GBDATA *PG_find_probe_group_for_species(GBDATA *node, const SpeciesBag& species);
 GBDATA *PG_find_best_covering_probe_group_for_species(GBDATA *rootNode, const SpeciesBag& species, int min_non_matched, int max_non_matched, int& groupsize);
-
-// void       PG_find_probe_for_subtree(GBDATA *node,const std::deque<SpeciesID>& species, std::deque<std::string>& probe);
-// SpeciesID  PG_get_id(GBDATA *node);
 
 #else
 #error pg_search.hxx included twice
