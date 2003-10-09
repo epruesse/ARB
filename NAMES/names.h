1f7d53914abf8473633bfc2534fec899cc373f4e
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-10-09 10:27:54 +0000

    cosmetics

--- NAMES/names.h
+++ NAMES/names.h
@@ -1,14 +1,17 @@
+
 #define MAX_TRY 10
-		// Live time of a name server in milliseconds 
+
+// Live time of a name server in milliseconds:
+// 10 minutes
 #define TIME_OUT (1000*60*10)
 
 struct Hs_struct;
 struct AN_gl_struct {
-	aisc_com	*cl_link;
-	struct Hs_struct		*server_communication;
-	T_AN_MAIN	cl_main;
-	char		*server_name;
-	};
+    aisc_com         *cl_link;
+    struct Hs_struct *server_communication;
+    T_AN_MAIN         cl_main;
+    char             *server_name;
+};
 extern struct AN_gl_struct AN_global;
 
 
