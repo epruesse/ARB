9515f139c7f939295f89b6edb716f7f32993b4fb
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2003-11-21 12:19:35 +0000

    - raise aw_message from external scripts

---
+++ trunk/TOOLS/arb_message.cxx
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <arbdb.h>
+#include <arbdbt.h>
+
+void show_error(GBDATA *gb_main) {
+    GBT_message(gb_main, GB_get_error());
+    GB_print_error();
+}
+
+// ---------------------------------------
+//      int main(int argc,char **argv)
+// ---------------------------------------
+int main(int argc,char **argv)
+{
+    if (argc == 1)  {
+        fprintf(stderr, "Usage: arb_message \"the message\"");
+        return -1;
+    }
+
+    GBDATA *gb_main = GB_open(":","r");
+    if (!gb_main){
+        fprintf(stderr, "%s: %s\n", argv[0], argv[1]);
+    }
+    else {
+        GBT_message(gb_main, argv[1]);
+        GB_close(gb_main);
+    }
+    return 0;
+}
