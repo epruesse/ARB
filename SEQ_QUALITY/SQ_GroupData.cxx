0fa2858e64cf6e830ae2fbb2088ef88d1b2894af
huberj <huberj@129cd134-2152-0410-ab65-f3f895bafe68>
2003-11-23 16:11:04 +0000

    *** empty log message ***

--- SEQ_QUALITY/SQ_GroupData.cxx
+++ SEQ_QUALITY/SQ_GroupData.cxx
@@ -73,21 +73,108 @@ double SQ_GroupData_RNA::SQ_test_against_consensus(const char *sequence) {
 }
 
 void SQ_GroupData_RNA::SQ_add_sequence(const char *sequence) {
+
     for (int i=0; i < size; i++) {
 #warning has to work with IUPAC codes!
         switch(sequence[i]) {
-            case 'A': consensus[i].i[0]++; break;
-            case 'T': consensus[i].i[1]++; break;
-            case 'C': consensus[i].i[2]++; break;
-            case 'G': consensus[i].i[3]++; break;
-            case 'U': consensus[i].i[4]++; break;
-            case '.': consensus[i].i[5]++; break;
-            case '-': consensus[i].i[6]++; break;
-            default :
-                seq_assert(0); // unhandled character
+            case 'A':
+                consensus[i].i[0] = consensus[i].i[0] + 100; //ich mach das noch mit operator +=
+                break;
+            case 'T':
+		consensus[i].i[1] = consensus[i].i[1] + 100;
+                break;
+            case 'C':
+		consensus[i].i[2] = consensus[i].i[2] + 100;
+                break;
+            case 'G':
+		consensus[i].i[3] = consensus[i].i[3] + 100;
+                break;
+            case 'U':
+		consensus[i].i[4] = consensus[i].i[4] + 100;
+                break;
+            case 'R':
+		consensus[i].i[0] = consensus[i].i[0] + 50;
+		consensus[i].i[3] = consensus[i].i[3] + 50;
+                break;
+            case 'Y':
+		consensus[i].i[2] = consensus[i].i[2] + 33;
+		consensus[i].i[1] = consensus[i].i[1] + 33;
+		consensus[i].i[4] = consensus[i].i[4] + 33;
+                break;
+            case 'M':
+		consensus[i].i[0] = consensus[i].i[0] + 50;
+		consensus[i].i[2] = consensus[i].i[2] + 50;
+                break;
+            case 'K':
+		consensus[i].i[3] = consensus[i].i[3] + 33;
+		consensus[i].i[1] = consensus[i].i[1] + 33;
+		consensus[i].i[4] = consensus[i].i[4] + 33;
+                break;
+            case 'W':
+		consensus[i].i[0] = consensus[i].i[0] + 33;
+		consensus[i].i[1] = consensus[i].i[1] + 33;
+		consensus[i].i[4] = consensus[i].i[4] + 33;
+                break;
+            case 'S':
+		consensus[i].i[3] = consensus[i].i[3] + 50;
+		consensus[i].i[2] = consensus[i].i[2] + 50;
+                break;
+            case 'B':
+		consensus[i].i[2] = consensus[i].i[2] + 25;
+		consensus[i].i[1] = consensus[i].i[1] + 25;
+		consensus[i].i[3] = consensus[i].i[3] + 25;
+		consensus[i].i[4] = consensus[i].i[4] + 25;
+                break;
+            case 'D':
+		consensus[i].i[0] = consensus[i].i[0] + 25;
+		consensus[i].i[1] = consensus[i].i[1] + 25;
+		consensus[i].i[3] = consensus[i].i[3] + 25;
+		consensus[i].i[4] = consensus[i].i[4] + 25;
+                break;
+            case 'H':
+		consensus[i].i[2] = consensus[i].i[2] + 25;
+		consensus[i].i[1] = consensus[i].i[1] + 25;
+		consensus[i].i[0] = consensus[i].i[0] + 25;
+		consensus[i].i[4] = consensus[i].i[4] + 25;
+                break;
+            case 'V':
+		consensus[i].i[0] = consensus[i].i[0] + 33;
+		consensus[i].i[2] = consensus[i].i[2] + 33;
+		consensus[i].i[3] = consensus[i].i[3] + 33;
+                break;
+            case 'N':
+		consensus[i].i[2] = consensus[i].i[2] + 20;
+		consensus[i].i[1] = consensus[i].i[1] + 20;
+		consensus[i].i[0] = consensus[i].i[0] + 20;
+		consensus[i].i[3] = consensus[i].i[3] + 20;
+		consensus[i].i[4] = consensus[i].i[4] + 20;
+                break;
+            case '.':
+		consensus[i].i[5] = consensus[i].i[5] + 1;
+                break;
+            case '-':
+		consensus[i].i[6] = consensus[i].i[6] + 1;
                 break;
         }
     }
+
+
+
+//     for (int i=0; i < size; i++) {
+// #warning has to work with IUPAC codes!
+//         switch(sequence[i]) {
+//             case 'A': consensus[i].i[0]++; break;
+//             case 'T': consensus[i].i[1]++; break;
+//             case 'C': consensus[i].i[2]++; break;
+//             case 'G': consensus[i].i[3]++; break;
+//             case 'U': consensus[i].i[4]++; break;
+//             case '.': consensus[i].i[5]++; break;
+//             case '-': consensus[i].i[6]++; break;
+//             default :
+//                 seq_assert(0); // unhandled character
+//                 break;
+//         }
+//     }
 }
 
 double SQ_GroupData_PRO::SQ_test_against_consensus(const char *sequence) {
