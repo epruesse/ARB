f7f5e6a17cee7b2915b3d0f5dc912466ddc67de3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-02-27 16:27:53 +0000

    increased delay

--- trunk/PROBE_SERVER/log_memuse.sh
+++ trunk/PROBE_SERVER/log_memuse.sh
@@ -3,7 +3,7 @@
 LOG=memuse.log
 
 log() {
-    ps -auxw | grep arb | grep -v grep >> $1
+    (date;ps -auxw | grep arb | grep -v grep)  >> $1
 }
 
 echo "log starts at `date`" > $LOG
@@ -11,6 +11,6 @@ tail -f $LOG &
 
 while [ 1 = 1 ]; do
     log $LOG
-    sleep 4
+    sleep 60
 done
 
