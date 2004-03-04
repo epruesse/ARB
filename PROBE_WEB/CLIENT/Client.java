70c671d966701aac4496910e3da26d0e10ef2bd0
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-03-04 11:12:30 +0000

    - mark species before printing details - uses StringBuffer to build details (better performance!) - detail display knows 3 different "states"

--- PROBE_WEB/CLIENT/Client.java
+++ PROBE_WEB/CLIENT/Client.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
 
 /**
@@ -27,7 +28,7 @@ private HashMap knownOptions()
          return hm;
      }
 
-public static String readTree(HttpSubsystem webAccess, boolean forceReload)
+public String readTree(HttpSubsystem webAccess, boolean forceReload) throws Exception
     {
         String     localTreeVersion  = null;
         String     serverTreeVersion = webAccess.getNeededTreeVersion();
@@ -53,8 +54,10 @@ public static String readTree(HttpSubsystem webAccess, boolean forceReload)
 
         if (!serverTreeVersion.equals(localTreeVersion))
         {
-            System.out.println("You have tree version '" + localTreeVersion+"'");
-            System.out.println("Downloading updated tree '"+serverTreeVersion+"' ..");
+            showMessage("You have tree version '" + localTreeVersion+"'");
+            showMessage("Downloading updated tree '"+serverTreeVersion+"' ..");
+//             System.out.println("You have tree version '" + localTreeVersion+"'");
+//             System.out.println("Downloading updated tree '"+serverTreeVersion+"' ..");
 
             error = webAccess.downloadZippedTree(localTreeFile);
             if (error == null) {
@@ -66,9 +69,12 @@ public static String readTree(HttpSubsystem webAccess, boolean forceReload)
             }
             else {
                 if (!tr.getVersionString().equals(serverTreeVersion)) {
-                    System.out.println("Warning: downloaded tree has unexpected version '"+tr.getVersionString()+"'");
+                    showMessage("Warning: downloaded tree has unexpected version '"+tr.getVersionString()+"'");
                 }
             }
+        }
+        else {
+            showMessage("Your tree is up to date.");
         }
 
         return tr.getTreeString();
@@ -77,50 +83,68 @@ public static String readTree(HttpSubsystem webAccess, boolean forceReload)
 public static void main(String[] args)
     {
         Client cl          = new Client();
-        Toolkit.clientName = "arb_probe_library";
+        Toolkit.clientName = "ARB probe library";
 
-        System.out.println(Toolkit.clientName+" v"+Toolkit.clientVersion+" -- (C) 2003 Lothar Richter & Ralf Westram");
-        CommandLine cmdline = new CommandLine(args, cl.knownOptions());
+        System.out.println(Toolkit.clientName+" v"+Toolkit.clientVersion+" -- (C) 2003/2004 Lothar Richter & Ralf Westram");
+        try {
+            CommandLine cmdline = new CommandLine(args, cl.knownOptions());
 
-        if (cmdline.getOption("server")) {
-            cl.baseurl = cmdline.getOptionValue("server");
-        }
-        else {
-//             cl.baseurl = new String("http://probeserver.mikro.biologie.tu-muenchen.de/probe_library/"); // final server URL (DNS not propagated yet)
-            cl.baseurl = new String("http://www2.mikro.biologie.tu-muenchen.de/probe_library/"); // final server URL  
-//             cl.baseurl = new String("http://www2.mikro.biologie.tu-muenchen.de/probeserver24367472/"); // URL for debugging
-        }
-
-        cl.webAccess = new HttpSubsystem(cl.baseurl);
-
-        // ask server for version info
-        cl.webAccess.retrieveVersionInformation(); // terminates on failure
-        if (!Toolkit.clientVersion.equals(cl.webAccess.getNeededClientVersion())) {
-            Toolkit.AbortWithError("Your client is out-of-date!\nPlease get the newest version from\n  "+cl.baseurl+"arb_probe_library.jar");
-        }
-
-        // load and parse the most recent tree
-        {
-            boolean reload_tree = cmdline.getOption("tree") && cmdline.getOptionValue("tree").equals("reload");
-            cl.treeString       = readTree(cl.webAccess, reload_tree); // terminates on failure
-            cl.root             = (new TreeParser(cl.treeString)).getRootNode();
-            cl.fillShortNameHash(cl.root);
-            // System.out.println("Number of shortNameHash-entries: " + cl.shortNameHash.size());
-        }
-
-        if (cl.root == null)
-            {
-                System.out.println("in Client(): no valid node given to display");
-                System.exit(1);
+            if (cmdline.getOption("server")) {
+                cl.baseurl = cmdline.getOptionValue("server");
+            }
+            else {
+                //             cl.baseurl = new String("http://probeserver.mikro.biologie.tu-muenchen.de/probe_library/"); // final server URL (DNS not propagated yet)
+                cl.baseurl = new String("http://www2.mikro.biologie.tu-muenchen.de/probe_library/"); // final server URL
+                //             cl.baseurl = new String("http://www2.mikro.biologie.tu-muenchen.de/probeserver24367472/"); // URL for debugging
             }
 
-        cl.root.setPath("");
-        cl.display = new ProbesGUI(cl.root, 10, Toolkit.clientName+" v"+Toolkit.clientVersion, cl);
+            cl.display = new ProbesGUI(10, Toolkit.clientName+" v"+Toolkit.clientVersion, cl);
+            
+            try {
+                cl.webAccess = new HttpSubsystem(cl.baseurl);
 
-        // cl.display.getTreeDisplay().setBoss(cl); // obtain reference to Treedisplay first !
-        // cl.display.setLocation(200, 200); // this seems to cause display problems with fvwm
-        // cl.display.setVisible(true);
-        // cl.display.setLocation(200, 200);
+                // ask server for version info
+                cl.showMessage("Contacting probe server ..");
+                cl.webAccess.retrieveVersionInformation(); // terminates on failure
+                if (!Toolkit.clientVersion.equals(cl.webAccess.getNeededClientVersion())) {
+                    Toolkit.AbortWithError("Your client is out-of-date!\nPlease get the newest version from\n  "+cl.baseurl+"arb_probe_library.jar");
+                }
+
+                // load and parse the most recent tree
+                {
+                    boolean reload_tree = cmdline.getOption("tree") && cmdline.getOptionValue("tree").equals("reload");
+                    cl.treeString       = cl.readTree(cl.webAccess, reload_tree); // terminates on failure
+                    cl.root             = (new TreeParser(cl.treeString)).getRootNode();
+                    cl.fillShortNameHash(cl.root);
+                    // System.out.println("Number of shortNameHash-entries: " + cl.shortNameHash.size());
+                }
+
+                if (cl.root == null) {
+                    Toolkit.AbortWithError("no valid node given to display");
+//                     System.out.println("in Client(): no valid node given to display");
+//                     System.exit(1);
+                }
+
+                cl.root.setPath("");
+                // cl.display = new ProbesGUI(cl.root, 10, Toolkit.clientName+" v"+Toolkit.clientVersion, cl);
+                cl.display.initTreeDisplay(cl.root);
+
+                // cl.display.getTreeDisplay().setBoss(cl); // obtain reference to Treedisplay first !
+                // cl.display.setLocation(200, 200); // this seems to cause display problems with fvwm
+                // cl.display.setVisible(true);
+                // cl.display.setLocation(200, 200);
+            }
+            catch (Exception e) {
+                cl.showError(e.getMessage());
+                MessageDialog popup = new MessageDialog(cl.getDisplay(), "Error:", e.getMessage());
+                while (!popup.okClicked()) ; // @@@ do non-busy wait here (how?)
+                System.exit(1);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
     }
 
 public Client()
@@ -146,12 +170,10 @@ public ProbesGUI getDisplay()
         return display;
     }
 
-public void matchProbes(String probeInfo) {
+public void matchProbes(String probeInfo) throws Exception {
     boolean needUpdate = false;
     String  error      = null;
 
-    clearDetails();
-    
     needUpdate = root.unmarkSubtree(); // unmark all
 
     if (probeInfo != null) {
@@ -162,14 +184,15 @@ public void matchProbes(String probeInfo) {
             error = "Kommas expected in '"+probeInfo+"'";
         }
         else {
+            showMessage("Retrieving match information..");
             String groupId = probeInfo.substring(komma2+1);
             members        = groupCache.getGroupMembers(webAccess, groupId, komma1);
             if (members == null) {
                 error = "during probe match: "+groupCache.getError();
             }
             else {
-                updateDetails(probeInfo,members);
                 needUpdate = root.markSpecies(members, shortNameHash);
+                updateDetails(probeInfo,members);
             }
         }
     }
@@ -186,13 +209,17 @@ public void matchProbes(String probeInfo) {
     else {
         System.out.println("Marks did not change.");
     }
+
+//     throw new Exception("Test exception");
+    //     Toolkit.AbortWithError("Test error");
 }
 
-public void updateNodeInformation(String encodedPath, boolean exactMatches)
+public void updateNodeInformation(String encodedPath, boolean exactMatches) throws Exception
     {
         ProbeList list = display.getProbeList();
         list.removeAll();
         list.add("wait..");
+        showMessage("Retrieving probes ..");
 
         String answer = webAccess.retrieveNodeInformation(encodedPath, exactMatches);
 
@@ -228,94 +255,114 @@ public void updateNodeInformation(String encodedPath, boolean exactMatches)
 
     // rest should be grouped in separate class later on
 
-public void clearDetails()
+    private int detailState = 0; // 0 = details displayed; 1 = messages/errors displayed; 2 = empty
+
+    public void clearDetails()
     {
-        TextArea ta = display.getDetails();
-        ta.setText("");
+        if (detailState != 2) {
+            TextArea ta = display.getDetails();
+            ta.setText("");
+            detailState  = 2;
+        }
     }
 
-public void showError(String error)
+    public void showError(String error)
     {
         TextArea ta = display.getDetails();
-        ta.setText(error);
-        ta.setCaretPosition(0);
+        if (detailState == 0)
+            ta.setText("Error: "+error+"\n");
+        else
+            ta.append("Error: "+error+"\n");
+
+        detailState = 1;
+        ta.setCaretPosition(999999999); // ugly way to go to end of text
     }
 
-public void updateDetails (String probeInfo, String shortNames)
+    public void showMessage(String message)
     {
         TextArea ta = display.getDetails();
-        ta.setText("");
+        if (detailState == 0)
+            ta.setText(message+"\n");
+        else
+            ta.append(message+"\n");
+
+        detailState = 1;
+        ta.setCaretPosition(999999999); // ugly way to go to end of text
+    }
+
+    public void updateDetails (String probeInfo, String shortNames)
+    {
+        TextArea ta = display.getDetails();
+        ta.setVisible(false);
         int komma1 = probeInfo.indexOf(',');
         int komma2 = probeInfo.indexOf(',', komma1+1);
 
         if (komma1 == -1 || komma2 == -1) {
             System.out.println("Kommas expected in '"+probeInfo+"'");
         }
-        else
+        else {
+            StringBuffer theText = new StringBuffer();
+
+            String probeSequenz = probeInfo.substring(0,komma1);
+            theText.append("Selected probe: " + probeSequenz + "\n");
+
+            int tm = 0;
+            for (int i = 0; i < probeSequenz.length(); i ++) {
+                tm = tm + ((probeSequenz.charAt(i) == 'A' || probeSequenz.charAt(i) == 'U') ? 2 : 4); // 2 + 4 rule
+            }
+            theText.append("Melting temperature (2+4 rule):  " + tm+ "°C\n");
+
+            // GC content
+            int gc = 0;
+            for (int i = 0; i < probeSequenz.length(); i ++) {
+                gc = gc + ((probeSequenz.charAt(i) == 'G' || probeSequenz.charAt(i) == 'C') ? 1: 0);
+            }
+            theText.append("%GC-Content:  " + (gc*100)/probeSequenz.length() + "%\n");
+
+            int snlen = shortNames.length();
+            if (snlen != 0)
             {
-                String probeSequenz = probeInfo.substring(0,komma1);
-                ta.append("Details for probe: " + probeSequenz + "\n");
-                int tm = 0;
-                for (int i = 0; i < probeSequenz.length(); i ++)
-                    {
-                        tm = tm + ((probeSequenz.charAt(i) == 'A' || probeSequenz.charAt(i) == 'U') ? 2 : 4); // 2 + 4 rule
-                    }
-                ta.append("Melting temperature (2+4 rule):  " + tm+ "°C\n");
-                // GC content
-                int gc = 0;
-                for (int i = 0; i < probeSequenz.length(); i ++)
-                    {
-                        gc = gc + ((probeSequenz.charAt(i) == 'G' || probeSequenz.charAt(i) == 'C') ? 1: 0);
-                    }
-                ta.append("%GC-Content:  " + (gc*100)/probeSequenz.length() + "%\n");
+                int komma       = 0;
+                int begin       = 0;
+                int hit_count = 0;
 
-                int snlen = shortNames.length();
-
-                if (snlen != 0)
-                    {
-                        int komma       = 0;
-                        int begin       = 0;
-                        int hit_count = 0;
-
-                        while (begin < snlen) { // detect number of hits
-                            komma                  = shortNames.indexOf(',', begin);
-                            if (komma == -1) komma = snlen;
-                            hit_count++;
-                            begin                  = komma+1;
-                        }
-
-                        ta.append("\nNumber of hits: "+hit_count+"\n");
-
-                        komma = 0;
-                        begin = 0;
-                        while (begin < snlen)
-                            {
-                                komma                  = shortNames.indexOf(',', begin);
-                                if (komma == -1) komma = snlen;
-
-                                String sn = shortNames.substring(begin,komma);
-                                String details;
-
-                                TreeNode ref = (TreeNode)shortNameHash.get(sn);
-                                details = ref.getFullName() + " , " + ref.getAccessionNumber() + "\n";
-                                ta.append(details);
-
-                                begin = komma + 1;
-                            }
-                    }
-                else {
-                    ta.append("Error: probe does not match any species (internal error)");
+                while (begin < snlen) { // detect number of hits
+                    komma                  = shortNames.indexOf(',', begin);
+                    if (komma == -1) komma = snlen;
+                    hit_count++;
+                    begin                  = komma+1;
                 }
 
-                // System.out.println("probeInfo:  " + probeInfo);
-                // System.out.println("probe sequence: " + probeSequenz);
+                theText.append("The probe matches "+hit_count+" species:\n");
 
+                komma = 0;
+                begin = 0;
+                while (begin < snlen) {
+                    komma                  = shortNames.indexOf(',', begin);
+                    if (komma == -1) komma = snlen;
 
+                    String   sn  = shortNames.substring(begin,komma);
+                    TreeNode ref = (TreeNode)shortNameHash.get(sn);
 
+                    // String details = ref.getFullName() + " , " + ref.getAccessionNumber() + "\n";
+                    // toAppend.append(details);
+                    theText.append(ref.getFullName());
+                    theText.append(" , ");
+                    theText.append(ref.getAccessionNumber());
+                    theText.append("\n");
+
+                    begin = komma + 1;
+                }
             }
+            else {
+                theText.append("Error: probe does not match any species (internal error)\n");
+            }
+            ta.setText(theText.toString());
+        }
         ta.setCaretPosition(0);
+        detailState = 0;
+        ta.setVisible(true);
     }
-
 
     public void fillShortNameHash(TreeNode node)
     {
