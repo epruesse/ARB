bce591cbd0b0760e2c4585df7326c48dfdfcc417
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-03-08 15:23:33 +0000

    - implements old ProbesGUIActionListener as   anonymous class - menu-def and command-execution now are next to each other - new menus mark/fold/search - many tree folding commands

--- PROBE_WEB/CLIENT/ProbeMenu.java
+++ PROBE_WEB/CLIENT/ProbeMenu.java
@@ -1,113 +1,137 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class ProbeMenu extends MenuBar
 {
+    private ProbesGUI gui;
+    
+    private Vector menuEntries; // work as command as well
+    private Vector menu;        // corresponding menus
+    private Vector hotkeys;     // corresponding hotkeys
 
+    private TreeDisplay tree_display() { return gui.getTreeDisplay(); }
 
-public ProbeMenu(ActionListener al)
-    {
-        Menu     m;
-        MenuItem mi;
-        boolean  showUnimplemented = false;
+    private void add_menu_entry(String m, String me, MenuShortcut hk) {
+        menu.addElement(m);
+        menuEntries.addElement(me);
+        hotkeys.addElement(hk);
+    }
+    private void add_menu_entry(String m, String me, int hk) {
+        add_menu_entry(m, me, hk == 0 ? null : new MenuShortcut(hk));
+    }
 
-        m = new Menu("File");
+    private void init_menus() {
+        menuEntries = new Vector();
+        menu        = new Vector();
+        hotkeys     = new Vector();
 
-        if (showUnimplemented) m.add(new MenuItem("Save"));
+        // add_menu_entry("File", "Save", null);
+        // add_menu_entry("File", "Dump Tree", null);
 
-        if (showUnimplemented) m.add(new MenuItem("Dump Tree"));
+        add_menu_entry("File", "Quit",                  KeyEvent.VK_Q);
 
-        mi = new MenuItem("Quit");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_Q));
-        m.add(mi);
+        add_menu_entry("Mark", "Unmark nodes",          KeyEvent.VK_U);
+        add_menu_entry("Mark", "Count marked species",  KeyEvent.VK_M);
 
-        m.addActionListener(al);
-        add(m);
+        add_menu_entry("Tree", "Reset root",            KeyEvent.VK_R);
+        add_menu_entry("Tree", "Go up",                 KeyEvent.VK_H);
+        add_menu_entry("Tree", "Enter upper branch",    KeyEvent.VK_O);
+        add_menu_entry("Tree", "Enter lower branch",    KeyEvent.VK_L);
+        add_menu_entry("Tree", "Back",                  KeyEvent.VK_B);
+        add_menu_entry("Tree", "Jump to marked",        KeyEvent.VK_J);
 
-        m = new Menu("Tree");
+        add_menu_entry("Fold", "Collapse all",            0);
+        add_menu_entry("Fold", "Collapse unmarked",       0);
+        add_menu_entry("Fold", "Collapse fully marked",   0);
+        add_menu_entry("Fold", "Collapse partial marked", 0);
+        add_menu_entry("Fold", "Expand all",              0);
+        add_menu_entry("Fold", "Expand unmarked",         0);
+        add_menu_entry("Fold", "Expand fully marked",     0);
+        add_menu_entry("Fold", "Expand partial marked",   KeyEvent.VK_P);
+        add_menu_entry("Fold", "Smart expand",            KeyEvent.VK_X);
+        add_menu_entry("Fold", "Smart collapse",          KeyEvent.VK_C);
 
-        mi = new MenuItem("Unmark Nodes");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_U));
-        m.add(mi);
+        add_menu_entry("Search", "Species by name",     0);
+        add_menu_entry("Search", "Species by ACC",      0);
+    }
 
-        mi = new MenuItem("Back to Previous Scope");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_B));
-        m.add(mi);
-
-        mi = new MenuItem("Go Down");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_H));
-        m.add(mi);
-
-        mi = new MenuItem("Enter Upper Branch");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_K));
-        m.add(mi);
-
-        mi = new MenuItem("Enter Lower Branch");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_J));
-        m.add(mi);
-
-        mi = new MenuItem("Reset Root");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_R));
-        m.add(mi);
-
-        mi = new MenuItem("Count Marked Species");
-        mi.setShortcut(new MenuShortcut(KeyEvent.VK_M));
-        m.add(mi);
-
-
-        if (showUnimplemented) m.add(new MenuItem("Span Marked"));
-        m.addActionListener(al);
-        add(m);
-
-        if (showUnimplemented) {
-            m = new Menu("Search");
-            m.add(new MenuItem("Find Species"));
-            m.add(new MenuItem("Find Probes"));
-            m.add(new MenuItem("Keywords"));
-            m.addActionListener(al);
-            add(m);
+    public void performCommand(String cmd) {
+        if (cmd.equals("Quit")) {
+            gui.getClient().saveConfig();
+            System.exit(0);
         }
 
-        if (showUnimplemented) {
-            m = new Menu("Customize");
-            m.add(new MenuItem("Adjust Displayed Levels"));
-            m.add(new MenuItem("Change Marked Color"));
-            m.add(new MenuItem("Change Source URL"));
-            m.add(new MenuItem("Change Debugging behaviour"));
-            m.addActionListener(al);
-            add(m);
+        else if (cmd.equals("Unmark nodes"))            tree_display().unmarkNodes();
+        else if (cmd.equals("Count marked species"))    tree_display().countMarkedSpecies();
+
+        else if (cmd.equals("Reset root"))              tree_display().resetRoot();
+        else if (cmd.equals("Go up"))                   tree_display().goUp();
+        else if (cmd.equals("Enter upper branch"))      tree_display().enterUBr();
+        else if (cmd.equals("Enter lower branch"))      tree_display().enterLBr();
+        else if (cmd.equals("Back"))                    tree_display().previousRoot();
+        else if (cmd.equals("Jump to marked"))          tree_display().gotoRootOfMarked();
+        
+        else if (cmd.equals("Collapse all"))              tree_display().foldAll();
+        else if (cmd.equals("Collapse unmarked"))         tree_display().foldUnmarked();
+        else if (cmd.equals("Collapse fully marked"))     tree_display().foldCompleteMarked();
+        else if (cmd.equals("Collapse partial marked"))   tree_display().foldPartiallyMarked();
+        else if (cmd.equals("Expand all"))                tree_display().unfoldAll();
+        else if (cmd.equals("Expand unmarked"))           tree_display().unfoldUnmarked();              
+        else if (cmd.equals("Expand fully marked"))       tree_display().unfoldCompleteMarked();              
+        else if (cmd.equals("Expand partial marked"))     tree_display().unfoldPartiallyMarked();
+        else if (cmd.equals("Smart expand"))              tree_display().smartUnfold();
+        else if (cmd.equals("Smart collapse"))            tree_display().smartFold();
+
+        else {
+            System.out.println("Command not implemented yet: '"+cmd+"'");
         }
     }
 
-// public ProbeMenu()
-//     {
-//         Menu m;
-//         m = new Menu("File");
-//         m.add(new MenuItem("Save"));
-//         m.add(new MenuItem("Dump Tree"));
-//         m.add(new MenuItem("Quit"));
-//         add(m);
-//         m = new Menu("Tree");
-//         m.add(new MenuItem("Unmark Nodes"));
-//         m.add(new MenuItem("Back to Previous Scope"));
-//         m.add(new MenuItem("Go down"));
-//         m.add(new MenuItem("Enter Upper Branch"));
-//         m.add(new MenuItem("Enter Lower Branch"));
-//         m.add(new MenuItem("Reset Root"));
-//         m.add(new MenuItem("Count Marked Species"));
-//         m.add(new MenuItem("Span Marked"));
-//         add(m);
-//         m = new Menu("Search");
-//         m.add(new MenuItem("Find Species"));
-//         m.add(new MenuItem("Find Probes"));
-//         m.add(new MenuItem("Keywords"));
-//         add(m);
-//         m = new Menu("Customize");
-//         m.add(new MenuItem("Adjust Displayed Levels"));
-//         m.add(new MenuItem("Change Marked Color"));
-//         m.add(new MenuItem("Change Source URL"));
-//         m.add(new MenuItem("Change Debugging behaviour"));
-//         add(m);
-//     }
+    private void build_menus(ActionListener al) {
+        int    size     = menuEntries.size();
+        String lastMenu = "";
+        Menu   m        = null;
+        Set    usedKeys = new HashSet();
 
+        for (int idx = 0; idx<size; ++idx) {
+            String menu_name = (String)menu.get(idx);
+            if (!menu_name.equals(lastMenu)) {
+                if (m != null) add(m);
+                m = new Menu(menu_name);
+                m.addActionListener(al);
+                lastMenu = menu_name;
+            }
+
+            String       entry  = (String)menuEntries.get(idx);
+            MenuItem     item   = new MenuItem(entry);
+            MenuShortcut hotkey = (MenuShortcut)hotkeys.get(idx);            
+            if (hotkey != null) {
+                item.setShortcut(hotkey);
+                if (usedKeys.contains(hotkey)) {
+                    System.out.println("Hotkey duplicated in '"+entry+"'");
+                }
+                usedKeys.add(hotkey);
+            }
+            m.add(item);
+        }
+
+        if (m != null) add(m);
+
+        menuEntries = null;
+        menu        = null;
+        hotkeys     = null;
+    }
+
+    public ProbeMenu(ProbesGUI gui) {
+        this.gui = gui;
+        init_menus();
+
+        build_menus(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String cmdName = e.getActionCommand();
+                    performCommand(cmdName);
+                }
+            });
+    }
 }
