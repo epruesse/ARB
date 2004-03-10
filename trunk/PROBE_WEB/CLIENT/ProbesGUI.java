865ebd46e506a384cc625f84942d56af2e1f9263
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-03-10 12:37:53 +0000

    - added toggleOverlap, showHelp - added toolbars - removed IOManager

--- trunk/PROBE_WEB/CLIENT/ProbesGUI.java
+++ trunk/PROBE_WEB/CLIENT/ProbesGUI.java
@@ -6,18 +6,14 @@ import java.awt.event.*;
 
 public class ProbesGUI extends Frame
 {
-    private ScrollPane              sc;
-    private TextArea                details;
-    private ProbeList               probe_list;
-    private int                     probeListWidth = 200;
-//     private int                     treeLevels;
-    private TreeDisplay             td;
-    private TreeNode                root;
-    private ProbeMenu               pm;
-//     private ProbesGUIActionListener al;
-    private ProbeListActionListener ll;
-    private Client                  client;
-    private IOManager iom;      // wrapper to save functions
+    private ScrollPane  sc;
+    private TreeDisplay td;
+    private TreeNode    root;
+    private TextArea    details;
+    private ProbeList   probe_list;
+    private int         probeListWidth = 200;
+    private ProbeMenu   pm;
+    private Client      client;
 
 
     public ProbesGUI(int levels, String title, Client cl) throws Exception
@@ -26,7 +22,7 @@ public class ProbesGUI extends Frame
         setVisible(true);
 
         Color backgroundColor = new Color(160, 180, 255);
-        
+
         setBackground(Color.lightGray);
         setLayout(new BorderLayout());
         setLocation(200, 200);
@@ -37,22 +33,18 @@ public class ProbesGUI extends Frame
         if (td == null) Toolkit.AbortWithError("constructor/ProbesGUI: no TreeDisplay received");
         td.setClient(client);
 
-//         al = new ProbesGUIActionListener(this);
-//         setMenuBar(pm = new ProbeMenu(al));
-        setMenuBar(pm = new ProbeMenu(this));
+        setMenuBar(pm = new ProbeMenu(this)); // menus
 
         {
             Color panelColor = Color.white;
             Panel panel      = new Panel();
             panel.setLayout(new BorderLayout());
 
-            probe_list = new ProbeList();
-            ll = new ProbeListActionListener(this);
-            probe_list.addItemListener(ll);
-            probe_list.add("Right click a node");
-            probe_list.add("to display probes.");
-            probe_list.setBackground(panelColor);
+            probe_list = new ProbeList(this, panelColor);
+            showHelp();
             panel.add(probe_list, BorderLayout.CENTER);
+
+            panel.add(new ProbeToolbar(this), BorderLayout.NORTH);
 
             details = new TextArea("Display detail information", 20, 40, TextArea.SCROLLBARS_BOTH);
             details.setBackground(panelColor);
@@ -61,19 +53,54 @@ public class ProbesGUI extends Frame
             add(panel, BorderLayout.EAST);
         }
 
-        sc = new ScrollPane();
+        {
+            Panel treePanel = new Panel();
+            treePanel.setLayout(new BorderLayout());
 
-        sc.add(td);
-        sc.getVAdjustable().setUnitIncrement(1);
-        sc.getHAdjustable().setUnitIncrement(1);
-        sc.setSize(600,600);
-        // sc.setWheelScrollingEnabled(true); // later - needs java 1.4 :(
+            treePanel.add(new TreeToolbar(this), BorderLayout.NORTH);
 
-        add(sc, BorderLayout.CENTER);
+            sc = new ScrollPane();
+
+            sc.add(td);
+            sc.getVAdjustable().setUnitIncrement(1);
+            sc.getHAdjustable().setUnitIncrement(1);
+            sc.setSize(600,600);
+            // sc.setWheelScrollingEnabled(true); // later - needs java 1.4 :(
+
+            treePanel.add(sc, BorderLayout.CENTER);
+
+            add(treePanel);
+        }
 
         addWindowListener(new WindowClosingAdapter(true));
 
         pack();
+    }
+
+    public void toggleOverlap() throws Exception {
+        // Note : intentionally placed here (because a 2nd probelist is planned)
+
+        Probe overlapping = Probe.getOverlapProbe();
+        Probe selected    = probe_list.getProbe(probe_list.getSelectedIndex());
+
+        System.out.println("toggleOverlap!");
+        if (overlapping != selected) {
+            // show overlaps with current probe
+            System.out.println("overlapping != selected => set selected");
+            probe_list.showOverlap(selected);
+        }
+        else {
+            System.out.println("clear overlap");
+            probe_list.showOverlap(null); // hide overlaps
+        }
+
+        probe_list.resort();
+    }
+
+    public void showHelp() {
+        probe_list.clear();
+        probe_list.add("Left click a branch to unfold or step.");
+        probe_list.add("Right click a branch to display probes.");
     }
 
     public void initTreeDisplay(TreeNode root)  throws Exception {
@@ -85,16 +112,6 @@ public class ProbesGUI extends Frame
     public ProbeList getProbeList() { return probe_list; }
     public Client getClient() { return client; }
     public TextArea getDetails() { return details; }
-
-
-    public void setIOManager(IOManager io){
-	iom = io;
-	if (iom == null){System.out.println("Cannot set IOManager null"); System.exit(1);}
-    }
-
-    public IOManager getIOManager(){
-	return iom;
-    }
 
 
 }
