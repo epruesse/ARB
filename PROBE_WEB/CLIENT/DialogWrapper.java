ac329b55c431aa266a2e367cd2cc7622886da577
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-03-11 23:17:03 +0000

    - compilation fix - indentation fixed - dialog window had zero size (with fvwm) - splits text at \n

--- PROBE_WEB/CLIENT/DialogWrapper.java
+++ PROBE_WEB/CLIENT/DialogWrapper.java
@@ -9,54 +9,95 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 
-public class DialogWrapper
-implements ActionListener
-
+public class DialogWrapper implements ActionListener
 {
     private Dialog dl;
     private String result;
 
 
-public DialogWrapper(Frame parent, String title, Vector buttons){
-    dl = new Dialog(parent, title, true);
-    dl.setLayout(new BorderLayout());
-    dl.setResizable(false);   
-    Point parloc = parent.getLocation();
-    // position inside parent window
-    dl.setLocation(parloc.x + 30, parloc.y + 30);
-    dl.add(new Label(title), BorderLayout.NORTH);
-    Panel p = new Panel();
-    p.setLayout(new FlowLayout(FlowLayout.CENTER));
-    Enumeration en = buttons.elements();
-    while (en.hasMoreElements()){
-	//	System.out.println("Adding Button");
-	Button aButton = new Button((String) en.nextElement());
-	aButton.addActionListener(this);
-	p.add(aButton, BorderLayout.EAST); 
-    }
-    dl.add(p, BorderLayout.SOUTH);
-    dl.pack();
-    dl.setVisible(true);
+    public DialogWrapper(Frame parent, String title, String text, Vector buttons){
+        dl = new Dialog(parent, title, true);
+        dl.setLayout(new BorderLayout());
+        // dl.setResizable(true);
+        
+        Point parloc = parent.getLocation();
+        // position inside parent window
+        dl.setLocation(parloc.x + 100, parloc.y + 100);
+
+        Component c = null;
+        {
+            int lf = text.indexOf('\n');
+            if (lf == -1) { // oneliner
+                Label t = new Label(text);
+                c       = t;
+            }
+            else {
+                Panel p = new Panel();
+
+                p.setLayout(new BorderLayout());
+                p.add(new Label(text.substring(0, lf)), BorderLayout.NORTH);
+                p.add(new Label(text.substring(lf+1)), BorderLayout.SOUTH);
+
+                c = p;
+            }
+        }
+        dl.add(c, BorderLayout.NORTH);
+
+        Panel p = new Panel();
+        p.setLayout(new FlowLayout());
+        {
+            Enumeration en = buttons.elements();
+            while (en.hasMoreElements()){
+                //  System.out.println("Adding Button");
+                Button aButton = new Button((String) en.nextElement());
+                aButton.addActionListener(this);
+                p.add(aButton);
+            }
+        }
+        dl.add(p, BorderLayout.SOUTH);
+
+        dl.pack();
+        {
+            Dimension cd = c.getSize();
+            Dimension pd = p.getSize();
+
+            // System.out.println("cd="+cd.width+"/"+cd.height);
+            // System.out.println("pd="+pd.width+"/"+pd.height);
+
+            dl.setSize((cd.width > pd.width ? pd.width : cd.width)+50,
+                       cd.height+pd.height+25);
+
+            // Dimension dld = dl.getSize();
+            // System.out.println("dld="+dld.width+"/"+dld.height);
+        }
+
+        dl.pack();
+
+        // Dimension dld = dl.getSize();
+        // System.out.println("dld="+dld.width+"/"+dld.height);
+
+        dl.setResizable(false);
+        dl.setVisible(true);
 
     }
-     
+
 
     public String getResult(){
-	return result;
+        return result;
     }
 
 
 
- public void actionPerformed(ActionEvent event)
+    public void actionPerformed(ActionEvent event)
     {
-	result = event.getActionCommand();
-	dl.setVisible(false);
-	dl.dispose();
+        result = event.getActionCommand();
+        dl.setVisible(false);
+        dl.dispose();
     }
 
-    classe WClAdapter extends WindowAdapter{
+//     class WClAdapter extends WindowAdapter{
 
 
-    }
+//     }
 
 } // class DialogWrapper
