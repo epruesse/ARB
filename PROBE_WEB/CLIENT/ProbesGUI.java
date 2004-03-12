5b0a5099680f7a693ce91844bd0214ef5ec98e5d
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-03-12 10:44:55 +0000

    - gets wanted size as parameter - ScrollPane does not prefer to have size of contained canvas - many weird calculations to approx. create a frame of wanted size

--- PROBE_WEB/CLIENT/ProbesGUI.java
+++ PROBE_WEB/CLIENT/ProbesGUI.java
@@ -11,12 +11,12 @@ public class ProbesGUI extends Frame
     private TreeNode    root;
     private TextArea    details;
     private ProbeList   probe_list;
-    private int         probeListWidth = 200;
+    // private int         probeListWidth = 200;
     private ProbeMenu   pm;
     private Client      client;
 
 
-    public ProbesGUI(int levels, String title, Client cl) throws Exception
+    public ProbesGUI(int levels, String title, Client cl, Point origin, Dimension size, int textWidth) throws Exception
     {
         super(title);
         setVisible(true);
@@ -25,7 +25,8 @@ public class ProbesGUI extends Frame
 
         setBackground(Color.lightGray);
         setLayout(new BorderLayout());
-        setLocation(200, 200);
+        // setLocation(10, 10);
+        setLocation(origin);
 
         client = cl;
 
@@ -35,6 +36,12 @@ public class ProbesGUI extends Frame
 
         setMenuBar(pm = new ProbeMenu(this)); // menus
 
+        // sum up dimension outside of ScrollPane 
+        int xtraWidth  = 0;
+        int xtraHeight = 0;
+
+        ProbeToolbar tb = new ProbeToolbar(this);
+        
         {
             Color panelColor = Color.white;
             Panel panel      = new Panel();
@@ -44,13 +51,16 @@ public class ProbesGUI extends Frame
             showHelp();
             panel.add(probe_list, BorderLayout.CENTER);
 
-            panel.add(new ProbeToolbar(this), BorderLayout.NORTH);
-
-            details = new TextArea("Display detail information", 20, 40, TextArea.SCROLLBARS_BOTH);
+            panel.add(tb, BorderLayout.NORTH);
+            details = new TextArea("Detail information", 20, textWidth, TextArea.SCROLLBARS_BOTH);
             details.setBackground(panelColor);
             panel.add(details, BorderLayout.SOUTH);
 
             add(panel, BorderLayout.EAST);
+
+            Dimension pdim = panel.getPreferredSize();
+            System.out.println("pdim = "+pdim.width+"/"+pdim.height);
+            xtraWidth += pdim.width;
         }
 
         {
@@ -59,12 +69,35 @@ public class ProbesGUI extends Frame
 
             treePanel.add(new TreeToolbar(this), BorderLayout.NORTH);
 
-            sc = new ScrollPane();
+            sc = new ScrollPane() {
+                    public Dimension getPreferredSize() {
+                        System.out.println("ScrollPane::getPreferredSize called");
+                        return getSize(); // ignores size of contained canvas!
+                    }
+                    public Dimension getMaximumSize() {
+                        System.out.println("ScrollPane::getMaximumSize called");
+                        return getSize(); // ignores size of contained canvas!
+                    }
+                    public Dimension getMinimumSize() {
+                        System.out.println("ScrollPane::getMinimumSize called");
+                        return getSize(); // ignores size of contained canvas!
+                    }
+                };
 
             sc.add(td);
             sc.getVAdjustable().setUnitIncrement(1);
             sc.getHAdjustable().setUnitIncrement(1);
-            sc.setSize(600,600);
+
+            System.out.println("xtraWidth="+xtraWidth+" xtraHeight="+xtraHeight);
+            System.out.println("wanted frame size = "+size.width+"/"+size.height);
+            size.width  -= xtraWidth;
+            System.out.println("calc. sc size = "+size.width+"/"+size.height);
+
+            sc.setSize(size);
+
+            Dimension scdim = sc.getSize();
+            System.out.println("resulting sc size = "+scdim.width+"/"+scdim.height);
+            //sc.setSize(640-20,480-20);
             // sc.setWheelScrollingEnabled(true); // later - needs java 1.4 :(
 
             treePanel.add(sc, BorderLayout.CENTER);
@@ -74,7 +107,35 @@ public class ProbesGUI extends Frame
 
         addWindowListener(new WindowClosingAdapter(true));
 
+        Dimension    tbdim  = tb.getPreferredSize();
+        System.out.println("tbdim = "+tbdim.width+"/"+tbdim.height);
+        xtraHeight         += tbdim.height;
+
+        // Dimension    pmdim  = pm.getSize();
+        // System.out.println("pmdim = "+pmdim.width+"/"+pmdim.height);
+        xtraHeight         += 40; // for menubar
+        
+        size.height -= xtraHeight;
+        System.out.println("calc. sc size = "+size.width+"/"+size.height+" (2nd correction)");
+
+        sc.setSize(size);
+
+
+        // setSize(size);
+        // invalidate();
+        Dimension scdim = sc.getSize();
+        System.out.println("scdim = "+scdim.width+"/"+scdim.height);
+        System.out.println("packing ProbesGUI Frame");
         pack();
+
+
+        // setSize(scdim);
+        scdim = sc.getSize();
+        System.out.println("final scdim = "+scdim.width+"/"+scdim.height);
+
+        Dimension pgdim = getSize();
+        System.out.println("final pgdim = "+pgdim.width+"/"+pgdim.height);
+
     }
 
     public void toggleOverlap() throws Exception {
