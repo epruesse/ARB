acff73625ed554a865b53ed1df77f81459fc7a82
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-03-15 11:16:20 +0000

    - indentation & spacing - '--help' also shows help

--- trunk/PROBE_WEB/CLIENT/CommandLine.java
+++ trunk/PROBE_WEB/CLIENT/CommandLine.java
@@ -7,59 +7,48 @@ import java.util.*;
 
 public class CommandLine
 {
+    private HashMap hm;
+    private HashMap knownOptions;
 
-private HashMap hm;
-private HashMap knownOptions;
-
-
-
-
-
-public CommandLine(String[] arguments, HashMap known) throws Exception 
-    {
+    public CommandLine(String[] arguments, HashMap known) throws Exception {
         hm            = new HashMap();
         knownOptions  = known;
         int     i     = 0;
         String  option;
         String  value;
 
-        while (i < arguments.length)
-        {
-            // System.out.println("Arg="+arguments[i]);
-            if(arguments[i].indexOf('=') != -1)
-            {
+        while (i < arguments.length) {
+            if(arguments[i].indexOf('=') != -1) {
                 option = arguments[i].substring(0,arguments[i].indexOf('='));
                 value  = arguments[i].substring(arguments[i].indexOf('=')+1);
             }
-            else
-            {
+            else {
                 option = arguments[i];
                 value = null;
             }
 
-            if (knownOptions.containsKey(option) || option == "help") {
+            
+
+            if (knownOptions.containsKey(option)) {
                 hm.put(option, value);
+            }
+            else if (option.indexOf("help") != -1) {
+                showHelp();
+                System.exit(0); // sucessfully displayed help                
             }
             else {
                 showHelpAndAbortWithError("Unknown option '"+option+"'");
             }
             i++;
         }
-
-        if (hm.containsKey("help")) {
-            showHelp();
-            System.exit(0); // sucessfully displayed help
-        }
     }
 
-public void showHelpAndAbortWithError(String error) throws Exception 
-    {
+    public void showHelpAndAbortWithError(String error) throws Exception {
         showHelp();
         Toolkit.AbortWithError(error);
     }
 
-public void showHelp()
-    {
+    public void showHelp() {
         System.out.println("Known options:");
         Set keys = knownOptions.keySet();
         for (Iterator it = keys.iterator(); it.hasNext(); ) {
@@ -71,57 +60,35 @@ public void showHelp()
         }
     }
 
-private void checkLegalOption(String optionName) throws Exception 
-    {
+    private void checkLegalOption(String optionName) throws Exception {
         if (!knownOptions.containsKey(optionName)) {
             showHelpAndAbortWithError("Unknown option '"+optionName+"'");
         }
     }
 
-public boolean getOption(String optionName) throws Exception 
-    {
+    public boolean getOption(String optionName) throws Exception {
         checkLegalOption(optionName);
-        if (hm.containsKey(optionName)) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return hm.containsKey(optionName);
     }
 
-
-public String getOptionValue(String optionName) throws Exception 
-    {
+    public String getOptionValue(String optionName) throws Exception {
         checkLegalOption(optionName);
         return (String) hm.get(optionName);
     }
 
-public void printArgs()
-    {
+    public void printArgs() {
         Set keys = hm.keySet();
-                System.out.println("OPTIONS:");
-        for(Iterator it = keys.iterator();it.hasNext();)
-            {
-                System.out.println((String) it.next());
-            }
+        System.out.println("OPTIONS:");
+        for (Iterator it = keys.iterator();it.hasNext();) {
+            System.out.println((String) it.next());
+        }
 
         Set values = hm.entrySet();
-                System.out.println("VALUES:");
-        for(Iterator it = values.iterator();it.hasNext();)
-            {
-                Object o = it.next();
-                if(o != null)
-                    {
-                        System.out.println( o);
-                    }
-                else
-                    {
-                        System.out.println("Object null");
-                    }
-            }
-
-
+        System.out.println("VALUES:");
+        for (Iterator it = values.iterator();it.hasNext();) {
+            Object o = it.next();
+            if (o != null) System.out.println( o);
+            else System.out.println("Object null");
+        }
     }
-
-
 }
