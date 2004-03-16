186fc134ce8b84818853a4d723602aa2756252c5
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-03-16 22:37:36 +0000

    - obsolete

--- NTREE/ad_ali.hxx
+++
@@ -1,6 +0,0 @@
-void create_transpro_variables(AW_root *root,AW_default db1);
-void create_transpro_menus(AW_window *awmm);
-void create_alignment_vars(AW_root *aw_root,AW_default aw_def);
-AW_window *create_alignment_window(AW_root *root,AW_default aw_def);
-AW_window *create_dna_2_pro_window(AW_root *root);
-AW_window *create_realign_dna_window(AW_root *root);
--- NTREE/ad_ext.hxx
+++
@@ -1,5 +0,0 @@
-#define AWAR_SAI_DEST "tmp/focus/extended_dest"
-
-void create_extendeds_var(AW_root *aw_root, AW_default aw_def);
-
-AW_window *create_extendeds_window(AW_root *aw_root);
