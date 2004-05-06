0146e0ee597d4c6e3ae5c997458d4a530bebcc5f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-05-06 14:36:32 +0000

    - if \n is used in shell script it is unencoded   now arb_message scans and replaces these \n's

--- TOOLS/arb_message.cxx
+++ TOOLS/arb_message.cxx
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <arbdb.h>
 #include <arbdbt.h>
 
@@ -18,13 +19,26 @@ int main(int argc,char **argv)
         return -1;
     }
 
+    const char *progname = argv[0];
+    if (!progname || progname[0] == 0) {
+        progname = "arb_message";
+    }
+
+    char *the_message  = strdup(argv[1]);
+    char *unencoded_lf = 0;
+    while ((unencoded_lf = strstr(the_message, "\\n")) != 0) {
+        unencoded_lf[0] = '\n';
+        strcpy(unencoded_lf+1, unencoded_lf+2);
+    }
+
     GBDATA *gb_main = GB_open(":","r");
     if (!gb_main){
-        fprintf(stderr, "%s: %s\n", argv[0], argv[1]);
+        fprintf(stderr, "%s: %s\n", progname, the_message);
     }
     else {
-        GBT_message(gb_main, argv[1]);
+        GBT_message(gb_main, the_message);
         GB_close(gb_main);
     }
+    free(the_message);
     return 0;
 }
