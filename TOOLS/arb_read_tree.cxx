b15a57dcd41b327f1f12d204f2d2bb12d0929d30
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-05-24 12:06:14 +0000

    - resets branchlengths to 0.1 if values were interpreted as bootstrap values

--- TOOLS/arb_read_tree.cxx
+++ TOOLS/arb_read_tree.cxx
@@ -44,6 +44,9 @@ void add_bootstrap(GBT_TREE *node, double hundred) {
     node->leftson->remark_branch  = GBS_global_string_copy("%2.0f%%", left_bs);
     node->rightson->remark_branch = GBS_global_string_copy("%2.0f%%", right_bs);
 
+    node->leftlen  = 0.1; // reset branchlengths
+    node->rightlen = 0.1;
+
     add_bootstrap(node->leftson, hundred);
     add_bootstrap(node->rightson, hundred);
 }
