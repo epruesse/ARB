259e4a9bdbbd25504b0c4c033d9dea9ead77595a
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-06-05 13:14:08 +0000

    - cosmetics

--- WINDOW/aw_Xm.hxx
+++ WINDOW/aw_Xm.hxx
@@ -3,22 +3,26 @@
 
 
 class AW_device_Xm: public AW_device {
-		int fastflag;
-	public:
-		AW_device_Xm(AW_common *commoni);
-		void	init(void);
-		AW_DEVICE_TYPE type(void);
-		int	line(int gc, AW_pos x0,AW_pos y0, AW_pos x1,AW_pos y1, AW_bitset filter, AW_CL cd1, AW_CL cd2);
-		int	text(int gc, const char *string,AW_pos x,AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL cd1, AW_CL cd2,long opt_strlen);
-		int	box(int gc, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
-		int	circle(int gc, AW_BOOL filled, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
-		void	clear();
-		void	clear_part(AW_pos x, AW_pos y,AW_pos width, AW_pos height);
-		void	clear_text(int gc, const char *string, AW_pos x, AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL cd1, AW_CL cd2);
-		void	fast(void);	// e.g. zoom linewidth off
-		void	slow(void);
-		void	flush(void);
-		void	move_region( AW_pos src_x, AW_pos src_y, AW_pos width, AW_pos height, AW_pos dest_x, AW_pos dest_y );
+    int fastflag;
+public:
+    AW_device_Xm(AW_common *commoni);
+    
+    void	   init(void);
+    AW_DEVICE_TYPE type(void);
+    
+    int	line(int gc, AW_pos x0,AW_pos y0, AW_pos x1,AW_pos y1, AW_bitset filter, AW_CL cd1, AW_CL cd2);
+    int	text(int gc, const char *string,AW_pos x,AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL cd1, AW_CL cd2,long opt_strlen);
+    int	box(int gc, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
+    int	circle(int gc, AW_BOOL filled, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
+    
+    void clear();
+    void clear_part(AW_pos x, AW_pos y,AW_pos width, AW_pos height);
+    void clear_text(int gc, const char *string, AW_pos x, AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL cd1, AW_CL cd2);
+    
+    void fast(void);            // e.g. zoom linewidth off
+    void slow(void);
+    void flush(void);
+    void move_region( AW_pos src_x, AW_pos src_y, AW_pos width, AW_pos height, AW_pos dest_x, AW_pos dest_y );
 };
 
 
--- WINDOW/aw_click.hxx
+++ WINDOW/aw_click.hxx
@@ -5,22 +5,26 @@
 
 
 class AW_device_click: public AW_device {
-	protected:
-		AW_pos		mouse_x,mouse_y;
-		AW_pos		max_distance_line;
-		AW_pos 		max_distance_text;
+protected:
+    AW_pos          mouse_x,mouse_y;
+    AW_pos          max_distance_line;
+    AW_pos          max_distance_text;
 
-	public:
-		AW_clicked_line opt_line;
-		AW_clicked_text opt_text;
-		AW_device_click(AW_common *commoni);
-		AW_DEVICE_TYPE type(void);
-		void	init(AW_pos mousex, AW_pos mousey, AW_pos max_distance_liniei, AW_pos max_distance_texti, AW_pos radi, AW_bitset filteri);
+public:
+    AW_clicked_line opt_line;
+    AW_clicked_text opt_text;
+    
+    AW_device_click(AW_common *commoni);
+    
+    AW_DEVICE_TYPE type(void);
+    
+    void init(AW_pos mousex, AW_pos mousey, AW_pos max_distance_liniei, AW_pos max_distance_texti, AW_pos radi, AW_bitset filteri);
 
-		int	line(int gc, AW_pos x0,AW_pos y0, AW_pos x1,AW_pos y1, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2);
-		int	text(int gc, const char *string,AW_pos x,AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2, long opt_strlen);
-		void	get_clicked_line(class AW_clicked_line *ptr);
-		void	get_clicked_text(class AW_clicked_text *ptr);
+    int line(int gc, AW_pos x0,AW_pos y0, AW_pos x1,AW_pos y1, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2);
+    int text(int gc, const char *string,AW_pos x,AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2, long opt_strlen);
+    
+    void get_clicked_line(class AW_clicked_line *ptr);
+    void get_clicked_text(class AW_clicked_text *ptr);
 };
 
 
