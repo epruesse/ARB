4d92ef844d68c37599183da04fd5ca66b6faefa6
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-06-25 15:14:50 +0000

    proto updated

--- NTREE/nt_internal.h
+++ NTREE/nt_internal.h
@@ -63,7 +63,7 @@ AW_window *NT_createMergeSimilarSpeciesAndConcatenateWindow(AW_root *aw_root, AW
 AW_window *NT_create_ad_list_reorder(AW_root *root, AW_CL cl_item_selector);
 AW_window *NT_create_ad_field_delete(AW_root *root, AW_CL cl_item_selector);
 AW_window *NT_create_ad_field_create(AW_root *root, AW_CL cl_item_selector);
-void NT_detach_information_window(AW_window *aww, AW_CL cl_pointer_to_aww, AW_CL cl_Awar_Callback_Info);
+void NT_detach_information_window(AW_window *aww, AW_CL cl_pointer_to_aww, AW_CL cl_AW_detach_information);
 AW_window *NT_create_species_window(AW_root *aw_root);
 AW_window *NT_create_organism_window(AW_root *aw_root);
 
