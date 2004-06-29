8bbb7d75b28cd1f0416633255f8bf28303b22356
richter <richter@129cd134-2152-0410-ab65-f3f895bafe68>
2004-06-29 18:34:46 +0000

    Interface slightly modified: accession numbers to be looked for, exclude accession numbers and version numbers can be supplied as quoted list as well as from file. All filtering functions implemented, log function not yet.

--- PERL_SCRIPTS/MAUS/mausdriver.pl
+++ PERL_SCRIPTS/MAUS/mausdriver.pl
@@ -1,8 +1,12 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 use strict;
 use diagnostics;
 
 use Maus;
+
+
+
+
 
 ## main section
 
@@ -11,19 +15,20 @@ my $logWanted = 0;
 
 my %options = %{&getOptions};
 my $testOptionsRef = intializeTestFields(\ %options);
-my %listArgs = ();
-$listArgs{"exclude"} = prepareExcludes(\ %options);
 
-foreach my $element(@{$listArgs{"exclude"}}){print $element."\n";}
+# combined preparation of exclude accession numbers and selected accession number in initialization step
+# introduction of excl and acpt instead of exfl, exff and acfl, acff
 
 my $seqSourceRef = \*STDIN;      # default STDIN
 my $seqDestRef = \*STDOUT;      # default STDOUT
 
 local $/ = "//\n";		# sequence entry separator in InputFile
 
-if (defined $options{if}){
-open (SEQ_SOURCE , "< $options{if}") or die "cannot open sequence file $options{if} to read\n";
-$seqSourceRef = \*SEQ_SOURCE;
+if (not defined $options{if}){
+  if (not exists $options{force}){die "You specified no input file. To read form STDIN use -force.\n";}
+}else{
+  open (SEQ_SOURCE , "< $options{if}") or die "cannot open sequence file $options{if} to read\n";
+  $seqSourceRef = \*SEQ_SOURCE;
 }
 if (defined $options{of}){
   open (SEQ_DEST , "> $options{of}") or die "cannot open sequence file $options{of} to write\n";
@@ -34,12 +39,12 @@ if (defined $options{of}){
 my $sequenceEntry = "";
 while ($sequenceEntry = <$seqSourceRef>){
 #print $sequenceEntry."\n";
-my $result = parseEntry(\$sequenceEntry, $testOptionsRef, \%listArgs, $logWanted);
+my $result = parseEntry(\$sequenceEntry, $testOptionsRef,$logWanted);
 
 #
 # analyze sequence entry
 #
 
-#if ($result != 0) {print $seqDestRef $sequenceEntry;}
+if ($result != 0) {print  "sequence accepted\n";}else{print "sequence rejected\n";}
 print $seqDestRef $sequenceEntry;
 }
