6bd21043968f03b7089958e7c308748d1acbe625
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-07-14 10:20:20 +0000

    - added GEN_pos_begin_entry_name, GEN_pos_end_entry_name

---
+++ trunk/GENOM/GEN_tools.hxx
@@ -0,0 +1,25 @@
+//  ==================================================================== // 
+//                                                                       // 
+//    File      : GEN_tools.hxx                                          // 
+//    Purpose   :                                                        // 
+//    Time-stamp: <Thu Jul/01/2004 08:52 MET Coder@ReallySoft.de>        // 
+//                                                                       // 
+//                                                                       // 
+//  Coded by Ralf Westram (coder@reallysoft.de) in July 2004             // 
+//  Copyright Department of Microbiology (Technical University Munich)   // 
+//                                                                       // 
+//  Visit our web site at: http://www.arb-home.de/                       // 
+//                                                                       // 
+//  ==================================================================== // 
+
+#ifndef GEN_TOOLS_HXX
+#define GEN_TOOLS_HXX
+
+const char *GEN_pos_begin_entry_name(int nr);
+const char *GEN_pos_end_entry_name(int nr);
+
+
+#else
+#error GEN_tools.hxx included twice
+#endif // GEN_TOOLS_HXX
+
