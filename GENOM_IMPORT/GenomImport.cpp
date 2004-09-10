a16d44c43da5b4eab466a260239c6563865ac405
artemov <artemov@129cd134-2152-0410-ab65-f3f895bafe68>
2004-09-10 12:51:12 +0000

    fix (ralf)

--- GENOM_IMPORT/GenomImport.cpp
+++ GENOM_IMPORT/GenomImport.cpp
@@ -13,9 +13,11 @@
 //                                                                       //
 //  ==================================================================== //
 
-#include <GenomImport.h>
-#include <GenomEmbl.h>
-#include <GenomArbConnection.h>
+#include <stdio.h>
+#include <arbdb.h>
+#include "GenomImport.h"
+//#include "GenomEmbl.h"
+//#include "GenomArbConnection.h"
 
 GB_ERROR Genom_read_embl_universal(GBDATA *gb_main, const char *filename, const char *ali_name) {
 	GB_ERROR error = "Not implemented yet";
