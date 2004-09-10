72e42ca07af6aaac8c2928d5e51a19e21bf87041
artemov <artemov@129cd134-2152-0410-ab65-f3f895bafe68>
2004-09-10 12:12:38 +0000

    Interface to ARB

---
+++ GENOM_IMPORT/GenomImport.cpp
@@ -0,0 +1,25 @@
+//  ==================================================================== //
+//                                                                       //
+//    File      : Genome_Import.cpp                                      //
+//    Purpose   : Interface to ARB                                       //
+//    Time-stamp: <Fri Sep/10/2004 13:34 MET Coder@ReallySoft.de>        //
+//                                                                       //
+//                                                                       //
+//  Coded by Ralf Westram (coder@reallysoft.de) in August 2004           //
+//  Copyright Department of Microbiology (Technical University Munich)   //
+//                                                                       //
+//  Visit our web site at: http://www.arb-home.de/                       //
+//                                                                       //
+//                                                                       //
+//  ==================================================================== //
+
+#include <GenomImport.h>
+#include <GenomEmbl.h>
+#include <GenomArbConnection.h>
+
+GB_ERROR Genom_read_embl_universal(GBDATA *gb_main, const char *filename, const char *ali_name) {
+	GB_ERROR error = "Not implemented yet";
+	return error;
+}
+
+
