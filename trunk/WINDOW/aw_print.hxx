6977c907b9e57ae4c2170d38164337838850d571
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-09-22 17:24:01 +0000

    - made 'out' private + added get_FILE() - fixed wrong prototype of AW_device_print

--- trunk/WINDOW/aw_print.hxx
+++ trunk/WINDOW/aw_print.hxx
@@ -3,21 +3,25 @@
 
 
 class AW_device_print: public AW_device {
-	public:
-	FILE *out;
+    FILE       *out;
+public:
+    // ********* real public
+    AW_device_print(AW_common *commoni);
+    void        init(void);
+    const char *open(const char *path);
+    void        close(void);
 
-	// ********* real public
-	AW_device_print(AW_common *commoni);
-	void	init(void);
-	const char *open(const char *path);
-	void	close(void);
+    FILE *get_FILE() { return out; }
 
-	AW_DEVICE_TYPE type(void); 
-	int	line(int gc, AW_pos x0,AW_pos y0, AW_pos x1,AW_pos y1, AW_bitset filter, AW_CL cd1, AW_CL cd2);
-	int	text(int gc, const char *string,AW_pos x,AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL cd1, AW_CL cd2,long opt_strlen);
-	int	box(int gc, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
-	int	circle(int gc, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
-	int	filled_area(int gc, int npoints, AW_pos *points, AW_bitset filteri, AW_CL cd1, AW_CL cd2);
+    // AW_device interface :
+
+    AW_DEVICE_TYPE type(void);
+
+    int line(int gc, AW_pos x0,AW_pos y0, AW_pos x1,AW_pos y1, AW_bitset filter, AW_CL cd1, AW_CL cd2);
+    int text(int gc, const char *string,AW_pos x,AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL cd1, AW_CL cd2,long opt_strlen);
+    int box(int gc, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
+    int circle(int gc, AW_BOOL filled, AW_pos x0,AW_pos y0,AW_pos width,AW_pos heigth, AW_bitset filter, AW_CL cd1, AW_CL cd2);
+    int filled_area(int gc, int npoints, AW_pos *points, AW_bitset filteri, AW_CL cd1, AW_CL cd2);
 };
 
 
