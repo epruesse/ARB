98df6eb0609b85122eb7da50c3ae598ca9a060d8
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-09-23 14:32:43 +0000

    - hardcoded seq-color mapping (was partly hardcoded/partly on defaults - KISS) - added create_seq_color_awars()

--- trunk/AWT/AWT_seq_colors.cxx
+++ trunk/AWT/AWT_seq_colors.cxx
@@ -14,15 +14,70 @@
 #include "awt_seq_colors.hxx"
 #include "awt.hxx"
 
-void awt_awar_changed_cb(GBDATA *, int *cl, GB_CB_TYPE){
+
+static int default_set = 0; // number of default set
+static const char *default_sets[AWT_SEQ_COLORS_MAX_SET] = {
+    //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+    "=2=0=3=0=0=0=4=0=0=0=0=0=0=6=0=0=0=0=0=5=5=0=0=0=0=0", // A, C, G, TU and N in 5 colors
+    "R2=0Y3=0=0=0R2=0=0=0=0=0=0=0=0=0=0=2=0Y3Y3=0=0=0=3=0", // AG and CTU in 2 colors
+    "=0=5=0=5=7=7=0=5=7=7=3=7=3=9=7=7=7=3=3=0=0=5=3=7=3=7", // ambiguity
+    // "=2=4=7=4=4=6=2=5=3=0=5=3=3=4=0=2=4=5=2=2=0=3=6=0=6=4", // Protein colors (old defaults)
+    "=7=0=7=8=2=9=8=9=3=0=2=3=7=8=0=8=2=2=2=2=0=3=9=0=9=0", // Protein colors
+    "=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0",
+};
+
+static bool seq_color_awars_created = false;
+
+// --------------------------------------------------------------------------------
+
+static const char *default_characters(int elem) {
+    static char result[3] = "xX";
+
+    result[0] = 'a'+elem;
+    result[1] = 'A'+elem;
+
+    return result;
+}
+static const char *default_color(int set, int elem) {
+    // returns default color numbers for seq-color-set
+    static char result[3] = "=0";
+    const char *pos       = default_sets[set]+2*elem;
+
+    result[0] = pos[0];
+    result[1] = pos[1];
+
+    return result;
+}
+
+static void awt_awar_changed_cb(GBDATA *, int *cl, GB_CB_TYPE){
     AWT_seq_colors *sc = (AWT_seq_colors *)cl;
     sc->reload();
 }
 
+static void create_seq_color_awars(AW_root *awr, AWT_seq_colors *asc) {
+    awt_assert(!seq_color_awars_created);
+
+    awr->awar_int(AWAR_SEQ_NAME_SELECTOR, default_set, AW_ROOT_DEFAULT)->add_callback((AW_RCB)awt_awar_changed_cb,(AW_CL)asc,0);;
+    for (int elem = 0; elem<AWT_SEQ_COLORS_MAX_ELEMS; ++elem) {
+        const char *awar_name = GBS_global_string(AWAR_SEQ_NAME_STRINGS_TEMPLATE, elem);
+        awr->awar_string(awar_name, default_characters(elem));
+
+        for (int set = 0; set<AWT_SEQ_COLORS_MAX_SET; ++set) {
+            awar_name = GBS_global_string(AWAR_SEQ_NAME_TEMPLATE, set, elem);
+            awr->awar_string(awar_name, default_color(set, elem));
+        }
+    }
+
+    seq_color_awars_created = true;
+}
+
 AW_window *create_seq_colors_window(AW_root *awr, AWT_seq_colors *asc){
-    char buf[256];
+    char                     buf[256];
     static AW_window_simple *aws = 0;
     if (aws) return aws;
+
+    if (!seq_color_awars_created) create_seq_color_awars(awr, asc);
+
     aws = new AW_window_simple;
     aws->init( awr, "SEQUENCE_COLOR_MAPPING", "SEQUENCE COLORS");
 
@@ -33,12 +88,11 @@ AW_window *create_seq_colors_window(AW_root *awr, AWT_seq_colors *asc){
     aws->callback     ( AW_POPUP_HELP,(AW_CL)"sequence_colors.hlp" );aws->create_button( "HELP", "HELP" );
     aws->at_newline();
 
-    aws->label_length( 6 );
+    aws->label_length( 6 ); // @@@ has no effect
     aws->button_length( 6 );
     int set;
     int elem;
 
-    awr->awar_int(AWAR_SEQ_NAME_SELECTOR)->add_callback((AW_RCB)awt_awar_changed_cb,(AW_CL)asc,0);
     aws->label("Select");
     aws->create_toggle_field(AWAR_SEQ_NAME_SELECTOR,1);
     for (set = 0; set < AWT_SEQ_COLORS_MAX_SET;set++){
@@ -65,11 +119,9 @@ AW_window *create_seq_colors_window(AW_root *awr, AWT_seq_colors *asc){
             int my_elem = elem+big_columns*AWT_SEQ_COLORS_MAX_ELEMS/2;
 
             sprintf(buf,AWAR_SEQ_NAME_STRINGS_TEMPLATE,my_elem);
-            awr->awar_string(buf);
             aws->create_input_field(buf,4);
             for (set = 0; set < AWT_SEQ_COLORS_MAX_SET;set++){
                 sprintf(buf,AWAR_SEQ_NAME_TEMPLATE,set,my_elem);
-                awr->awar_string(buf);
                 aws->create_input_field(buf,4);
             }
             if (big_columns == 0)  {
@@ -83,8 +135,6 @@ AW_window *create_seq_colors_window(AW_root *awr, AWT_seq_colors *asc){
     return (AW_window *)aws;
 }
 
-
-
 void AWT_seq_colors::run_cb(){
     if (callback && aww) callback(aww,cd1,cd2);
 }
@@ -95,28 +145,27 @@ void AWT_seq_colors::reload(){
     memset(char_2_gc,base_gc,256);
     int i;
     for (i=0;i<256;i++) char_2_char[i] = i;
-    long set = GBT_read_int2(gb_def,AWAR_SEQ_NAME_SELECTOR,1);
+    long set = GBT_read_int2(gb_def,AWAR_SEQ_NAME_SELECTOR, default_set);
     if (set <0 || set >= AWT_SEQ_COLORS_MAX_SET) return;
     int elem;
     int s2;
     for (elem = 0; elem < AWT_SEQ_COLORS_MAX_ELEMS; elem++){
         sprintf(buf,AWAR_SEQ_NAME_STRINGS_TEMPLATE,elem);
-        unsigned char *sc = (unsigned char *)GBT_read_string2(gb_def,buf,GBS_global_string("%c%c",'a'+elem,'A'+elem));
+        unsigned char *sc = (unsigned char *)GBT_read_string2(gb_def,buf,default_characters(elem));
         if (!cbexists){
             GBDATA *gb_ne = GB_search(gb_def,buf,GB_STRING);
             GB_add_callback(gb_ne,GB_CB_CHANGED,awt_awar_changed_cb,(int *)this);
             for (s2=0;s2 <  AWT_SEQ_COLORS_MAX_SET; s2++){
                 sprintf(buf,AWAR_SEQ_NAME_TEMPLATE,s2,elem);
-                GBT_read_string2(gb_def,buf,"=0");
+                GBT_read_string2(gb_def,buf,default_color(s2, elem));
                 gb_ne = GB_search(gb_def,buf,GB_STRING);
                 GB_add_callback(gb_ne,GB_CB_CHANGED,awt_awar_changed_cb,(int *)this);
             }
         }
         sprintf(buf,AWAR_SEQ_NAME_TEMPLATE,(int)set,elem);
-        char *val = GBT_read_string2(gb_def,buf,"AA");
+        char *val = GBT_read_string(gb_def,buf);
         if (strlen(val) != 2 || val[1] >'9' || val[1] < '0'){
-            aw_message(GB_export_error(
-                                       "Error in Color Lookup Table '%s' is not of type X#",val));
+            aw_message(GB_export_error("Error in Color Lookup Table: '%s' is not of type X#",val));
             delete val;
             delete sc;
             continue;
