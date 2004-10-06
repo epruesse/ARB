034ef8c1d6286bd19c0bf3d3524a0b31b0b66ec1
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-10-06 19:28:53 +0000

    - removed nearly everything - this is meant to be an interface

--- GENOM_IMPORT/GAGenomImport.h
+++ GENOM_IMPORT/GAGenomImport.h
@@ -1,34 +1,6 @@
 #ifndef GAGENOMIMPORT_H
 #define GAGENOMIMPORT_H
 
-#include "../GENOM_IMPORT/GAGenomReferenceEmbl.h"
-#include "../GENOM_IMPORT/GAGenomFeatureTableEmbl.h"
-#include "../GENOM_IMPORT/GAGenomGeneEmbl.h"
-#include "../GENOM_IMPORT/GAGenomGeneLocationEmbl.h"
-#include "../GENOM_IMPORT/GAGenomFeatureTableSourceEmbl.h"
-
-#ifndef _CPP_STRING
-#include <string>
-#endif
-
-#include <arbdbt.h>
-
-namespace gellisary {
-
-class GAGenomImport{
-public:
-	static GB_ERROR executeQuery(GBDATA *, const char *,const char *);
-	static void writeReferenceEmbl(GBDATA *, gellisary::GAGenomReferenceEmbl *);
-	static void writeFeatureTableEmbl(GBDATA *, gellisary::GAGenomFeatureTableEmbl *);
-	static void writeGeneEmbl(GBDATA *, gellisary::GAGenomGeneEmbl *);
-	static bool writeLocationEmbl(GBDATA *, gellisary::GAGenomGeneLocationEmbl *, std::vector<int> *);
-	static void writeByte(GBDATA *, std::string *, int);
-	static void writeSourceEmbl(GBDATA *, gellisary::GAGenomFeatureTableSourceEmbl *);
-	static void writeString(GBDATA *, std::string *, std::string *);
-	static void writeInteger(GBDATA *, std::string *, int);
-	static void writeGenomSequence(GBDATA *, std::string *, const char *);
-};
-
-};
+GB_ERROR GI_importGenomeFile(GBDATA * gb_main, const char * file_name, const char * ali_name);
 
 #endif // GAGENOMIMPORT_H
