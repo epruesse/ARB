3c9fba319d99ef4c8e3c4d0075bc9247f8ec41b4
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-10-07 12:52:15 +0000

    - added std:: - fixed include wrappers

--- PROBE_SET/ps_defs.hxx
+++ PROBE_SET/ps_defs.hxx
@@ -1,16 +1,16 @@
 #ifndef PS_DEFS_HXX
 #define PS_DEFS_HXX
 
-#ifndef __MAP__
+#ifndef _CPP_MAP
 #include <map>
 #endif
-#ifndef __SET__
+#ifndef _CPP_SET
 #include <set>
 #endif
-#ifndef __VECTOR__
+#ifndef _CPP_VECTOR
 #include <vector>
 #endif
-#ifndef __STRING__
+#ifndef _CPP_STRING
 #include <string>
 #endif
 
@@ -19,46 +19,46 @@ typedef int SpeciesID;
 // ----------------------------------------------------------------
 // SpeciesID <-> shortname mapping
 // ----------------------------------------------------------------
-typedef map<string, SpeciesID>     Name2IDMap;
-typedef Name2IDMap::iterator       Name2IDMapIter;
-typedef Name2IDMap::const_iterator Name2IDMapCIter;
-typedef map<SpeciesID,string>      ID2NameMap;
-typedef ID2NameMap::iterator       ID2NameMapIter;
-typedef ID2NameMap::const_iterator ID2NameMapCIter;
+typedef std::map<std::string, SpeciesID> Name2IDMap;
+typedef Name2IDMap::iterator             Name2IDMapIter;
+typedef Name2IDMap::const_iterator       Name2IDMapCIter;
+typedef std::map<SpeciesID,std::string>  ID2NameMap;
+typedef ID2NameMap::iterator             ID2NameMapIter;
+typedef ID2NameMap::const_iterator       ID2NameMapCIter;
 
 // ----------------------------------------------------------------
 // SpeciesID list (as vector)
 // ----------------------------------------------------------------
-typedef vector<SpeciesID>          IDVector;
-typedef IDVector::iterator         IDVectorIter;
-typedef IDVector::const_iterator   IDVectorCIter;
+typedef std::vector<SpeciesID>   IDVector;
+typedef IDVector::iterator       IDVectorIter;
+typedef IDVector::const_iterator IDVectorCIter;
 
 // ----------------------------------------------------------------
 // SpeciesID list (as set)
 // ----------------------------------------------------------------
-typedef set<SpeciesID>             IDSet;
-typedef IDSet::iterator            IDSetIter;
-typedef IDSet::const_iterator      IDSetCIter;
+typedef std::set<SpeciesID>   IDSet;
+typedef IDSet::iterator       IDSetIter;
+typedef IDSet::const_iterator IDSetCIter;
 
 // ----------------------------------------------------------------
 // SpeciesID <-> SpeciesID (as map)
 // ----------------------------------------------------------------
-typedef map<SpeciesID,SpeciesID>   ID2IDMap;
-typedef ID2IDMap::iterator         ID2IDMapIter;
-typedef ID2IDMap::const_iterator   ID2IDMapCIter;
+typedef std::map<SpeciesID,SpeciesID> ID2IDMap;
+typedef ID2IDMap::iterator            ID2IDMapIter;
+typedef ID2IDMap::const_iterator      ID2IDMapCIter;
 
 // ----------------------------------------------------------------
 // SpeciesID <-> SpeciesID (as set)
 // ----------------------------------------------------------------
-typedef pair<SpeciesID,SpeciesID>  ID2IDPair;
-typedef set<ID2IDPair>             ID2IDSet;
-typedef ID2IDSet::iterator         ID2IDSetIter;
-typedef ID2IDSet::const_iterator   ID2IDSetCIter;
+typedef std::pair<SpeciesID,SpeciesID> ID2IDPair;
+typedef std::set<ID2IDPair>            ID2IDSet;
+typedef ID2IDSet::iterator             ID2IDSetIter;
+typedef ID2IDSet::const_iterator       ID2IDSetCIter;
 
 // ----------------------------------------------------------------
 // (SpeciesID,SpeciesID) <-> set of SpeciesID (as map)
 // ----------------------------------------------------------------
-typedef map<ID2IDPair,IDSet>          IDID2IDSetMap;
+typedef std::map<ID2IDPair,IDSet>     IDID2IDSetMap;
 typedef IDID2IDSetMap::iterator       IDID2IDSetMapIter;
 typedef IDID2IDSetMap::const_iterator IDID2IDSetMapCIter;
 
