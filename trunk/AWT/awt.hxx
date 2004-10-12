dad76465b7125fb0bf05c7e0bb9b17a032567d91
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-10-12 19:56:40 +0000

    - added AWT_get_item_id() + AWT_get_item_with_id() - added find_item_by_id - renamed generate_item_name -> generate_item_id

--- trunk/AWT/awt.hxx
+++ trunk/AWT/awt.hxx
@@ -60,13 +60,14 @@ struct ad_item_selector {
     // - for species: AWAR_SPECIES_NAME is changed (item_name = 'species_name')
     // - for genes: AWAR_GENE_NAME and AWAR_SPECIES_NAME are changed (item_name = 'species_name/gene_name')
     void (*update_item_awars)(GBDATA* gb_main, AW_root *aw_root, const char *item_name);
-    char *(*generate_item_name)(GBDATA *gb_main, AW_root *aw_root, GBDATA *gb_item);
+    char *(*generate_item_id)(GBDATA *gb_main, GBDATA *gb_item);
+    GBDATA *(*find_item_by_id)(GBDATA *gb_main, const char *id);
     AW_CB selection_list_rescan_cb;
     int   item_name_length;
 
     const char *change_key_path;
-    const char *item_name;      // "species" or "gene"
-    const char *items_name;     // "species" or "genes"
+    const char *item_name;      // "species" or "gene" or "experiment" or "organism"
+    const char *items_name;     // "species" or "genes" or "experiments" or "organisms"
 
     GBDATA *(*get_first_item_container)(GBDATA *, AW_root *, AWT_QUERY_RANGE); // for species this is normally awt_get_species_data
     GBDATA *(*get_next_item_container)(GBDATA *, AWT_QUERY_RANGE); // for species this is normally a function returning 0
@@ -75,6 +76,9 @@ struct ad_item_selector {
 
     GBDATA *(*get_selected_item)(GBDATA *gb_main, AW_root *aw_root); // searches the currently selected item
 };
+
+char *AWT_get_item_id(ad_item_selector *sel, GBDATA *gb_item);
+GBDATA *AWT_get_item_with_id(ad_item_selector *sel, const char *id);
 
 extern ad_item_selector AWT_species_selector;
 extern ad_item_selector AWT_organism_selector;
