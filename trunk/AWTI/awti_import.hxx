77e669322bbda6ebdb012a35cc0504c864458172
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-11-11 11:52:45 +0000

    - added AWTI_ImportType (differs between old and new flatfile importer)

--- trunk/AWTI/awti_import.hxx
+++ trunk/AWTI/awti_import.hxx
@@ -3,6 +3,13 @@
 
 #define AWAR_READ_GENOM_DB "tmp/import/genom_db"
 
+#if defined(DEVEL_ARTEM)
+typedef enum { IMP_GENOME_FLATFILE, IMP_PLAIN_SEQUENCE } AWTI_ImportType;
+#else
+typedef enum { IMP_GENOME_GENEBANK, IMP_GENOME_EMBL, IMP_PLAIN_SEQUENCE } AWTI_ImportType;
+#endif // DEVEL_ARTEM
+
+
 #define AWTC_RCB(func) void (*func)(AW_root*,AW_CL,AW_CL)
 
 GBDATA *open_AWTC_import_window(AW_root *awr,const char *defname, int do_exit,AWTC_RCB(func), AW_CL cd1, AW_CL cd2);
