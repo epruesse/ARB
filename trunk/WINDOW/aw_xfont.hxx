b1cc758380cbde1a1f29c8fedac166d49c6b5f7c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2004-12-17 19:14:16 +0000

    * WINDOW/AW_xfont.cxx: - added some new fonts

--- trunk/WINDOW/aw_xfont.hxx
+++ trunk/WINDOW/aw_xfont.hxx
@@ -1,39 +1,39 @@
 #ifndef aw_xfont_hxx_included
 #define aw_xfont_hxx_included
 
-#define		NORMAL_FONT	"fixed"
-#define		BOLD_FONT	"8x13bold"
-#define		BUTTON_FONT	"6x13"
+#define         NORMAL_FONT     "fixed"
+#define         BOLD_FONT       "8x13bold"
+#define         BUTTON_FONT     "6x13"
 
 struct xfont {
-    int		    size;	/* size in points */
-    Font	    fid;	/* X font id */
-    const char	   *fname;	/* actual name of X font found */
-    XFontStruct	   *fstruct;	/* X font structure */
-    struct xfont   *next;	/* next in the list */
+    int           size;         /* size in points */
+    Font          fid;          /* X font id */
+    char         *fname;        /* actual name of X font found */
+    XFontStruct  *fstruct;      /* X font structure */
+    struct xfont *next;         /* next in the list */
 };
 
 struct _fstruct {
-    const char	   *name;	/* Postscript font name */
-    int		    xfontnum;	/* template for locating X fonts */
+    const char     *name;       /* Postscript font name */
+    int             xfontnum;   /* template for locating X fonts */
 };
 
 struct _xfstruct {
-    const char	   *templat;	/* templat for locating X fonts */
-    struct xfont   *xfontlist;	/* linked list of X fonts for different point
-				 * sizes */
+    const char     *templat;    /* templat for locating X fonts */
+    struct xfont   *xfontlist;  /* linked list of X fonts for different point
+                                 * sizes */
 };
 
 typedef struct _appres {
-	AW_BOOL		SCALABLEFONTS;	/* hns 5 Nov 91 */
-	AW_BOOL		debug;	/* hns 5 Nov 91 */
-	Display 	*display;
-}		appresStruct, *appresPtr;
+    AW_BOOL  SCALABLEFONTS;     /* hns 5 Nov 91 */
+    AW_BOOL  debug;             /* hns 5 Nov 91 */
+    Display *display;
+}               appresStruct, *appresPtr;
 extern appresStruct appres;
 
 #define DEFAULT               (-1)
 #define DEF_FONTSIZE 12
-#define	NORMAL_FONT	"fixed"
+#define NORMAL_FONT     "fixed"
 
 
 #endif
