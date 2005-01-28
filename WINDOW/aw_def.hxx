8f1da60bb105b26c3d73f16bf2a10428ff54186f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2005-01-28 14:27:31 +0000

    - global definitions (currently only fontsizes)

---
+++ WINDOW/aw_def.hxx
@@ -0,0 +1,34 @@
+// ==================================================================== //
+//                                                                      //
+//   File      : aw_def.hxx                                             //
+//   Purpose   : WINDOW library global defines                          //
+//   Time-stamp: <Thu Jan/20/2005 14:43 MET Coder@ReallySoft.de>        //
+//                                                                      //
+//                                                                      //
+// Coded by Ralf Westram (coder@reallysoft.de) in January 2005          //
+// Copyright Department of Microbiology (Technical University Munich)   //
+//                                                                      //
+// Visit our web site at: http://www.arb-home.de/                       //
+//                                                                      //
+// ==================================================================== //
+
+//  ----------------------------------------------------
+//      This file is located in WINDOW/aw_def.hxx
+//      (AWT/aw_def.hxx is just a link)
+//  ----------------------------------------------------
+
+#ifndef AW_DEF_HXX
+#define AW_DEF_HXX
+
+#define NORMAL_FONT "fixed"
+#define BOLD_FONT   "8x13bold"
+#define BUTTON_FONT "6x13"
+
+#define MIN_FONTSIZE 2
+#define DEF_FONTSIZE 12
+#define MAX_FONTSIZE 50
+
+#else
+#error aw_def.hxx included twice
+#endif // AW_DEF_HXX
+
