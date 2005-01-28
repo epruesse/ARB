f0fd4c3aefeb448bb7a2da2fe4b84323ef76d267
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2005-01-28 14:30:57 +0000

    - AW_GC_Xm::set_font returns the size of the used font - added AW_GC_Xm::get_available_fontsizes()

--- trunk/WINDOW/aw_commn.hxx
+++ trunk/WINDOW/aw_commn.hxx
@@ -7,49 +7,52 @@
 
 class AW_GC_Xm {
 public:
-	GC			gc;
-	class AW_common		*common;
-	XFontStruct		curfont;
-	short			width_of_chars[256];
-	short			ascent_of_chars[256];
-	short			descent_of_chars[256];
-	AW_font_information	fontinfo;
-	short			line_width;
-	AW_linestyle 		style;
-	short			color;
+    GC                   gc;
+    class AW_common     *common;
+    XFontStruct          curfont;
+    short                width_of_chars[256];
+    short                ascent_of_chars[256];
+    short                descent_of_chars[256];
+    AW_font_information  fontinfo;
+    short                line_width;
+    AW_linestyle         style;
+    short                color;
 
-	short			fontsize;
-	AW_font			fontnr;
+    short   fontsize;
+    AW_font fontnr;
 
-	AW_function function;
-	AW_pos			grey_level;
-	AW_GC_Xm(class AW_common	*common);
-	~AW_GC_Xm();
-	void set_fill(AW_grey_level grey_level);								// <0 dont fill	 0.0 white 1.0 black
-	void set_font(AW_font font_nr, int size);
-	void set_lineattributes(AW_pos width, AW_linestyle style);
-	void set_function(AW_function function);
-	void set_foreground_color(unsigned long color);
-	void set_background_color(unsigned long color);
+    AW_function function;
+    AW_pos      grey_level;
+    
+    AW_GC_Xm(class AW_common *common);
+    ~AW_GC_Xm();
+    void set_fill(AW_grey_level grey_level); // <0 dont fill  0.0 white 1.0 black
+    void set_font(AW_font font_nr, int size, int *found_size);
+    void set_lineattributes(AW_pos width, AW_linestyle style);
+    void set_function(AW_function function);
+    void set_foreground_color(unsigned long color);
+    void set_background_color(unsigned long color);
+
+    int get_available_fontsizes(AW_font font_nr, int *available_sizes);
 };
 
 
 class AW_common {
-	public:
-	AW_common(AW_window *aww, AW_area area,	Display *display_in,XID window_id_in,unsigned long *fcolors,unsigned int **dcolors);
-	unsigned long *frame_colors;
-	unsigned long **data_colors;
-	AW_root		*root;
-	AW_rectangle	screen;
-	int		screen_x_offset;
-	int		screen_y_offset;
-	AW_GC_Xm	**gcs;
-	int		ngcs;
-	Display		*display;
-	XID		window_id;
+public:
+    AW_common(AW_window *aww, AW_area area, Display *display_in,XID window_id_in,unsigned long *fcolors,unsigned int **dcolors);
+    
+    unsigned long  *frame_colors;
+    unsigned long **data_colors;
+    AW_root        *root;
+    AW_rectangle    screen;
+    int             screen_x_offset;
+    int             screen_y_offset;
+    AW_GC_Xm      **gcs;
+    int             ngcs;
+    Display        *display;
+    XID             window_id;
 
-	AW_pos	x_alignment(AW_pos x_pos,AW_pos x_size,AW_pos alignment)
-		{ return x_pos- x_size*alignment; };
+    AW_pos x_alignment(AW_pos x_pos,AW_pos x_size,AW_pos alignment) { return x_pos- x_size*alignment; };
 };
 
 
