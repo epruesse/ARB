9d4b184b56e974af20d683bb85bf09c2207c2cf3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2005-03-17 15:28:53 +0000

    - perl script to change embl feature table for more simple   ift-parsing

---
+++ PERL_SCRIPTS/IFTHELP/embl_gen_long_features.pl
@@ -0,0 +1,64 @@
+#!perl
+#  =============================================================== #
+#                                                                  #
+#    File      : embl_gen_long_features.pl                         #
+#    Purpose   : modifies embl feature table for easy scanning     #
+#                with ARBs ift                                     #
+#    Time-stamp: <Thu Mar/17/2005 16:07 MET Coder@ReallySoft.de>   #
+#                                                                  #
+#    Coded by Ralf Westram (coder@reallysoft.de) in March 2005     #
+#    Institute of Microbiology (Technical University Munich)       #
+#    http://www.arb-home.de/                                       #
+#                                                                  #
+#  =============================================================== #
+
+use strict;
+use warnings;
+
+my $last_feature = undef;
+my $line_number = 0;
+my $line_to_print = undef;
+
+eval {
+  foreach (<>) {
+    $line_number++;
+    if (/^FT   /) {
+      chomp;
+      my $rest = $';
+      if ($rest =~ /^([a-zA-Z0-9_]+)([ ]+)([^ ].*)$/) { # new feature
+        my ($name,$spaces,$content) = ($1,$2,$3);
+        $last_feature = $name.$spaces;
+        if (defined $line_to_print) { print $line_to_print."\n"; $line_to_print=undef; }
+        print "FT   $last_feature$content\n";
+      }
+      else { # continue last feature
+        if (defined $last_feature) {
+          if ($rest =~ /^                (.*)$/) {
+            my $content = $1;
+            if ($content =~ /^\//) { # start of new sub-entry
+              if (defined $line_to_print) { print $line_to_print."\n"; $line_to_print=undef; }
+              $line_to_print = "FTx  $last_feature$content";
+            }
+            else {
+              $line_to_print .= "$content"; # append to previously started sub entry
+            }
+          }
+          else {
+            die "Expected some content behind 'FT'";
+          }
+        }
+        else {
+          die "Expected start of feature (e.g. 'FT   bla')\n";
+        }
+      }
+    }
+    else {
+      if (defined $line_to_print) { print $line_to_print."\n"; $line_to_print=undef; }
+      print $_;
+    }
+  }
+  if (defined $line_to_print) { print $line_to_print."\n"; $line_to_print=undef; }
+};
+if ($@) {
+  die "$@ in line $line_number of inputfile\n";
+}
