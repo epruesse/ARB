741f85494aa6a9a283116a9e4fde42dc1343f4fc
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2005-05-24 11:32:46 +0000

    - former AWTC code

---
+++ SL/AW_NAME/AW_rename.hxx
@@ -0,0 +1,24 @@
+#ifndef AW_RENAME_HXX
+#define AW_RENAME_HXX
+
+
+#define AWT_RENAME_USE_ADVICE "awt_rename/use_advice"
+#define AWT_RENAME_SAVE_DATA "awt_rename/save_data"
+#define NAMES_FILE_LOCATION "$(ARBHOME)/lib/nas/names.dat"
+
+void       AWTC_create_rename_variables(AW_root *root,AW_default db1);
+AW_window *AWTC_create_rename_window(AW_root *root, AW_CL gb_main);
+void       awt_rename_cb(AW_window *aww,GBDATA *gb_main);
+
+AW_window *create_awtc_names_admin_window(AW_root *aw_root);
+
+GB_ERROR AWTC_pars_names(GBDATA *gb_main, int update_status = 0);
+GB_ERROR AWTC_generate_one_name(GBDATA *gb_main, const char *full_name, const char *acc, char*& new_name, bool openstatus);
+GB_ERROR AWTC_recreate_name(GBDATA *gb_main, bool update_status);
+
+char *AWTC_makeUniqueShortName(const char *prefix, GBDATA *gb_species_data);
+char *AWTC_generate_random_name(GBDATA *gb_species_data);
+
+#else
+#error AW_rename.hxx included twice
+#endif
