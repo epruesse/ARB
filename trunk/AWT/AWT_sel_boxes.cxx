aea882a3c663977b88f66b52a5765c1a36c6ceeb
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2005-05-25 17:10:31 +0000

    - added AWT_popup_select_species_field_window (former AWT_create_select_nds_window) - moved macro recording functions to AWT_macro.cxx

--- trunk/AWT/AWT_sel_boxes.cxx
+++ trunk/AWT/AWT_sel_boxes.cxx
@@ -16,10 +16,12 @@
 #include <aw_global.hxx>
 #include "awt.hxx"
 #include "awtlocal.hxx"
+#include "awt_sel_boxes.hxx"
+#include "awt_changekey.hxx"
 
 // ******************** selection boxes on alignments ********************
 
-void awt_create_selection_list_on_ad_cb(GBDATA *dummy, struct adawcbstruct *cbs)
+static void awt_create_selection_list_on_ad_cb(GBDATA *dummy, struct adawcbstruct *cbs)
 {
     GBDATA *gb_alignment;
     GBDATA *gb_alignment_name;
@@ -740,113 +742,32 @@ void awt_edit(AW_root */*awr*/, const char *path, int /*x*/, int /*y*/, const ch
 }
 
 
-// ---------------
-//      MACROS
-// ---------------
-
-#define AWAR_MACRO_BASE                 "tmp/macro"
-#define AWAR_MACRO_RECORDING_MACRO_TEXT AWAR_MACRO_BASE"/button_label"
-
-void awt_delete_macro_cb(AW_window *aww){
-    AW_root *awr   = aww->get_root();
-    char    *mn    = awt_get_selected_fullname(awr, AWAR_MACRO_BASE);
-    int      error = GB_unlink(mn);
-
-    if (error) aw_message(GB_export_IO_error("deleting", mn));
-    awt_refresh_selection_box(awr, AWAR_MACRO_BASE);
-    free(mn);
-}
-
-
-
-void awt_exec_macro_cb(AW_window *aww){
-    AW_root  *awr   = aww->get_root();
-    char     *mn    = awt_get_selected_fullname(awr, AWAR_MACRO_BASE);
-    GB_ERROR  error = awr->execute_macro(mn);
-
-    if (error) aw_message(error);
-    free(mn);
-}
-
-void awt_start_macro_cb(AW_window *aww,const char *application_name_for_macros){
-    static int toggle = 0;
-
-    AW_root  *awr = aww->get_root();
-    //     char     *mn  = awr->awar(AWAR_MACRO_FILENAME)->read_string();
-    GB_ERROR  error;
-
-    if (!toggle){
-        char *sac = GBS_global_string_copy("%s/%s",aww->window_defaults_name,AWAR_MACRO_RECORDING_MACRO_TEXT);
-        char *mn  = awt_get_selected_fullname(awr, AWAR_MACRO_BASE);
-        error     = awr->start_macro_recording(mn,application_name_for_macros,sac);
-        free(mn);
-        free(sac);
-        if (!error){
-            awr->awar(AWAR_MACRO_RECORDING_MACRO_TEXT)->write_string("STOP");
-            toggle = 1;
-        }
-    }
-    else {
-        error = awr->stop_macro_recording();
-        awt_refresh_selection_box(awr, AWAR_MACRO_BASE);
-        awr->awar(AWAR_MACRO_RECORDING_MACRO_TEXT)->write_string("RECORD");
-        toggle = 0;
-    }
-    if (error) aw_message(error);
-}
-
-void awt_stop_macro_cb(AW_window *aww){
-    AW_root  *awr   = aww->get_root();
-    GB_ERROR  error = awr->stop_macro_recording();
-
-    awt_refresh_selection_box(awr, AWAR_MACRO_BASE);
-    if (error) aw_message(error);
-}
-
-void awt_edit_macro_cb(AW_window *aww){
-    //     char *mn = aww->get_root()->awar(AWAR_MACRO_FILENAME)->read_string();
-    //     char *path = 0;
-    //     if (mn[0] == '/'){
-    //         path = strdup(mn);
-    //     }else{
-    //         path = GBS_global_string_copy("%s/%s",GB_getenvARBMACROHOME(),mn);
-    //     }
-    char *path = awt_get_selected_fullname(aww->get_root(), AWAR_MACRO_BASE);
-    GB_edit(path);
-    free(path);
-    //     delete mn;
-}
-
-AW_window *awt_open_macro_window(AW_root *aw_root,const char *application_id){
+void AWT_popup_select_species_field_window(AW_window *aww, AW_CL cl_awar_name, AW_CL cl_gb_main)
+{
     static AW_window_simple *aws = 0;
-    if (aws) return aws;
-    aws = new AW_window_simple;
-    aws->init( aw_root, "MACROS", "MACROS");
-    aws->load_xfig("macro_select.fig");
 
-    aw_create_selection_box_awars(aw_root, AWAR_MACRO_BASE, ".", ".amc", "");
+    // everytime map selection awar to latest user awar:
+    AW_root    *aw_root   = aww->get_root();
+    const char *awar_name = (const char *)cl_awar_name;
+    aw_root->awar("tmp/viewkeys/key_text_select")->map(awar_name);
 
-    aw_root->awar_string(AWAR_MACRO_RECORDING_MACRO_TEXT,"RECORD");
+    if (!aws) {
+        aws = new AW_window_simple;
+        
+        aws->init( aw_root, "SELECT_SPECIES_FIELD", "Select species field");
+        aws->load_xfig("awt/nds_sel.fig");
+        aws->button_length(13);
 
-    aws->at("close");aws->callback((AW_CB0)AW_POPDOWN);
-    aws->create_button("CLOSE", "CLOSE","C");
+        aws->callback( AW_POPDOWN);
+        aws->at("close");
+        aws->create_button("CLOSE", "CLOSE","C");
 
-    aws->at("help");aws->callback(AW_POPUP_HELP,(AW_CL)"macro.hlp");
-    aws->create_button("HELP", "HELP");
-
-    aws->at("start");aws->callback((AW_CB1)awt_start_macro_cb,(AW_CL)application_id);
-    aws->create_button(0, AWAR_MACRO_RECORDING_MACRO_TEXT);
-
-    aws->at("delete");aws->callback(awt_delete_macro_cb);
-    aws->create_button("DELETE", "DELETE");
-
-    aws->at("edit");aws->callback(awt_edit_macro_cb);
-    aws->create_button("EDIT", "EDIT");
-
-    aws->at("exec");aws->callback(awt_exec_macro_cb);
-    aws->create_button("EXECUTE", "EXECUTE");
-
-    awt_create_selection_box((AW_window *)aws,AWAR_MACRO_BASE,"","ARBMACROHOME^ARBMACRO");
-
-    return (AW_window *)aws;
+        awt_create_selection_list_on_scandb((GBDATA *)cl_gb_main,
+                                            aws,
+                                            "tmp/viewkeys/key_text_select",
+                                            AWT_NDS_FILTER,
+                                            "scandb","rescandb", &AWT_species_selector, 20, 10);
+    }
+    aws->show();
 }
+
