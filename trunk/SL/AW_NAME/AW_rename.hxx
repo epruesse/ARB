22e144a51d1509675ca323f9e8d9ebe0bdec253b
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2006-08-11 09:06:14 +0000

    - changed prototypes for name generating functions - UniqueNameDetector now global

--- trunk/SL/AW_NAME/AW_rename.hxx
+++ trunk/SL/AW_NAME/AW_rename.hxx
@@ -13,13 +13,28 @@ void       awt_rename_cb(AW_window *aww,GBDATA *gb_main);
 AW_window *create_awtc_names_admin_window(AW_root *aw_root);
 
 GB_ERROR AWTC_pars_names(GBDATA *gb_main, int update_status = 0);
-GB_ERROR AWTC_generate_one_name(GBDATA *gb_main, const char *full_name, const char *acc, char*& new_name, bool openstatus);
+GB_ERROR AWTC_generate_one_name(GBDATA *gb_main, const char *full_name, const char *acc, char*& new_name, bool openstatus, bool showstatus);
 GB_ERROR AWTC_recreate_name(GBDATA *gb_main, bool update_status);
 
-char *AWTC_makeUniqueShortName(const char *prefix, GBDATA *gb_species_data);
-char *AWTC_generate_random_name(GBDATA *gb_species_data);
+class UniqueNameDetector {
+    // Note: If you add new items to the DB while one instance of this class exists,
+    //       you have to call add_name() for these new species!
+
+    GB_HASH *hash;
+public:
+    UniqueNameDetector(GBDATA *gb_item_data, int additionalEntries = 0);
+    ~UniqueNameDetector();
+
+    bool name_known(const char *name) { return GBS_read_hash(hash, name) == 1; }
+    void add_name(const char *name) { GBS_write_hash(hash, name, 1); }
+};
+
+char *AWTC_makeUniqueShortName(const char *prefix, UniqueNameDetector& existingNames);
+char *AWTC_generate_random_name(UniqueNameDetector& existingNames);
 
 struct PersistantNameServerConnection {
+    // create a PersistantNameServerConnection instance while calling AWTC_generate_one_name
+    // to avoid repeated re-connections to name server
     bool dummy;
     PersistantNameServerConnection();
     ~PersistantNameServerConnection();
