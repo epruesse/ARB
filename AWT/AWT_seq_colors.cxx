676cc61f5f590435062a1eeae305868175a28416
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2006-11-24 15:10:39 +0000

    - added defaults for new sequence color elements (stop codon and gaps) - removed duplicated code - changed default set for Proteins

--- AWT/AWT_seq_colors.cxx
+++ AWT/AWT_seq_colors.cxx
@@ -15,15 +15,16 @@
 #include "awt.hxx"
 
 
-static int default_set = 0; // number of default set
+static int default_NUC_set = 0;     // number of default nucleotide set
+static int default_AMI_set = 3;     // number of default amino acid set
+
 static const char *default_sets[AWT_SEQ_COLORS_MAX_SET] = {
-    //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-    "=2=0=3=0=0=0=4=0=0=0=0=0=0=6=0=0=0=0=0=5=5=0=0=0=0=0", // A, C, G, TU and N in 5 colors
-    "R2=0Y3=0=0=0R2=0=0=0=0=0=0=0=0=0=0=2=0Y3Y3=0=0=0=3=0", // AG and CTU in 2 colors
-    "=0=5=0=5=7=7=0=5=7=7=3=7=3=9=7=7=7=3=3=0=0=5=3=7=3=7", // ambiguity
-    // "=2=4=7=4=4=6=2=5=3=0=5=3=3=4=0=2=4=5=2=2=0=3=6=0=6=4", // Protein colors (old defaults)
-    "=7=0=7=8=2=9=8=9=3=0=2=3=7=8=0=8=2=2=2=2=0=3=9=0=9=0", // Protein colors
-    "=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0",
+    //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z * -
+    "=2=0=3=0=0=0=4=0=0=0=0=0=0=6=0=0=0=0=0=5=5=0=0=0=0=0=0=6", // A, C, G, TU and N in 5 colors
+    "R2=0Y3=0=0=0R2=0=0=0=0=0=0=0=0=0=0=2=0Y3Y3=0=0=0=3=0=0=6", // AG and CTU in 2 colors
+    "=0=5=0=5=7=7=0=5=7=7=3=7=3=9=7=7=7=3=3=0=0=5=3=7=3=7=0=6", // ambiguity
+    "=7=0=7=8=2=9=8=9=3=0=2=3=7=8=0=8=2=2=2=2=0=3=9=6=9=0=0=6", // Protein colors
+    "=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=6",
 };
 
 static bool seq_color_awars_created = false;
@@ -33,8 +34,18 @@ static bool seq_color_awars_created = false;
 static const char *default_characters(int elem) {
     static char result[3] = "xX";
 
-    result[0] = 'a'+elem;
-    result[1] = 'A'+elem;
+    if (elem<26) { //  first 26 elements (0-25) are characters 
+        result[0] = 'a'+elem;
+        result[1] = 'A'+elem;
+    }
+    else if (elem == 26) { // stop codon
+        result[0] = '*';
+        result[1] = 0;
+    }
+    else if (elem == 27) { // gaps
+        result[0] = '-';
+        result[1] = '.';
+    }
 
     return result;
 }
@@ -57,8 +68,9 @@ static void awt_awar_changed_cb(GBDATA *, int *cl, GB_CB_TYPE){
 static void create_seq_color_awars(AW_root *awr, AWT_seq_colors *asc) {
     awt_assert(!seq_color_awars_created);
 
-    awr->awar_int(AWAR_SEQ_NAME_SELECTOR_NA, default_set, AW_ROOT_DEFAULT)->add_callback((AW_RCB)awt_awar_changed_cb,(AW_CL)asc,0);;
-    awr->awar_int(AWAR_SEQ_NAME_SELECTOR_AA, default_set, AW_ROOT_DEFAULT)->add_callback((AW_RCB)awt_awar_changed_cb,(AW_CL)asc,0);; //ykadi
+    awr->awar_int(AWAR_SEQ_NAME_SELECTOR_NA, default_NUC_set, AW_ROOT_DEFAULT)->add_callback((AW_RCB)awt_awar_changed_cb, (AW_CL)asc, 0);
+    awr->awar_int(AWAR_SEQ_NAME_SELECTOR_AA, default_AMI_set, AW_ROOT_DEFAULT)->add_callback((AW_RCB)awt_awar_changed_cb, (AW_CL)asc, 0);
+    
     for (int elem = 0; elem<AWT_SEQ_COLORS_MAX_ELEMS; ++elem) {
         const char *awar_name = GBS_global_string(AWAR_SEQ_NAME_STRINGS_TEMPLATE, elem);
         awr->awar_string(awar_name, default_characters(elem));
@@ -92,28 +104,27 @@ AW_window *create_seq_colors_window(AW_root *awr, AWT_seq_colors *asc){
     //    aws->label_length( 6 ); // @@@ has no effect
     //    aws->button_length( 6 );
     int set;
-    int elem;
 
-    aws->label("Select Colors For Nucleotides (NA) :");
-    aws->create_toggle_field(AWAR_SEQ_NAME_SELECTOR_NA,1);
-    for (set = 0; set < AWT_SEQ_COLORS_MAX_SET;set++){
-        sprintf(buf,"S_%i",set);
-        aws->insert_toggle( buf," ",set );
+    for (int seqType=0; seqType<2; seqType++) {
+        if (seqType==0) {
+            aws->label("Select Colors For Nucleotides (NA) :");
+            aws->create_toggle_field(AWAR_SEQ_NAME_SELECTOR_NA,1);
+        }
+        else {
+            aws->label("Select Colors For Amino Acids (AA) :");
+            aws->create_toggle_field(AWAR_SEQ_NAME_SELECTOR_AA,1);
+        }
+        
+        for (set = 0; set < AWT_SEQ_COLORS_MAX_SET;set++){
+            sprintf(buf,"S_%i",set);
+            aws->insert_toggle( buf," ",set );
+        }
+        aws->update_toggle_field();
+        aws->at_newline();
     }
-    aws->update_toggle_field();
-    aws->at_newline();
 
-    aws->label("Select Colors For Amino Acids (AA) :");
-    aws->create_toggle_field(AWAR_SEQ_NAME_SELECTOR_AA,1);
-    for (set = 0; set < AWT_SEQ_COLORS_MAX_SET;set++){
-        sprintf(buf,"S_%i",set);
-        aws->insert_toggle( buf," ",set );
-    }
-    aws->update_toggle_field();
-    aws->at_newline();
-
-     aws->label_length( 6 ); 
-     aws->button_length( 6 );
+    aws->label_length( 6 ); 
+    aws->button_length( 6 );
  
     for (int big_columns = 0; big_columns <= 1; ++big_columns) {
         aws->create_button(0,"Char");
@@ -127,7 +138,7 @@ AW_window *create_seq_colors_window(AW_root *awr, AWT_seq_colors *asc){
     aws->at_newline();
     aws->auto_space(2,2);
 
-    for (elem = 0; elem < (AWT_SEQ_COLORS_MAX_ELEMS/2); elem++){
+    for (int elem = 0; elem < (AWT_SEQ_COLORS_MAX_ELEMS/2); elem++){
         for (int big_columns = 0; big_columns <= 1; ++big_columns) {
             int my_elem = elem+big_columns*AWT_SEQ_COLORS_MAX_ELEMS/2;
 
@@ -153,60 +164,32 @@ void AWT_seq_colors::run_cb(){
 }
 
 void AWT_seq_colors::reload(){
-    char buf[256];
+    char           buf[256];
     GB_transaction dummy(gb_def);
-    memset(char_2_gc,base_gc,256);
-    int i;
-    for (i=0;i<256;i++) char_2_char[i] = i;
-    long set = GBT_read_int2(gb_def,AWAR_SEQ_NAME_SELECTOR_NA, default_set);
-    if (set <0 || set >= AWT_SEQ_COLORS_MAX_SET) return;
-    int elem;
-    int s2;
-    for (elem = 0; elem < AWT_SEQ_COLORS_MAX_ELEMS; elem++){
-        sprintf(buf,AWAR_SEQ_NAME_STRINGS_TEMPLATE,elem);
-        unsigned char *sc = (unsigned char *)GBT_read_string2(gb_def,buf,default_characters(elem));
-        if (!cbexists){
-            GBDATA *gb_ne = GB_search(gb_def,buf,GB_STRING);
-            GB_add_callback(gb_ne,GB_CB_CHANGED,awt_awar_changed_cb,(int *)this);
-            for (s2=0;s2 <  AWT_SEQ_COLORS_MAX_SET; s2++){
-                sprintf(buf,AWAR_SEQ_NAME_TEMPLATE,s2,elem);
-                GBT_read_string2(gb_def,buf,default_color(s2, elem));
-                gb_ne = GB_search(gb_def,buf,GB_STRING);
-                GB_add_callback(gb_ne,GB_CB_CHANGED,awt_awar_changed_cb,(int *)this);
-            }
-        }
-        sprintf(buf,AWAR_SEQ_NAME_TEMPLATE,(int)set,elem);
-        char *val = GBT_read_string(gb_def,buf);
-        if (strlen(val) != 2 || val[1] >'9' || val[1] < '0'){
-            aw_message(GB_export_error("Error in Color Lookup Table: '%s' is not of type X#",val));
-            delete val;
-            delete sc;
-            continue;
-        }
-        for (i=0;sc[i];i++){
-            char_2_gc[sc[i]] = val[1]-'0' + base_gc;
-            if (val[0] != '='){
-                char_2_char[sc[i]] = val[0];
-            }
-        }
-        free(val);
-        free(sc);
+    int  i;
+
+    for (i=0;i<256;i++) {
+        char_2_gc[i]   = char_2_gc_aa[i]   = base_gc;
+        char_2_char[i] = char_2_char_aa[i] = i;
     }
-    {
-        memset(char_2_gc_aa,base_gc,256);
-        int i;
-        for (i=0;i<256;i++) char_2_char_aa[i] = i;
-        long set = GBT_read_int2(gb_def,AWAR_SEQ_NAME_SELECTOR_AA, default_set);
-        if (set <0 || set >= AWT_SEQ_COLORS_MAX_SET) return;
-        int elem;
-        int s2;
-        for (elem = 0; elem < AWT_SEQ_COLORS_MAX_ELEMS; elem++){
+
+    const char *selector_awar[2] = { AWAR_SEQ_NAME_SELECTOR_NA, AWAR_SEQ_NAME_SELECTOR_AA };
+
+    for (int selector = 0; selector<2; selector++) {
+        long def_set = selector == 0 ? default_NUC_set : default_AMI_set;
+        long set     = GBT_read_int2(gb_def, selector_awar[selector], def_set);
+
+        if (set < 0 || set >= AWT_SEQ_COLORS_MAX_SET) {
+            set = def_set;
+        }
+
+        for (int elem = 0; elem < AWT_SEQ_COLORS_MAX_ELEMS; elem++){
             sprintf(buf,AWAR_SEQ_NAME_STRINGS_TEMPLATE,elem);
             unsigned char *sc = (unsigned char *)GBT_read_string2(gb_def,buf,default_characters(elem));
-            if (!cbexists){
+            if (!cbexists) {
                 GBDATA *gb_ne = GB_search(gb_def,buf,GB_STRING);
                 GB_add_callback(gb_ne,GB_CB_CHANGED,awt_awar_changed_cb,(int *)this);
-                for (s2=0;s2 <  AWT_SEQ_COLORS_MAX_SET; s2++){
+                for (int s2=0; s2<AWT_SEQ_COLORS_MAX_SET; s2++){
                     sprintf(buf,AWAR_SEQ_NAME_TEMPLATE,s2,elem);
                     GBT_read_string2(gb_def,buf,default_color(s2, elem));
                     gb_ne = GB_search(gb_def,buf,GB_STRING);
@@ -221,16 +204,26 @@ void AWT_seq_colors::reload(){
                 delete sc;
                 continue;
             }
-            for (i=0;sc[i];i++){
-                char_2_gc_aa[sc[i]] = val[1]-'0' + base_gc;
-                if (val[0] != '='){
-                    char_2_char_aa[sc[i]] = val[0];
+
+            if (selector == 0) { // Nucleotide colors
+                for (i=0;sc[i];i++) {
+                    char_2_gc[sc[i]] = val[1]-'0' + base_gc;
+                    if (val[0] != '=') char_2_char[sc[i]] = val[0];
                 }
             }
+            else {
+                for (i=0;sc[i];i++) {
+                    char_2_gc_aa[sc[i]] = val[1]-'0' + base_gc;
+                    if (val[0] != '=') char_2_char_aa[sc[i]] = val[0];
+                }                    
+            }
+
             free(val);
             free(sc);
         }
     }
+}
+
     cbexists = 1;
     run_cb();
 }
