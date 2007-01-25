6a03d3052ce8186dce5a18248b9e47d96124499f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-01-25 15:28:53 +0000

    - added awars defs for export

--- AWTI/awti_exp_local.hxx
+++ AWTI/awti_exp_local.hxx
@@ -1,7 +1,14 @@
-#define AWAR_EXPORT_FILE "tmp/export_db/file"
-#define AWAR_EXPORT_FORM "tmp/export/form"
-#define AWAR_EXPORT_ALI "tmp/export/alignment"
+#define AWAR_EXPORT_FILE           "tmp/export_db/file"
+#define AWAR_EXPORT_FORM           "export/form"
+#define AWAR_EXPORT_ALI            "tmp/export/alignment"
 #define AWAR_EXPORT_MULTIPLE_FILES "tmp/export/multiple_files"
+#define AWAR_EXPORT_MARKED         "export/marked"
+#define AWAR_EXPORT_COMPRESS       "export/compress"
+#define AWAR_EXPORT_FILTER_PREFIX  "export/filter"
+#define AWAR_EXPORT_FILTER_NAME    AWAR_EXPORT_FILTER_PREFIX "/name"
+#define AWAR_EXPORT_FILTER_FILTER  AWAR_EXPORT_FILTER_PREFIX "/filter"
+#define AWAR_EXPORT_FILTER_ALI     AWAR_EXPORT_FILTER_PREFIX "/alignment"
+#define AWAR_EXPORT_CUTSTOP        "export/cutstop"
 
 struct export_format_struct {
 	char *system;
