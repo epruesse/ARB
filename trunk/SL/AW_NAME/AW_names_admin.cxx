3f5cefc5001eea71a722d2f59868b0ab24aa209e
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-03-09 20:31:17 +0000

    - added namesFilename(): give the name of the current names.dat - use namesFilename - config field for additional nameserver field.   On change -> instantly test nameserver

--- trunk/SL/AW_NAME/AW_names_admin.cxx
+++ trunk/SL/AW_NAME/AW_names_admin.cxx
@@ -5,43 +5,68 @@
 #include <aw_device.hxx>
 #include <aw_window.hxx>
 #include <awt.hxx>
+#include <awt_advice.hxx>
 #include "AW_rename.hxx"
 
-void awtc_delete_names_file(AW_window *aws){
+static char *namesFilename(AW_CL cl_gb_main) {
+    GBDATA     *gb_main = (GBDATA*)cl_gb_main;
+    char       *path    = GBS_eval_env("$(ARBHOME)/lib/nas");
+    char       *fullname;
+    const char *field   = AW_get_nameserver_addid(gb_main);
+
+    if (field[0]) fullname = GBS_global_string_copy("%s/names_%s.dat", path, field);
+    else fullname          = GBS_global_string_copy("%s/names.dat", path);
+
+    free(path);
+
+    return fullname;
+}
+
+void awtc_delete_names_file(AW_window *aws, AW_CL cl_gb_main) {
     AWUSE(aws);
-    char *path = GBS_eval_env(NAMES_FILE_LOCATION);
-    char *newpath = GBS_string_eval(path,"*=*%",0);
-    GB_ERROR error = GB_rename_file(path,newpath);
+    char     *path    = namesFilename(cl_gb_main);
+    char     *newpath = GBS_string_eval(path,"*=*%",0);
+    GB_ERROR  error   = GB_rename_file(path,newpath);
     if (error) aw_message(error);
-    delete path;
-    delete newpath;
+    free(newpath);
+    free(path);
 }
 
-void awtc_edit_names_file(AW_window *aws){
-    char *path = GBS_eval_env(NAMES_FILE_LOCATION);
+void awtc_edit_names_file(AW_window *aws, AW_CL cl_gb_main){
+    char *path = namesFilename(cl_gb_main);
     awt_edit(aws->get_root(),path,1000,700);
-    delete path;
+    free(path);
 }
 
-void awtc_remove_arb_acc(AW_window *aws){
+void awtc_remove_arb_acc(AW_window *aws, AW_CL cl_gb_main){
     AWUSE(aws);
-    char *path = GBS_eval_env(NAMES_FILE_LOCATION);
+    char *path    = namesFilename(cl_gb_main);
     char *newpath = GBS_string_eval(path,"*=*%",0);
-    char command[1024];
-    sprintf(command,"cp %s %s;arb_replace -l '*ACC {}*=:*ACC {TUM_*=:*ACC {ARB_*=' %s",
+    char  command[1024];
+    sprintf(command,
+            "cp %s %s;arb_replace -l '*ACC {}*=:*ACC {TUM_*=:*ACC {ARB_*=' %s",
             path, newpath, path);
-    delete path;
-    delete newpath;
+    free(newpath);
+    free(path);
 }
 
+static void addid_changed_cb(AW_root *awr, AW_CL cl_gb_main) {
+    GBDATA   *gb_main = (GBDATA*)cl_gb_main;
+    GB_ERROR  error   = AW_test_nameserver(gb_main);
 
+    if (error) aw_message(error);
+    else AWT_advice("Calling 'Species/Generate New Names' is highly recommended", AWT_ADVICE_TOGGLE|AWT_ADVICE_HELP, 0, "namesadmin.hlp");
+}
 
-AW_window *create_awtc_names_admin_window( AW_root *root)  {
+void AW_create_namesadmin_awars(AW_root *awr, GBDATA *gb_main) {
+    awr->awar_string(AWAR_NAMESERVER_ADDID, "", gb_main);
+}
+
+AW_window *AW_create_namesadmin_window(AW_root *root, AW_CL cl_gb_main)  {
     AW_window_simple *aws = new AW_window_simple;
     aws->init( root, "NAME_SERVER_ADMIN", "NAME_SERVER ADMIN");
 
     aws->load_xfig("awtc/names_admin.fig");
-
 
     aws->callback( AW_POPUP_HELP,(AW_CL)"namesadmin.hlp");
     aws->at("help");
@@ -54,17 +79,23 @@ AW_window *create_awtc_names_admin_window( AW_root *root)  {
     aws->button_length(30);
 
     aws->at("delete");
-    aws->callback(awtc_delete_names_file);
+    aws->callback(awtc_delete_names_file, cl_gb_main);
     aws->create_button("DELETE_OLD_NAMES_FILE", "Delete old names file");
 
     aws->at("edit");
-    aws->callback(awtc_edit_names_file);
+    aws->callback(awtc_edit_names_file, cl_gb_main);
     aws->create_button("EDIT_NAMES_FILE", "Edit names file");
 
     aws->at("remove_arb");
-    aws->callback(awtc_remove_arb_acc);
+    aws->callback(awtc_remove_arb_acc, cl_gb_main);
     aws->create_button("REMOVE_SUPERFLUOUS_ENTRIES_IN_NAMES_FILE",
                        "Remove all entries with an\n'ARB_XXXX' accession number\nfrom names file");
+
+    AW_awar *awar_addid = root->awar(AWAR_NAMESERVER_ADDID);
+    awar_addid->add_callback(addid_changed_cb, cl_gb_main);
+    
+    aws->at("add_field");
+    aws->create_input_field(AWAR_NAMESERVER_ADDID, 20);
 
     return aws;
 }
