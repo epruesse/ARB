73029c783322c3431b8e8e2c96a94907f07e316c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-03-09 09:53:30 +0000

    - added enum for toggle values - removed duplicate of class PH_filter; include PH_filter.hxx

--- trunk/PHYLO/phylo.hxx
+++ trunk/PHYLO/phylo.hxx
@@ -26,28 +26,50 @@
 #include <awt_tree.hxx>
 #endif
 
+#ifndef ARB_ASSERT_H
+#include <arb_assert.h>
+#endif
+
+#ifndef PH_FILTER_HXX
+#include "PH_filter.hxx"
+#endif
+
+#define ph_assert(cond) arb_assert(cond)
+
+enum {
+    DONT_COUNT           = 0,
+    SKIP_COLUMN_IF_MAX   = 1,
+    SKIP_COLUMN_IF_OCCUR = 2,
+    COUNT_DONT_USE_MAX   = 3,
+    TREAT_AS_UPPERCASE   = 4,
+    TREAT_AS_REGULAR     = 5,
+
+    FILTER_MODES, // has to be last!
+};
+
 
 #define PH_DB_CACHE_SIZE    2000000
 
-#define AP_F_LOADED ((AW_active)1)
-#define AP_F_NLOADED ((AW_active)2)
+#define AP_F_LOADED    ((AW_active)1)
+#define AP_F_NLOADED   ((AW_active)2)
 #define AP_F_SEQUENCES ((AW_active)4)
-#define AP_F_MATRIX ((AW_active)8)
-#define AP_F_TREE ((AW_active)16)
-#define AP_F_ALL ((AW_active)-1)
+#define AP_F_MATRIX    ((AW_active)8)
+#define AP_F_TREE      ((AW_active)16)
+#define AP_F_ALL       ((AW_active)-1)
+
 #define PH_CORRECTION_BANDELT_STRING "bandelt"
 
 #define NIL 0
 
 // matrix definitions
-#define PH_TRANSFORMATION_JUKES_CANTOR_STRING "J+C"
-#define PH_TRANSFORMATION_KIMURA_STRING "KIMURA"
-#define PH_TRANSFORMATION_TAJIMA_NEI_STRING "T+N"
+#define PH_TRANSFORMATION_JUKES_CANTOR_STRING        "J+C"
+#define PH_TRANSFORMATION_KIMURA_STRING              "KIMURA"
+#define PH_TRANSFORMATION_TAJIMA_NEI_STRING          "T+N"
 #define PH_TRANSFORMATION_TAJIMA_NEI_PAIRWISE_STRING "T+N-P"
-#define PH_TRANSFORMATION_BANDELT_STRING "B"
-#define PH_TRANSFORMATION_BANDELT_JC_STRING "B+J+C"
-#define PH_TRANSFORMATION_BANDELT2_STRING "B2"
-#define PH_TRANSFORMATION_BANDELT2_JC_STRING "B2+J+C"
+#define PH_TRANSFORMATION_BANDELT_STRING             "B"
+#define PH_TRANSFORMATION_BANDELT_JC_STRING          "B+J+C"
+#define PH_TRANSFORMATION_BANDELT2_STRING            "B2"
+#define PH_TRANSFORMATION_BANDELT2_JC_STRING         "B2+J+C"
 
 typedef enum {
     PH_CORRECTION_NONE,
@@ -91,33 +113,9 @@ typedef enum {
     PH_TRANSFORMATION_BANDELT2_JC} PH_TRANSFORMATION;
 
 
-
-/////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////
-    //                                                             //
-    //                      class definitions                      //
-    //                                                             //
-    /////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////
-
-
-    class PH_filter {
-    public:
-        char    *filter;    // 0 1
-        long    filter_len;
-        long    real_len;   // how many 1
-        long    update;
-        long    *options_vector;   // options used to calculate current filter
-        //  float   *markerline;       // line to create filter (according to options_vector)
-        char    *init(char *filter, char *zerobases, long size);
-        char    *init(long size);
-
-        PH_filter(void);
-        ~PH_filter(void);
-        float *calculate_column_homology(void);
-    };
-
-
+// --------------------------
+//      class definitions
+// --------------------------
 
 class AP_root {
 private:
@@ -141,9 +139,6 @@ extern AP_root *ap_main;
 
 GBT_TREE *neighbourjoining(char **names, AP_FLOAT **m, long size, size_t structure_size);
 
-
-
-
 /////////////////////////////////////////////////////////////////
 //                                                             //
 // class_name : PHDATA                                         //
@@ -163,41 +158,43 @@ struct elem;
 class PHDATA {
 private:
     struct PHENTRY
-    { unsigned int key;
-        char *name;
-        char *full_name;
-        GBDATA *gb_species_data_ptr;
+    {
+        unsigned int    key;
+        char           *name;
+        char           *full_name;
+        GBDATA         *gb_species_data_ptr;
         struct PHENTRY *next;
         struct PHENTRY *prev;
-        int group_members; /* >0: this elem is grouphead */
-        struct elem *first_member; /* !=NULL: elem is grouphead */
-        AW_BOOL selected;
+        int             group_members; /* >0: this elem is grouphead */
+        struct elem    *first_member; /* !=NULL: elem is grouphead */
+        AW_BOOL         selected;
     };
     unsigned int last_key_number;
-    long    seq_len;
+    long         seq_len;
 
     AW_root *aw_root;       // only link
 
 public:
-    GBDATA *gb_main;
-    char    *use;
-    struct PHENTRY *entries;
+    GBDATA          *gb_main;
+    char            *use;
+    struct PHENTRY  *entries;
     struct PHENTRY **hash_elements;
-    unsigned int nentries;      // total number of entries
-    static PHDATA *ROOT; // 'global' pointer
-    AP_smatrix *distance_table;  // weights between different characters
-    AP_smatrix *matrix;    // calculated matrix
+    unsigned int     nentries;  // total number of entries
+    static PHDATA   *ROOT;      // 'global' pointer
+    AP_smatrix      *distance_table; // weights between different characters
+    AP_smatrix      *matrix;    // calculated matrix
+    float           *markerline;
 
     PHDATA(AW_root *awr);
     ~PHDATA(void);
 
-    char *load(char *use);  // open database and get pointers to it
-    char *unload(void);
-    GB_ERROR save(char *filename);
-    void print(void);
-    GB_ERROR calculate_matrix(const char *cancel, double alpha, PH_TRANSFORMATION transformation);
+    char     *load(char *use);  // open database and get pointers to it
+    char     *unload(void);
+    GB_ERROR  save(char *filename);
+    void      print(void);
+    GB_ERROR  calculate_matrix(const char *cancel, double alpha, PH_TRANSFORMATION transformation);
     long get_seq_len(void) { return seq_len; };
-    float *markerline;
+    
 };
 
 
