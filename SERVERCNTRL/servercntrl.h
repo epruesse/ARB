4da84dba726bb97e8721b425f9db374e514c5709
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-03-09 20:31:11 +0000

    - added field to arb_params

--- SERVERCNTRL/servercntrl.h
+++ SERVERCNTRL/servercntrl.h
@@ -13,6 +13,7 @@ struct arb_params {
     char *extended_name;
     char *alignment;
     char *default_file;
+    char *field;
 
     int  read_only;
 
@@ -28,6 +29,7 @@ struct arb_params {
 extern "C" {
 #endif
     struct arb_params *arb_trace_argv(int *argc, char **argv);
+    void               arb_print_server_params();
     GB_ERROR           arb_look_and_start_server(long magic_number, const char *arb_tcp_env, GBDATA *gbmain);
     GB_ERROR           arb_start_server(const char *arb_tcp_env, GBDATA *gbmain, int sleep);
     GB_ERROR           arb_look_and_kill_server(int magic_number, char *arb_tcp_env);
