c0c9922b17b83b3eeeddccd29765e2abac2ddbe2
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-03-09 09:53:08 +0000

    - enumerated PH_filter::options_vector - use the PH_filter version defined here

--- trunk/PHYLO/PH_filter.hxx
+++ trunk/PHYLO/PH_filter.hxx
@@ -1,16 +1,33 @@
-class PH_filter {
-	public:
-	char	*filter;	// 0 1
-	long	filter_len;
-	long	real_len;	// how many 1
-	long	update;
-	long    *options_vector;   // options used to calculate current filter
-//	float   *markerline;       // line to create filter (according to options_vector)
-	char 	*init(char *filter, char *zerobases, long size);
-	char	*init(long size);
-	
-	PH_filter::PH_filter(void);
-	~PH_filter(void);
-	float *calculate_column_homology(void);
-};	
+#ifndef PH_FILTER_HXX
+#define PH_FILTER_HXX
 
+enum {
+    OPT_START_COL = 0, 
+    OPT_STOP_COL = 1, 
+    OPT_MIN_HOM = 2, 
+    OPT_MAX_HOM = 3, 
+    OPT_FILTER_POINT = 4, 
+    OPT_FILTER_MINUS = 5, 
+    OPT_FILTER_AMBIG = 6, 
+    OPT_FILTER_LOWER = 7, 
+};
+
+class PH_filter {
+public:
+    char *filter;               // 0 1
+    long  filter_len;
+    long  real_len;             // how many 1
+    long  update;
+    long *options_vector;       // options used to calculate current filter
+    //  float   *markerline;       // line to create filter (according to options_vector)
+    char *init(char *filter, char *zerobases, long size);
+    char *init(long size);
+
+    PH_filter(void);
+    ~PH_filter(void);
+    float *calculate_column_homology(void);
+};
+
+#else
+#error PH_filter.hxx included twice
+#endif
