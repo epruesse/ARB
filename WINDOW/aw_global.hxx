37a3b1e3117a35b443ea0d56a12fb6b65e1ef583
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-03-29 16:53:55 +0000

    - changed prototype of aw_create_selection_box_awars

--- WINDOW/aw_global.hxx
+++ WINDOW/aw_global.hxx
@@ -2,7 +2,7 @@
 //                                                                       // 
 //    File      : aw_global.hxx                                          // 
 //    Purpose   : global functions from WINDOWS lib                      // 
-//    Time-stamp: <Mon Jun/07/2004 13:38 MET Coder@ReallySoft.de>        // 
+//    Time-stamp: <Wed Mar/28/2007 19:04 MET Coder@ReallySoft.de>        // 
 //                                                                       // 
 //                                                                       // 
 //  Coded by Ralf Westram (coder@reallysoft.de) in June 2004             // 
@@ -15,7 +15,9 @@
 #ifndef AW_GLOBAL_HXX
 #define AW_GLOBAL_HXX
 
-void aw_create_selection_box_awars(AW_root *awr, const char *awar_base, const char *directory, const char *filter, const char *file_name, AW_default default_file = AW_ROOT_DEFAULT);
+void aw_create_selection_box_awars(AW_root *awr, const char *awar_base,
+                                   const char *directory, const char *filter, const char *file_name,
+                                   AW_default default_file = AW_ROOT_DEFAULT, bool resetValues = false);
 
 #else
 #error aw_global.hxx included twice
