373906b015bdca1ac13cf5e567e0e3635ec46009
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-04-18 21:31:41 +0000

    - added wrapper to use header w/o gui-parts - added AW_select_nameserver()

--- SL/AW_NAME/AW_rename.hxx
+++ SL/AW_NAME/AW_rename.hxx
@@ -6,12 +6,18 @@
 
 #define AWAR_NAMESERVER_ADDID "nt/nameserver_addid"
 
+#ifndef AW_RENAME_SKIP_GUI
+
 void       AWTC_create_rename_variables(AW_root *root,AW_default db1);
 AW_window *AWTC_create_rename_window(AW_root *root, AW_CL gb_main);
 void       awt_rename_cb(AW_window *aww,GBDATA *gb_main);
 
 void       AW_create_namesadmin_awars(AW_root *aw_root, GBDATA *gb_main);
 AW_window *AW_create_namesadmin_window(AW_root *aw_root, AW_CL cl_gb_main);
+
+GB_ERROR AW_select_nameserver(GBDATA *gb_main, GBDATA *gb_other_main);
+
+#endif // AW_RENAME_SKIP_GUI
 
 GB_ERROR AWTC_pars_names(GBDATA *gb_main, int update_status = 0);
 GB_ERROR AWTC_generate_one_name(GBDATA *gb_main, const char *full_name, const char *acc, const char *addid, char*& new_name, bool openstatus, bool showstatus);
