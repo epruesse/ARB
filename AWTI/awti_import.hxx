70a4900de02a3b445fc755be3542f95000b9e425
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-04-18 18:13:28 +0000

    - pass main DB to open_AWTC_import_window

--- AWTI/awti_import.hxx
+++ AWTI/awti_import.hxx
@@ -7,7 +7,7 @@ typedef enum { IMP_GENOME_FLATFILE, IMP_PLAIN_SEQUENCE } AWTI_ImportType;
 
 #define AWTC_RCB(func) void (*func)(AW_root*,AW_CL,AW_CL)
 
-GBDATA *open_AWTC_import_window(AW_root *awr,const char *defname, int do_exit,AWTC_RCB(func), AW_CL cd1, AW_CL cd2);
+GBDATA *open_AWTC_import_window(AW_root *awr,const char *defname, bool do_exit, GBDATA *gb_main, AWTC_RCB(func), AW_CL cd1, AW_CL cd2);
 void    AWTC_import_set_ali_and_type(AW_root *awr, const char *ali_name, const char *ali_type, GBDATA *gbmain);
 
 #endif
