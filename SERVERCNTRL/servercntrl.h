40d6790984c56c7bdc53bc501592b97da9ac7204
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-04-27 09:38:19 +0000

    - changed param of arb_look_and_kill_server

--- SERVERCNTRL/servercntrl.h
+++ SERVERCNTRL/servercntrl.h
@@ -28,11 +28,12 @@ struct arb_params {
 #ifdef __cplusplus
 extern "C" {
 #endif
-    struct arb_params *arb_trace_argv(int *argc, char **argv);
-    void               arb_print_server_params();
+    
+    GB_ERROR           arb_start_server(const char *arb_tcp_env, GBDATA *gbmain, int do_sleep);
     GB_ERROR           arb_look_and_start_server(long magic_number, const char *arb_tcp_env, GBDATA *gbmain);
-    GB_ERROR           arb_start_server(const char *arb_tcp_env, GBDATA *gbmain, int sleep);
-    GB_ERROR           arb_look_and_kill_server(int magic_number, char *arb_tcp_env);
+    GB_ERROR           arb_look_and_kill_server(int magic_number, const char *arb_tcp_env);
+    void               arb_print_server_params(void);
+    struct arb_params *arb_trace_argv(int *argc, char **argv);
 
 #ifdef __cplusplus
 }
