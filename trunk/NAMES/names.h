6a41cc838e52a0d564fd242f80c4ac2c7c3196b0
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-05-11 11:46:00 +0000

    - changes server-timeout in DEBUG version

--- trunk/NAMES/names.h
+++ trunk/NAMES/names.h
@@ -3,7 +3,11 @@
 #define RETRY_SLEEP 3           // in seconds
 
 // Lifetime of nameserver in seconds
+#if defined(DEBUG)
+#define NAME_SERVER_TIMEOUT (1*60) // 1 minute (for debugging)
+#else
 #define NAME_SERVER_TIMEOUT (15*60) // 15 minutes
+#endif // DEBUG
 
 // Sleeptime of nameserver in seconds
 #define NAME_SERVER_SLEEP 5
