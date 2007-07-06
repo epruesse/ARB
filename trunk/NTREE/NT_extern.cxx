b2356cbe934924ad066ee384c13d62ca61b9be19
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-07-06 13:03:52 +0000

    - enabled chimere_check in NDEBUG VERSION

--- trunk/NTREE/NT_extern.cxx
+++ trunk/NTREE/NT_extern.cxx
@@ -1410,16 +1410,10 @@ AW_window * create_nt_main_window(AW_root *awr, AW_CL clone){
             AWMIMT("arb_dist",  "Compare sequences using Distance Matrix", "D", "dist.hlp",                AWM_SEQ, (AW_CB)NT_system_cb,    (AW_CL)"arb_dist &",    0);
             SEP________________________SEP();
 
-            AWMIMT("seq_quality", "Check Sequence Quality",    "",  "seq_quality.hlp",   AWM_EXP,  AW_POPUP, (AW_CL)SQ_create_seq_quality_window, 0);
-
-#if defined(DEBUG)
-#if defined(DEVEL_RALF)
-#warning chimere check needs to be fixed
-#endif // DEVEL_RALF
-            AWMIMT("seq_quality", "Chimere Check [broken!]",   "m", "check_quality.hlp", AWM_SEQ2, AW_POPUP, (AW_CL)st_create_quality_check_window, (AW_CL)gb_main);
+            AWMIMT("seq_quality",   "Check Sequence Quality", "",  "seq_quality.hlp",   AWM_EXP,  AW_POPUP, (AW_CL)SQ_create_seq_quality_window,   0)     ;
+            AWMIMT("chimere_check", "Chimere Check",          "m", "check_quality.hlp", AWM_SEQ2, AW_POPUP, (AW_CL)st_create_quality_check_window, (AW_CL)gb_main);
 
             SEP________________________SEP();
-#endif // DEBUG
 
             GDE_load_menu(awm,"pretty_print");
         }
