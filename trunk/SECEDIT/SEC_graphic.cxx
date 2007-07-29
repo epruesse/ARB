e89c672ca0ceba3183f0e52861feafd36a4253aa
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-07-29 14:27:45 +0000

    - default directory to save secondary structure changed into .arb_prop/secondary_structure - renamed AWAR_SECEDIT_IMEXPORT_BASE -> AWAR_SECEDIT_SAVEDIR

--- trunk/SECEDIT/SEC_graphic.cxx
+++ trunk/SECEDIT/SEC_graphic.cxx
@@ -28,8 +28,8 @@ void SEC_create_awars(AW_root *aw_root,AW_default def)
     aw_root->awar_string(AWAR_FOOTER);
 
     {
-        char *dir = GBS_global_string_copy("%s/lib/secondary_structure", GB_getenvARBHOME());
-        aw_create_selection_box_awars(aw_root, AWAR_SECEDIT_IMEXPORT_BASE, dir, ".ass", "noname.ass");
+        char *dir = GBS_global_string_copy("%s/.arb_prop/secondary_structure", GB_getenvHOME());
+        aw_create_selection_box_awars(aw_root, AWAR_SECEDIT_SAVEDIR, dir, ".ass", "noname.ass");
         free(dir);
     }
 
