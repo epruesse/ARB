2590bc39772474b4ada9a8329e1df2009907d87e
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-09-19 14:00:01 +0000

    - replaced macros by inline-member

--- WINDOW/AW_size.cxx
+++ WINDOW/AW_size.cxx
@@ -8,104 +8,99 @@
 #include "aw_commn.hxx"
 #include <aw_size.hxx>
 
-//*****************************************************************************************
-//					size_device
-//*****************************************************************************************
+#include <algorithm>
 
-AW_device_size::AW_device_size(AW_common *commoni): AW_device(commoni) {
-	;
-}
+using namespace std;
+
+//*****************************************************************************************
+  //                                    size_device
+  //*****************************************************************************************
+
+  AW_device_size::AW_device_size(AW_common *commoni): AW_device(commoni) {
+      ;
+  }
 
 void AW_device_size::init() {
-	this->drawn = AW_FALSE;
+    drawn = AW_FALSE;
+
+    size_information.t = 0;
+    size_information.b = 0;
+    size_information.l = 0;
+    size_information.r = 0;
 }
 
 AW_DEVICE_TYPE AW_device_size::type(void) { return AW_DEVICE_SIZE; }
-void	AW_device_size::_privat_reset(void){
-	this->init();
+
+void AW_device_size::privat_reset(void){
+    this->init();
 }
 
-#define XSET(X) if(!this->drawn || size_information.l > X)\
-			size_information.l = X;\
-		if(!this->drawn || size_information.r < X)\
-			size_information.r = X;
+inline void AW_device_size::dot_transformed(AW_pos X, AW_pos Y) {
+    if (drawn) {
+        size_information.l = min(size_information.l, X);
+        size_information.r = max(size_information.r, X);
+        size_information.t = min(size_information.t, Y);
+        size_information.b = max(size_information.b, Y);
+    }
+    else {
+        size_information.l = size_information.r = X;
+        size_information.t = size_information.b = Y;
+        drawn              = true;
+    }
+}
 
-#define YSET(Y) if(!this->drawn || size_information.t > Y)\
-			size_information.t = Y;\
-		if(!this->drawn || size_information.b < Y)\
-			size_information.b = Y;
+inline void AW_device_size::dot(AW_pos x, AW_pos y) {
+    AW_pos X, Y;
+    transform(x, y, X, Y);
+    dot_transformed(X, Y);
+}
 
 /***********************************************************************************************************************/
 /* line  text  zoomtext  box *******************************************************************************************/
 /***********************************************************************************************************************/
 
-int AW_device_size::invisible(int gc, AW_pos x, AW_pos y, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2) {
-	AW_pos X,Y;							// Transformed pos
-	if(filteri & filter) {
-		this->transform(x,y,X,Y);
-		XSET(X);
-		YSET(Y);
-		this->drawn = AW_TRUE;
-	}
-	return AW_device::invisible(gc,x,y,filteri,clientdata1,clientdata2);
+bool AW_device_size::invisible(int gc, AW_pos x, AW_pos y, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2) {
+    if (filteri & filter) dot(x, y);
+    return AW_device::invisible(gc,x,y,filteri,clientdata1,clientdata2);
 }
 
 
-int	AW_device_size::line(int gc, AW_pos x0, AW_pos y0, AW_pos x1, AW_pos y1, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2) {
-	AWUSE(clientdata1);AWUSE(clientdata2);
-	AWUSE(gc);
-	AW_pos X0,Y0,X1,Y1;							// Transformed pos
+int AW_device_size::line(int gc, AW_pos x0, AW_pos y0, AW_pos x1, AW_pos y1, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2) {
+    AWUSE(clientdata1);AWUSE(clientdata2);
+    AWUSE(gc);
 
-	if(filteri & filter) {
-		this->transform(x0,y0,X0,Y0);
-		this->transform(x1,y1,X1,Y1);
-		XSET(X0);
-		YSET(Y0);
-		this->drawn = AW_TRUE;
-		XSET(X1);
-		YSET(Y1);
-		return AW_TRUE;
-	}else{
-		return AW_FALSE;
-	}
+    if (filteri & filter) {
+        dot(x0, y0);
+        dot(x1, y1);
+        return AW_TRUE;
+    }
+    return AW_FALSE;
 }
-
 
 int AW_device_size::text(int gc, const char *str, AW_pos x, AW_pos y, AW_pos alignment, AW_bitset filteri, AW_CL clientdata1, AW_CL clientdata2, long opt_strlen) {
-	AWUSE(clientdata1);AWUSE(clientdata2);
-	register XFontStruct *xfs = &(common->gcs[gc]->curfont);
-	AW_pos 			X0,Y0;	// Transformed pos
-	AW_pos			l_width, l_ascent, l_descent;
+    AWUSE(clientdata1);AWUSE(clientdata2);
 
-	if(filteri & filter) {
-		this->transform(x,y,X0,Y0);
-		l_ascent  = xfs->max_bounds.ascent;
-		l_descent = xfs->max_bounds.descent;
-		l_width   = this->get_string_size(gc,str,opt_strlen);
-		X0        = common->x_alignment(X0,l_width,alignment);
+    if(filteri & filter) {
+        XFontStruct *xfs = &(common->gcs[gc]->curfont);
+        
+        AW_pos X0,Y0;           // Transformed pos
+        this->transform(x,y,X0,Y0);
 
-        if( !drawn || size_information.l > X0 )                 size_information.l = X0;
-        if( !drawn || size_information.r < X0 + l_width )       size_information.r = X0 + l_width;
-        if( !drawn || size_information.t > Y0 - l_ascent )      size_information.t = Y0 - l_ascent;
-        if( !drawn || size_information.b < Y0 + l_descent )     size_information.b = Y0 + l_descent;
+        AW_pos l_ascent  = xfs->max_bounds.ascent;
+        AW_pos l_descent = xfs->max_bounds.descent;
+        AW_pos l_width   = get_string_size(gc, str, opt_strlen);
+        X0               = common->x_alignment(X0,l_width,alignment);
 
-        drawn = AW_TRUE;
-		return 1;
-	}
-    else {
-		return 0;
-	}
+        dot_transformed(X0, Y0-l_ascent);
+        dot_transformed(X0+l_width, Y0+l_descent);
+        return 1;
+    }
+    return 0;
 }
 
 
 
 void AW_device_size::get_size_information(AW_world *ptr) {
-	if(!this->drawn) {
-		size_information.l = 0.0;
-		size_information.r = 0.0;
-		size_information.t = 0.0;
-		size_information.b = 0.0;
-	}
-	*ptr = this->size_information;
+    *ptr = size_information;
 }
 
