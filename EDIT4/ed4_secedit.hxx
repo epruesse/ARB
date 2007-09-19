3ad19357f7563a9c945bd3b50cce4ce30c44893d
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-09-19 14:08:50 +0000

    - added include wrapper

--- EDIT4/ed4_secedit.hxx
+++ EDIT4/ed4_secedit.hxx
@@ -1,2 +1,20 @@
+// =============================================================== //
+//                                                                 //
+//   File      : ed4_secedit.hxx                                   //
+//   Purpose   : start secondary editor                            //
+//   Time-stamp: <Wed Sep/12/2007 09:41 MET Coder@ReallySoft.de>   //
+//                                                                 //
+//   Coded by Ralf Westram (coder@reallysoft.de) in August 2007    //
+//   Institute of Microbiology (Technical University Munich)       //
+//   http://www.arb-home.de/                                       //
+//                                                                 //
+// =============================================================== //
 
-void ED4_SECEDIT_start(AW_window *aw, AW_CL, AW_CL);
+#ifndef ED4_SECEDIT_HXX
+#define ED4_SECEDIT_HXX
+
+void ED4_SECEDIT_start(AW_window *aw, AW_CL cl_gbmain, AW_CL);
+
+#else
+#error ed4_secedit.hxx included twice
+#endif // ED4_SECEDIT_HXX
