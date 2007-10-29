2d258b2514d9ccd0b0d5c1aa55739e5ec4474235
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-10-29 13:43:26 +0000

    - stricter error handling - prints what has been done

--- trunk/PERL_SCRIPTS/SPECIES/markSpecies.pl
+++ trunk/PERL_SCRIPTS/SPECIES/markSpecies.pl
@@ -1,12 +1,9 @@
-#! /usr/bin/perl
+#!/usr/bin/perl
 
 #############################################################################
 # Harald Meier (meierh@in.tum.de); 2006 LRR, Technische Universität München #
 #############################################################################
 
-########################
-#BEGIN {print "1..1\n";}
-#END {print "not ok 1\n" unless $loaded;}
 use strict;
 use warnings;
 use diagnostics;
@@ -20,71 +17,68 @@ BEGIN {
 
 use ARB;
 
-#$loaded = 1;
-#print "ok 1\n";
-#######################
+eval {
+  my $gb_main = ARB::open(":","rw"); # connect to running ARB
+  if (!$gb_main) { die "No running ARB found\n"; }
 
-#An einen laufenden ARB-Server hängen
-my $gb_main = ARB::open(":","rw");
-if (!$gb_main)   {
-  printf "You must run an arbdb server first \n";
-  exit;
-}
-else {
-  print "Connected to a running instance of ARB!\n";
-}
+  print "Connected to running ARB.\n";
 
+  eval {
+    my $error = ARB::begin_transaction($gb_main); # start transaction
+    if ($error) { die "transaction could not be initialized\n"; }
 
-#Mit der Transaktion beginnen
-my $error = ARB::begin_transaction($gb_main);
-if ($error) {
-  print ("Error, transaction could not be initiated!");
-  exit;
-}
-
-# Einlesen über Stdin und in Hash laden
-# @ARGV=("/home/meierh/ARB/PERL2ARB/otu_test_ohne_short");
-my %marklist;
-while (<>){
-  chomp;
-  $marklist{$_}=1;
-}
-
-my $gb_species = BIO::first_species($gb_main);
-
-if (!$gb_species){
-  print ("Cannot find a species in your ARB database");
-} else {
-  #  print ("$gb_species \n");
-
-
-  while ($gb_species) {
-    my $species_name = BIO::read_string($gb_species, "name");
-    if ($marklist{$species_name}) {
-      $error=ARB::write_flag($gb_species, "1");
-      delete ($marklist{$species_name});
+    # read species names from STDIN into hash
+    my %marklist = ();
+    while (<>) {
+      chomp;
+      $marklist{$_} = 1;
     }
-    
-    else {
-      # print ("$species_name nonononononononono \n");
+
+    my $inCount = scalar(keys %marklist);
+    if ($inCount<1) { print "Warning: Empty input\n"; }
+    else { print "Read $inCount species names\n"; }
+
+    my $been_marked    = 0;
+    my $already_marked = 0;
+
+    my $gb_species = BIO::first_species($gb_main);
+    if (!$gb_species) { die "Database doesn't contain any species\n"; }
+
+    while ($gb_species) {
+      my $species_name = BIO::read_string($gb_species, "name");
+      my $was_marked = ARB::read_flag($gb_species);
+      if ($marklist{$species_name}) {
+        if ($was_marked==1) {
+          $already_marked++;
+        }
+        else {
+          $error = ARB::write_flag($gb_species, "1");
+          $been_marked++;
+        }
+        delete $marklist{$species_name};
+      }
+      $gb_species = BIO::next_species($gb_species);
     }
-    $gb_species = BIO::next_species($gb_species);
+
+    my @notFound = keys %marklist;
+    my $notFound = scalar(@notFound);
+    if ($notFound>0) {
+      print("$notFound species not found in ARBDB:\n");
+      print @notFound."\n";
+    }
+    print "Marked $been_marked species";
+    if ($already_marked>0) { print " ($already_marked species already marked)"; }
+    print "\n";
+
+    ARB::commit_transaction($gb_main);
+  };
+  if ($@) {
+    ARB::abort_transaction($gb_main);
+    print "Error in markSpecies.pl (aborting transaction):\n$@\n";
   }
-  print("Following species not in ARB-DB: \n");
-  print keys(%marklist);
+
+  ARB::close($gb_main); # close connection to ARBDB
+};
+if ($@) {
+  print "Error in markSpecies.pl: $@\n";
 }
-
-#FILE schließen
-#close (FILEHANDLER);
-
-#Datenbankänderungen bestätigen
-ARB::commit_transaction($gb_main);
-print("\nTransaction committed \n");
-
-#Verbindung zur ARB-DB beenden
-ARB::close($gb_main);
-print("Database connection closed \n");
-
-#print "All tests passed.\n";
-# while(<>) { chomp;$mark{$_} = 1;
-
