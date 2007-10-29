f23b78de387787e0844ec78c6028648b8949c1b6
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-10-29 13:43:06 +0000

    - cleanup

--- trunk/EDIT4/ed4_tools.hxx
+++ trunk/EDIT4/ed4_tools.hxx
@@ -12,7 +12,6 @@ char ED4_encode_iupac(const char bases[/*4*/], GB_alignment_type ali);
 const char* ED4_decode_iupac(char iupac, GB_alignment_type ali);
 
 void ED4_set_clipping_rectangle(AW_rectangle *rect);
-// void ED4_reduce_clipping_rectangle(AW_pos x1, AW_pos y1, AW_pos x2, AW_pos y2);
 
 void ED4_aws_init(AW_root *root, AW_window_simple *aws, GB_CSTR macro_format, GB_CSTR window_format, GB_CSTR typeId);
 
