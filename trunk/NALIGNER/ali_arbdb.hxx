d10ae135bc967a08e5a2d33f47efa070ce8f6eda
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-11-06 10:57:11 +0000

    - remove param 'info' from put..-functions. Param was always default.   Functions always created an empty 'mark' entry, which is used nowhere.

--- trunk/NALIGNER/ali_arbdb.hxx
+++ trunk/NALIGNER/ali_arbdb.hxx
@@ -35,9 +35,9 @@ public:
     char *get_sequence_string(char *name, int and_mark = 0);
     ALI_SEQUENCE *get_sequence(char *name, int and_mark = 0);
     char *get_extended(char *name);
-    int put_sequence_string(char *name, char *sequence, char *info = 0);
-    int put_sequence(char *name, ALI_SEQUENCE *sequence, char *info = 0);
-    int put_extended(const char *name, char *sequence, char *info = 0);
+    int put_sequence_string(char *name, char *sequence);
+    int put_sequence(char *name, ALI_SEQUENCE *sequence);
+    int put_extended(const char *name, char *sequence);
 };
 
 
