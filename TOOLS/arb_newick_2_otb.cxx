ed3caba31595ef36b52fcffd1eb12cb8f3564066
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-11-08 14:15:33 +0000

    - fix call to GBT_load_tree()

--- TOOLS/arb_newick_2_otb.cxx
+++ TOOLS/arb_newick_2_otb.cxx
@@ -23,7 +23,7 @@ int main(int argc,char **argv)
 	char *otb_name = argv[2];
 
 
-	GBT_TREE *tree = GBT_load_tree(nname,sizeof(GBT_TREE), 0, 1);
+	GBT_TREE *tree = GBT_load_tree(nname,sizeof(GBT_TREE), 0, 1, 0);
 	if (!tree) {
 	    GB_print_error();
 	    return -1;
