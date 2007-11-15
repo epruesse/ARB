343d73cb3e93d85fa2c93cc825e5835bf519b94d
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-11-15 11:59:18 +0000

    - added new keymap defining all MOD-flavors of contained keys - changed typename

--- trunk/WINDOW/aw_xkey.hxx
+++ trunk/WINDOW/aw_xkey.hxx
@@ -1,7 +1,7 @@
 #ifndef AW_XKEY_HXX
 #define AW_XKEY_HXX
 
-struct awxkeymap_struct {
+struct awXKeymap {
     int          xmod;
     int          xkey;
     const char  *xstr;
@@ -10,8 +10,14 @@ struct awxkeymap_struct {
     char        *awstr;
 };
 
+struct awXKeymap_modfree { // automatically defines key with SHIFT, ALT(META) and CTRL
+    int          xkey;
+    const char  *xstr_suffix;
+    AW_key_code  awkey;
+};
+
 void aw_install_xkeys(Display *display);
-const struct awxkeymap_struct *aw_xkey_2_awkey(XKeyEvent *xkeyevent);
+const awXKeymap* aw_xkey_2_awkey(XKeyEvent *xkeyevent);
 
 #else
 #error aw_xkey.hxx included twice
