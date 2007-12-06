8d89640e89538dba4565dc78f83d23825111a051
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2007-12-06 17:58:20 +0000

    - converts secondary structure into 2nd species

--- PERL_SCRIPTS/ARBTOOLS/IFTHELP/dssp2fasta.pl
+++ PERL_SCRIPTS/ARBTOOLS/IFTHELP/dssp2fasta.pl
@@ -6,6 +6,7 @@ use strict;
 my $line = undef;
 my $source = undef;
 my $seq = '';
+my $second = '';
 my $mode = 0;
 foreach $line (<>) {
     chomp($line);
@@ -21,8 +22,9 @@ foreach $line (<>) {
 	}
     }
     elsif ($mode==2) {
-	if ($line =~ /^\s+[0-9]+\s+[0-9]+\s+([A-Z])\s+/io) {
+	if ($line =~ /^\s+[0-9]+\s+[0-9]+\s+([A-Z])\s\s([A-Z\s])/io) {
 	    $seq .= $1;
+            $second .= $2 eq ' ' ? '-' : $2;
 	}
 	else {
 	    die "Can't parse '$line'";
@@ -35,3 +37,5 @@ if ($mode!=2) { die "Unknown parse error"; }
 
 print ">$source\n";
 print "$seq\n";
+print ">fold_$source\n";
+print "$second\n";
