78a08852539fe543533f2489dfd3e024439625c9
baderk <baderk@129cd134-2152-0410-ab65-f3f895bafe68>
2007-12-19 11:13:53 +0000

    Reformat only (tabs,indention,...) - no code change

--- trunk/STAT/ST_window.cxx
+++ trunk/STAT/ST_window.cxx
@@ -14,20 +14,16 @@
 #include "st_ml.hxx"
 #include "st_quality.hxx"
 
-void st_ok_cb(AW_window * aww, ST_ML * st_ml)
-{
+void st_ok_cb(AW_window * aww, ST_ML * st_ml) {
     AW_root *root = aww->get_root();
     GB_transaction dummy(st_ml->gb_main);
-    char *alignment_name =
-        root->awar_string(AWAR_DEFAULT_ALIGNMENT, "-none-",
-                          st_ml->gb_main)->read_string();
+    char *alignment_name = root->awar_string(AWAR_DEFAULT_ALIGNMENT, "-none-",
+            st_ml->gb_main)->read_string();
     char *tree_name = root->awar_string(AWAR_TREE, "tree_stat",
-                                        st_ml->gb_main)->read_string();
-    int marked_only =
-        root->awar_int(ST_ML_AWAR_CQ_MARKED_ONLY)->read_int();
-    GB_ERROR error =
-        st_ml->init(tree_name, alignment_name, (char *) 0, marked_only,
-                    (char *) 0, st_ml->awt_csp);
+            st_ml->gb_main)->read_string();
+    int marked_only = root->awar_int(ST_ML_AWAR_CQ_MARKED_ONLY)->read_int();
+    GB_ERROR error = st_ml->init(tree_name, alignment_name, (char *) 0,
+            marked_only, (char *) 0, st_ml->awt_csp);
 
     if (error) {
         aw_message(error);
@@ -40,11 +36,9 @@ void st_ok_cb(AW_window * aww, ST_ML * st_ml)
 }
 
 AW_window *st_create_main_window(AW_root * root, ST_ML * st_ml,
-                                 AW_CB0 refresh_func, AW_window * win)
-{
+        AW_CB0 refresh_func, AW_window * win) {
     AW_window_simple *aws = new AW_window_simple;
-    aws->init(root, "ENABLE_ONLINE_STATISTIC",
-              "ACTIVATE ONLINE STATISTIC");
+    aws->init(root, "ENABLE_ONLINE_STATISTIC", "ACTIVATE ONLINE STATISTIC");
 
     aws->load_xfig("stat_main.fig");
 
@@ -73,13 +67,13 @@ AW_window *st_create_main_window(AW_root * root, ST_ML * st_ml,
 
     aws->at("awt_csp");
     aws->callback(AW_POPUP, (AW_CL) create_csp_window,
-                  (AW_CL) st_ml->awt_csp);
+    (AW_CL) st_ml->awt_csp);
     aws->button_length(20);
     aws->create_button("SELECT_CSP", ST_ML_AWAR_CSP);
 
     aws->at("marked");
     aws->create_toggle_field(ST_ML_AWAR_CQ_MARKED_ONLY, "Calculate for ..",
-                             "");
+    "");
     aws->insert_toggle("All species", "A", 0);
     aws->insert_toggle("Marked species", "M", 1);
     aws->update_toggle_field();
@@ -87,30 +81,24 @@ AW_window *st_create_main_window(AW_root * root, ST_ML * st_ml,
     return aws;
 }
 
-ST_ML *new_ST_ML(GBDATA * gb_main)
-{
+ST_ML *new_ST_ML(GBDATA * gb_main) {
     return new ST_ML(gb_main);
 }
 
 ST_ML_Color *st_ml_get_color_string(ST_ML * st_ml, char *species_name,
-                                    AP_tree * node, int start_ali_pos,
-                                    int end_ali_pos)
-{
+        AP_tree * node, int start_ali_pos, int end_ali_pos) {
     return st_ml->get_color_string(species_name, node, start_ali_pos,
-                                   end_ali_pos);
+            end_ali_pos);
 }
 
 int st_ml_update_ml_likelihood(ST_ML * st_ml, char *result[4],
-                               int *latest_update, char *species_name,
-                               AP_tree * node)
-{
+        int *latest_update, char *species_name, AP_tree * node) {
     return st_ml->update_ml_likelihood(result, latest_update, species_name,
-                                       node);
+            node);
 }
 
 AP_tree *st_ml_convert_species_name_to_node(ST_ML * st_ml,
-                                            const char *species_name)
-{
+        const char *species_name) {
     AP_tree *node;
     if (!st_ml->hash_2_ap_tree)
         return 0;
@@ -118,13 +106,11 @@ AP_tree *st_ml_convert_species_name_to_node(ST_ML * st_ml,
     return node;
 }
 
-int st_is_inited(ST_ML * st_ml)
-{
+int st_is_inited(ST_ML * st_ml) {
     return st_ml->is_inited;
 }
 
-void st_check_cb(AW_window * aww, GBDATA * gb_main, AWT_csp * awt_csp)
-{
+void st_check_cb(AW_window * aww, GBDATA * gb_main, AWT_csp * awt_csp) {
     GB_begin_transaction(gb_main);
     AW_root *r = aww->get_root();
     char *alignment_name = r->awar(ST_ML_AWAR_ALIGNMENT)->read_string();
@@ -132,15 +118,11 @@ void st_check_cb(AW_window * aww, GBDATA * gb_main, AWT_csp * awt_csp)
     char *tree_name = r->awar(AWAR_TREE)->read_string();
     char *dest_field = r->awar(ST_ML_AWAR_CQ_DEST_FIELD)->read_string();
     int marked_only = r->awar(ST_ML_AWAR_CQ_MARKED_ONLY)->read_int();
-    char *filter_string =
-        r->awar(ST_ML_AWAR_CQ_FILTER_FILTER)->read_string();
-    st_report_enum report =
-        (st_report_enum) r->awar(ST_ML_AWAR_CQ_REPORT)->read_int();
-    GB_ERROR error =
-        st_ml_check_sequence_quality(gb_main, tree_name, alignment_name,
-                                     awt_csp, bucket_size,
-                                     marked_only, report,
-                                     filter_string, dest_field);
+    char *filter_string = r->awar(ST_ML_AWAR_CQ_FILTER_FILTER)->read_string();
+    st_report_enum report = (st_report_enum) r->awar(ST_ML_AWAR_CQ_REPORT)->read_int();
+    GB_ERROR error = st_ml_check_sequence_quality(gb_main, tree_name,
+            alignment_name, awt_csp, bucket_size, marked_only, report,
+            filter_string, dest_field);
     free(filter_string);
     free(dest_field);
     free(alignment_name);
@@ -153,15 +135,14 @@ void st_check_cb(AW_window * aww, GBDATA * gb_main, AWT_csp * awt_csp)
     }
 }
 
-AW_window *st_create_quality_check_window(AW_root * root, GBDATA * gb_main)
-{
+AW_window *st_create_quality_check_window(AW_root * root, GBDATA * gb_main) {
     static AW_window_simple *aws = 0;
     if (aws)
         return aws;
     static AWT_csp *awt_csp;
     aws = new AW_window_simple;
     aws->init(root, "SEQUENCE_QUALITY_CHECK",
-              "CHECK QUALITY OF MARKED SEQUENCES");
+            "CHECK QUALITY OF MARKED SEQUENCES");
 
     aws->load_xfig("check_quality.fig");
     //    aws->load_xfig("stat_main.fig");
@@ -188,7 +169,7 @@ AW_window *st_create_quality_check_window(AW_root * root, GBDATA * gb_main)
 
     root->awar_string(ST_ML_AWAR_ALIGNMENT)->map(AWAR_DEFAULT_ALIGNMENT);
     root->awar_string(ST_ML_AWAR_CQ_FILTER_ALIGNMENT)->
-        map(AWAR_DEFAULT_ALIGNMENT);
+    map(AWAR_DEFAULT_ALIGNMENT);
 
     awt_csp = new AWT_csp(gb_main, root, ST_ML_AWAR_CSP);
     //AW_CL filter_cl =
@@ -218,11 +199,9 @@ AW_window *st_create_quality_check_window(AW_root * root, GBDATA * gb_main)
     aws->at("sb");
     aws->create_input_field(ST_ML_AWAR_CQ_BUCKET_SIZE);
 
-
-    awt_create_selection_list_on_scandb(gb_main, aws,
-                                        ST_ML_AWAR_CQ_DEST_FIELD,
-                                        1 << GB_STRING, "dest", 0,
-                                        &AWT_species_selector, 20, 10);
+    awt_create_selection_list_on_scandb(gb_main, aws, 
+    ST_ML_AWAR_CQ_DEST_FIELD, 1 << GB_STRING, "dest", 0, &AWT_species_selector,
+            20, 10);
     aws->at("GO");
     aws->callback((AW_CB) st_check_cb, (AW_CL) gb_main, (AW_CL) awt_csp);
     aws->create_button("GO", "GO", "G");
--- trunk/STAT/st_window.hxx
+++ trunk/STAT/st_window.hxx
@@ -18,7 +18,7 @@ class AP_tree;
 class AWT_csp;
 
 AW_window *st_create_main_window(AW_root * aw_root, ST_ML * st_ml,
-                                 AW_CB0 refresh_func, AW_window * win);
+        AW_CB0 refresh_func, AW_window * win);
 ST_ML *new_ST_ML(GBDATA * gb_main);
 int st_is_inited(ST_ML * st_ml);
 
@@ -29,28 +29,19 @@ enum st_report_enum {
     ST_QUALITY_REPORT_YES
 };
 
-
 AP_tree *st_ml_convert_species_name_to_node(ST_ML * st_ml,
-                                            const char *species_name);
+        const char *species_name);
 
 ST_ML_Color *st_ml_get_color_string(ST_ML * st_ml, char *species_name,
-                                    AP_tree * node, int start_ali_pos,
-                                    int end_ali_pos);
+        AP_tree * node, int start_ali_pos, int end_ali_pos);
 int st_ml_update_ml_likelihood(ST_ML * st_ml, char *result[4],
-                               int *latest_update, char *species_name,
-                               AP_tree * node);
+        int *latest_update, char *species_name, AP_tree * node);
 
-AW_window *st_create_quality_check_window(AW_root * aw_root,
-                                          GBDATA * gb_main);
+AW_window *st_create_quality_check_window(AW_root * aw_root, GBDATA * gb_main);
 
-GB_ERROR st_ml_check_sequence_quality(GBDATA * gb_main,
-                                      const char *tree_name,
-                                      const char *alignment_name,
-                                      AWT_csp * awt_csp,
-                                      int bucket_size, int marked_only,
-                                      st_report_enum report,
-                                      const char *filter_string,
-                                      const char *dest_field);
-
+GB_ERROR st_ml_check_sequence_quality(GBDATA * gb_main, const char *tree_name,
+        const char *alignment_name, AWT_csp * awt_csp, int bucket_size,
+        int marked_only, st_report_enum report, const char *filter_string,
+        const char *dest_field);
 
 #endif
