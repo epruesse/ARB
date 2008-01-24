648d43d7742cbad636933e7396d8a4378bbc950d
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2008-01-24 17:03:57 +0000

    - can also mark by acc etc. - option to ignore leading/trailing spaces

--- trunk/PERL_SCRIPTS/SPECIES/markSpecies.pl
+++ trunk/PERL_SCRIPTS/SPECIES/markSpecies.pl
@@ -18,6 +18,38 @@ BEGIN {
 use ARB;
 
 eval {
+  my $args = scalar(@ARGV);
+  if ($args<1 or $ARGV[0] eq '-h') {
+    die(
+        "Missing arguments\n".
+        "\n".
+        "Usage: cat yourFile \| perl markSpecies.pl [options] field\n".
+        "Purpose: Marks all species for which the content of the given <field>\n".
+        "         matches one line of <yourFile>.\n".
+        "         Commonly used <field>s are 'name' or 'acc', given you have a list of\n".
+        "         species shortnames or accession numbers.\n".
+        "options: -w             remove leading/trailing whitespace\n".
+        "");
+  }
+
+  my $removeWhitespace = 0;
+
+  while ($args>1) {
+    my $flag = $ARGV[0];
+    shift @ARGV;
+    $args--;
+
+    if ($flag eq '-w') {
+      $removeWhitespace = 1;
+    }
+    else {
+      die "Unkown argument '$flag'";
+    }
+  }
+
+  my $field = $ARGV[0];
+  shift @ARGV; # needed (otherwise PERL spits some strange error: implicit open..)
+
   my $gb_main = ARB::open(":","rw"); # connect to running ARB
   if (!$gb_main) { die "No running ARB found\n"; }
 
@@ -31,23 +63,29 @@ eval {
     my %marklist = ();
     while (<>) {
       chomp;
+      if ($removeWhitespace==1) {
+        $_ =~ s/^[ \t]+//go;
+        $_ =~ s/[ \t]+$//go;
+      }
       $marklist{$_} = 1;
     }
 
     my $inCount = scalar(keys %marklist);
     if ($inCount<1) { print "Warning: Empty input\n"; }
-    else { print "Read $inCount species names\n"; }
+    else { print "Read $inCount $field\'s from input\n"; }
 
     my $been_marked    = 0;
     my $already_marked = 0;
 
     my $gb_species = BIO::first_species($gb_main);
-    if (!$gb_species) { die "Database doesn't contain any species\n"; }
+    if (!$gb_species) { die "Database doesn't contain any matching species\n"; }
 
+    my %seen = ();
     while ($gb_species) {
-      my $species_name = BIO::read_string($gb_species, "name");
+      #my $species_name = BIO::read_string($gb_species, "name");
+      my $species_content = BIO::read_string($gb_species, $field);
       my $was_marked = ARB::read_flag($gb_species);
-      if ($marklist{$species_name}) {
+      if ($marklist{$species_content}) {
         if ($was_marked==1) {
           $already_marked++;
         }
@@ -55,16 +93,28 @@ eval {
           $error = ARB::write_flag($gb_species, "1");
           $been_marked++;
         }
-        delete $marklist{$species_name};
+        $seen{$species_content} = 1;
+        # delete $marklist{$species_content};
       }
       $gb_species = BIO::next_species($gb_species);
+    }
+
+    foreach (keys %seen) {
+      delete $marklist{$_};
     }
 
     my @notFound = keys %marklist;
     my $notFound = scalar(@notFound);
     if ($notFound>0) {
-      print("$notFound species not found in ARBDB:\n");
-      print @notFound."\n";
+      print("$notFound $field\'s not found in ARBDB:\n");
+      my $i;
+      for ($i=0; $i<$notFound and $i<15; $i++) {
+        print $notFound[$i]."\n";
+      }
+      # print @notFound."\n";
+      if ($i<$notFound) {
+        print "[Rest skipped]\n";
+      }
     }
     print "Marked $been_marked species";
     if ($already_marked>0) { print " ($already_marked species already marked)"; }
