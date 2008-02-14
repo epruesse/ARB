c99d2f0201cc4a8fbf92fb83bc47515b67088a91
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2008-02-14 15:10:32 +0000

    - untabified - new param 'dest_is_new' for MG_start_cb2() - fixed include wrapper

--- trunk/MERGE/mg_merge.hxx
+++ trunk/MERGE/mg_merge.hxx
@@ -1,17 +1,16 @@
-#ifndef mg_merge_hxx_included
-#define mg_merge_hxx_included
+#ifndef MG_MERGE_HXX
+#define MG_MERGE_HXX
 
 extern GBDATA *gb_merge;
 extern GBDATA *gb_dest;
 extern GBDATA *gb_main;
 
+AW_window *create_MG_main_window(AW_root *aw_root);
+void       MG_create_all_awars(AW_root *awr, AW_default aw_def, const char *fname_one = "db1.arb", const char *fname_two = "db2.arb");
+void       MG_start_cb(AW_window *aww); // load the databases
+void       MG_start_cb2(AW_window *aww, AW_root *aw_root, bool save_enabled, bool dest_is_new); // uses loaded databases
+GB_ERROR   MG_simple_merge(AW_root *awr); // simple merge of species
 
-AW_window	*create_MG_main_window(AW_root *aw_root);
-void 		MG_create_all_awars(AW_root *awr, AW_default aw_def, const char *fname_one = "db1.arb", const char *fname_two = "db2.arb");
-void 		MG_start_cb(AW_window *aww);	// load the databases
-void 		MG_start_cb2(AW_window *aww,AW_root *aw_root, int save_enabled = 1);	// uses loaded databases
-GB_ERROR	MG_simple_merge(AW_root *awr);						// simple merge of species
-
-
-
+#else
+#error mg_merge.hxx included twice
 #endif
