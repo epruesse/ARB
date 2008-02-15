50e85e252a82c404e381a49bbb0ace83673c0b49
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2008-02-15 17:19:26 +0000

    - fixed include wrapper - cleanup

--- ARB_GDE/gde.hxx
+++ ARB_GDE/gde.hxx
@@ -1,19 +1,30 @@
-#ifndef gde_hxx_included
-#define gde_hxx_included
+// ================================================================ //
+//                                                                  //
+//   File      : gde.hxx                                            //
+//   Purpose   : external interface of GDE functionality            //
+//   Time-stamp: <Fri Feb/15/2008 18:21 MET Coder@ReallySoft.de>    //
+//                                                                  //
+//   Institute of Microbiology (Technical University Munich)        //
+//   http://www.arb-home.de/                                        //
+//                                                                  //
+// ================================================================ //
 
-// char *(*func)(void *THIS, GBDATA **&the_species, char **&the_sequences,
-//		long &numberspecies,long &maxalignlen)
+#ifndef GDE_HXX
+#define GDE_HXX
+
 typedef unsigned char uchar;
 typedef enum { CGSS_WT_DEFAULT, CGSS_WT_EDIT, CGSS_WT_EDIT4 } gde_cgss_window_type;
 
 void create_gde_var(AW_root *aw_root, AW_default aw_def,
-		    char *(*get_sequences)(void *THIS, GBDATA **&the_species,
-					   uchar **&the_names,
-					   uchar **&the_sequences,
-					   long &numberspecies,long &maxalignlen)=0,
-		    gde_cgss_window_type wt=CGSS_WT_DEFAULT,
-		    void *THIS = 0);
-AW_window *AP_open_gde_window( AW_root *root );
+                    char *(*get_sequences)(void *THIS, GBDATA **&the_species,
+                                           uchar **&the_names,
+                                           uchar **&the_sequences,
+                                           long &numberspecies,long &maxalignlen)=0,
+                    gde_cgss_window_type wt=CGSS_WT_DEFAULT,
+                    void *THIS = 0);
+
 void GDE_load_menu(AW_window *awm,const char *menulabel=0,const char *menuitemlabel=0 );
 
-#endif
+#else
+#error gde.hxx included twice
+#endif // GDE_HXX
