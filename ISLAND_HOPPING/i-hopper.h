fe3bc161d90e5cefcf87f2623b37fc4cfffd6ed2
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2008-03-01 06:57:17 +0000

    - dos2unix, untabified, indented

--- ISLAND_HOPPING/i-hopper.h
+++ ISLAND_HOPPING/i-hopper.h
@@ -13,11 +13,11 @@
 EXT const char *Error;
 
 void Align(
- int nX,char X[],int secX[],char **XX,int nY,char Y[],int secY[],char **YY,
- int freqs,double fT,double fC,double fA,double fG,
- double rTC,double rTA,double rTG,double rCA,double rCG,double rAG,
- double dist,double supp,double gapA,double gapB,double gapC,double thres
-);
+           int nX,char X[],int secX[],char **XX,int nY,char Y[],int secY[],char **YY,
+           int freqs,double fT,double fC,double fA,double fG,
+           double rTC,double rTA,double rTG,double rCA,double rCG,double rAG,
+           double dist,double supp,double gapA,double gapB,double gapC,double thres
+           );
 
 #undef EXT
 
