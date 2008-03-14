3368c1111f02629ac7751b731080042510e75399
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2008-03-14 12:41:56 +0000

    - if no result-file is found, pick bipartitions-file or   parsimonyTree-file instead - can create consense-tree alternatively to importing   multiple trees - writes content of RAxML-info file into ARB tree comment

--- trunk/PERL_SCRIPTS/ARBTOOLS/raxml2arb.pl
+++ trunk/PERL_SCRIPTS/ARBTOOLS/raxml2arb.pl
@@ -4,7 +4,8 @@
 #   File      : raxml2arb.pl                                      #
 #   Purpose   : Import XX best of YY calculated raxml trees into  #
 #               ARB and generate comment containing likelyhood    #
-#   Time-stamp: <Wed Mar/05/2008 12:03 MET Coder@ReallySoft.de>   #
+#               and content of info file                          #
+#   Time-stamp: <Fri Mar/14/2008 13:09 MET Coder@ReallySoft.de>   #
 #                                                                 #
 #   Coded by Ralf Westram (coder@reallysoft.de) in March 2008     #
 #   Institute of Microbiology (Technical University Munich)       #
@@ -17,13 +18,19 @@ use warnings;
 
 sub arb_message($) {
   my ($msg) = @_;
+  print "Message: $msg\n";
   system("arb_message \"$msg\"");
 }
 
 sub error($) {
   my ($msg) = @_;
+  if ($msg =~ /at.*\.pl/o) {
+    my ($pe,$loc) = ($`,$&.$');
+    chomp $loc;
+    $msg = $pe."\n(".$loc.")\nPlease check console for additional error reasons";
+  }
   arb_message($msg);
-  die $msg."\n";
+  die $msg;
 }
 
 sub raxml_filename($$$) {
@@ -42,8 +49,15 @@ sub someWhat($$) {
 sub treeInfo($$) {
   my ($name,$run) = @_;
 
-  my $log    = raxml_filename('log',$name,$run);
-  my $result = raxml_filename('result',$name,$run);
+  my $result       = raxml_filename('result',$name,$run);
+  my $bipartitions = raxml_filename('bipartitions',$name,$run);
+  my $parsimony    = raxml_filename('parsimonyTree',$name,$run);
+  my $log          = raxml_filename('log',$name,$run);
+
+  if (not -f $result) { # no result tree, try bipartitions or parsimonyTree
+    if (-f $bipartitions) { return ($bipartitions,'unknown'); }
+    if (-f $parsimony) { return ($parsimony,'unknown'); }
+  }
 
   open(LOG,'<'.$log) || die "can't open '$log' (Reason: $!)";
   my $line = undef;
@@ -81,12 +95,13 @@ sub main() {
   eval {
     my $args = scalar(@ARGV);
 
-    if ($args != 3) {
-      die "Usage: raxml2arb.pl RUNNAME NUMBEROFRUNS TAKETREES\n".
-        "       Reads the best TAKETREES trees of NUMBEROFRUNS generated trees into ARB";
+    if ($args != 4) {
+      die "Usage: raxml2arb.pl RUNNAME NUMBEROFRUNS TAKETREES [import|consense]\n".
+        "       import: Import the best TAKETREES trees of NUMBEROFRUNS generated trees into ARB\n".
+        "       consense: Create and import consense tree of best TAKETREES trees of NUMBEROFRUNS generated trees\n";
     }
 
-    my ($RUNNAME,$NUMBEROFRUNS,$TAKETREES) = @ARGV;
+    my ($RUNNAME,$NUMBEROFRUNS,$TAKETREES,$CONSENSE) = @ARGV;
 
     if ($NUMBEROFRUNS<1) { die "NUMBEROFRUNS has to be 1 or more ($NUMBEROFRUNS)"; }
 
@@ -96,32 +111,93 @@ sub main() {
     my $createdTrees = scalar(keys %likelyhood);
     print "Found ".someWhat($createdTrees,'tree').":\n";
 
-    if ($TAKETREES>$createdTrees) {
-      arb_message("Cant take more trees ($TAKETREES) than calculated ($NUMBEROFRUNS)");
+    if ($TAKETREES > $createdTrees) {
+      arb_message("Cant take more trees ($TAKETREES) than calculated ($NUMBEROFRUNS) .. using all");
       $TAKETREES = $createdTrees;
+    }
+
+    my $calc_consense = 0;
+    if ($CONSENSE eq 'consense') {
+      if ($TAKETREES<2) {
+        arb_message("Need to take at least 2 trees to create a consense tree - importing..");
+        $CONSENSE = 'import';
+      }
+      else {
+        $calc_consense = 1;
+      }
+    }
+    elsif ($CONSENSE ne 'import') {
+      die "Unknown value '$CONSENSE' (expected 'import' or 'consense')";
     }
 
     my @sortedTrees = sort { $likelyhood{$b} <=> $likelyhood{$a}; } keys %likelyhood;
     foreach (@sortedTrees) { print "  $_ = ".$likelyhood{$_}."\n"; }
 
-    my @treesToImport = splice(@sortedTrees,0,$TAKETREES);
-    if (scalar(@treesToImport)<1) { die "No trees to import"; }
+    my @treesToTake = splice(@sortedTrees,0,$TAKETREES);
+    if (scalar(@treesToTake)<1) { die "No trees to $CONSENSE"; }
 
-    my $treesToImport = scalar(@treesToImport);
-    print "Importing ".someWhat($treesToImport,'tree').":\n";
-
-    my $count = undef;
-    if ($treesToImport>1) { $count = 1; }
+    my $treesToTake = scalar(@treesToTake);
     my $treename = 'tree_RAxML_'.$$;
+    my $infofile = 'RAxML_info.'.$RUNNAME;
 
-    foreach (@treesToImport) {
-      print "  $_ = ".$likelyhood{$_}."\n";
-      my $currTreename = $treename;
-      if (defined $count) {
-        $currTreename .= '_'.$count;
-        $count++;
+    if ($calc_consense==0) {
+      print 'Importing '.someWhat($treesToTake,'tree').":\n";
+      my $count = undef;
+      if ($treesToTake>1) { $count = 1; }
+
+      foreach (@treesToTake) {
+        print "  $_ = ".$likelyhood{$_}."\n";
+        my $currTreename = $treename;
+        if (defined $count) {
+          $currTreename .= '_'.$count;
+          $count++;
+        }
+        my $command = 'arb_read_tree '.$currTreename.' '.$_.' "likelyhood='.$likelyhood{$_}.'"';
+        if (-f $infofile) { $command .= ' -commentFromFile '.$infofile; }
+
+        system($command)==0 || die "can't execute '$command' (Reason: $!)";
       }
-      my $command = 'arb_read_tree '.$currTreename.' '.$_.' "likelyhood='.$likelyhood{$_}.'"';
+    }
+    else { # calc consense tree
+      my $minLH  = undef;
+      my $maxLH  = undef;
+      my $meanLH = 0;
+
+      print 'Consensing '.someWhat($treesToTake,'tree').":\n";
+      open(INTREE,'>intree') || die "can't write 'intree' (Reason: $!)";
+      foreach (@treesToTake) {
+        my $LH = $likelyhood{$_};
+        print "  $_ = $LH\n";
+        if (not defined $minLH) {
+          $minLH = $maxLH = $LH;
+        }
+        else {
+          if ($LH < $minLH) { $minLH = $LH; }
+          if ($LH > $maxLH) { $maxLH = $LH; }
+        }
+        $meanLH += $LH;
+
+        open(RAXMLTREE,'<'.$_) || die "can't read '$_' (Reason: $!)";
+        foreach (<RAXMLTREE>) {
+          print INTREE $_;
+        }
+        close(RAXMLTREE);
+        print INTREE "\n";
+      }
+      close(INTREE);
+
+      $meanLH /= $treesToTake;
+
+      my $command = 'arb_echo y | consense';
+      system($command)==0 || die "can't execute '$command' (Reason: $!)";
+
+      if (not -f 'outtree') {
+        die "Consense failed (no 'outtree' generated)";
+      }
+
+      my $comment = "Consense tree of $treesToTake trees\nLikelyhood: min=$minLH mean=$meanLH max=$maxLH";
+      $command = 'arb_read_tree -consense '.$treesToTake.' '.$treename.' outtree "'.$comment.'"';
+      if (-f $infofile) { $command .= ' -commentFromFile '.$infofile; }
       system($command)==0 || die "can't execute '$command' (Reason: $!)";
     }
   };
