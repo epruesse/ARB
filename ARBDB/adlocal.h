5b1b5f56537f1ea6c3cdf71e45dc839861efa888
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2008-05-26 11:00:39 +0000

    - defines hash-index macros (now used by adhash.c and adindex.c)

--- ARBDB/adlocal.h
+++ ARBDB/adlocal.h
@@ -595,4 +595,36 @@ struct GBL_command_table {
     GBL_COMMAND function;
 };
 
+// -------------------------------
+//      hash index calculation
+// -------------------------------
+
+#define GB_CALC_HASH_INDEX_CASE_SENSITIVE(string,index,size) do {       \
+        const char *local_ptr = (string);                               \
+        int local_i;                                                    \
+        (index) = 0xffffffffL;                                          \
+        while ((local_i=(*(local_ptr++)))) {                            \
+            (index) = crctab[((int)(index)^local_i) & 0xff] ^ ((index) >> 8); \
+        }                                                               \
+        (index) = (index) % (size);                                     \
+    } while(0)
+
+#define GB_CALC_HASH_INDEX_CASE_IGNORED(string,index,size) do {         \
+        const char *local_ptr = (string);                               \
+        int local_i;                                                    \
+        (index) = 0xffffffffL;                                          \
+        while ( (local_i = *(local_ptr++))){                            \
+            (index) = crctab[((int) (index) ^ toupper(local_i)) & 0xff] ^ ((index) >> 8); \
+        }                                                               \
+        (index) = (index) % (size);                                     \
+    } while(0)
+
+#define GB_CALC_HASH_INDEX(string, index, size, caseSens) do {          \
+        if ((caseSens) == GB_IGNORE_CASE)                               \
+            GB_CALC_HASH_INDEX_CASE_IGNORED(string, index, size);       \
+        else                                                            \
+            GB_CALC_HASH_INDEX_CASE_SENSITIVE(string, index, size);     \
+    } while(0)
+
+
 #endif
