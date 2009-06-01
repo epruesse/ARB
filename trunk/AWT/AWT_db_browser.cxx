b3fdf56ce114d29653fce9afa81fa7a864a34f52
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2009-06-01 10:33:02 +0000

    * debug module calls each callback only once 	  - added 'Forget called' 	  - automatically forbid functions for clone window, if forbidden in main window

--- trunk/AWT/AWT_db_browser.cxx
+++ trunk/AWT/AWT_db_browser.cxx
@@ -922,6 +922,8 @@ void AWT_create_debug_menu(AW_window *awmm) {
         awmm->insert_menu_topic("!run_all_cbs_nloc",  "Call all callbacks (reverse)",     "", "", AWM_ALL, callallcallbacks, 3, 0);
         awmm->insert_menu_topic("!run_all_cbs_rnd",   "Call all callbacks (random)",      "", "", AWM_ALL, callallcallbacks, 10, 0);
         awmm->insert_menu_topic("!run_all_cbs_inf",   "Call all callbacks (forever)",     "", "", AWM_ALL, callallcallbacks, 11, 0);
+        awmm->insert_separator();
+        awmm->insert_menu_topic("!forget_called_cbs", "Forget called",     "", "", AWM_ALL, callallcallbacks, -1, 0);
         awmm->close_sub_menu();
     }
 }
--- trunk/WINDOW/AW_debug.cxx
+++ trunk/WINDOW/AW_debug.cxx
@@ -30,14 +30,34 @@ using namespace std;
 typedef vector<string> CallbackArray;
 typedef CallbackArray::const_iterator CallbackIter;
 
-static GB_HASH *dontCallHash = 0;
+static GB_HASH *dontCallHash      = 0;
+static GB_HASH *alreadyCalledHash = 0;
+
+static void forgetCalledCallbacks() {
+    if (alreadyCalledHash) GBS_free_hash(alreadyCalledHash);
+    alreadyCalledHash = GBS_create_hash(5000, GB_MIND_CASE);
+}
+
+static long auto_dontcall1(const char *key, long value, void *cl_hash) {
+    if (strncmp(key, "ARB_NT/", 7) == 0) {
+        GB_HASH *autodontCallHash = (GB_HASH*)cl_hash;
+        GBS_write_hash(autodontCallHash, GBS_global_string("ARB_NT_1/%s", key+7), value);
+    }
+    return value;
+}
+static long auto_dontcall2(const char *key, long value, void *) {
+    GBS_write_hash(dontCallHash, key, value);
+    return value;
+}
 
 static void build_dontCallHash() {
     aw_assert(!dontCallHash);
     dontCallHash = GBS_create_hash(30, GB_MIND_CASE);
+    forgetCalledCallbacks();
 
-    GBS_write_hash(dontCallHash, "ARB_NT/QUIT", 1);
-    GBS_write_hash(dontCallHash, "quit",        1);
+    GBS_write_hash(dontCallHash, "ARB_NT/QUIT",    1);
+    GBS_write_hash(dontCallHash, "quit",           1);
+    GBS_write_hash(dontCallHash, "ARB_EDIT4/QUIT", 1);
 
     // avoid start of some external programs:
     GBS_write_hash(dontCallHash, "xterm",                    2);
@@ -54,16 +74,37 @@ static void build_dontCallHash() {
     GBS_write_hash(dontCallHash, "primer_design",            2);
     GBS_write_hash(dontCallHash, "CHECK_GCG_LIST/SHOW_FILE", 2);
 
+    // avoid saving
+    GBS_write_hash(dontCallHash, "save_changes", 3);
+    GBS_write_hash(dontCallHash, "save_props",   3);
+
 #if defined(DEVEL_RALF)
 #warning crashing - fix later
-    GBS_write_hash(dontCallHash, "ARB_NT/mark_duplicates",         1);
-    GBS_write_hash(dontCallHash, "ARB_NT/view_probe_group_result", 1);
+    GBS_write_hash(dontCallHash, "ARB_NT/mark_duplicates",         4);
+    GBS_write_hash(dontCallHash, "ARB_NT/view_probe_group_result", 4);
+    GBS_write_hash(dontCallHash, "check_gcg_list",                 4);
 #endif // DEVEL_RALF
+
+#if defined(DEVEL_RALF)
+#warning test callbacks asking questions again later    
+    GBS_write_hash(dontCallHash, "ARB_NT/mark_deep_branches",  5);
+    GBS_write_hash(dontCallHash, "ARB_NT/mark_degen_branches", 5);
+    GBS_write_hash(dontCallHash, "ARB_NT/mark_long_branches",  5);
+    GBS_write_hash(dontCallHash, "ARB_NT/tree_scale_lengths",  5);
+    GBS_write_hash(dontCallHash, "del_marked",                 5);
+#endif // DEVEL_RALF
+
+    GB_HASH *autodontCallHash = GBS_create_hash(30, GB_MIND_CASE);
+    GBS_hash_do_loop(dontCallHash, auto_dontcall1, autodontCallHash);
+    GBS_hash_do_loop(autodontCallHash, auto_dontcall2, dontCallHash);
+    GBS_free_hash(autodontCallHash);
 }
 
 static long collect_callbacks(const char *key, long value, void *cl_callbacks) {
-    CallbackArray *callbacks = reinterpret_cast<CallbackArray*>(cl_callbacks);
-    callbacks->push_back(string(key));
+    if (GBS_read_hash(alreadyCalledHash, key) == 0) { // don't call twice
+        CallbackArray *callbacks = reinterpret_cast<CallbackArray*>(cl_callbacks);
+        callbacks->push_back(string(key));
+    }
     return value;
 }
 
@@ -98,9 +139,12 @@ void AW_root::callallcallbacks(int mode) {
     if (mode == 11) {
         aw_message("Calling callbacks forever");
         for (int iter = 1; ; ++iter) { // forever
-            callallcallbacks(2);
+            callallcallbacks(10); // call all in random order
             aw_message(GBS_global_string("All callbacks were called (iteration %i)", iter));
         }
+    }
+    else if (mode == -1) {
+        forgetCalledCallbacks();
     }
     else {
         CallbackArray callbacks;
@@ -126,13 +170,14 @@ void AW_root::callallcallbacks(int mode) {
         }
 
         CallbackIter end  = callbacks.end();
-        size_t       curr = 1;
 
         for (int pass = 1; pass <= 2; ++pass) {
-            CallbackIter cb = callbacks.begin();
+            size_t       curr = 1;
+            CallbackIter cb   = callbacks.begin();
+
 #if defined(DEVEL_RALF)
             // skip first xxx callbacks (to go to bug quicker)
-            int skip  = 0; // 150;                             
+            int skip  = 0;                          // 150;
             advance(cb, skip);
             curr     += skip;
 #endif // DEVEL_RALF
@@ -142,29 +187,32 @@ void AW_root::callallcallbacks(int mode) {
                 bool        this_pass      = remote_command[0] == '-' ? (pass == 2) : (pass == 1);
 
                 if (this_pass) {
-                    if (remote_command[0] == '!' || GBS_read_hash(dontCallHash, remote_command)) {
-                        aw_message(GBS_global_string("Skipped callback %zu/%zu (%s)", curr, count, remote_command)); { {
-                            }
-                        }
+                    GBS_write_hash(alreadyCalledHash, remote_command, 1); // don't call twice
+
+                    AW_cb_struct *cbs = (AW_cb_struct *)GBS_read_hash(prvt->action_hash, remote_command);
+                    bool skipcb = remote_command[0] == '!' || GBS_read_hash(dontCallHash, remote_command);
+                    if (!skipcb) {
+                        if (cbs->f == (AW_CB)AW_help_entry_pressed) skipcb = true;
+                    }
+                    if (skipcb) {
+                        fprintf(stderr, "Skipped callback %zu/%zu (%s)\n", curr, count, remote_command);
                     }
                     else {
-                        aw_message(GBS_global_string("Calling back %zu/%zu (%s)", curr, count, remote_command)); {
-                        }
+                        fprintf(stderr, "Calling back %zu/%zu (%s)\n", curr, count, remote_command);
                         
-                        AW_cb_struct *cbs = (AW_cb_struct *)GBS_read_hash(prvt->action_hash, remote_command);
                         GB_clear_error();
 
                         cbs->run_callback();
                         process_events();
 
                         if (GB_have_error()) {
-                            aw_message(GBS_global_string("Unhandled error in '%s': %s", remote_command, GB_await_error()));
+                            fprintf(stderr, "Unhandled error in '%s': %s\n", remote_command, GB_await_error());
                         }
 
                         if (cbs->f == AW_POPUP) {
                             AW_window *awp = cbs->pop_up_window;
                             if (awp) {
-                                aw_message(GBS_global_string("Popping down window '%s'", awp->get_window_id()));
+                                fprintf(stderr, "Popping down window '%s'\n", awp->get_window_id());
                                 awp->hide();
                                 process_events();
                             }
@@ -173,14 +221,14 @@ void AW_root::callallcallbacks(int mode) {
                 }
                 else {
                     if (pass == 1) {
-                        aw_message(GBS_global_string("Delayed callback %zu/%zu (%s)", curr, count, remote_command));
+                        fprintf(stderr, "Delayed callback %zu/%zu (%s)", curr, count, remote_command);
                     }
                 }
 
                 curr++;
             }
 
-            if (pass == 1) aw_message("Executing delayed callbacks:");
+            if (pass == 1) fprintf(stderr, "Executing delayed callbacks:\n");
         }
     }
 }
