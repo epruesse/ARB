5b20ff157d31ea15acedf4fe3d9ca84b7569682c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2009-06-06 13:53:23 +0000

    * renamed defines

--- trunk/SL/TREE_READ/TreeRead.h
+++ trunk/SL/TREE_READ/TreeRead.h
@@ -16,8 +16,8 @@
 #include <arbdbt.h>
 #endif
 
-#define DEFAULT_LENGTH        0.1 /* default length of tree-edge w/o given length */
-#define DEFAULT_LENGTH_MARKER -1000.0 /* tree-edges w/o length are marked with this value during read and corrected in TREE_scale */
+#define TREE_DEFLEN        0.1 /* default length of tree-edge w/o given length */
+#define TREE_DEFLEN_MARKER -1000.0 /* tree-edges w/o length are marked with this value during read and corrected in TREE_scale */
 
 GBT_TREE *TREE_load(const char *path, int structuresize, char **commentPtr, int allow_length_scaling, char **warningPtr);
 void      TREE_scale(GBT_TREE *tree, double length_scale, double bootstrap_scale);
