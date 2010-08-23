34aa925ac6b3b57a0b060d1a74fba95188c2988f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2010-08-23 09:51:51 +0000

    * unit tests now also work with object files (not only with libs) 	* added unit tests to 'arb_probe.o' 	  - added a test pt-server to arb_tcp.dat 	  - start small pt-server (pre-build) 	  - test one probe match hitting one target 	  - test one probe design and match designed probe 	* time compilation and tests 	* minor fixes for patch generation

--- trunk/TOOLS/arb_probe.cxx
+++ trunk/TOOLS/arb_probe.cxx
@@ -64,7 +64,7 @@ int init_local_com_struct()
     return 0;
 }
 
-void aw_message(const char *error) {
+static void aw_message(const char *error) {
     printf("%s\n", error);
 }
 
@@ -72,6 +72,10 @@ static const char *AP_probe_pt_look_for_server()
 {
     char choice[256];
     sprintf(choice, "ARB_PT_SERVER%i", P.SERVERID);
+
+#if defined(UNIT_TESTS)
+    if (P.SERVERID == -1) strcpy(choice, "ARB_TEST_PT_SERVER");
+#endif // UNIT_TESTS
 
     GB_ERROR error = arb_look_and_start_server(AISC_MAGIC_NUMBER, choice, 0);
     if (error) {
@@ -82,14 +86,14 @@ static const char *AP_probe_pt_look_for_server()
 }
 
 
-int probe_design_send_data(T_PT_PDC  pdc) {
+static int probe_design_send_data(T_PT_PDC  pdc) {
     if (aisc_put(pd_gl.link, PT_PDC, pdc, PDC_CLIPRESULT, P.DESIGNCPLIPOUTPUT, NULL))
         return 1;
 
     return 0;
 }
 
-void AP_probe_design_event() {
+static char *AP_probe_design_event() {
     T_PT_PDC     pdc;
     T_PT_TPROBE  tprobe;
     bytestring   bs;
@@ -98,17 +102,17 @@ void AP_probe_design_event() {
     {
         const char *servername = AP_probe_pt_look_for_server();
 
-        if (!servername) return;
+        if (!servername) return NULL;
         pd_gl.link = (aisc_com *)aisc_open(servername, &pd_gl.com, AISC_MAGIC_NUMBER);
     }
 
     if (!pd_gl.link) {
         aw_message ("Cannot contact Probe bank server ");
-        return;
+        return NULL;
     }
     if (init_local_com_struct()) {
         aw_message ("Cannot contact Probe bank server (2)");
-        return;
+        return NULL;
     }
 
     bs.data = (char*)(P.DESINGNAMES);
@@ -132,7 +136,7 @@ void AP_probe_design_event() {
 
     if (probe_design_send_data(pdc)) {
         aw_message ("Connection to PT_SERVER lost (1)");
-        return;
+        return NULL;
     }
     apd_sequence *s;
     for (s = P.sequence; s; s = s->next) {
@@ -157,7 +161,7 @@ void AP_probe_design_event() {
                       LOCS_ERROR,    &locs_error,
                       NULL)) {
             aw_message ("Connection to PT_SERVER lost (1)");
-            return;
+            return NULL;
         }
         if (*locs_error) {
             aw_message(locs_error);
@@ -167,14 +171,17 @@ void AP_probe_design_event() {
 
     aisc_get(pd_gl.link, PT_PDC, pdc,
               PDC_TPROBE, &tprobe,
-              NULL);
+             NULL);
 
+
+    GBS_strstruct *outstr = GBS_stropen(1000);
 
     if (tprobe) {
         aisc_get(pd_gl.link, PT_TPROBE, tprobe,
-                  TPROBE_INFO_HEADER,   &match_info,
-                  NULL);
-        printf("%s\n", match_info);
+                 TPROBE_INFO_HEADER,   &match_info,
+                 NULL);
+        GBS_strcat(outstr, match_info);
+        GBS_chrcat(outstr, '\n');
         free(match_info);
     }
 
@@ -184,13 +191,16 @@ void AP_probe_design_event() {
                       TPROBE_NEXT,      &tprobe,
                       TPROBE_INFO,      &match_info,
                       NULL)) break;
-        printf("%s\n", match_info);
+        GBS_strcat(outstr, match_info);
+        GBS_chrcat(outstr, '\n');
     }
+
     aisc_close(pd_gl.link); pd_gl.link = 0;
-    return;
+
+    return GBS_strclose(outstr);
 }
 
-void AP_probe_match_event()
+static char *AP_probe_match_event()
 {
     T_PT_PDC        pdc;
     T_PT_MATCHLIST  match_list;
@@ -200,17 +210,17 @@ void AP_probe_match_event()
     {
         const char *servername = AP_probe_pt_look_for_server();
 
-        if (!servername) return;
+        if (!servername) return NULL;
         pd_gl.link = (aisc_com *)aisc_open(servername, &pd_gl.com, AISC_MAGIC_NUMBER);
     }
 
     if (!pd_gl.link) {
         aw_message ("Cannot contact Probe bank server ");
-        return;
+        return NULL;
     }
     if (init_local_com_struct()) {
         aw_message ("Cannot contact Probe bank server (2)");
-        return;
+        return NULL;
     }
 
     aisc_create(pd_gl.link, PT_LOCS, pd_gl.locs,
@@ -218,7 +228,7 @@ void AP_probe_match_event()
                 NULL);
     if (probe_design_send_data(pdc)) {
         aw_message ("Connection to PT_SERVER lost (2)");
-        return;
+        return NULL;
     }
 
 
@@ -233,7 +243,7 @@ void AP_probe_match_event()
                   NULL)) {
         free(probe);
         aw_message ("Connection to PT_SERVER lost (2)");
-        return;
+        return NULL;
     }
 
     long match_list_cnt;
@@ -249,29 +259,22 @@ void AP_probe_match_event()
         aw_message(locs_error);
     }
     free(locs_error);
-
-    char toksep[2];
-    toksep[0] = 1;
-    toksep[1] = 0;
-    printf("%s\n", bs.data);
-
     aisc_close(pd_gl.link);
-    free(bs.data);
 
-    return;
+    return bs.data; // freed by caller
 }
 
-int pargc;
-char **pargv;
-int helpflag;
+static int          pargc;
+static const char **pargv;
+static int          helpflag;
 
-int getInt(const char *param, int val, int min, int max, const char *description) {
+static int getInt(const char *param, int val, int min, int max, const char *description) {
     if (helpflag) {
         printf("    %s=%3i [%3i:%3i] %s\n", param, val, min, max, description);
         return 0;
     }
     int   i;
-    char *s = 0;
+    const char *s = 0;
 
     arb_assert(pargc >= 1);     // otherwise s stays 0
 
@@ -295,14 +298,14 @@ int getInt(const char *param, int val, int min, int max, const char *description
     return val;
 }
 
-const char *getString(const char *param, const char *val, const char *description) {
+static const char *getString(const char *param, const char *val, const char *description) {
     if (helpflag) {
         if (!val) val = "";
         printf("    %s=%s   %s\n", param, val, description);
         return 0;
     }
     int   i;
-    char *s = 0;
+    const char *s = 0;
 
     arb_assert(pargc >= 1);     // otherwise s stays 0
 
@@ -322,11 +325,12 @@ const char *getString(const char *param, const char *val, const char *descriptio
     return s;
 }
 
-int main(int argc, char ** argv) {
+static int parseCommandLine(int argc, const char ** argv) {
     pargc = argc;
     pargv = argv;
+    
     if (argc<=1) helpflag = 1;
-    else        helpflag = 0;
+    else helpflag         = 0;
 
     P.SERVERID = getInt("serverid", 0, 0, 100, "Server Id, look into $ARBHOME/lib/arb_tcp.dat");
 
@@ -358,18 +362,97 @@ int main(int argc, char ** argv) {
     P.COMPLEMENT = getInt("matchcomplement", 0, 0, 1, "Match reversed and complemented probe");
     P.WEIGHTED   = getInt("matchweighted", 0, 0, 1,  "Use weighted mismatches");
 
-
     if (pargc>1) {
         printf("Unknown Parameter %s\n", pargv[1]);
-        exit(-1);
+        return false;
     }
-    if (helpflag) return 0;
+    return !helpflag;
+}
 
+static char *execute() {
+    char *answer;
     if (*P.DESINGNAMES || P.sequence) {
-        AP_probe_design_event();
+        answer = AP_probe_design_event();
     }
     else {
-        AP_probe_match_event();
+        answer = AP_probe_match_event();
     }
-    return 0;
+    return answer;
 }
+
+int main(int argc, const char ** argv) {
+    bool ok = parseCommandLine(argc, argv);
+    if (ok) {
+        char *answer = execute();
+        fputs(answer, stdout);
+        free(answer);
+    }
+    return ok ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+// --------------------------------------------------------------------------------
+
+#if (UNIT_TESTS == 1)
+
+#include <test_unit.h>
+
+static void test_arb_probe(int fake_argc, const char **fake_argv, const char *expected) {
+    TEST_ASSERT_EQUAL(true, parseCommandLine(fake_argc, fake_argv));
+    P.SERVERID = -1; // use fake server
+    TEST_ASSERT_EQUAL(execute(), expected);
+}
+
+#define COUNT(array) sizeof(array)/sizeof(*array)
+
+void TEST_SLOW_match_probe() {
+    const char *arguments[] = {
+        "fake", // "program"-name 
+        "matchsequence=UAUCGGAGAGUUUGA", 
+    };
+    const char *expected =
+        "    name---- fullname mis N_mis wmis pos rev          'UAUCGGAGAGUUUGA'\1"
+        "BcSSSS00\1" "  BcSSSS00            0     0  0.0   2 0   .......UU-===============-UCAAGUCGA\1";
+
+    test_arb_probe(COUNT(arguments), arguments, expected);
+}
+
+void TEST_SLOW_design_probe() {
+    const char *arguments[] = {
+        "fake", // "program"-name
+        "designnames=ClnCorin#CltBotul#CPPParap#ClfPerfr",
+        "designmintargets=100",
+    };
+    const char *expected = 
+        "Probe design Parameters:\n"
+        "Length of probe      18\n"
+        "Temperature        [ 0.0 -400.0]\n"
+        "GC-Content         [30.0 -80.0]\n"
+        "E.Coli Position    [   0 -10000]\n"
+        "Max Non Group Hits     0\n"
+        "Min Group Hits       100%\n"
+        "Target             le     apos ecol grps  G+C 4GC+2AT Probe sequence     | Decrease T by n*.3C -> probe matches n non group species\n"
+        "CGAAAGGAAGAUUAAUAC 18 A=    93   93    4 33.3 48.0    GUAUUAAUCUUCCUUUCG |  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,\n"
+        "GAAAGGAAGAUUAAUACC 18 A+     1   94    4 33.3 48.0    GGUAUUAAUCUUCCUUUC |  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,\n"
+        "UCAAGUCGAGCGAUGAAG 18 B=    17   17    4 50.0 54.0    CUUCAUCGCUCGACUUGA |  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,\n"
+        "AUCAAGUCGAGCGAUGAA 18 B-     1   16    4 44.4 52.0    UUCAUCGCUCGACUUGAU |  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  2,  3,\n";
+    
+    test_arb_probe(COUNT(arguments), arguments, expected);
+}
+
+void TEST_SLOW_match_designed_probe() {
+    const char *arguments[] = {
+        "fake", // "program"-name 
+        "matchsequence=UCAAGUCGAGCGAUGAAG", 
+    };
+    const char *expected =
+    "    name---- fullname mis N_mis wmis pos rev          'UCAAGUCGAGCGAUGAAG'\1"
+    "ClnCorin\1" "  ClnCorin            0     0  0.0  17 0   .GAGUUUGA-==================-UUCCUUCGG\1"
+    "CltBotul\1" "  CltBotul            0     0  0.0  17 0   ........A-==================-CUUCUUCGG\1"
+    "CPPParap\1" "  CPPParap            0     0  0.0  17 0   AGAGUUUGA-==================-UUCCUUCGG\1"
+    "ClfPerfr\1" "  ClfPerfr            0     0  0.0  17 0   AGAGUUUGA-==================-UUUCCUUCG\1";
+
+    test_arb_probe(COUNT(arguments), arguments, expected);
+}
+
+
+#endif
