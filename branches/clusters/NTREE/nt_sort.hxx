8df8b08586af54d258af2e3eddfbc3ce2fdac9bb
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2010-08-25 11:29:19 +0000

    obsolete (already merged)

--- branches/clusters/NTREE/nt_sort.hxx
+++
@@ -1,8 +0,0 @@
-#error "bad header"
-
-extern GBDATA *GLOBAL_gb_main;
-
-AW_window *NT_build_resort_window(AW_root *awr);
-void       NT_build_resort_awars(AW_root *awr, AW_default aw_def);
-void       NT_resort_data_by_phylogeny(AW_window *dummy, GBT_TREE **ptree);
-GB_ERROR   NT_resort_data_base(GBT_TREE *tree, const char *key1, const char *key2, const char *key3) __ATTR__USERESULT;
