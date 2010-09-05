6a58cb8dd350f3bc5130f52b7c016e3778b32ab1
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2010-09-05 23:04:20 +0000

    untagged old_secedit

--- tags/old_secedit/NTREE/ad_trees.hxx
+++
@@ -1,3 +0,0 @@
-void create_trees_var(AW_root *aw_root, AW_default aw_def);
-
-AW_window *create_trees_window(AW_root *aw_root);
--- tags/old_secedit/ORS_SERVER/ors_lib.h
+++
@@ -1,6 +0,0 @@
-// time and date formats for  time_and_date_string()
-#define TIME_HM 1
-#define DATE_DMY 2
-#define DATE_TIME 3
-
-#define ROOT_USERPATH "/"
