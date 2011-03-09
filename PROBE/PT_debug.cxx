3c3a67aa0e87f7e5d3eeca0fff839626fea44526
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-03-09 12:57:43 +0000

    - stub for debugging code

---
+++ PROBE/PT_debug.cxx
@@ -0,0 +1,12 @@
+// ============================================================= //
+//                                                               //
+//   File      : PT_debug.cxx                                    //
+//   Purpose   : debugging code                                  //
+//                                                               //
+//   Coded by Ralf Westram (coder@reallysoft.de) in March 2011   //
+//   Institute of Microbiology (Technical University Munich)     //
+//   http://www.arb-home.de/                                     //
+//                                                               //
+// ============================================================= //
+
+
