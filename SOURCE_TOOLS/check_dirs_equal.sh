86b7e4a70a55ad0bf4015fd60078d33b954d3e7f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-03-31 16:02:24 +0000

    - check_dirs_equal.sh compares complete trees

--- SOURCE_TOOLS/check_dirs_equal.sh
+++ SOURCE_TOOLS/check_dirs_equal.sh
@@ -6,9 +6,9 @@ check_existance_in() {
     local RESULT=true
 
     while [ ! -z "$FILE" ]; do
-        local NAME=`basename $FILE`
-        if [ ! -e $OTHER/$NAME ]; then
-            echo "$OTHER/$NAME:0: Error: has not been generated"
+        # local NAME=`basename $FILE`
+        if [ ! -e $OTHER/$FILE ]; then
+            echo "$OTHER/$FILE:0: Error: has not been generated"
             RESULT=false
         fi
         shift
@@ -39,14 +39,17 @@ check_equality() {
     local RESULT=true
 
     while [ ! -z "$NEWFILE" ]; do
-        local NAME=`basename $NEWFILE`
-        local OLDFILE=$OLDDIR/$NAME
-        local ISDIFF=`diff $NEWFILE $OLDFILE | wc -l`
-        if [ "$ISDIFF" != "0" ]; then
-            diff_as_grep $OLDFILE $NEWFILE
-            RESULT=false
-        else
-            echo "equal $NEWFILE"
+        # local NAME=`basename $NEWFILE`
+        local OLDFILE=$OLDDIR/$NEWFILE
+
+        if [ ! -d $OLDFILE -a ! -d $NEWFILE ]; then
+            local ISDIFF=`diff $NEWFILE $OLDFILE | wc -l`
+            if [ "$ISDIFF" != "0" ]; then
+                diff_as_grep $OLDFILE $NEWFILE
+                RESULT=false
+            # else
+                # echo "equal $NEWFILE"
+            fi
         fi
         shift
         NEWFILE=$2
@@ -54,39 +57,74 @@ check_equality() {
     $RESULT
 }
 
+get_files() {
+    # returns relative paths of files matching WILDCARD 
+    local DIR=$1
+    local WILDCARD=$2
+    # echo "DIR='$DIR'" 1>&2
+    # echo "WILDCARD='$WILDCARD'" 1>&2
+    ( \
+        cd $DIR || exit; \
+        find -P . -name "$WILDCARD" \
+        | grep -v '\/\.svn\/' \
+        | grep -v '\/patches\.arb\/' \
+        | sed 's/^\.\///' \
+        )
+}
+
 check_missing() {
     local D1=$1
     local D2=$2
-    local TYPE=$3
+    local WILDCARD="$3"
 
-    local FILES1=`dir $D1/*.$TYPE`
-    local FILES2=`dir $D2/*.$TYPE`
+    local FILES1=`get_files $D1 "$WILDCARD"`
+    local FILES2=`get_files $D2 "$WILDCARD"`
 
-    check_existance_in $D2 $FILES1 && \
-    check_existance_in $D1 $FILES2
+    local RESULT=true
+    
+    check_existance_in $D2 $FILES1 || RESULT=false
+    check_existance_in $D1 $FILES2 || RESULT=false
+
+    $RESULT
 }
 
 compare_dirs() {
     local NEWDIR=$1
     local OLDDIR=$2
-    local TYPE=$3
+    local WILDCARD="$3"
+    local RECURSE=$4
 
-    local NEWFILES=`dir $NEWDIR/*.$TYPE`
+    local NEWFILES=`get_files $NEWDIR "$WILDCARD"`
 
-    check_missing $NEWDIR $OLDDIR $TYPE && \
-    check_missing $OLDDIR $NEWDIR $TYPE && \
-    check_equality $OLDDIR $NEWFILES
+    local RESULT=true
+    
+    check_missing $NEWDIR $OLDDIR "$WILDCARD" || RESULT=false
+    check_equality $OLDDIR $NEWFILES || RESULT=false
+
+    $RESULT
 }
 
 # --------------------
 
-NEWDIR=$1
-OLDDIR=$2
-TYPE=$3
+GLOBAL_NEWDIR=$1
+GLOBAL_OLDDIR=$2
+GLOBAL_WILDCARD="$3"
 
-if [ -z "$TYPE" ]; then
-    echo "Usage: check_dirs_equal.sh newdir olddir extension"
+if [ -z "$GLOBAL_WILDCARD" ]; then
+    echo "Usage: check_dirs_equal.sh newdir olddir wildcard"
+    echo "Recursively compares two directory-trees"
     false
 else
-    compare_dirs $NEWDIR $OLDDIR $TYPE
+    cd $GLOBAL_NEWDIR || exit
+    GLOBAL_NEWDIR=`pwd`
+    cd - >/dev/null
+    cd $GLOBAL_OLDDIR || exit
+    GLOBAL_OLDDIR=`pwd`
+    cd - >/dev/null
+
+    # echo "GLOBAL_NEWDIR='$GLOBAL_NEWDIR'"
+    # echo "GLOBAL_OLDDIR='$GLOBAL_OLDDIR'"
+    
+    compare_dirs $GLOBAL_NEWDIR $GLOBAL_OLDDIR "$GLOBAL_WILDCARD" 
 fi
+
