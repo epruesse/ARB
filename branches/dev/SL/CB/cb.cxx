6f292dd54d40f009de033fb4ae33ee9410077c74
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-08-12 11:55:10 +0000

    * new module CB     - setup test     - added stubs

---
+++ branches/dev/SL/CB/cb.cxx
@@ -0,0 +1,27 @@
+// ============================================================== //
+//                                                                //
+//   File      : cb.cxx                                           //
+//   Purpose   : currently only a test sandbox                    //
+//                                                                //
+//   Coded by Ralf Westram (coder@reallysoft.de) in August 2011   //
+//   Institute of Microbiology (Technical University Munich)      //
+//   http://www.arb-home.de/                                      //
+//                                                                //
+// ============================================================== //
+
+#include "cbtypes.h"
+
+using namespace std;
+
+// --------------------------------------------------------------------------------
+
+#ifdef UNIT_TESTS
+#include <test_unit.h>
+
+void TEST_cbs() {
+    // TEST_ASSERT(0);
+}
+
+#endif // UNIT_TESTS
+
+
