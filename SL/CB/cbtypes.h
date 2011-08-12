803fb1b3cd566180a80f7c5e5c9176be54ce248f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-08-12 11:55:10 +0000

    * new module CB     - setup test     - added stubs

---
+++ SL/CB/cbtypes.h
@@ -0,0 +1,22 @@
+// ============================================================== //
+//                                                                //
+//   File      : cbtypes.h                                        //
+//   Purpose   : generic cb types                                 //
+//                                                                //
+//   Coded by Ralf Westram (coder@reallysoft.de) in August 2011   //
+//   Institute of Microbiology (Technical University Munich)      //
+//   http://www.arb-home.de/                                      //
+//                                                                //
+// ============================================================== //
+
+#ifndef CBTYPES_H
+#define CBTYPES_H
+
+#ifndef CB_H
+#include <cb.h>
+#endif
+
+
+#else
+#error cbtypes.h included twice
+#endif // CBTYPES_H
