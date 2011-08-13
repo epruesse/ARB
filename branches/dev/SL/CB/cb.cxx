58c1d8933538b065fda20f895169d89428063db7
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-08-13 13:13:11 +0000

    * added typesafe callbacks     - checks types between callback-function and parameters specified during instanciation     - internally each !CallbackGroup uses the same specialization       * depends on return-type and first parameter only       * additional parameters are casted to AW_CL   * wrote tests for typesafe callbacks

--- branches/dev/SL/CB/cb.cxx
+++ branches/dev/SL/CB/cb.cxx
@@ -10,6 +10,8 @@
 // ============================================================== //
 
 #include "cbtypes.h"
+#include <attributes.h>
+
 
 using namespace std;
 
@@ -18,8 +20,199 @@ using namespace std;
 #ifdef UNIT_TESTS
 #include <test_unit.h>
 
+#define TRACE
+
+static unsigned long traceChecksum;
+
+STATIC_ATTRIBUTED(__ATTR__FORMAT(1), void tracef(const char *format, ...)) {
+    va_list parg;
+    va_start(parg, format);
+
+    const int BUFFERSIZE = 100;
+    char      buffer[BUFFERSIZE];
+    int       printed    = vsnprintf(buffer, BUFFERSIZE, format, parg);
+
+    va_end(parg);
+
+    TEST_ASSERT(printed<BUFFERSIZE);
+#if defined(TRACE)
+    fputs(buffer, stdout);
+#endif
+
+    traceChecksum = 0;
+    for (int p = 0; p<printed; ++p) {
+        traceChecksum = (traceChecksum<<1)^buffer[p];
+    }
+}
+
+static AW_root   *fake_root = (AW_root*)1;
+static AW_window *fake_win  = (AW_window*)2;
+
+static void rcb0(AW_root *r) {
+    TEST_ASSERT(r == fake_root);
+    tracef("rcb0()\n");
+}
+static void rcb1(AW_root *r, const char *name) {
+    TEST_ASSERT(r == fake_root);
+    tracef("rcb1(%s)\n", name);
+}
+static void rcb2(AW_root *r, const char *name, int val) {
+    TEST_ASSERT(r == fake_root);
+    tracef("rcb2(%s=%i) [int]\n", name, val);
+}
+static void rcb2(AW_root *r, const char *name, long val) {
+    TEST_ASSERT(r == fake_root);
+    tracef("rcb2(%s=%li) [long]\n", name, val);
+}
+static void rcb2(AW_root *r, const char *name, double val) {
+    TEST_ASSERT(r == fake_root);
+    tracef("rcb2(%s=%4.3f) [double]\n", name, val);
+}
+
+static void wcb0(AW_window *w) {
+    TEST_ASSERT(w == fake_win);
+    tracef("wcb0()\n");
+}
+static void wcb1(AW_window *w, const char *name) {
+    TEST_ASSERT(w == fake_win);
+    tracef("wcb1(%s)\n", name);
+}
+static void wcb2(AW_window *w, const char *name, int val) {
+    TEST_ASSERT(w == fake_win);
+    tracef("wcb2(%s=%i) [int]\n", name, val);
+}
+static void wcb2(AW_window *w, const char *name, long val) {
+    TEST_ASSERT(w == fake_win);
+    tracef("wcb2(%s=%li) [long]\n", name, val);
+}
+static void wcb2(AW_window *w, const char *name, double val) {
+    TEST_ASSERT(w == fake_win);
+    tracef("wcb2(%s=%4.3f) [double]\n", name, val);
+}
+static void wcb2(AW_window *w, char c, long long val) {
+    TEST_ASSERT(w == fake_win);
+    tracef("wcb2(%c=%lli) [long long]\n", c, val);
+}
+
+static AW_window *wccb0(AW_root *r) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb0()\n");
+    return fake_win;
+}
+static AW_window *wccb1(AW_root *r, int x) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb1(%i)\n", x);
+    return fake_win;
+}
+static AW_window *wccb1(AW_root *r, const long *lp) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb1(%li) [long ptr]\n", *lp);
+    return fake_win;
+}
+static AW_window *wccb1(AW_root *r, const int *x) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb1(%i) [const ptr]\n", *x);
+    return fake_win;
+}
+static AW_window *wccb1(AW_root *r, int *x) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb1(%i) [mutable ptr]\n", *x);
+    *x *= 2; // modify callback argument!
+    return fake_win;
+}
+static AW_window *wccb1(AW_root *r, char* s) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb1(%s) [mutable]\n", s);
+    return fake_win;
+}
+static AW_window *wccb1(AW_root *r, const char* s) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb1(%s) [const]\n", s);
+    return fake_win;
+}
+static AW_window *wccb2(AW_root *r, const char *s1, const char *s2) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb2(%s=%s) [const const]\n", s1, s2);
+    return fake_win;
+}
+static AW_window *wccb2(AW_root *r, const char *s1, char *s2) {
+    TEST_ASSERT(r == fake_root);
+    tracef("wccb2(%s=%s) [const mutable]\n", s1, s2);
+    return fake_win;
+}
+
+typedef AW_window *(*AWC_CB)(AW_root *, AW_CL, AW_CL);
+
+DECLARE_CBTYPE_AND_BUILDERS(RootCallback, void, AW_root*, AW_RCB);
+DECLARE_CBTYPE_AND_BUILDERS(WindowCallback, void, AW_window*, AW_CB);
+DECLARE_CBTYPE_AND_BUILDERS(WindowCreatorCallback, AW_window*, AW_root*, AWC_CB);
+
+inline void call(const RootCallback& rcb) { rcb(fake_root); }
+inline void call(const WindowCallback& wcb) { wcb(fake_win); }
+inline void call(const WindowCreatorCallback& wccb) { TEST_ASSERT(wccb(fake_root) == fake_win); }
+
+#define TEST_CB(cb,expectedChecksum) do {                       \
+        call(cb);                                               \
+        TEST_ASSERT_EQUAL(traceChecksum, (unsigned long)(expectedChecksum)); \
+    } while(0)
+
 void TEST_cbs() {
-    // TEST_ASSERT(0);
+    MISSING_TEST("please log");
+    // for (int i = 0; i<100000; ++i)
+    {
+        TEST_CB(makeRootCallback(rcb0),                0x17b8);
+        TEST_CB(makeRootCallback(rcb1,  "dispatched"), 0x5d9780);
+        TEST_CB(makeRootCallback(rcb2, "age",  46),    0x176c160);
+        TEST_CB(makeRootCallback(rcb2, "size", 178L),  0xbaf72ec);
+        TEST_CB(makeRootCallback(rcb2, "rate", 7.5),   0xba9bbc04);
+
+        TEST_CB(makeWindowCallback(wcb0),               0x16f8);
+        TEST_CB(makeWindowCallback(wcb1, "dispatched"), 0x589780);
+        TEST_CB(makeWindowCallback(wcb2, "age",  46),   0x162c160);
+        TEST_CB(makeWindowCallback(wcb2, "size", 178L), 0xb0f72ec);
+        TEST_CB(makeWindowCallback(wcb2, "rate", 7.5),  0xb09bbc04);
+
+        TEST_CB(makeWindowCallback(wcb2, 'l', 0xb09bbc04b09bbcLL),  0xb22c830caac);
+
+        TEST_CB(makeWindowCreatorCallback(wccb0),     0x2878);
+        TEST_CB(makeWindowCreatorCallback(wccb1, 77), 0xa19c);
+
+        // test const vs non-const pointers:
+        char       *mut = strdup("mut");
+        const char *con = "con";
+
+        TEST_CB(makeWindowCreatorCallback(wccb2, con, con), 0x515078338);  // exact match
+        TEST_CB(makeWindowCreatorCallback(wccb2, mut, con), 0x514678338);  // fallback to const/const
+        TEST_CB(makeWindowCreatorCallback(wccb2, con, mut), 0x1454460ac4); // exact match
+        TEST_CB(makeWindowCreatorCallback(wccb2, mut, mut), 0x514718338);  // fallback to const const
+
+        // test reference arguments
+        int       imut = 17;
+        const int icon = 23;
+
+        const long lcon = 775L;
+
+        TEST_CB(makeWindowCreatorCallback(wccb1, imut), 0xa1ac);
+        TEST_CB(makeWindowCreatorCallback(wccb1, icon), 0xa1a4);
+        TEST_CB(makeWindowCreatorCallback(wccb1, &lcon), 0xa188418);
+
+        TEST_CB(makeWindowCreatorCallback(wccb1, &icon), 0xa1b2158);
+        TEST_CB(makeWindowCreatorCallback(wccb1, &icon), 0xa1b2158);
+        
+        TEST_CB(makeWindowCreatorCallback(wccb1, &imut), 0x286ec698); // modifies 'imut'
+        TEST_CB(makeWindowCreatorCallback(wccb1, &imut), 0x2869c698); // modifies 'imut'
+        TEST_CB(makeWindowCreatorCallback(wccb1, &imut), 0x286fc698); // modifies 'imut'
+
+        TEST_CB(makeWindowCreatorCallback(wccb1, mut),  0x5169cc4);
+        TEST_CB(makeWindowCreatorCallback(wccb1, con),  0x145feb8);
+
+        free(mut);
+
+        // parameter too big (shall fail to compile):
+        // TEST_CB(makeWindowCallback(wcb2, 'f', 0.1234567890123456789012345678901234567890L),  0x0);
+        // TEST_CB(makeWindowCallback(wcb1, 0.1234567890123456789012345678901234567890L),  0x0);
+    }
+
 }
 
 #endif // UNIT_TESTS
--- branches/dev/SL/CB/cbtypes.h
+++ branches/dev/SL/CB/cbtypes.h
@@ -15,6 +15,151 @@
 #ifndef CB_H
 #include <cb.h>
 #endif
+#ifndef TTYPES_H
+#include <ttypes.h>
+#endif
+#ifndef STATIC_ASSERT_H
+#include <static_assert.h>
+#endif
+
+
+// ---------------------------------
+//      function type inspection
+
+template<typename RT, typename P1, typename P2, typename P3>
+struct Function {
+    enum { NumParams = 3 };
+    typedef RT (*Type)(P1,P2,P3);
+    typedef RT ResultType;
+};
+template<typename RT, typename P1, typename P2>
+struct Function<RT, P1, P2, void> {
+    enum { NumParams = 2 };
+    typedef RT (*Type)(P1,P2);
+};
+template<typename RT, typename P1>
+struct Function<RT, P1, void, void> {
+    enum { NumParams = 1 };
+    typedef RT (*Type)(P1);
+};
+template<typename RT>
+struct Function<RT, void, void, void> {
+    enum { NumParams = 0 };
+    typedef RT (*Type)();
+};
+
+// ---------------------------
+//      forward parameters
+
+template<typename T>
+class ForwardParamT {
+public:
+    typedef typename IfThenElseType< TypeT<T>::IsClassT,
+                                     typename TypeOp<T>::RefConstT,
+                                     typename TypeOp<T>::ArgT >::ResultType Type;
+};
+
+template<typename T>
+class ForwardParamT<T*> {
+public:
+    typedef typename TypeOp<T>::ArgT *Type;
+};
+template<> class ForwardParamT<void> { class Unused {}; public: typedef Unused Type; };
+
+template<typename T>
+class ConstParamT {
+public:
+    typedef typename IfThenElseType< TypeT<T>::IsClassT,
+                                     typename TypeOp<T>::RefConstT,
+                                     typename TypeOp<T>::ConstT >::ResultType Type;
+};
+template<typename T>
+class ConstParamT<T*> {
+public:
+    typedef typename TypeOp<T>::ConstT *Type;
+};
+template<> class ConstParamT<void> { class Unused {}; public: typedef Unused Type; };
+
+
+// -----------------------
+//      typed callback
+
+template<typename RT, typename P1 = void, typename P2 = void, typename P3 = void>
+class StrictlyTypedCallback {
+protected: 
+    typedef typename Function<RT,P1,P2,P3>::Type FuncType;
+
+    typedef typename ForwardParamT<P1>::Type FP1;
+    typedef typename ForwardParamT<P2>::Type FP2;
+    typedef typename ForwardParamT<P3>::Type FP3;
+
+private:
+    FuncType cb;
+
+public:
+    enum { NumParams = Function<RT,P1,P2,P3>::NumParams };
+
+    StrictlyTypedCallback(FuncType CB) : cb(CB) {}
+
+    RT operator()(FP1 p1, FP2 p2, FP3 p3) const { return cb(p1, p2, p3); }
+    RT operator()(FP1 p1, FP2 p2)         const { return cb(p1, p2); }
+    RT operator()(FP1 p1)                 const { return cb(p1); }
+    RT operator()()                       const { return cb(); }
+};
+
+// ------------------------
+//      casted callback
+
+template<typename RT, typename FIXED>
+class CallbackGroup {
+public:
+    typedef StrictlyTypedCallback<RT,FIXED,AW_CL,AW_CL> Signature;
+
+private:
+    Signature cb;
+    AW_CL     p1, p2;
+
+public:
+    CallbackGroup(Signature CB, AW_CL P1, AW_CL P2) : cb(CB), p1(P1), p2(P2) {}
+    RT operator()(FIXED fixed) const { return cb(fixed, p1, p2); }
+};
+
+// ---------------------------
+//      convenience macros
+
+#define CASTABLE_TO_AW_CL(TYPE) (sizeof(TYPE) <= sizeof(AW_CL))
+
+#define CBTYPE_BUILDER_TEMPLATES(BUILDER,CB,RESULT,FIXED,SIG)           \
+    inline CB BUILDER(RESULT (*cb)(FIXED)) {                            \
+        return CB((SIG)cb, 0, 0);                                       \
+    }                                                                   \
+    template<typename P1>                                               \
+    inline CB BUILDER(RESULT (*cb)(FIXED, P1), P1 p1) {                 \
+        COMPILE_ASSERT(CASTABLE_TO_AW_CL(P1));                          \
+        return CB((SIG)cb, AW_CL(p1), 0);                               \
+    }                                                                   \
+    template<typename P1>                                               \
+    inline CB BUILDER(RESULT (*cb)(FIXED, typename ConstParamT<P1>::Type), P1 p1) { \
+        COMPILE_ASSERT(CASTABLE_TO_AW_CL(P1));                          \
+        return CB((SIG)cb, AW_CL(p1), 0);                               \
+    }                                                                   \
+    template<typename P1, typename P2>                                  \
+    inline CB BUILDER(RESULT (*cb)(FIXED, P1, P2), P1 p1, P2 p2) {      \
+        COMPILE_ASSERT(CASTABLE_TO_AW_CL(P1) && CASTABLE_TO_AW_CL(P2)); \
+        return CB((SIG)cb, AW_CL(p1), AW_CL(p2));                       \
+    }                                                                   \
+    template<typename P1, typename P2>                                  \
+    inline CB BUILDER(RESULT (*cb)(FIXED, typename ConstParamT<P1>::Type, typename ConstParamT<P2>::Type), P1 p1, P2 p2) {      \
+        COMPILE_ASSERT(CASTABLE_TO_AW_CL(P1) && CASTABLE_TO_AW_CL(P2)); \
+        return CB((SIG)cb, AW_CL(p1), AW_CL(p2));                       \
+    }
+
+// declares the callback type (CBTYPE) and the makeCBTYPE() templates needed to ensure
+// typecheck between callback-signature and bound parameters
+#define DECLARE_CBTYPE_AND_BUILDERS(CBTYPE,RESULT,FIXED,SIG)            \
+    typedef CallbackGroup<RESULT, FIXED> CBTYPE;                        \
+    CBTYPE_BUILDER_TEMPLATES(make##CBTYPE, CBTYPE, RESULT, FIXED, SIG);
+
 
 
 #else
