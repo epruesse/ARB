0d00778a4b6e45f7f5047ac2105cc7e2d4442192
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-08-24 15:44:43 +0000

    * avoid that one advice may popup multiple times (e.g. when putting the advice into a loop)     - track open advices     - [TDD] generalized tracking of open and disabled advices

--- trunk/WINDOW/AW_advice.cxx
+++ trunk/WINDOW/AW_advice.cxx
@@ -28,6 +28,7 @@ using namespace std;
 #define AWAR_ADVICE_UNDERSTOOD AWAR_ADVICE_TMP "understood"
 
 #define AWAR_ADVICE_DISABLED "/advices/disabled"
+#define AWAR_ADVICE_SHOWN    "/tmp/advices/shown"
 
 // -------------------------
 //      internal data :
@@ -53,26 +54,56 @@ void init_Advisor(AW_root *awr) {
 //      disabled advices :
 // ----------------------------
 
-static AW_awar *get_disabled_advices() {
-    return advice_root->awar_string(AWAR_ADVICE_DISABLED, "");
+inline AW_awar *get_disabled_advices() { return advice_root->awar_string(AWAR_ADVICE_DISABLED, ""); }
+inline AW_awar *get_shown_advices()    { return advice_root->awar_string(AWAR_ADVICE_SHOWN, ""); }
+
+inline int advice_id_offset(const char* id, const char *idlist) {
+    const char *found = strstr(idlist, GBS_global_string(";%s;", id));
+    return found ? found-idlist : -1;
 }
 
-static bool advice_disabled(const char* id, AW_awar *var = 0) {
-    if (!var) var       = get_disabled_advices();
-    char *disabled_list = var->read_string();
-    bool  is_disabled   = strstr(disabled_list, GBS_global_string(";%s;", id)) != 0;
-    free(disabled_list);
-    return is_disabled;
-}
-static void disable_advice(const char* id) {
-    AW_awar *var = get_disabled_advices();
-    if (!advice_disabled(id, var)) {
-        char *disabled_list = var->read_string();
-        if (strlen(disabled_list)) var->write_string(GBS_global_string("%s%s;", disabled_list, id));
+inline bool advice_id_is_set(const char* id, const char *idlist) { return advice_id_offset(id, idlist) >= 0; }
+
+inline void set_advice_id(const char* id, AW_awar *var) {
+    const char *idlist = var->read_char_pntr();
+    if (!advice_id_is_set(id, idlist)) {
+        if (idlist[0]) var->write_string(GBS_global_string("%s%s;", idlist, id));
         else var->write_string(GBS_global_string(";%s;", id));
-        free(disabled_list);
     }
 }
+inline void remove_advice_id(const char* id, AW_awar *var) {
+    const char *idlist = var->read_char_pntr();
+    if (advice_id_is_set(id, idlist)) {
+        int offset = advice_id_offset(id, idlist);
+        if (offset >= 0) {
+            char *newList = 0;
+            if (offset == 0) {
+                newList = strdup(idlist+strlen(id)+1);
+            }
+            else {
+                newList     = strdup(idlist);
+                char *idPos = newList+offset;
+                strcpy(idPos, idPos+strlen(id)+1);
+            }
+            var->write_string(newList);
+            free(newList);
+        }
+    }
+}
+inline void toggle_advice_id(const char* id, AW_awar *var) {
+    if (advice_id_is_set(id, var->read_char_pntr())) {
+        remove_advice_id(id, var);
+    }
+    else {
+        set_advice_id(id, var);
+    }
+}
+
+inline bool advice_disabled(const char* id) { return advice_id_is_set(id, get_disabled_advices()->read_char_pntr()); }
+inline void disable_advice(const char* id)  { set_advice_id(id, get_disabled_advices()); }
+
+inline bool advice_currently_shown(const char *id) { return advice_id_is_set(id, get_shown_advices()->read_char_pntr()); }
+inline void toggle_advice_shown(const char *id)    { toggle_advice_id(id, get_shown_advices()); }
 
 // -------------------------------------------
 
@@ -82,10 +113,11 @@ static void advice_close_cb(AW_window *aww, AW_CL cl_id, AW_CL type) {
     // switch to 'not understood'. Has to be checked by user for each advice.
     advice_root->awar(AWAR_ADVICE_UNDERSTOOD)->write_int(0);
     aww->hide();
+    
+    const char *id = (const char *)cl_id;
+    toggle_advice_shown(id);
 
     if (understood) {
-        const char *id = (const char *)cl_id;
-
         disable_advice(id);
         if (type & AW_ADVICE_TOGGLE) {
             static bool in_advice = false;
@@ -133,7 +165,7 @@ void AW_advice(const char *message, int type, const char *title, const char *cor
     long    crc32       = GB_checksum(message, message_len, true, " .,-!"); // checksum is used to test if advice was shown
     char   *advice_id   = GBS_global_string_copy("%lx", crc32); // do not delete (bound to cbs)
 
-    bool show_advice = !advice_disabled(advice_id);
+    bool show_advice = !advice_disabled(advice_id) && !advice_currently_shown(advice_id);
 
     if (show_advice) {
         AW_awar *understood = advice_root->awar(AWAR_ADVICE_UNDERSTOOD);
@@ -188,7 +220,55 @@ void AW_advice(const char *message, int type, const char *title, const char *cor
         }
 
         aws->window_fit();
+        aws->allow_delete_window(false); // disable closing the window via 'X'-button
         aws->show();
+        toggle_advice_shown(advice_id);
     }
 }
 
+// --------------------------------------------------------------------------------
+
+#ifdef UNIT_TESTS
+#include <test_unit.h>
+
+void TEST_advice_id_awar_handling() {
+    GB_shell  shell;
+    AW_root  root("min_ascii.arb", "test", true); // @@@ leaks
+    init_Advisor(&root);
+
+    const char *one = "one";
+    const char *two = "second";
+
+    TEST_ASSERT(!advice_disabled(one));
+    TEST_ASSERT(!advice_disabled(two));
+
+    disable_advice(one);
+    TEST_ASSERT(advice_disabled(one));
+    TEST_ASSERT(!advice_disabled(two));
+
+    disable_advice(two);
+    TEST_ASSERT(advice_disabled(one));
+    TEST_ASSERT(advice_disabled(two));
+
+
+    TEST_ASSERT(!advice_currently_shown(one));
+    TEST_ASSERT(!advice_currently_shown(two));
+
+    toggle_advice_shown(two);
+    TEST_ASSERT(!advice_currently_shown(one));
+    TEST_ASSERT(advice_currently_shown(two));
+
+    toggle_advice_shown(one);
+    TEST_ASSERT(advice_currently_shown(one));
+    TEST_ASSERT(advice_currently_shown(two));
+    
+    toggle_advice_shown(two);
+    TEST_ASSERT(advice_currently_shown(one));
+    TEST_ASSERT(!advice_currently_shown(two));
+
+    toggle_advice_shown(one);
+    TEST_ASSERT(!advice_currently_shown(one));
+    TEST_ASSERT(!advice_currently_shown(two));
+}
+
+#endif // UNIT_TESTS
