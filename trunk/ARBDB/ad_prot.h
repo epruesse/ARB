5eb960b3b542ac2e6e3bf15db6830515393668e8
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-09-13 14:30:52 +0000

    - GBS_readable_size() accepts prev hardcoded unit_suffix

--- trunk/ARBDB/ad_prot.h
+++ trunk/ARBDB/ad_prot.h
@@ -154,7 +154,7 @@ void GBS_fwrite_string(const char *strngi, FILE *out);
 char *GBS_fread_string(FILE *in);
 char *GBS_fconvert_string(char *buffer);
 char *GBS_replace_tabs_by_spaces(const char *text);
-const char *GBS_readable_size(unsigned long long size);
+const char *GBS_readable_size(unsigned long long size, const char *unit_suffix);
 char *GBS_trim(const char *str);
 char *GBS_log_dated_action_to(const char *comment, const char *action);
 
