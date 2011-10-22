8364809487f9b17767d0e97948534f18c9649c52
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-10-22 07:04:37 +0000

    - fix edit4

---
+++ branches/e4fix/CONSENSUS_TREE/CT_ntree.hxx
@@ -0,0 +1,39 @@
+// ============================================================= //
+//                                                               //
+//   File      : CT_ntree.hxx                                    //
+//   Purpose   :                                                 //
+//                                                               //
+//   Institute of Microbiology (Technical University Munich)     //
+//   http://www.arb-home.de/                                     //
+//                                                               //
+// ============================================================= //
+
+#ifndef CT_NTREE_HXX
+#define CT_NTREE_HXX
+
+#ifndef CT_PART_HXX
+#include "CT_part.hxx"
+#endif
+
+struct NSONS;
+
+struct NT_NODE {
+    PART  *part;
+    NSONS *son_list;
+};
+
+
+struct NSONS {
+    NT_NODE *node;
+    NSONS   *prev, *next;
+};
+
+
+void ntree_init();
+void insert_ntree(PART *part);
+
+NT_NODE *ntree_get();
+
+#else
+#error CT_ntree.hxx included twice
+#endif // CT_NTREE_HXX
