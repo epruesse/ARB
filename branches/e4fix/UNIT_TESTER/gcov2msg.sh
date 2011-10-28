fbec28fe4649ef949f6591f617404ec2f2a7f31a
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-10-28 07:42:20 +0000

    * coverage reports now controlled via Makefile.setup.local     - default restricts like tests are restricted by RESTRICT_MODULE (the common case is to test and coverage one or few module(s))   * fixed failure propagation from gcov2msg.* (but does not fail tests)

--- branches/e4fix/UNIT_TESTER/gcov2msg.sh
+++ branches/e4fix/UNIT_TESTER/gcov2msg.sh
@@ -3,16 +3,16 @@
 CAKEDEPTH=3
 
 enter() {
-    DIR=$1
+    local DIR=$1
     echo "cake[$CAKEDEPTH]: Entering directory \`$DIR'"
 }
 leave() {
-    DIR=$1
+    local DIR=$1
     echo "cake[$CAKEDEPTH]: Leaving directory \`$DIR'"
 }
 
 show_coverage() {
-    DIR=$1
+    local DIR=$1
     if [ -d $DIR/GENC ]; then
         # handle special compilation in PROBE_COM/GENC and NAMES_COM/GENC
         ./gcov2msg.pl --srcdirs=$DIR/C --builddir=$DIR $DIR/O 
@@ -22,24 +22,32 @@ show_coverage() {
 }
 
 show_coverage_otherdir() {
-    DIR=$1
+    local DIR=$1
     enter $DIR
     show_coverage $DIR
+    local EXITCODE=$?
     leave $DIR
+    return $EXITCODE
 }
 
 # ------------------------------------------------------------
 
-FULLUNITDIR=$1
-UNITLIBNAME=$2
+main () {
+    local FULLUNITDIR=$1
+    local UNITLIBNAME=$2
 
-if [ -z "$UNITLIBNAME" ]; then
-    echo "Usage: gcov2msg.sh fullunitdir unitlibname"
-    false
-else
-    show_coverage $FULLUNITDIR 
-    # perform additional coverage analysis for some tests
-    if [ "$UNITLIBNAME" = "arb_probe" ]; then
-        show_coverage_otherdir `dirname $FULLUNITDIR`/PROBE_COM
+    if [ -z "$UNITLIBNAME" ]; then
+        echo "Usage: gcov2msg.sh fullunitdir unitlibname"
+        false
+    else
+        # perform additional coverage analysis for some tests
+        if [ "$UNITLIBNAME" = "arb_probe" ]; then
+            show_coverage $FULLUNITDIR && \
+            show_coverage_otherdir `dirname $FULLUNITDIR`/PROBE_COM
+        else
+            show_coverage $FULLUNITDIR
+        fi
     fi
-fi
+}
+
+main $*
