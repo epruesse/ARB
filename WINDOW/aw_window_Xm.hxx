6787b5c41abee24e8594073145d61bfb7dd43b43
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2011-12-07 13:33:25 +0000

    * moved AW_window awar-create/read/write into member-functions   * several fixes related to [8278]     - if window-position is unknown in expose callback       * set it again from awars and       * delay WM_offset calculation using timed-callback     - never save values into awars, if WM_offsets have not been calculated     - make sure that window-position set and stored-in-awars is the same (in show_internal)       * needed for correct WM_offset-calculation       * popup-windows which are repositioned on show, no longer popup on (old) wrong position and then jump to wanted position (instead they instantly appear at wanted position).     - only add expose-callback once   * changed behavior of modal prompter windows     - faster reaction (100ms instead of 500ms delay)     - the window is not centered on mouse pos, instead its just moved that far that the mouse is inside the window.     - considers WM_offsets     - fallback to center-jump if window-position is unknown

--- WINDOW/aw_window_Xm.hxx
+++ WINDOW/aw_window_Xm.hxx
@@ -300,8 +300,11 @@ public:
     AW_key_mod        keymodifier;
 
     AW_area_management *areas[AW_MAX_AREA];
-    int                 WM_top_offset; // WM top area
-    int                 WM_left_offset;
+
+    int WM_top_offset;                 // correction between position set and position reported ( = size of window frame - in most cases!)
+    int WM_left_offset;
+
+    bool knows_WM_offset() const { return WM_top_offset != AW_CALC_OFFSET_ON_EXPOSE; }
 
     AW_window_Motif();
     ~AW_window_Motif() {};
