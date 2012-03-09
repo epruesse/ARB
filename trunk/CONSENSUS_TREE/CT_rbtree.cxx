ea16a1664091ca6437ef7b5ab317168eb46f2027
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-03-09 12:33:02 +0000

    * added second test (which drops some tree nodes)     - deactivated 1st test (due to non-deterministic behavior)     - DRYed both tests

--- trunk/CONSENSUS_TREE/CT_rbtree.cxx
+++ trunk/CONSENSUS_TREE/CT_rbtree.cxx
@@ -94,7 +94,7 @@ static RB_INFO *rbtree(NT_NODE *tree, GBT_TREE *father)
 
     nsonp = nsonp->next;
     if (!nsonp) {
-        // arb_assert(0); // @@@ invalid tree would be generated here (only leftson)
+        // arb_assert(0); // @@@ invalid tree would be generated here (only leftson); see ../TOOLS/arb_consensus_tree.cxx@disabled_single_son_assertion
         return info;
     }
 
@@ -104,7 +104,7 @@ static RB_INFO *rbtree(NT_NODE *tree, GBT_TREE *father)
     gbtnode->rightlen = info_res->len;
     free(info_res);
 
-    arb_assert(nsonp->next == NULL); // otherwise some sons would be silently dropped
+    // arb_assert(nsonp->next == NULL); // @@@ otherwise some sons would be silently dropped; see  ../TOOLS/arb_consensus_tree.cxx@disabled_multifurc_assertion
 
     return info;
 }
