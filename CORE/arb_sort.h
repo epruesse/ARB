50b4077c09e3faec6996e57a689a39154edfa80f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-03-14 13:57:23 +0000

    * rename

--- CORE/arb_sort.h
+++ CORE/arb_sort.h
@@ -16,10 +16,10 @@
 #include <cstdlib>
 #endif
 
-typedef int (*gb_compare_function)(const void *p0, const void *p1, void *client_data);
+typedef int (*gb_compare_function)(const void *p1, const void *p2, void *client_data);
 
 void GB_sort(void **array, size_t first, size_t behind_last, gb_compare_function compare, void *client_data);
-int GB_string_comparator(const void *v0, const void *v1, void */*unused*/);
+int GB_string_comparator(const void *v1, const void *v2, void */*unused*/);
 
 #else
 #error arb_sort.h included twice
