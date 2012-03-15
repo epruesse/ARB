ad9427d5c97b0b545ebca99e9a9eae3bcbb4be08
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-03-15 07:36:37 +0000

    * move globals from CT_part.cxx into class !PartitionSize   * class PART     - private member variables     - moved many global methods into     - stores und updates member-count

--- CONSENSUS_TREE/CT_dtree.cxx
+++ CONSENSUS_TREE/CT_dtree.cxx
@@ -21,24 +21,25 @@ PART *ConsensusTree::dtree(const GBT_TREE *tree, int weight, GBT_LEN len) {
        all sons to build the father partition. All partitions are inserted in the
        hashtable */
     // caution: I use the fact that each inner node must have two sons.
-    PART *ptree = part_new();
+    PART *ptree = 0;
     if (tree->is_leaf) {
-        part_setbit(ptree, get_species_index(tree->name));
+        ptree = new PART(size);
+        ptree->setbit(get_species_index(tree->name));
     }
     else {
         // In any possible case left and rightson always exist ...
         PART *p1 = dtree(tree->leftson, weight, tree->leftlen);
         PART *p2 = dtree(tree->rightson, weight, tree->rightlen);
 
-        arb_assert(are_brothers(p1, p2));
+        arb_assert(p1->distinct_from(p2));
 
-        part_copy(p1, ptree);
-        part_or(p2, ptree);
+        ptree = p1->clone();
+        ptree->add_from(p2);
 
         hash_insert(p1, weight);
         hash_insert(p2, weight);
     }
-    part_setlen(ptree, len);
+    ptree->set_len(len);
     return ptree;
 }
 
@@ -51,11 +52,11 @@ void ConsensusTree::remember_subtrees(const GBT_TREE *tree, int weight) {
     PART *p1 = dtree(tree->leftson, weight, 0.0);
     PART *p2 = dtree(tree->rightson, weight, 0.0);
 
-    arb_assert(are_brothers(p1, p2));
+    arb_assert(p1->distinct_from(p2));
 
-    part_free(p1);
+    delete p1;
 
-    part_setlen(p2, (tree->leftlen + tree->rightlen));
+    p2->set_len(tree->leftlen + tree->rightlen);
     hash_insert(p2, weight);
 }
 
