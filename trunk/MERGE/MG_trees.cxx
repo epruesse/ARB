f21acef3b1064281a2f395ff0f78d3e45c55ca39
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-04-05 16:16:19 +0000

    * merge tool (trees)     - transfer trees with order: trees get inserted behind the tree selected in the dest-tree-list       * after a tree has been transferred, step to next tree in both lists         - => repeatedly clicking 'Transfer tree' copies a sequence of trees     - fixed initial value for tree awars     - extracted plain transfer-code into separate function

--- trunk/MERGE/MG_trees.cxx
+++ trunk/MERGE/MG_trees.cxx
@@ -1,7 +1,7 @@
 // =============================================================== //
 //                                                                 //
 //   File      : MG_trees.cxx                                      //
-//   Purpose   :                                                   //
+//   Purpose   : Merge trees between databases                     //
 //                                                                 //
 //   Institute of Microbiology (Technical University Munich)       //
 //   http://www.arb-home.de/                                       //
@@ -15,49 +15,98 @@
 #include <aw_awar.hxx>
 #include <aw_msg.hxx>
 #include <arbdbt.h>
+#include <arb_global_defs.h>
 
 #define AWAR_TREE_NAME1 "tmp/merge1/tree_name"
 #define AWAR_TREE_NAME2 "tmp/merge2/tree_name"
 
 void MG_create_trees_awar(AW_root *aw_root, AW_default aw_def) {
-    aw_root->awar_string(AWAR_TREE_NAME1, "", aw_def);
-    aw_root->awar_string(AWAR_TREE_NAME2, "", aw_def);
+    aw_root->awar_string(AWAR_TREE_NAME1, NO_TREE_SELECTED, aw_def);
+    aw_root->awar_string(AWAR_TREE_NAME2, NO_TREE_SELECTED, aw_def);
 
     TreeAdmin::create_awars(aw_root, aw_def);
 }
 
+static GB_ERROR transfer_tree(const char *tree_name, bool overwrite, const char *behind_name) {
+    GB_ERROR  error   = NULL;
+    GBDATA   *gb_tree = GBT_find_tree(GLOBAL_gb_merge, tree_name);
+    if (!gb_tree) {
+        if (strcmp(tree_name, NO_TREE_SELECTED) == 0) {
+            error = "No tree selected in source DB";
+        }
+        else {
+            error = GBS_global_string("No tree '%s' in source DB", tree_name);
+        }
+    }
+    else {
+        GBDATA *gb_behind_tree = NULL;
+        if (behind_name && strcmp(behind_name, NO_TREE_SELECTED) != 0) {
+            gb_behind_tree = GBT_find_tree(GLOBAL_gb_dest, behind_name);
+            if (!gb_behind_tree) {
+                error = GBS_global_string("Can't position tree behind '%s' (no such tree)", behind_name);
+            }
+        }
+
+        if (!error) {
+            GBDATA *gb_dest_tree = GBT_find_tree(GLOBAL_gb_dest, tree_name);
+            if (gb_dest_tree)  {
+                if (!overwrite) {
+                    error = GBS_global_string("Tree '%s' already exists in destination DB", tree_name);
+                }
+                else {
+                    error        = GB_delete(gb_dest_tree);
+                    gb_dest_tree = NULL;
+                }
+            }
+        }
+
+        if (!error) {
+            GBDATA *gb_dest_tree_data     = GBT_get_tree_data(GLOBAL_gb_dest);
+            if (!gb_dest_tree_data) error = GB_await_error();
+            else {
+                GBDATA *gb_dest_tree     = GB_create_container(gb_dest_tree_data, tree_name);
+                if (!gb_dest_tree) error = GB_await_error();
+                else {
+                    error = GB_copy(gb_dest_tree, gb_tree);
+                    if (!error) {
+                        if (!gb_behind_tree) gb_behind_tree = GBT_find_bottom_tree(GLOBAL_gb_dest);
+                        if (gb_behind_tree) error           = GBT_move_tree(gb_dest_tree, GBT_BEHIND, gb_behind_tree);
+                    }
+                }
+            }
+        }
+    }
+    return error;
+}
+
 static void MG_transfer_tree(AW_window *aww) {
-    AW_root *awr    = aww->get_root();
-    char    *source = awr->awar(AWAR_TREE_NAME1)->read_string();
-    char    *dest   = awr->awar(AWAR_TREE_NAME1)->read_string();
+    AW_root *awr = aww->get_root();
+
+    AW_awar *awar_tree_source = awr->awar(AWAR_TREE_NAME1);
+    AW_awar *awar_tree_dest   = awr->awar(AWAR_TREE_NAME2);
+
+    char *source_name = awar_tree_source->read_string();
+    char *behind_name = awar_tree_dest->read_string();
 
     GB_ERROR error    = GB_begin_transaction(GLOBAL_gb_dest);
     if (!error) error = GB_begin_transaction(GLOBAL_gb_merge);
 
     if (!error) {
-        GBDATA *gb_tree_data1 = GBT_get_tree_data(GLOBAL_gb_merge);
-        GBDATA *gb_tree_data2 = GBT_get_tree_data(GLOBAL_gb_dest);
-
-        if (!gb_tree_data1 || !gb_tree_data2) error = GB_await_error();
-        else {
-            GBDATA *gb_source     = GB_entry(gb_tree_data1, source);
-            GBDATA *gb_dest_tree  = GB_entry(gb_tree_data2, dest);
-
-            if (!gb_source) error = "Please select the tree you want to transfer";
-            else if (gb_dest_tree) error = GBS_global_string("Tree '%s' already exists, delete it first", dest);
-            else {
-                gb_dest_tree             = GB_create_container(gb_tree_data2, dest);
-                if (!gb_dest_tree) error = GB_await_error();
-                else error               = GB_copy(gb_dest_tree, gb_source);
-            }
-        }
+        error = transfer_tree(source_name, false, behind_name);
     }
 
     error = GB_end_transaction(GLOBAL_gb_dest, error);
     GB_end_transaction_show_error(GLOBAL_gb_merge, error, aw_message);
 
-    free(source);
-    free(dest);
+    if (!error) {
+        awar_tree_dest->write_string(source_name);
+        GB_transaction ta(GLOBAL_gb_merge);
+        GBDATA *gb_next = GBT_get_next_tree(GBT_find_tree(GLOBAL_gb_merge, source_name));
+        awar_tree_source->write_string(gb_next ? GBT_get_tree_name(gb_next) : NO_TREE_SELECTED);
+    }
+
+    free(behind_name);
+    free(source_name);
 }
 
 AW_window *MG_merge_trees_cb(AW_root *awr) {
