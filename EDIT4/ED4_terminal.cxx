a8f44c0a73c91fed7d93d2682827111af215db82
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-04-20 07:07:28 +0000

    merge from e4fix [8414] [8415] [8416] [8424]
      * cursor was not erased from consensi of folded groups (because ED4_cursor reported itself invisible in that case)
      * right click on consensus name now also (un)marks SAI (not only species)
      * fix unwanted terminal change
        - occurred when 2 edit-windows open and 2 different species selected:
          * pressing Ctrl-K in the 1st window moved its cursor to the species selected in the 2nd window
          * similar things happened after other actions which changed the folding
        - properly protect global awars from unneeded refresh, by telling updateAwars whether the cursor changed its terminal
      * replaced one more direct call to ::Show by triggering refresh

--- EDIT4/ED4_terminal.cxx
+++ EDIT4/ED4_terminal.cxx
@@ -390,7 +390,7 @@ ED4_returncode  ED4_terminal::move_requested_by_parent(ED4_move_info *) { // han
 
 void ED4_multi_species_manager::toggle_selected_species() {
     if (all_are_selected()) deselect_all_species_and_SAI();
-    else select_all_species(); // @@@ should select everything here
+    else select_all(false); // species and SAI
 
     ED4_correctBlocktypeAfterSelection();
 }
@@ -432,7 +432,7 @@ ED4_returncode  ED4_terminal::event_sent_by_parent(AW_event *event, AW_window *a
                                 if (!containing_species_manager()->is_selected()) {
                                     ED4_ROOT->add_to_selected(dragged_name_terminal);
                                     dragged_was_selected = 0;
-                                    ED4_ROOT->main_manager->Show(); // @@@ critical direct call to Show (fix: do NOT select on drag)
+                                    ED4_trigger_instant_refresh();
                                 }
                                 else {
                                     dragged_was_selected = 1;
