58fbc152d86b9445981648a6cf7eae8e222f65ce
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-04-22 16:14:11 +0000

    * fixed gb_put_number / gb_get_number     - numbers above 0xfffffff       * able to write (just did nothing in NDEBUG mode, where that limit may more likely be reached)       * fix reading (returned wrong value)

--- ARBDB/ad_io_inline.h
+++ ARBDB/ad_io_inline.h
@@ -46,67 +46,48 @@ inline uint32_t gb_read_in_uint32(FILE *in, bool reversed) {
     return reversed ? reverse_byteorder(val) : val;
 }
 
-inline void gb_put_number(long i, FILE *out) {
+inline void gb_put_number(long b0, FILE *out) {
     // opposite of gb_get_number
-    long j;
-    if (i< 0x80) { putc((int)i, out); return; }
-    if (i<0x4000) {
-        j = (i>>8) | 0x80;
-        putc((int)j, out);
-        putc((int)i, out);
-        return;
+    typedef unsigned char uc;
+    if (b0 >= 0x80) {
+        long b1 = b0>>8;
+        if (b1 >= 0x40) {
+            long b2 = b1>>8;
+            if (b2 >= 0x20) {
+                long b3 = b2>>8;
+                if (b3 >= 0x10) putc(0xf0, out);
+                else b3 |= 0xE0;
+                putc(uc(b3), out);
+            }
+            else b2 |= 0xC0;
+            putc(uc(b2), out);
+        }
+        else b1 |= 0x80;
+        putc(uc(b1), out);
     }
-    if (i<0x200000) {
-        j = (i>>16) | 0xC0;
-        putc((int)j, out);
-        j = (i>>8);
-        putc((int)j, out);
-        putc((int)i, out);
-        return;
-    }
-    if (i<0x10000000) {
-        j = (i>>24) | 0xE0;
-        putc((int)j, out);
-        j = (i>>16);
-        putc((int)j, out);
-        j = (i>>8);
-        putc((int)j, out);
-        putc((int)i, out);
-        return;
-    }
-    // gb_assert(0); // overflow
-    return;
+    putc(uc(b0), out);
 }
 
 inline long gb_get_number(FILE *in) {
     // opposite of gb_put_number
-    unsigned int c0 = getc(in);
-    if (c0 & 0x80) {
-        unsigned int c1 = getc(in);
-        if (c0 & 0x40) {
-            unsigned int c2 = getc(in);
-            if (c0 & 0x20) {
-                unsigned int c3 = getc(in);
-                if (c0 &0x10) {
-                    unsigned int c4 = getc(in);
-                    return c4 | (c3<<8) | (c2<<16) | (c1<<8);
-                }
-                else {
-                    return (c3) | (c2<<8) | (c1<<16) | ((c0 & 0x0f)<<24);
-                }
+    unsigned int b0  = getc(in);
+    unsigned int RES = b0;
+    if (b0 & 0x80) {
+        RES = getc(in);
+        if (b0 & 0x40) {
+            RES = (RES << 8) | getc(in);
+            if (b0 & 0x20) {
+                RES = (RES << 8) | getc(in);
+                if (b0 & 0x10) RES = (RES << 8) | getc(in);
+                else RES |= (b0 & 0x0f) << 24;
             }
-            else {
-                return (c2) | (c1<<8) | ((c0 & 0x1f)<<16);
-            }
+            else RES |= (b0 & 0x1f) << 16;
         }
-        else {
-            return (c1) | ((c0 & 0x3f)<<8);
-        }
+        else RES |= (b0 & 0x3f) << 8;
     }
-    else {
-        return c0;
-    }
+    return RES;
 }
+
 
 #else
 #error ad_io_inline.h included twice
