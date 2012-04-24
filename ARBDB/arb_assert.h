6ff913153f1a2d418947e86be0a0b87d130a456a
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-04-24 14:18:34 +0000

    - provoke_core_dump via volatile pointer (otherwise clang will skip the code which is crashing on purpose)

--- ARBDB/arb_assert.h
+++ ARBDB/arb_assert.h
@@ -88,8 +88,9 @@
 
 #if defined(__cplusplus)
 inline void provoke_core_dump() {
+    volatile int *np = 0; // if not volatile, the clang compiler will skip the crashing code
     // cppcheck-suppress nullPointer
-    *(int*)0 = 0;
+    *(int*)np = 666;
 }
 #else // !defined(__cplusplus)
 #define provoke_core_dump() do { *(int*)0 = 0; } while(0)
