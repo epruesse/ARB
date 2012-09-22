8cf44b1f66724dc7fb574da89d8f439586786275
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-09-22 17:49:56 +0000

    * inlines are static by default

--- SL/FAST_ALIGNER/seq_search.hxx
+++ SL/FAST_ALIGNER/seq_search.hxx
@@ -35,8 +35,8 @@
 #define MAX_TRIPLES (SEQ_CHARS*SEQ_CHARS*SEQ_CHARS+1) // one faked triple for all non-char triples
 #define GAP_CHARS   ".-~?"      // Characters treated as gaps
 
-static inline int max(int i1, int i2) { return i1>i2 ? i1 : i2; }
-static inline bool is_ali_gap(char c)  { return strchr(GAP_CHARS, c)!=0; }
+inline int max(int i1, int i2) { return i1>i2 ? i1 : i2; }
+inline bool is_ali_gap(char c)  { return strchr(GAP_CHARS, c)!=0; }
 
 class CompactedSubSequence;
 
