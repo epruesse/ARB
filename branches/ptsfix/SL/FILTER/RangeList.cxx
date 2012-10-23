20296f7271a8a3df38011b49fa0dce8d184b02a9
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2012-10-23 11:55:13 +0000

    * minor change of GBS_strstruct

--- branches/ptsfix/SL/FILTER/RangeList.cxx
+++ branches/ptsfix/SL/FILTER/RangeList.cxx
@@ -49,7 +49,7 @@ RangeList build_RangeList_from_string(const char *SAI_data, const char *set_bits
 
 static const char *dump(const RangeList& rlist) {
     static GBS_strstruct buf;
-    buf.reset_pos();
+    buf.erase();
     for (RangeList::iterator i = rlist.begin(); i != rlist.end(); ++i) {
         buf.nprintf(40, "%i-%i,", i->start(), i->end());
     }
@@ -58,7 +58,7 @@ static const char *dump(const RangeList& rlist) {
 }
 static const char *dump_reverse(const RangeList& rlist) {
     static GBS_strstruct buf;
-    buf.reset_pos();
+    buf.erase();
     for (RangeList::reverse_iterator i = rlist.rbegin(); i != rlist.rend(); ++i) {
         buf.nprintf(40, "%i-%i,", i->start(), i->end());
     }
