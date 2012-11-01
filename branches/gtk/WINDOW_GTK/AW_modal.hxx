3cf5c0fa5fe370e90d5f18c19abbb5d6e8069771
aboeckma <aboeckma@129cd134-2152-0410-ab65-f3f895bafe68>
2012-11-01 13:15:42 +0000

    Added aw_modal.

---
+++ branches/gtk/WINDOW_GTK/AW_modal.hxx
@@ -0,0 +1,13 @@
+/* 
+ * File:   AW_modal.hxx
+ * Author: aboeckma
+ *
+ * Created on November 1, 2012, 11:36 AM
+ */
+
+#pragma once
+
+void message_cb(AW_window *, AW_CL cd1);
+void input_history_cb(AW_window *aw, AW_CL cl_mode);
+void input_cb(AW_window *aw, AW_CL cd1);
+void file_selection_cb(AW_window *aw, AW_CL cd1);
