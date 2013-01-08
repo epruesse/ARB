331e83668a86712c257a90599e6c1e3b4306a073
epruesse <epruesse@129cd134-2152-0410-ab65-f3f895bafe68>
2013-01-08 17:17:25 +0000

    add init_Advisor

--- branches/gtk/WINDOW_GTK/AW_advice.cxx
+++ branches/gtk/WINDOW_GTK/AW_advice.cxx
@@ -15,6 +15,29 @@
 
 #include "aw_advice.hxx"
 #include "aw_gtk_migration_helpers.hxx"
+#include "aw_root.hxx"
+
+#define AWAR_ADVICE_TMP "/tmp/advices/"
+
+#define AWAR_ADVICE_TEXT       AWAR_ADVICE_TMP "text"
+#define AWAR_ADVICE_UNDERSTOOD AWAR_ADVICE_TMP "understood"
+
+// -------------------------
+//      internal data :
+
+static bool     initialized = false;
+static AW_root *advice_root = 0;
+
+void init_Advisor(AW_root *awr) {
+    if (!initialized) {
+        advice_root  = awr;
+
+        advice_root->awar_string(AWAR_ADVICE_TEXT, "<no advice>");
+        advice_root->awar_int(AWAR_ADVICE_UNDERSTOOD, 0);
+
+        initialized = true;
+    }
+}
 
 void AW_advice(const char */*message*/,
                int type /*= AW_ADVICE_SIMPLE*/,
