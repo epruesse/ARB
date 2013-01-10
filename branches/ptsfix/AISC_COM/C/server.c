0b061c700d57d2e7602cfd7e2b2e747b1c83b96c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-01-10 12:49:08 +0000

    merge from ptsstartup [9287] [9288] [9289]   * DRYed code common to AISC_COM-client and -server

--- branches/ptsfix/AISC_COM/C/server.c
+++ branches/ptsfix/AISC_COM/C/server.c
@@ -33,6 +33,7 @@
 #include <SigHandler.h>
 #include <arb_cs.h>
 #include <static_assert.h>
+#include "common.h"
 
 // AISC_MKPT_PROMOTE:#ifndef _STDIO_H
 // AISC_MKPT_PROMOTE:#include <stdio.h>
@@ -273,157 +274,6 @@ static const char *aisc_get_object_attribute(long i, long j)
     return aisc_attribute_names_list[i][j];
 }
 
-#if defined(WARN_TODO)
-#warning DRY client.c vs server.c (->clientserver.c).
-/* e.g. aisc_get_m_id and aisc_client_get_m_id
- * aisc_open_socket() / aisc_client_open_socket()
- * etc.
- */
-#endif
-
-static const char *aisc_get_m_id(const char *path, char **m_name, int *id) {
-    // Warning: duplicated in client.c@aisc_client_get_m_id
-    if (!path) {
-        return "OPEN_ARB_DB_CLIENT ERROR: missing hostname:socketid";
-    }
-    if (!strcmp(path, ":")) {
-        path = (char *)getenv("SOCKET");
-        if (!path) return "environment socket not found";
-    }
-    
-    const char *p = strchr(path, ':');
-    if (path[0] == '*' || path[0] == ':') {     // UNIX MODE
-        char buffer[128];
-        if (!p) {
-            return "OPEN_ARB_DB_CLIENT ERROR: missing ':' in *:socketid";
-        }
-        if (p[1] == '~') {
-            sprintf(buffer, "%s%s", getenv("HOME"), p+2);
-            *m_name = (char *)strdup(buffer);
-        }
-        else {
-            *m_name = (char *)strdup(p+1);
-        }
-        *id = -1;
-        return 0;
-    }
-    if (!p) {
-        return "OPEN_ARB_DB_CLIENT ERROR: missing ':' in netname:socketid";
-    }
-
-    char *mn = (char *) calloc(sizeof(char), p - path + 1);
-    strncpy(mn, path, p - path);
-
-    if (strcmp(mn, "localhost") == 0) freedup(mn, arb_gethostname());
-    *m_name = mn;
-
-    int i = atoi(p + 1);
-    if ((i < 1024) || (i > 4096)) {
-        return "OPEN_ARB_DB_CLIENT ERROR: socketnumber not in [1024..4095]";
-    }
-    *id = i;
-    return 0;
-}
-
-
-static const char *aisc_open_socket(const char *path, int delay, int do_connect, int *psocket, char **unix_name) {
-    struct in_addr  addr;       // union -> u_long
-    struct hostent *he;
-    const char     *err;
-    static int      socket_id;
-    char           *mach_name = NULL;
-    FILE           *test;
-
-    err = aisc_get_m_id(path, &mach_name, &socket_id);
-
-    // @@@ mem assigned to mach_name is leaked often
-    // @@@ refactor aisc_open_socket -> one exit point
-    // @@@ note that the code is nearly duplicated in client.c@aisc_client_open_socket
-    // @@@ a good place for DRYed code would be ../../CORE/arb_cs.cxx
-
-    if (err) {
-        return err;
-    }
-    if (socket_id >= 0) {       // UNIX
-        sockaddr_in so_ad;
-        memset((char *)&so_ad, 0, sizeof(sockaddr_in));
-        *psocket = socket(PF_INET, SOCK_STREAM, 0);
-        if (*psocket <= 0) {
-            return "CANNOT CREATE SOCKET";
-        }
-
-        arb_gethostbyname(mach_name, he, err);
-        if (err) return err;
-
-        // simply take first address
-        addr.s_addr = *(int *) (he->h_addr);
-        so_ad.sin_addr = addr;
-        so_ad.sin_family = AF_INET;
-        so_ad.sin_port = htons(socket_id);      // @@@ = pb_socket
-        if (do_connect) {
-            if (connect(*psocket, (struct sockaddr*)&so_ad, 16)) {
-                return "";
-            }
-        }
-        else {
-            static int one = 1;
-            setsockopt(*psocket, SOL_SOCKET, SO_REUSEADDR, (const char *)&one, sizeof(one));
-            if (bind(*psocket, (struct sockaddr*)&so_ad, 16)) {
-                return "Could not open socket on Server (1)";
-            }
-        }
-        if (delay == TCP_NODELAY) {
-            static int      optval;
-            optval = 1;
-            setsockopt(*psocket, IPPROTO_TCP, TCP_NODELAY, (char *)&optval, 4);
-        }
-        *unix_name = 0;
-    }
-    else {
-        struct sockaddr_un so_ad;
-        *psocket = socket(PF_UNIX, SOCK_STREAM, 0);
-        if (*psocket <= 0) {
-            return "CANNOT CREATE SOCKET";
-        }
-        so_ad.sun_family = AF_UNIX;
-        strcpy(so_ad.sun_path, mach_name);
-        if (do_connect) {
-            if (connect(*psocket, (struct sockaddr*)&so_ad, strlen(mach_name)+2)) {
-                return "";
-            }
-        }
-        else {
-            static int one = 1;
-            test = fopen(mach_name, "r");
-            if (test) {
-                struct stat stt;
-                fclose(test);
-                if (!stat(path, &stt)) {
-                    if (S_ISREG(stt.st_mode)) {
-                        fprintf(stderr, "%X\n", stt.st_mode);
-                        return "Socket already exists as a file";
-                    }
-                }
-            }
-            if (unlink(mach_name)) {
-                ;
-            }
-            else {
-                printf("old socket found\n");
-            }
-            setsockopt(*psocket, SOL_SOCKET, SO_REUSEADDR, (const char *)&one, sizeof(one));
-            if (bind(*psocket, (struct sockaddr*)&so_ad, strlen(mach_name)+2)) {
-                return "Could not open socket on Server (2)";
-            }
-            if (chmod(mach_name, 0777)) return "Cannot change mode of socket";
-        }
-
-        reassign(*unix_name, mach_name);
-    }
-    free(mach_name);
-    return 0;
-}
-
 Hs_struct *open_aisc_server(const char *path, int timeout, int fork) {
     Hs_struct *hs = (Hs_struct *)calloc(sizeof(Hs_struct), 1);
     if (!hs) return 0;
@@ -432,7 +282,7 @@ Hs_struct *open_aisc_server(const char *path, int timeout, int fork) {
     hs->fork    = fork;
     
     static int  so;
-    const char *err = aisc_open_socket(path, TCP_NODELAY, 0, &so, &hs->unix_name);
+    const char *err = aisc_server_open_socket(path, TCP_NODELAY, 0, &so, &hs->unix_name);
 
     if (err) {
         if (*err) printf("Error in open_aisc_server: %s\n", err);
