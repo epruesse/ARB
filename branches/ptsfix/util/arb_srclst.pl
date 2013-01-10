d840c445e600366efa959b46c5f0b972888327fa
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-01-10 12:03:31 +0000

    * merge from ptsstartup [9205] [9221] [9372]    - minor changes (non-ptserver)

--- branches/ptsfix/util/arb_srclst.pl
+++ branches/ptsfix/util/arb_srclst.pl
@@ -131,6 +131,7 @@ my @skipped_when_matches = (
                             qr/^arbsrc.*\.tgz$/o,
                             qr/^arbsrc\.lst$/o,
                             qr/^arbsrc\.lst\.tmp$/o,
+                            qr/^callgrind\./o,
                            );
 
 my @used_when_matchesFull = (
