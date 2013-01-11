3ec6003128a6f263e40c4d1602da259a8243842b
epruesse <epruesse@129cd134-2152-0410-ab65-f3f895bafe68>
2013-01-11 15:56:04 +0000

    implement AW_window_message

--- branches/gtk/WINDOW_GTK/AW_window_message.cxx
+++ branches/gtk/WINDOW_GTK/AW_window_message.cxx
@@ -2,42 +2,29 @@
 #include "aw_window_gtk.hxx"
 
 AW_window_message::AW_window_message() {
-    GTK_NOT_IMPLEMENTED;
 }
 
 AW_window_message::~AW_window_message() {
-    GTK_NOT_IMPLEMENTED;
 }
 
-void AW_window_message::init(AW_root */*root_in*/, const char */*windowname*/, bool /*allow_close*/) {
-    GTK_NOT_IMPLEMENTED;
-//    root = root_in; // for macro
-//
-//    int width  = 100;
-//    int height = 100;
-//    int posx   = 50;
-//    int posy   = 50;
-//
-//    window_name = strdup(windowname);
-//    window_defaults_name = GBS_string_2_key(window_name);
-//
-//    // create shell for message box
-//    p_w->shell = aw_create_shell(this, true, allow_close, width, height, posx, posy);
-//
-//    // disable resize or maximize in simple dialogs (avoids broken layouts)
-//    XtVaSetValues(p_w->shell, XmNmwmFunctions, MWM_FUNC_MOVE | MWM_FUNC_CLOSE,
-//            NULL);
-//
-//    p_w->areas[AW_INFO_AREA] = new AW_area_management(root, p_w->shell, XtVaCreateManagedWidget("info_area",
-//                    xmDrawingAreaWidgetClass,
-//                    p_w->shell,
-//                    XmNheight, 0,
-//                    XmNbottomAttachment, XmATTACH_NONE,
-//                    XmNtopAttachment, XmATTACH_FORM,
-//                    XmNleftAttachment, XmATTACH_FORM,
-//                    XmNrightAttachment, XmATTACH_FORM,
-//                    NULL));
-//
-//    aw_realize_widget(this);
+void AW_window_message::init(AW_root *root_in, const char *window_name_in, bool allow_close) {
+    root = root_in;
+    window_name = strdup(window_name_in);
+    int width  = 100;
+    int height = 100;
+    int posx   = 50;
+    int posy   = 50;
+
+    gtk_window_set_title(prvt->window, window_name);
+    gtk_window_set_default_size(prvt->window, width, height);
+    gtk_window_set_resizable(prvt->window, false);
+    allow_delete_window(allow_close);
+
+    GtkWidget *fixed_size_area = gtk_fixed_new();
+    prvt->fixed_size_area = GTK_FIXED(fixed_size_area);
+    prvt->areas[AW_INFO_AREA] = new AW_area_management(root, fixed_size_area, fixed_size_area); 
+    gtk_container_add(GTK_CONTAINER(prvt->window), fixed_size_area);
+
+    create_devices();
 }
 
