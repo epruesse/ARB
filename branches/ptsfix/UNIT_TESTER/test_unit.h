95bf330a40d6e7d61d349538b061ba8a22aaa72a
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-01-27 10:12:17 +0000

    * added test macros to test for any error

--- branches/ptsfix/UNIT_TESTER/test_unit.h
+++ branches/ptsfix/UNIT_TESTER/test_unit.h
@@ -1117,6 +1117,8 @@ namespace arb_test {
 
 #define TEST_EXPECT_ERROR_CONTAINS(call,part)         TEST_EXPECTATION        (reported_error_contains(call, part))
 #define TEST_EXPECT_ERROR_CONTAINS__BROKEN(call,part) TEST_EXPECTATION__BROKEN(reported_error_contains(call, part))
+#define TEST_EXPECT_ANY_ERROR(call)                   TEST_EXPECTATION        (reports_error(call))
+#define TEST_EXPECT_ANY_ERROR__BROKEN(call)           TEST_EXPECTATION__BROKEN(reports_error(call))
 #define TEST_EXPECT_NO_ERROR(call)                    TEST_EXPECTATION        (doesnt_report_error(call))
 #define TEST_EXPECT_NO_ERROR__BROKEN(call)            TEST_EXPECTATION__BROKEN(doesnt_report_error(call))
 
