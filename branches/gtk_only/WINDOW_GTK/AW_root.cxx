b27d196ed0aed9ddcbf719a9c66ed461ac967b99
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-03-01 10:04:15 +0000

    - "merge" [9642]

--- branches/gtk_only/WINDOW_GTK/AW_root.cxx
+++ branches/gtk_only/WINDOW_GTK/AW_root.cxx
@@ -82,15 +82,15 @@ AW_ProcessEventType AW_root::peek_key_event(AW_window *) {
 }
 
 AW_default AW_root::load_properties(const char *default_name) {
-
     GBDATA   *gb_default = GB_open(default_name, "rwcD");
-    GB_ERROR  error      = NULL;
+    GB_ERROR  error;
 
     if (gb_default) {
-        GB_no_transaction(gb_default);
-
-        GBDATA *gb_tmp = GB_search(gb_default, "tmp", GB_CREATE_CONTAINER);
-        error          = GB_set_temporary(gb_tmp);
+        error = GB_no_transaction(gb_default);
+        if (!error) {
+            GBDATA *gb_tmp = GB_search(gb_default, "tmp", GB_CREATE_CONTAINER);
+            error          = GB_set_temporary(gb_tmp);
+        }
     }
     else {
         error = GB_await_error();
