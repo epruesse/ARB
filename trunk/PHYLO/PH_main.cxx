e06fbffa3c486e873fd3098f511ed43d970bd035
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-05-17 10:35:50 +0000

    * AW_manage_GC no longer hardcodes window-id as basename for GCs     - pass down window-id from all callers (i.e. no change in behavior yet)

--- trunk/PHYLO/PH_main.cxx
+++ trunk/PHYLO/PH_main.cxx
@@ -415,24 +415,26 @@ static AW_window *create_phyl_main_window(AW_root *aw_root, PH_root *ph_root, AW
     //
 
     AW_gc_manager gcmiddle = AW_manage_GC(awm,
-                                           awm->get_device (AW_MIDDLE_AREA),
-                                           PH_GC_0, PH_GC_0_DRAG, AW_GCM_DATA_AREA,
-                                           resize_callb, 0, 0,
-                                           false, // no color groups
-                                           "#CC9AF8",
-                                           "#SEQUENCE$#000000",
-                                           "#MARKER$#FF0000",
-                                           "NOT_MARKER$#A270C0",
-                                           NULL);
-    /* AW_gc_manager gcbottom = */ AW_manage_GC(awm,
-                                           awm->get_device (AW_BOTTOM_AREA),
-                                           PH_GC_0, PH_GC_0_DRAG, AW_GCM_WINDOW_AREA,
-                                           resize_callb, 0, 0,
-                                           false, // no color groups
-                                           "pink",
-                                           "#FOOTER", NULL);
+                                          awm->get_window_id(),
+                                          awm->get_device(AW_MIDDLE_AREA),
+                                          PH_GC_0, PH_GC_0_DRAG, AW_GCM_DATA_AREA,
+                                          resize_callb, 0, 0,
+                                          false, // no color groups
+                                          "#CC9AF8",
+                                          "#SEQUENCE$#000000",
+                                          "#MARKER$#FF0000",
+                                          "NOT_MARKER$#A270C0",
+                                          NULL);
+    AW_manage_GC(awm,
+                 awm->get_window_id(),
+                 awm->get_device(AW_BOTTOM_AREA),
+                 PH_GC_0, PH_GC_0_DRAG, AW_GCM_WINDOW_AREA,
+                 resize_callb, 0, 0,
+                 false, // no color groups
+                 "pink",
+                 "#FOOTER",
+                 NULL);
 
-    // GBUSE(gcbottom);
 
 #if defined(DEBUG)
     AWT_create_debug_menu(awm);
--- trunk/WINDOW/aw_preset.hxx
+++ trunk/WINDOW/aw_preset.hxx
@@ -38,23 +38,27 @@ enum AW_GCM_AREA {
 /* creates some GC pairs: one for normal operation,
                     the other for drag mode
         eg.
-        AW_manage_GC(aww,device,10,20,AW_GCM_DATA_AREA, my_expose_cb, cd1 ,cd2, "name","#sequence",NULL);
+        AW_manage_GC(aww,"ARB_NT",device,10,20,AW_GCM_DATA_AREA, my_expose_cb, cd1 ,cd2, "name","#sequence",NULL);
 
                 (see implementation for more details on parameter strings)
 
         will create 4 GCs:
             GC 10 (normal) and 20 (drag)
             GC 11 (normal and monospaced (indicated by '#')
-               21 drag and monospaced
+            21 drag and monospaced
             don't forget the 0 at the end of the fontname field
 
             When the GCs are modified the 'changecb' is called
 */
-AW_gc_manager AW_manage_GC(AW_window                                       *aww,
-                           AW_device                                       *device, int base_gc, int base_drag, AW_GCM_AREA area,
-                           void (*changecb)(AW_window*, AW_CL, AW_CL), AW_CL  cd1, AW_CL cd2,
-                           bool                                             define_color_groups,
-                           const char                                      *default_background_color,
+
+typedef void (*AW_GC_change_cb)(AW_window*, AW_CL, AW_CL);
+
+AW_gc_manager AW_manage_GC(AW_window       *aww,
+                           const char      *gc_base_name,
+                           AW_device       *device, int base_gc, int base_drag, AW_GCM_AREA area,
+                           AW_GC_change_cb  changecb, AW_CL cd1, AW_CL cd2,
+                           bool             define_color_groups,
+                           const char      *default_background_color,
                            ...) __ATTR__SENTINEL;
 
 
