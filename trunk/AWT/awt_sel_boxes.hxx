7e7be7354b5c49f8f61e8aa12000edd5991d0130
epruesse <epruesse@129cd134-2152-0410-ab65-f3f895bafe68>
2013-06-07 16:59:17 +0000

    revert r10128 (contained accidental delete of gb_data.h range):-/

--- trunk/AWT/awt_sel_boxes.hxx
+++ trunk/AWT/awt_sel_boxes.hxx
@@ -34,9 +34,9 @@ class AP_filter;
 class AW_DB_selection;
 class AW_selection;
 class AW_selection_list;
-struct StrArray;
-struct ConstStrArray;
-struct CharPtrArray;
+class StrArray;
+class ConstStrArray;
+class CharPtrArray;
 struct adfiltercbstruct;
 
 class TypedSelectionList {
