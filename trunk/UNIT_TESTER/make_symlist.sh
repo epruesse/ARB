323e358ddfd4a7fb45e491a7543eb1ede9ef4b6c
aboeckma <aboeckma@129cd134-2152-0410-ab65-f3f895bafe68>
2013-06-14 09:53:17 +0000

    A step towards working unit tests on mac os x

--- trunk/UNIT_TESTER/make_symlist.sh
+++ trunk/UNIT_TESTER/make_symlist.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
 FULLLIB=$1
 SYMLIST=$2
 
+if [ $(uname) = "Darwin" ]; then
+#nm is unabled to demangle names on os x. use c++filt instead for the job
+CMD="nm -l $FULLLIB | c++filt > $SYMLIST"
+else
+CMD="nm -C -l $FULLLIB > $SYMLIST"
+fi
+
 # temp workaround for using gcc 4.8 on dwarf 3 system
-( nm -C -l $FULLLIB > $SYMLIST ) 2>&1 \
+( eval "$CMD" ) 2>&1 \
     | grep -v "this reader only handles version 2 and 3 information" \
     | grep -v "this reader can not handle sizes greater than '8'" \
     | grep -v "this reader can only handle address sizes '2', '4' and '8'"
