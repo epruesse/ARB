1db193fc0e666560080c9f9ff4749e920657d082
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-07-05 16:27:15 +0000

    * set_input_callback uses !WindowCallback

--- trunk/WINDOW/aw_window_Xm.hxx
+++ trunk/WINDOW/aw_window_Xm.hxx
@@ -153,7 +153,7 @@ public:
 
     void set_expose_callback(AW_window *aww, void (*f)(AW_window*, AW_CL, AW_CL), AW_CL cd1, AW_CL cd2);
     void set_resize_callback(AW_window *aww, void (*f)(AW_window*, AW_CL, AW_CL), AW_CL cd1, AW_CL cd2);
-    void set_input_callback(AW_window *aww, void (*f)(AW_window*, AW_CL, AW_CL), AW_CL cd1, AW_CL cd2);
+    void set_input_callback(AW_window *aww, const WindowCallback& wcb);
     void set_double_click_callback(AW_window *aww, void (*f)(AW_window*, AW_CL, AW_CL), AW_CL cd1, AW_CL cd2);
     void set_motion_callback(AW_window *aww, void (*f)(AW_window*, AW_CL, AW_CL), AW_CL cd1, AW_CL cd2);
 
