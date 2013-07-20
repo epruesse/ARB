6ff878dbc3bbe8ba7482b71724d2cf3b91d6b41b
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-07-20 13:19:28 +0000

    * added static (template-)functions creating !WindowCallback from functions expecting AW_root* (and 0 or 1 extra argument)    * works w/o extra copy of the callback data (but creates a wrapper for each argument type)

--- branches/dbui/SL/CB/rootAsWin.h
+++ branches/dbui/SL/CB/rootAsWin.h
@@ -12,14 +12,12 @@
 #ifndef ROOTASWIN_H
 #define ROOTASWIN_H
 
+#ifndef CB_H
+#include <cb.h>
+#endif
+
 class RootAsWindowCallback {
     // Allows to use any RootCallback where a WindowCallback is expected.
-    //
-    // It's not recommended to use this widely, mainly this is a helper for refactoring.
-    // Normally you should better adapt your callback-type.
-    //
-    // Use it in cases where a user-provided callback has to be used
-    // both as RootCallback and as WindowCallback.
 
     RootCallback rcb;
     void call(AW_window *aww) const { rcb(aww->get_root()); }
@@ -28,10 +26,33 @@ class RootAsWindowCallback {
     static void window_cb(AW_window *aww, const RootAsWindowCallback *rawcb) { rawcb->call(aww); }
     static void delete_raw_cb(RootAsWindowCallback *raw_cb) { delete raw_cb; }
 
+    template<typename T>
+    static void call_root_as_win(AW_window *aww, void (*root_cb)(AW_root*, T), T t) { root_cb(aww->get_root(), t); }
+
+    static void call_root_as_win(AW_window *aww, void (*root_cb)(AW_root*)) { root_cb(aww->get_root()); }
+
 public:
 
     static WindowCallback reuse(const RootCallback& rcb) {
+        // It's not recommended to use this widely, mainly this is a helper for refactoring.
+        // Normally you should better adapt your callback-type.
+        //
+        // Use it in cases where a user-provided callback has to be used
+        // both as RootCallback and as WindowCallback.
+
         return makeWindowCallback(window_cb, delete_raw_cb, new RootAsWindowCallback(rcb));
+    }
+
+    template<typename T>
+    static WindowCallback simple(void (*root_cb)(AW_root*, T), T t) {
+        // usable for simple root callbacks with only one argument
+        // (automatically creates one general wrapper function for each type T)
+        return makeWindowCallback(call_root_as_win, root_cb, t);
+    }
+
+    static WindowCallback simple(void (*root_cb)(AW_root*)) {
+        // usable for simple root callbacks with no argument
+        return makeWindowCallback(call_root_as_win, root_cb);
     }
 };
 
