d50e6a49e116026f17e5ab4fd4c0aca455ad7f54
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-07-26 07:35:07 +0000

    * abort script, if unknown OSNAME or MODE used

--- util/jenkins_build.sh
+++ util/jenkins_build.sh
@@ -2,13 +2,14 @@
 set -x
 set -o errexit
 
-# set stamdard variables
+# set standard variables expected by ARB build
 export ARBHOME=`pwd`
 export PATH=$ARBHOME/bin:$PATH
 export LD_LIBRARY_PATH=$ARBHOME/lib
 
-#set by OS variables
-case `uname -s` in
+# OS dependant settings
+OSNAME=`uname -s`
+case $OSNAME in
 Darwin)
   export PREFIX=/opt/local
   export PATH=$PATH:$PREFIX/sbin:$PREFIX/bin
@@ -19,43 +20,54 @@ Darwin)
 Linux)
 ;;
 *)
-# unsupported OS?
+ echo "Error: unhandled OSNAME '$OSNAME'"
+ false
 ;;
 esac
-
 
 rm -f config.makefile
 
 # prepare config.makefile
+CFG=config.makefile
+UNIT_TESTS=1# default is to run unit tests
 case $MODE in
 DEBUG)
- echo "DEBUG := 1" >> config.makefile
+ echo "DEBUG := 1" >> $CFG
  ;;
 RELEASE)
- echo "DEBUG := 0" >> config.makefile
+ echo "DEBUG := 0" >> $CFG
+ ;;
+*)
+ echo "Error: unknown MODE '$MODE' passed to jenkins_build.sh"
+ false
  ;;
 esac
 
-case `uname -s` in
+case $OSNAME in
 Darwin)
- echo "DARWIN := 1" >> config.makefile
- echo "MACH := DARWIN" >> config.makefile
- echo "UNIT_TESTS := 0" >> config.makefile
+ echo "DARWIN := 1" >> $CFG
+ echo "MACH := DARWIN" >> $CFG
+ UNIT_TESTS=0
  ;;
 Linux)
- echo "LINUX := 1" >> config.makefile
- echo "MACH := LINUX" >> config.makefile
- echo "UNIT_TESTS := 1" >> config.makefile
+ echo "LINUX := 1" >> $CFG
+ echo "MACH := LINUX" >> $CFG
  ;;
+*)
+ echo "Error: unhandled OSNAME '$OSNAME'"
+ false
+;;
 esac
 
-echo "OPENGL := 0" >> config.makefile
-echo "DEVELOPER := ANY" >> config.makefile
-echo "DEBUG_GRAPHICS := 0" >> config.makefile
-echo "PTPAN := 0" >> config.makefile
-echo "ARB_64 := 1" >> config.makefile
-echo "TRACESYM := 1" >> config.makefile
-echo "COVERAGE := 0" >> config.makefile
+echo "UNIT_TESTS := $UNIT_TESTS" >> $CFG
+echo "OPENGL := 0" >> $CFG
+echo "DEVELOPER := ANY" >> $CFG
+echo "DEBUG_GRAPHICS := 0" >> $CFG
+echo "PTPAN := 0" >> $CFG
+echo "ARB_64 := 1" >> $CFG
+echo "TRACESYM := 1" >> $CFG
+echo "COVERAGE := 0" >> $CFG
+# done with config.makefile
 
 make build
 make tarfile_quick
