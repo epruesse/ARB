fc2aff7cd2f0371bd3c7e32d49e45f8347582145
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-08-26 09:15:45 +0000

    * mask some non-printing characters from NDS output (esp. LFs and TABs)     - motivation: gtk renders LFs at tree tips (resulting in overlapping leaf names)     - replace LF/CR by '#' and TAB by one space; replace other chars < ASCII 32 by '?'     - affects:       * displays (tree-tips, edit4 info-area)       * exported files (trees, calc-sheets, distance matrixes)   * link edit4 against NDS (edits should use code from NDS, not its own code!)
    (reverted by [10488]; superseeded by [10494])

--- GENOM/GEN_nds.cxx
+++ GENOM/GEN_nds.cxx
@@ -176,7 +176,8 @@ char *GEN_make_node_text_nds(GBDATA *gb_main, GBDATA * gbd, int mode) {
         }
     }
     *bp = 0;
-    return gen_nds_ms->buf;
+
+    return NDS_mask_nonprintable_chars(gen_nds_ms->buf);
 }
 
 
--- SL/NDS/nds.h
+++ SL/NDS/nds.h
@@ -33,6 +33,8 @@ enum NDS_Type {
 const char *make_node_text_nds(GBDATA *gb_main, GBDATA * gbd, NDS_Type format, GBT_TREE *species, const char *tree_name);
 void        make_node_text_init(GBDATA *gb_main);
 
+char *NDS_mask_nonprintable_chars(char *inStr);
+
 AW_window *AWT_create_nds_window(AW_root *aw_root, AW_CL cgb_main);
 void       create_nds_vars(AW_root *aw_root, AW_default awdef, GBDATA *gb_main);
 void       AWT_create_select_srtaci_window(AW_window *aww, AW_CL awar_acisrt, AW_CL awar_short);
