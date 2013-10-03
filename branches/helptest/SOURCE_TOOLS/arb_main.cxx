92157e37ee50bf3c28d3c253895b3450e6c0c513
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-10-03 11:11:20 +0000

    remove reintegrated branch 'helptest'

--- branches/helptest/SOURCE_TOOLS/arb_main.cxx
+++
@@ -1,9 +0,0 @@
-// main-wrapper
-
-int ARB_main(int argc, char *argv[]);
-int main(int argc, char **argv) {
-    // this is the only main() we like in production code
-    // other main()'s occur in test-code
-
-    return ARB_main(argc, argv);
-}
