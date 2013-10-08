03a1c1eba84d4896156b9923b369c6ca549aa034
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-10-08 16:31:43 +0000

    * fix some failing issues in generated helpfiles

--- branches/help/HELP_SOURCE/genhelp/postprocess_genhelp.pl
+++ branches/help/HELP_SOURCE/genhelp/postprocess_genhelp.pl
@@ -3,11 +3,41 @@
 use strict;
 use warnings;
 
+sub clustalTopic2Section($) {
+  my ($line) = @_;
+  if ($line =~ /^\s*\>\>\s*([^<>]+)\s*\<\<\s*(.*)$/o) {
+    # '>>topic<< moretext' -> 'SECTION topic moretext'
+    $line = "SECTION $1 $2\n";
+  }
+  return $line;
+}
+
+sub plus2star($) {
+  my ($line) = @_;
+  $line =~ s/^(\s+)\+\s/$1\* /;
+  return $line;
+}
+
 sub main() {
   my $args = scalar(@ARGV);
   if ($args!=1) {
     die "Usage: postprocess_genhelp.pl filename\n".
       "(filters stdin to stdout, filename is only passed to trigger specific behavior)\n ";
+  }
+  my $filename = $ARGV[0];
+
+  my @reg = ();
+  my @fun = ();
+
+  if ($filename =~ /clustalw/) {
+    push @reg, qr/([^\s]\s)\s+([\(])/;
+    push @reg, qr/([\.:])(\s)\s+/;
+    push @fun, \&clustalTopic2Section;
+  }
+  elsif ($filename =~ /readseq/) {
+    push @reg, qr/([\.])(\s)\s+/;
+    push @reg, qr/(LINE\s)\s+([0-9]+)/;
+    push @fun, \&plus2star;
   }
 
   my $titled = 0;
@@ -20,6 +50,15 @@ sub main() {
         s/^SECTION/TITLE/;
         $titled=1;
       }
+    }
+
+    foreach my $re (@reg) {
+      while ($_ =~ $re) {
+        $_ = $`.$1.$2.$';
+      }
+    }
+    foreach my $fn (@fun) {
+      $_ = &$fn($_);
     }
     print $_;
   }
