430c05f7af4ec0ee05c412a90728d29b429bb2e9
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-10-17 12:12:37 +0000

    - do not merge stdout and stderr; instead only pipe stderr through error-filter (allows to use xsltproc w/o --output, i.e. writing to stdout)

--- branches/help/HELP_SOURCE/arb_xslt.sh
+++ branches/help/HELP_SOURCE/arb_xslt.sh
@@ -9,5 +9,5 @@ format_error_messages() {
     perl -pne 's/^compilation\s+error:\s+file\s+([^\s]+)\s+line\s+([0-9]+)\s+/\1:\2: Error: /'
 }
         
-xslt "$@" 2>&1 | format_error_messages
+( xslt "$@" ) 2> >( format_error_messages >&2 )
 exit ${PIPESTATUS[0]}
