d5e7674b046ab33291575ba9738e56122ecfa433
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-10-21 12:25:02 +0000

    * merge from 'trunk' (10865) into 'gtk_only':  * adds:    - log:trunk@10865

--- branches/gtk_only/WINDOW/aw_help.hxx
+++ branches/gtk_only/WINDOW/aw_help.hxx
@@ -8,7 +8,6 @@
 #pragma once
 
 void AW_help_popup(AW_window *aw, const char *help_file);
-__ATTR__DEPRECATED("use AW_help_popup or makeHelpCallback") inline void AW_POPUP_HELP(AW_window *aw, AW_CL helpfile) { AW_help_popup(aw, (const char *)helpfile); }
 inline WindowCallback makeHelpCallback(const char *helpfile) { return makeWindowCallback(AW_help_popup, helpfile); }
 
 void AW_insert_default_help_entries(AW_window*);
