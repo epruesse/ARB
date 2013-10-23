f348f4edc0741bd2a4dc760c2016eae5beecbfe3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-10-23 11:28:51 +0000

    * DRYed comment

--- trunk/PARSIMONY/pars_dtree.hxx
+++ trunk/PARSIMONY/pars_dtree.hxx
@@ -22,11 +22,6 @@ class AWT_graphic_parsimony : public AWT_graphic_tree, virtual Noncopyable {
     ArbParsimony& parsimony;
 
     virtual AW_gc_manager init_devices(AW_window *, AW_device *, AWT_canvas *ntw) OVERRIDE;
-    /* init gcs, if any gc is changed you may call
-       AWT_expose_cb(aw_window,ntw,cd2);
-       or AWT_resize_cb(aw_window,ntw,cd2);
-       The function may return a pointer to a preset window */
-
 
     virtual void show(AW_device *device) OVERRIDE;
     virtual void command(AW_device *device, AWT_COMMAND_MODE cmd, int button, AW_key_mod key_modifier, AW_key_code key_code, char key_char,
