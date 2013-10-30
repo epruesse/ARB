a553c9aac5aafb414cfcdd6cb79e67e0ed356473
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-10-30 16:43:06 +0000

    - comment out leftover expectation (failing for clang)

--- trunk/SL/INSDEL/insdel.cxx
+++ trunk/SL/INSDEL/insdel.cxx
@@ -864,8 +864,8 @@ void TEST_AliData() {
                 TEST_COMPARE_WORKS(start_gap1, same_as_start_gap1, 0);
 
                 TEST_EXPECT(start_gap1->differs_from(*start_gap3));
-                TEST_EXPECT_EQUAL(strcmp(".CGCC------C-GG-C-GG.A.-C------GG-.C..UCAGU",        // start_gap1
-                                         "...CGCC------C-GG-C-GG.A.-C------GG-.C..UCAGU"), 1); // start_gap3
+                // TEST_EXPECT_EQUAL(strcmp(".CGCC------C-GG-C-GG.A.-C------GG-.C..UCAGU",        // start_gap1
+                                         // "...CGCC------C-GG-C-GG.A.-C------GG-.C..UCAGU"), 1); // start_gap3
 
                 TEST_EXPECT_EQUAL(start_gap1->cmp_whole_data(*start_gap3),  1);
                 TEST_EXPECT_EQUAL(start_gap3->cmp_whole_data(*start_gap1), -1);
