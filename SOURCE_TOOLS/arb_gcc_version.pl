dfea5d983258017f45d42c33f71da8755c9ed8d5
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-10-30 14:01:37 +0000

    * full update from 'trunk' into 'gtk_only':  * adds:    - log:trunk@10963:10964,10966:10971

--- SOURCE_TOOLS/arb_gcc_version.pl
+++ SOURCE_TOOLS/arb_gcc_version.pl
@@ -4,22 +4,29 @@ use strict;
 use warnings;
 
 sub main() {
-  my $gcc = $ENV{GCC};
-  if (not defined $gcc) { $gcc = 'gcc'; }
-  my $dumpedVersion = `$gcc -dumpversion`;
-  my $detectedVersion = 'unknown_gcc_version';
+  my $detectedVersion = 'failed_to_detect_compiler';
 
-  if ($dumpedVersion =~ /\..*\./) {
-    $detectedVersion = $dumpedVersion;
+  my $args = scalar(@ARGV);
+  if ($args != 1) {
+    print STDERR 'Usage: arb_gcc_version.pl ${CXX}'."\n";
+    $detectedVersion .= '__missing_arg_to__arb_gcc_version';
   }
   else {
-    # version info does not contain patchlevel
-    my $detailedVersion = `$gcc --version`;
-    if ($detailedVersion =~ /\s([0-9]+\.[0-9]+\.[0-9]+)\s/) {
-      $detectedVersion = $1;
+    my $gcc = $ARGV[0];
+    $detectedVersion = 'unknown_gcc_version';
+
+    my $dumpedVersion = `$gcc -dumpversion`;
+    if ($dumpedVersion =~ /\..*\./) {
+      $detectedVersion = $dumpedVersion;
+    }
+    else {
+      # version info does not contain patchlevel
+      my $detailedVersion = `$gcc --version`;
+      if ($detailedVersion =~ /\s([0-9]+\.[0-9]+\.[0-9]+)\s/) {
+        $detectedVersion = $1;
+      }
     }
   }
-
   chomp($detectedVersion);
   print $detectedVersion."\n";
 }
