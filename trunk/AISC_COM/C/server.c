7695215c7f1e069f3540f6d53843de776b6eb70c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-11-03 15:22:52 +0000

    * stop using malloc.h

--- trunk/AISC_COM/C/server.c
+++ trunk/AISC_COM/C/server.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <malloc.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
--- trunk/SOURCE_TOOLS/generate_all_links.sh
+++ trunk/SOURCE_TOOLS/generate_all_links.sh
@@ -171,6 +171,7 @@ symlink_file ../TEMPLATES/cxxforward.h INCLUDE/cxxforward.h &&
 symlink_file ../TEMPLATES/downcast.h INCLUDE/downcast.h &&
 symlink_file ../TEMPLATES/dupstr.h INCLUDE/dupstr.h &&
 symlink_file ../TEMPLATES/gccver.h INCLUDE/gccver.h &&
+symlink_file ../TEMPLATES/malloc.h INCLUDE/malloc.h &&
 symlink_file ../TEMPLATES/mode_text.h INCLUDE/mode_text.h &&
 symlink_file ../TEMPLATES/output.h INCLUDE/output.h &&
 symlink_file ../TEMPLATES/perf_timer.h INCLUDE/perf_timer.h &&
