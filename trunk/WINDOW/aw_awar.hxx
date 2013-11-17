35f43fd19345dcac43165f4629c4563ffed556e9
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-11-17 11:58:24 +0000

    - add AW_awar::set_min

--- trunk/WINDOW/aw_awar.hxx
+++ trunk/WINDOW/aw_awar.hxx
@@ -136,7 +136,7 @@ public:
     AW_awar *set_minmax(float min, float max);
     float    get_min() const;
     float    get_max() const;
-
+    AW_awar *set_min(float min) { return set_minmax(min, get_max()); }
     AW_awar *set_srt(const char *srt);
 
     AW_awar *map(const char *awarn);
