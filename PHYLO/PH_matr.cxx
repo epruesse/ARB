0c7096992f59ca6203d5674060ffc4642ea716df
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-11-26 17:08:34 +0000

    * eliminate unneeded includes

--- PHYLO/PH_matr.cxx
+++ PHYLO/PH_matr.cxx
@@ -10,16 +10,11 @@
 
 #include "phylo.hxx"
 
-#include <aw_file.hxx>
-#include <aw_window.hxx>
 #include <aw_awar.hxx>
-#include <aw_msg.hxx>
 #include <aw_root.hxx>
 
-#include <AP_pro_a_nucs.hxx>
-
 void PH_create_matrix_variables(AW_root *aw_root, AW_default def) {
-    aw_root->awar_string(AWAR_PHYLO_ALIGNMENT, "",       def);
-    aw_root->awar_string(AWAR_PHYLO_FILTER_FILTER,    "",     def);
+    aw_root->awar_string(AWAR_PHYLO_ALIGNMENT,     "", def);
+    aw_root->awar_string(AWAR_PHYLO_FILTER_FILTER, "", def);
 }
 
