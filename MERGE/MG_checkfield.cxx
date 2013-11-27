7bc70f11ea51bb530048fac9ef1d1eaf80f402a3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-11-27 13:31:43 +0000

    * use typed callbacks in MERGE   * removed MG_popup_if_renamed (using [11242] instead)

--- MERGE/MG_checkfield.cxx
+++ MERGE/MG_checkfield.cxx
@@ -293,7 +293,7 @@ static void mg_check_field_cb(AW_window *aww) {
 }
 
 
-AW_window *create_mg_check_fields(AW_root *aw_root) {
+AW_window *create_mg_check_fields_window(AW_root *aw_root) {
     aw_root->awar_string(AWAR_SOURCE_FIELD);
     aw_root->awar_string(AWAR_DEST_FIELD, "tmp", AW_ROOT_DEFAULT);
     aw_root->awar_string(AWAR_EXCLUDE, ".-", AW_ROOT_DEFAULT);
