4fc084c7a26dd6489a36d480fa77e2a0c3269cb3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-12-01 16:38:19 +0000

    - added convenient versions of 'if_fail'-macros that set a BREAKPOINT

--- WINDOW/aw_assert.hxx
+++ WINDOW/aw_assert.hxx
@@ -20,14 +20,21 @@
 #error do not redefine aw_assert!
 #endif
 
-#define aw_return_if_fail(cond) g_return_if_fail(cond)
-#define aw_return_val_if_fail(cond, val) g_return_val_if_fail(cond, val)
-#define aw_return_if_reached() g_return_if_reached()
-#define aw_return_val_if_reached(val) g_return_val_if_reached(val)
-#define aw_warn_if_fail(cond) g_warn_if_fail(cond)
-#define aw_warn_if_reached() g_warn_if_reached()
-#define aw_warning g_warning
-#define aw_debug g_debug
+#define aw_return_if_fail(cond)         g_return_if_fail(cond)
+#define aw_return_val_if_fail(cond,val) g_return_val_if_fail(cond, val)
+#define aw_return_if_reached()          g_return_if_reached()
+#define aw_return_val_if_reached(val)   g_return_val_if_reached(val)
+#define aw_warn_if_fail(cond)           g_warn_if_fail(cond)
+#define aw_warn_if_reached()            g_warn_if_reached()
+
+#define aw_warning    g_warning
+#define aw_debug      g_debug
 #define AW_BREAKPOINT G_BREAKPOINT
+
+#if defined(DEBUG)
+#define aw_return_if_fail_BREAKPOINT(cond)         do { bool c = (cond); if (!c) AW_BREAKPOINT(); aw_return_if_fail(c); } while(0)
+#define aw_return_val_if_fail_BREAKPOINT(cond,val) do { bool c = (cond); if (!c) AW_BREAKPOINT(); aw_return_val_if_fail(c,val); } while(0)
+#define aw_warn_if_fail_BREAKPOINT(cond)           do { bool c = (cond); if (!c) AW_BREAKPOINT(); aw_warn_if_fail(c); } while(0)
+#endif
 
 #define aw_assert(cond) aw_warn_if_fail(cond)
