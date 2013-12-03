0fbbf694b8166d25c2c940e0d93a52cf7022a717
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-12-03 18:01:51 +0000

    * new functions 'Remove zombies/marked from ALL trees'     - finished disabled but nearly finished code

--- branches/tree/NTREE/NT_extern.cxx
+++ branches/tree/NTREE/NT_extern.cxx
@@ -837,7 +837,7 @@ static void NT_update_marked_counter(GBDATA*, AW_window* aww) {
 
 // --------------------------------------------------------------------------------------------------
 
-static void NT_alltree_remove_leafs(AW_window *, GBT_TreeRemoveType mode, GBDATA *gb_main) { // @@@ test and activate for public
+static void NT_alltree_remove_leafs(AW_window *, GBT_TreeRemoveType mode, GBDATA *gb_main) {
     GB_ERROR       error = 0;
     GB_transaction ta(gb_main);
 
@@ -845,9 +845,21 @@ static void NT_alltree_remove_leafs(AW_window *, GBT_TreeRemoveType mode, GBDATA
     GBT_get_tree_names(tree_names, gb_main, false);
 
     if (!tree_names.empty()) {
-        int           treeCount    = tree_names.size();
-        arb_progress  progress("Deleting from trees", treeCount);
+        int treeCount = tree_names.size();
+
+        const char *whats_removed;
+        switch (mode) {
+            case GBT_REMOVE_ZOMBIES: whats_removed = "zombies"; break;
+            case GBT_REMOVE_MARKED:  whats_removed = "marked";  break;
+            default: nt_assert(0); break;
+        }
+
+        const char *task = GBS_global_string("Deleting %s from %i trees", whats_removed, treeCount);
+
+        arb_progress  progress(task, treeCount);
         GB_HASH      *species_hash = GBT_create_species_hash(gb_main);
+
+        int modified = 0;
 
         for (int t = 0; t<treeCount && !error; t++) {
             progress.subtitle(tree_names[t]);
@@ -869,17 +881,30 @@ static void NT_alltree_remove_leafs(AW_window *, GBT_TreeRemoveType mode, GBDATA
                 else {
                     if (removed>0) {
                         error = GBT_write_tree(gb_main, tree_names[t], tree);
-                        if (groups_removed>0) {
-                            aw_message(GBS_global_string("Removed %i species and %i groups from '%s'", removed, groups_removed, tree_names[t]));
+                        if (error) {
+                            aw_message(GBS_global_string("Failed to write '%s' (Reason: %s)", tree_names[t], error));
                         }
                         else {
-                            aw_message(GBS_global_string("Removed %i species from '%s'", removed, tree_names[t]));
+                            if (groups_removed>0) {
+                                aw_message(GBS_global_string("Removed %i species and %i groups from '%s'", removed, groups_removed, tree_names[t]));
+                            }
+                            else {
+                                aw_message(GBS_global_string("Removed %i species from '%s'", removed, tree_names[t]));
+                            }
+                            modified++;
                         }
                     }
                     GBT_delete_tree(tree);
                 }
             }
             progress.inc_and_check_user_abort(error);
+        }
+
+        if (modified) {
+            aw_message(GBS_global_string("Changed %i of %i trees.", modified, treeCount));
+        }
+        else {
+            aw_message("No tree modified");
         }
 
         GBS_free_hash(species_hash);
@@ -1342,14 +1367,9 @@ static AW_window *popup_new_main_window(AW_root *awr, AW_CL clone) {
             awm->insert_menu_topic(awm->local_id("tree_remove_deleted"), "Remove zombies", "z", "trm_del.hlp",    AWM_ALL, makeWindowCallback(NT_remove_leafs, ntw, AWT_REMOVE_ZOMBIES));
             awm->insert_menu_topic(awm->local_id("tree_remove_marked"),  "Remove marked",  "m", "trm_mrkd.hlp",   AWM_ALL, makeWindowCallback(NT_remove_leafs, ntw, AWT_REMOVE_MARKED));
             awm->insert_menu_topic(awm->local_id("tree_keep_marked"),    "Keep marked",    "K", "tkeep_mrkd.hlp", AWM_ALL, makeWindowCallback(NT_remove_leafs, ntw, AWT_KEEP_MARKED));
-#if defined(DEVEL_RALF)
-#if defined(WARN_TODO)
-#warning add "remove duplicates from tree"
-#endif
             awm->sep______________();
-            awm->insert_menu_topic("all_tree_remove_deleted", "Remove zombies from all trees", "a", "trm_del.hlp", AWM_ALL, makeWindowCallback(NT_alltree_remove_leafs, GBT_REMOVE_ZOMBIES, GLOBAL.gb_main));
-            awm->insert_menu_topic("all_tree_remove_marked",  "Remove marked from all trees",  "l", "trm_del.hlp", AWM_ALL, makeWindowCallback(NT_alltree_remove_leafs, GBT_REMOVE_MARKED,  GLOBAL.gb_main));
-#endif // DEVEL_RALF
+            awm->insert_menu_topic("all_tree_remove_deleted", "Remove zombies from ALL trees", "A", "trm_del.hlp",  AWM_ALL, makeWindowCallback(NT_alltree_remove_leafs, GBT_REMOVE_ZOMBIES, GLOBAL.gb_main));
+            awm->insert_menu_topic("all_tree_remove_marked",  "Remove marked from ALL trees",  "L", "trm_mrkd.hlp", AWM_ALL, makeWindowCallback(NT_alltree_remove_leafs, GBT_REMOVE_MARKED,  GLOBAL.gb_main));
         }
         awm->close_sub_menu();
 
