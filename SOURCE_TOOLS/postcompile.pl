98f5e12fef48f2020ba7c4f8ba7f0c10c438ce74
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2013-12-12 15:15:48 +0000

    * fix detection of "required by"-annotations (gcc 4.8.x)

--- SOURCE_TOOLS/postcompile.pl
+++ SOURCE_TOOLS/postcompile.pl
@@ -32,7 +32,7 @@ my $reg_is_error = qr/^error:\s/i;
 my $reg_is_warning = qr/^warning:\s/i;
 my $reg_is_note = qr/^note:\s/i;
 my $reg_is_instantiated = qr/^\s\s+instantiated\sfrom\s/;
-my $reg_is_required = qr/^\s\s+required\sfrom\s/;
+my $reg_is_required = qr/^\s\s+required\s(from|by)\s/;
 
 # regexps for warning messages (for part behind 'warning: ')
 my $reg_shadow_warning = qr/^declaration\sof\s.*\sshadows\s/;
