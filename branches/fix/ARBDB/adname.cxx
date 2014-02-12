961f39cacd16f5c35d0b7a5342ecbc5172d3d840
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-02-12 14:39:19 +0000

    * full update from 'trunk' into 'fix'   * adds:     - log:trunk@11579:11582

--- branches/fix/ARBDB/adname.cxx
+++ branches/fix/ARBDB/adname.cxx
@@ -199,6 +199,7 @@ GB_ERROR GBT_commit_rename_session() { // goes to header: __ATTR__USERESULT
                     progress.inc_and_check_user_abort(error);
                 }
                 else {
+                    GBT_message(NameSession.gb_main, GBS_global_string("Warning: failed to read '%s' (Reason: %s)", tname, GB_await_error()));
                     ++progress;
                     ++progress;
                 }
