cbd03d8f48d21c04985b669a95c6290c3fde5428
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-02-25 15:57:30 +0000

    - tested several locale settings (ubuntu 13.10)  - temporarily hardcode locale (german for LC_NUMERIC; C for rest) to see whether tests fail in jenkins

--- trunk/SOURCE_TOOLS/arb_main.h
+++ trunk/SOURCE_TOOLS/arb_main.h
@@ -14,10 +14,58 @@
 #include <locale.h>
 
 static void start_of_main() {
+    // const char *USER_LOCALE = ""; // -> use user-defined locale
+    const char *USER_LOCALE = "de_DE.UTF-8"; // use german locale
+
+
+    // gtk apparently calls 'setlocale(LC_ALL, "");'
+    // @@@ we should already call it here, to make ARB-motif-version behave samesame.
+
+    // --------------------------------------------------------------------------------
+    // Fails-counts mentioned below were determined under ubuntu 13.10/64bit.
+    // Make sure only one section is enabled!
+
 #if 0
-    setlocale(LC_ALL, ""); // this is apparently done by gtk as well.
-                           // already call it here to make motif-version behave samesame.
+    // enabling this section does not fail any unit test
+    setlocale(LC_ALL,  "C");
+#endif
+
+#if 0
+    // enabling this section does not fail any unit test
+    setlocale(LC_ALL,     USER_LOCALE);
     setlocale(LC_NUMERIC, "C");
+#endif
+
+#if 0
+    // enabling this section does not fail any unit test
+    setlocale(LC_ALL,      "C");
+    setlocale(LC_COLLATE,  USER_LOCALE);
+    setlocale(LC_CTYPE,    USER_LOCALE);
+    setlocale(LC_MESSAGES, USER_LOCALE);
+    setlocale(LC_MONETARY, USER_LOCALE);
+    setlocale(LC_TIME,     USER_LOCALE);
+#endif
+
+
+#if 0
+    // enabling this section fails 57 unit tests (for german USER_LOCALE)
+    setlocale(LC_ALL, USER_LOCALE);
+#endif
+
+#if 0
+    // enabling this section fails 57 unit tests (for german USER_LOCALE)
+    setlocale(LC_COLLATE,  "C");
+    setlocale(LC_CTYPE,    "C");
+    setlocale(LC_MESSAGES, "C");
+    setlocale(LC_MONETARY, "C");
+    setlocale(LC_NUMERIC,  USER_LOCALE);
+    setlocale(LC_TIME,     "C");
+#endif
+
+#if 1
+    // enabling this section fails 57 unit tests (for german USER_LOCALE)
+    setlocale(LC_ALL,     "C");
+    setlocale(LC_NUMERIC, USER_LOCALE);
 #endif
 }
 
