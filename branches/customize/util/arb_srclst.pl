b67259fe702ed562958e8b0234a98656288c8a95
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-03-23 11:59:29 +0000

    fix 'savetest'

--- branches/customize/util/arb_srclst.pl
+++ branches/customize/util/arb_srclst.pl
@@ -27,7 +27,7 @@ my @skipped_directories = (
                            qr/\/HEADERLIBS\/[^\/]+/o,
                            qr/\/UNIT_TESTER\/logs$/o,
                            qr/\/UNIT_TESTER\/tests$/o,
-                           qr/\/UNIT_TESTER\/run\/fakehome$/o,
+                           qr/\/UNIT_TESTER\/run\/fakehome\/.arb_prop\/(macros|cfgSave)$/o,
                            qr/^\.\/ARB_SOURCE_DOC/o,
                            qr/^\.\/dep_graphs/o,
                            qr/^\.\/INCLUDE$/o,
