b05bc646e61fd0a7249d424a3a66ee027e6ab5c6
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-04-03 11:30:33 +0000

    * merge [11629:11638] from 'trunk' into 'gtk_only'   * adds:     - log:trunk@11630,11632:11638

--- branches/gtk_only/TEMPLATES/arb_version.h
+++ branches/gtk_only/TEMPLATES/arb_version.h
@@ -13,26 +13,20 @@
 
 // exports the following variables:
 //
-// ARB_BUILD_DATE            contains the date on which arb was build
-// ARB_BUILD_YEAR            contains the year in which arb was build
+// ARB_BUILD_DATE           contains the date on which arb was build
+// ARB_BUILD_YEAR           contains the year in which arb was build
 // ARB_BUILD_HOST           contains the host on which arb was build
 // ARB_BUILD_USER           contains the user by whom  arb was build
 //
-// ARB_VERSION          arb version
-//
-// may define the following variable:
-//
-// SHOW_WHERE_BUILD     show build location/user (at arb_ntree command line and in mails sent from ARB)
-//                      has to be set for non-official builds!
+// ARB_VERSION              arb version (no revision number for rc and stable)
+// ARB_VERSION_DETAILED     arb version (always including revision number)
 
 #include <arb_build.h>
 #include <svn_revision.h>
 
-#ifndef ARB_VERSION_MAJOR
+#ifndef ARB_VERSION
 #error No version defined
 #endif
-
-# define ARB_VERSION ARB_VERSION_MAJOR "." ARB_VERSION_MINOR "-" ARB_VERSION_TAG "-" ARB_SVN_REVISION
 
 #else
 #error arb_version.h included twice
