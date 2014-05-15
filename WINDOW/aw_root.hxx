c3657959419e851038f8ed398efb2c68fc54bd6c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-05-15 15:59:57 +0000

    * elim unused function aw_input2awar

--- WINDOW/aw_root.hxx
+++ WINDOW/aw_root.hxx
@@ -37,8 +37,6 @@ char *aw_input(const char *title, const char *prompt, const char *default_input)
 char *aw_input(const char *prompt, const char *default_input);
 inline char *aw_input(const char *prompt) { return aw_input(prompt, NULL); }
 
-char *aw_input2awar(const char *prompt, const char *awar_value);
-
 char *aw_string_selection     (const char *title, const char *prompt, const char *default_value, const char *value_list, const char *buttons);
 char *aw_string_selection2awar(const char *title, const char *prompt, const char *awar_name,     const char *value_list, const char *buttons);
 
