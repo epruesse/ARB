9d5054b3844d30db1bb7c34c45dc6f2aa7fba301
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-05-24 12:05:22 +0000

    - do not compute 'no tree' (fixes #551)

--- trunk/SL/TREEDISP/TreeDisplay.hxx
+++ trunk/SL/TREEDISP/TreeDisplay.hxx
@@ -191,7 +191,10 @@ class AWT_graphic_tree : public AWT_graphic, virtual Noncopyable {
     }
 
     virtual void read_tree_settings();
-    void update_structure() { get_root_node()->compute_tree(); }
+    void update_structure() {
+        AP_tree *root = get_root_node();
+        if (root) root->compute_tree();
+    }
     void apply_zoom_settings_for_treetype(AWT_canvas *ntw);
 
     int draw_branch_line(int gc, const AW::Position& root, const AW::Position& leaf, AW_bitset filter) {
