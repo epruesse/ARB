be28f9d8ce71ca9595858a5a0c59893222330bfb
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-05-31 14:39:27 +0000

    * "merge" WINDOW-changes from 'trunk' into 'gtk_only'   * adds:     - log:trunk@12331,12335,12343:12344

--- branches/gtk_only/WINDOW/aw_window.hxx
+++ branches/gtk_only/WINDOW/aw_window.hxx
@@ -437,12 +437,13 @@ public:
 private:
     static void popper(AW_window *, CreateWindowCallback *windowMaker);
     static void replacer(AW_window *aww, CreateWindowCallback *windowMaker);
+    static void destroyCreateWindowCallback(CreateWindowCallback *windowMaker);
 public:
     static WindowCallback makeWindowPopper(const CreateWindowCallback& cwcb) {
-        return makeWindowCallback(popper, new CreateWindowCallback(cwcb));
+        return makeWindowCallback(popper, destroyCreateWindowCallback, new CreateWindowCallback(cwcb));
     }
     static WindowCallback makeWindowReplacer(const CreateWindowCallback& cwcb) {
-        return makeWindowCallback(replacer, new CreateWindowCallback(cwcb));
+        return makeWindowCallback(replacer, destroyCreateWindowCallback, new CreateWindowCallback(cwcb));
     }
 
     // normal callbacks
