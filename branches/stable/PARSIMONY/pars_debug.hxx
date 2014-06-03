80737603e60a0c80d9fbf20c4734acfcc39003bf
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-06-03 16:30:23 +0000

    [branch_stable] delete branch 'stable'

--- branches/stable/PARSIMONY/pars_debug.hxx
+++
@@ -1,3 +0,0 @@
-// dummy funktion fuer darstellung
-void     display_out(double * liste, int anz, double prev, double pars_start, int rek_deep);
-void     display_clear(double(*funktion) (double, double *, int), double * parm_liste, int param_anz, int pars_start, int max_rek_deep);
