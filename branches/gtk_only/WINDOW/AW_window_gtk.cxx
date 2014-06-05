8ba10827daa74052282e6e1df5658ff1f0c8aefb
epruesse <epruesse@129cd134-2152-0410-ab65-f3f895bafe68>
2014-06-05 17:47:12 +0000

    fix double allocation of AW_window_gtk and make it a toplevel class

--- branches/gtk_only/WINDOW/AW_window_gtk.cxx
+++ branches/gtk_only/WINDOW/AW_window_gtk.cxx
@@ -1,42 +1,53 @@
 #include "aw_window.hxx"
 #include "aw_window_gtk.hxx"
 
-AW_window::AW_window_gtk::AW_window_gtk() 
-  :   window(GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL))), 
-      fixed_size_area(NULL), menu_bar(NULL), help_menu(NULL),
-      mode_menu(NULL), radio_last(NULL), 
+AW_window_gtk::AW_window_gtk() 
+  :   window(NULL), 
+      fixed_size_area(NULL), 
+      menu_bar(NULL), 
+      help_menu(NULL),
+      mode_menu(NULL),
+      radio_last(NULL), 
       selection_list(NULL),
       toggle_field(NULL), 
       combo_box(NULL),       
-      accel_group(gtk_accel_group_new()),
+      accel_group(NULL),
       areas(AW_MAX_AREA, NULL),
-      drawing_area(NULL), hide_on_close(true), close_action(NULL)
+      drawing_area(NULL), 
+      hide_on_close(true),
+      close_action(NULL)
 {
+    window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
+    accel_group = gtk_accel_group_new();
+
     gtk_window_add_accel_group(window, accel_group);
     aw_assert(areas.size() == AW_MAX_AREA);
 } 
 
-void AW_window::AW_window_gtk::show() {
+AW_window_gtk::~AW_window_gtk() {
+}
+
+void AW_window_gtk::show() {
     gtk_widget_show_all(GTK_WIDGET(window));
 }
 
-void AW_window::AW_window_gtk::set_title(const char* title) {
+void AW_window_gtk::set_title(const char* title) {
     gtk_window_set_title(window, title);
 }
 
-void AW_window::AW_window_gtk::set_size(int width, int height) {
+void AW_window_gtk::set_size(int width, int height) {
     gtk_window_set_default_size(window, width, height);
 }
 
-void AW_window::AW_window_gtk::get_size(int& width, int& height) {
+void AW_window_gtk::get_size(int& width, int& height) {
     gtk_window_get_size(window, &width, &height);
 }
 
-void AW_window::AW_window_gtk::set_resizable(bool resizable) {
+void AW_window_gtk::set_resizable(bool resizable) {
     gtk_window_set_resizable(window, resizable);
 }
 
-void AW_window::AW_window_gtk::get_font_size(int &width, int& height) {
+void AW_window_gtk::get_font_size(int &width, int& height) {
     PangoContext  *pc = gtk_widget_get_pango_context(GTK_WIDGET(window));
     PangoFontMetrics *pfm = pango_context_get_metrics(pc, NULL, NULL);
     height = PANGO_PIXELS(pango_font_metrics_get_ascent(pfm) +
@@ -46,3 +57,4 @@ void AW_window::AW_window_gtk::get_font_size(int &width, int& height) {
                          
     pango_font_metrics_unref(pfm);
 }
+
