068b0ea7bb9404688e367aae6d7eeddc611e9166
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2014-09-11 12:31:41 +0000

    * "merge" [12850] from 'trunk' into 'gtk_only'

--- WINDOW/AW_modal.cxx
+++ WINDOW/AW_modal.cxx
@@ -31,7 +31,6 @@
 
 using namespace std;
 
-const char *awar_button_name = "tmp/input/button"; // @@@ eliminate this awar (a module-global int-variable serves better)
 const char *awar_string_name = "tmp/input/string";
 
 static char *input2awar(const char *prompt, const char *awar_name, const char *title) {
@@ -68,94 +67,6 @@ char *aw_input(const char *title, const char *prompt, const char *default_input)
 
 char *aw_input(const char *prompt, const char *default_input) {
     return aw_input("Enter string", prompt, default_input);
-}
-
-int aw_string_selection_button() {
-    AW_awar *awar_button = AW_root::SINGLETON->awar(awar_button_name);
-    return awar_button->read_int();
-}
-
-/**
- * Popups up a modal window asking for a string input with 
- * a selection of defaults offered.
- * @param  title          The title of the window.
- * @param  prompt         A prompt shown above the input field.
- * @param  default_input  A default string shown in the input field (or NULL)
- * @param  value_liist    A semi-colon separated list of defaults (or NULL)
- * @param  buttons        A comma separated list of buttons (default is "Ok,Cancel").
- *                        Answer can be retrieved with aw_string_selection_button()
- * @return The string.
- */
-char *aw_string_selection(const char *title, const char *prompt, const char *default_input, const char *value_list, const char *buttons) {
-    aw_return_val_if_fail(title, NULL);
-    aw_return_val_if_fail(prompt, NULL);
-
-    
-    AW_awar *awar_button = AW_root::SINGLETON->awar_int(awar_button_name);
-    AW_awar *awar_string = AW_root::SINGLETON->awar_string(awar_string_name);
-    if (default_input) {
-        awar_string->write_string(default_input);
-    }
-
-    AW_dialog dialog(title, prompt);
-    dialog.create_input_field(awar_string);
-    AW_selection_list *slist = dialog.create_selection_list(awar_string, false);
-    dialog.create_buttons(buttons ? buttons : "Ok,-Abort");
-    
-    slist->clear();
-    slist->insert_default("<new>", "");
-    if (value_list) {
-        char *values = strdup(value_list);
-        char *word;
-
-        for (word = strtok(values, ";"); word; word = strtok(0, ";")) {
-            slist->insert(word, word);
-        }
-        free(values);
-    }
-    slist->update();
-
-    dialog.run();
-
-    int buttonNr = dialog.get_result();
-    awar_button->write_int(buttonNr);
-
-    return buttonNr>=0 ? awar_string->read_string() : NULL;
-
-    /*
-    char *this_input = root->awar(AW_INPUT_AWAR)->read_string();
-    if (strcmp(this_input, last_input) != 0) {
-        if (check_fun) {
-            char *corrected_input = check_fun(this_input);
-            if (corrected_input) {
-                if (strcmp(corrected_input, this_input) != 0) {
-                    root->awar(AW_INPUT_AWAR)->write_string(corrected_input);
-                }
-                        free(corrected_input);
-            }
-        }
-        reassign(last_input, this_input);
-    }
-    free(this_input);
-    */
-}
-
-char *aw_string_selection2awar(const char *title, const char *prompt, const char *awar_name, const char *value_list, const char *buttons) {
-    // params see aw_string_selection
-    //
-    // default for string is taken from awar 'awar_name'.
-    // result is written back to awar.
-    // if abort button is pressed, old value is written back to awar.
-
-    AW_root *aw_root   = AW_root::SINGLETON;
-    AW_awar *awar      = aw_root->awar(awar_name);
-    char    *old_value = awar->read_string();
-    char    *result    = aw_string_selection(title, prompt, old_value, value_list, buttons);
-
-    awar->write_string(result ? result : old_value);
-    free(old_value);
-
-    return result;
 }
 
 // --------------------------
