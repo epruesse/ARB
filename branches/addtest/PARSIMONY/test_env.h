1b8a5e7915c8a131ab19a6d0386d130c0f9b7a66
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2015-03-13 10:39:44 +0000

    branch 'addtest' from [13122/branches/pars] [[BR]]Reason: recreate reintegrated branch [13260/branches/addtest] to add test for branchlength calculation

---
+++ branches/addtest/PARSIMONY/test_env.h
@@ -0,0 +1,106 @@
+// ================================================================= //
+//                                                                   //
+//   File      : test_env.h                                          //
+//   Purpose   :                                                     //
+//                                                                   //
+//   Coded by Ralf Westram (coder@reallysoft.de) in September 2014   //
+//   http://www.arb-home.de/                                         //
+//                                                                   //
+// ================================================================= //
+
+#ifndef TEST_ENV_H
+#define TEST_ENV_H
+
+void fake_AW_init_color_groups();
+
+template<typename SEQTYPE>
+struct fake_agt : public AWT_graphic_parsimony, virtual Noncopyable {
+    SEQTYPE *templ;
+
+    fake_agt(ArbParsimony& parsimony_)
+        : AWT_graphic_parsimony(parsimony_, GLOBAL_gb_main, NULL),
+          templ(NULL)
+    {
+    }
+    ~fake_agt() {
+        delete templ;
+    }
+    void init(AliView *aliview) {
+        fake_AW_init_color_groups(); // acts like no species has a color
+        delete templ;
+        templ = aliview->has_data() ? new SEQTYPE(aliview) : NULL;
+        AWT_graphic_tree::init(new AP_TreeNlenNodeFactory, aliview, templ, true, false);
+    }
+};
+
+
+template<typename SEQTYPE>
+class PARSIMONY_testenv : virtual Noncopyable {
+    GB_shell           shell;
+    AP_main            apMain;
+    fake_agt<SEQTYPE> *agt;
+    ArbParsimony       parsimony;
+
+    void common_init(const char *dbname) {
+        GLOBAL_gb_main = NULL;
+        apMain.open(dbname);
+
+        TEST_EXPECT_NULL(ap_main);
+        ap_main = &apMain;
+
+        agt = new fake_agt<SEQTYPE>(parsimony);
+        apMain.set_tree_root(agt);
+    }
+
+public:
+    PARSIMONY_testenv(const char *dbname)
+        : parsimony(NULL)
+    {
+        common_init(dbname);
+        agt->init(new AliView(GLOBAL_gb_main));
+    }
+    PARSIMONY_testenv(const char *dbname, const char *aliName);
+    ~PARSIMONY_testenv() {
+        if (root_node()) {
+            AP_tree_edge::destroy(root_node());
+        }
+
+        TEST_EXPECT_EQUAL(ap_main, &apMain);
+        ap_main = NULL;
+
+        delete agt;
+        GB_close(GLOBAL_gb_main);
+        GLOBAL_gb_main = NULL;
+    }
+
+    AP_tree_nlen *root_node() { return apMain.get_root_node(); }
+    AP_tree_root *tree_root() { return agt->get_tree_root(); }
+
+    GB_ERROR load_tree(const char *tree_name) {
+        GB_transaction ta(GLOBAL_gb_main);      // @@@ do inside AWT_graphic_tree::load?
+        GB_ERROR       error = agt->load(GLOBAL_gb_main, tree_name, 0, 0);
+        if (!error) {
+            AP_tree_edge::initialize(rootNode());   // builds edges
+
+            ap_assert(root_node());
+            ap_assert(root_node() == rootNode()); // need tree-access via global 'ap_main' (too much code is based on that)
+            ap_assert(rootEdge());
+
+            TEST_ASSERT_VALID_TREE(root_node());
+        }
+        return error;
+    }
+
+    void push() { apMain.push(); }
+    void pop() { apMain.pop(); }
+
+    AWT_graphic_parsimony *graphic_tree() { return agt; }
+
+    GBDATA *gbmain() const { return GLOBAL_gb_main; }
+};
+
+
+#else
+#error test_env.h included twice
+#endif // TEST_ENV_H
+
---
+++ branches/addtest/WINDOW/aw_color_groups.hxx
@@ -0,0 +1,63 @@
+//  =============================================================== //
+//                                                                  //
+//    File      : aw_color_groups.cxx                               //
+//    Purpose   :                                                   //
+//                                                                  //
+//    Coded by Ralf Westram (coder@reallysoft.de) in June 2001      //
+//    Institute of Microbiology (Technical University Munich)       //
+//    http://www.arb-home.de/                                       //
+//                                                                  //
+//  =============================================================== //
+
+#ifndef AW_COLOR_GROUPS_HXX
+#define AW_COLOR_GROUPS_HXX
+
+#ifndef AW_BASE_HXX
+#include "aw_base.hxx"
+#endif
+#ifndef ARBDB_BASE_H
+#include <arbdb_base.h>
+#endif
+
+
+// color groups define special colors used to colorize
+// groups of genes or species (or any other items)
+//
+// to activate color_group-colors you need the following:
+//
+//  - call AW_init_color_group_defaults with current program name
+//
+//  - call AW_manage_GC with define_color_groups set to true
+//    This defines extra GC's for color_groups (named color_group_xxx)
+//    If such GCs are defined the normal properties window (AW_create_gc_window)
+//    automatically gets a sub-window called 'Edit color-groups'
+//
+//  - add a label in your GC enum (at the end before BLA_GC_MAX)
+//    like this:
+//
+//          BLA_GC_FIRST_COLOR_GROUP,
+//          BLA_GC_MAX = BLA_GC_FIRST_COLOR_GROUP + AW_COLOR_GROUPS
+//
+//  - use AW_set_color_group to set and AW_find_color_group to read the
+//    color of a species/gene/experiment/organism/etc.
+
+#define AWAR_COLOR_GROUPS_PREFIX "color_groups"
+#define AWAR_COLOR_GROUPS_USE    AWAR_COLOR_GROUPS_PREFIX "/use" // int : whether to use the colors in display or not
+
+#define AW_COLOR_GROUP_PREFIX     "color_group_"
+#define AW_COLOR_GROUP_PREFIX_LEN 12
+#define AW_COLOR_GROUP_NAME_LEN   (AW_COLOR_GROUP_PREFIX_LEN+2)
+
+#define AW_COLOR_GROUP_ENTRY "ARB_color"
+#define AW_COLOR_GROUPS      12
+
+GB_ERROR  AW_set_color_group(GBDATA *gbd, long color_group);
+long      AW_find_color_group(GBDATA *gbd, bool ignore_usage_flag = false);
+char     *AW_get_color_group_name(AW_root *awr, int color_group);
+
+void AW_init_color_group_defaults(const char *for_program);
+
+#else
+#error aw_color_groups.hxx included twice
+#endif // AW_COLOR_GROUPS_HXX
+
