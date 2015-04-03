4d7751d51c39f1cc43c96ebcdf2d5aaa0c3b3a13
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2015-04-03 11:20:25 +0000

    - fix progress counter

--- PRIMER_DESIGN/PRD_Design.cxx
+++ PRIMER_DESIGN/PRD_Design.cxx
@@ -492,7 +492,7 @@ void PrimerDesign::matchSequenceAgainstPrimerTrees()
     printf("root2 : [C %p, G %p, A %p, TU %p]\n", root2->child[0], root2->child[1], root2->child[2], root2->child[3]);
 #endif
 
-    arb_progress progress(seqLength);
+    arb_progress progress(seqLength*2);
 
     progress.subtitle("match possible primers vs. seq. (forward)");
 
