fa35839c2e78509d47edad541619e180377e16d9
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2015-05-27 05:50:14 +0000

    - do not use more than 80% of cores  - run at least 2 jobs (on single cores)

--- util/usecores.pl
+++ util/usecores.pl
@@ -7,6 +7,6 @@ if ($cores<1) { $cores = 1; }
 my $usedcores = int($cores * 0.8); # reserve some cores for docker etc.
 if ($usedcores<1) { $usedcores = 1; }
 
-my $jobs = $usedcores + 1;
+my $jobs = ($usedcores < 2) ? 2 : $usedcores; # use at least 2 jobs
 print "$jobs";
 
