73cb90aa73d189eee18f9c2b2fa6e9ad44c83a8f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2015-07-10 15:04:59 +0000

    * merge [13763] from 'trunk' into 'alilink'     - resolve conflict caused by code move in 'alilink' (ad_transpro.cxx->!TranslateRealign.cxx)

--- branches/alilink/SL/ALILINK/TranslateRealign.cxx
+++ branches/alilink/SL/ALILINK/TranslateRealign.cxx
@@ -946,6 +946,7 @@ struct Data : virtual Noncopyable {
     Data(GBDATA *gb_species, const char *aliName)
         : gb_data(NULL),
           data(NULL),
+          len(0),
           error(NULL)
     {
         GBDATA *gb_ali = GB_entry(gb_species, aliName);
