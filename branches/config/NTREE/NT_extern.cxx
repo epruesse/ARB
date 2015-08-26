6438e4f7828ce2a5807be4195cc8b0e8e5a7c2b2
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2015-08-26 13:01:50 +0000

    * terminology fixes (species selection vs. editor configuration)

--- branches/config/NTREE/NT_extern.cxx
+++ branches/config/NTREE/NT_extern.cxx
@@ -1082,7 +1082,7 @@ static AW_window *popup_new_main_window(AW_root *awr, int clone) {
             NT_insert_mark_submenus(awm, ntw, 1);
             awm->insert_menu_topic("mark_by_ref",     "Mark by reference..", "r", "markbyref.hlp",       AWM_EXP, makeCreateWindowCallback(create_mark_by_refentries_window, GLOBAL.gb_main));
             awm->insert_menu_topic("species_colors",  "Colors ...",          "l", "colorize.hlp",        AWM_ALL, create_colorize_species_window);
-            awm->insert_menu_topic("selection_admin", "Configurations",      "o", "species_configs.hlp", AWM_ALL, makeWindowCallback(NT_popup_configuration_admin, ntw));
+            awm->insert_menu_topic("selection_admin", "Selections ...",      "o", "species_configs.hlp", AWM_ALL, makeWindowCallback(NT_popup_configuration_admin, ntw));
 
             awm->sep______________();
 
