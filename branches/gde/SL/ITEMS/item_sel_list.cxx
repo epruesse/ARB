b876ed9234b5438882f468aaa7e9877948829871
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2016-04-04 09:50:06 +0000

    * introduce new interface for writing/reading database fields     - motivation:       * wherever the user selects database fields to hold some data, ARB has to ensure the field is capable to store that data (w/o precision loss)       * currently this is not ensured, resulting in failures if a wrong database field is selected     - provided changes:       * lossless read/write functions (for BYTE, INT and FLOAT types)       * redefines field-filter-types (for BYTE, INT and FLOAT types): INT may no longer be written into FLOAT fields       * added unittest for lossless read/write; also tests field-filter-validity

--- branches/gde/SL/ITEMS/item_sel_list.cxx
+++ branches/gde/SL/ITEMS/item_sel_list.cxx
@@ -188,3 +188,156 @@ Itemfield_Selection *create_selection_list_on_itemfields(GBDATA         *gb_main
     return selection;
 }
 
+// --------------------------------------------------------------------------------
+
+#ifdef UNIT_TESTS
+#ifndef TEST_UNIT_H
+#include <test_unit.h>
+#endif
+
+#include <arb_defs.h>
+
+void TEST_lossless_conversion() {
+    GB_shell  shell;
+    GBDATA   *gb_main = GB_open("no.arb", "c");
+
+    {
+        GB_transaction ta(gb_main);
+
+        const long tested_filter[] = {
+            FIELD_FILTER_INT_WRITEABLE,
+            FIELD_FILTER_BYTE_WRITEABLE,
+            FIELD_FILTER_FLOAT_WRITEABLE,
+        };
+
+        for (unsigned f = 0; f<ARRAY_ELEMS(tested_filter); ++f) {
+            const long filter = tested_filter[f];
+
+            for (GB_TYPES type = GB_BYTE; type<=GB_STRING; type = GB_TYPES(type+1)) {
+                if (type == GB_POINTER) continue;
+
+                TEST_ANNOTATE(GBS_global_string("type=%i", int(type)));
+                GBDATA *gb_field = GB_create(gb_main, "any", type);
+                TEST_REJECT_NULL(gb_field);
+
+                if (filter & (1<<type)) {
+                    switch (filter) {
+                        case FIELD_FILTER_INT_WRITEABLE: {
+                            const int I_min(-2147483648);    // 32bit int min ..
+                            const int I_max(2147483647);     // .. and max
+
+                            int tested_int_value[] = { I_min, -1, 0, 1, I_max-1, I_max };
+                            for (unsigned i = 0; i<ARRAY_ELEMS(tested_int_value); ++i) {
+                                int written = tested_int_value[i];
+
+                                TEST_ANNOTATE(GBS_global_string("type=%i INT written=%i", int(type), written));
+                                TEST_EXPECT_NO_ERROR(GB_write_lossless_int(gb_field, written));
+
+                                GB_ERROR error = NULL;
+                                int      read  = GB_read_lossless_int(gb_field, error);
+
+                                TEST_EXPECT_NO_ERROR(error);
+                                TEST_EXPECT_EQUAL(written, read);
+                            }
+                            break;
+                        }
+                        case FIELD_FILTER_BYTE_WRITEABLE: {
+                            uint8_t tested_byte_value[] = { 0, 1, 127, 128, 255 };
+                            for (unsigned i = 0; i<ARRAY_ELEMS(tested_byte_value); ++i) {
+                                uint8_t written = tested_byte_value[i];
+
+                                TEST_ANNOTATE(GBS_global_string("type=%i BYTE written=%u", int(type), written));
+                                TEST_EXPECT_NO_ERROR(GB_write_lossless_byte(gb_field, written));
+
+                                GB_ERROR error = NULL;
+                                uint8_t  read  = GB_read_lossless_byte(gb_field, error);
+
+                                TEST_EXPECT_NO_ERROR(error);
+                                TEST_EXPECT_EQUAL(written, read);
+                            }
+                            break;
+                        }
+                        case FIELD_FILTER_FLOAT_WRITEABLE: {
+                            float tested_double_value[] = {
+                                0.0, 1.0, 0.5,
+                                1/3.0,
+                                3.123456789,
+                                1234567891.,
+                                123456789.1,
+                                12345678.91,
+                                1234567.891,
+                                123456.7891,
+                                12345.67891,
+                                1234.567891,
+                                123.4567891,
+                                12.34567891,
+                                1.234567891,
+                                .1234567891,
+                                .0123456789,
+                                .00123456789,
+                                .000123456789,
+                                .0000123456789,
+                                .00000123456789,
+                                .000000123456789,
+                                .0000000123456789,
+                                .00000000123456789,
+                                .000000000123456789,
+                                .0000000000123456789,
+                                .00000000000000000000123456789,
+                                .000000000000000000000000000000123456789,
+                                .0000000000000000000000000000000000000000123456789,
+                                .00000000000000000000000000000000000000000000000000123456789,
+                                .000000000000000000000000000000000000000000000000000000000000123456789,
+                                123456789.123456,
+                                123456789.123456789,
+                                M_PI,
+                                123456789.3,
+                            };
+
+                            for (unsigned i = 0; i<ARRAY_ELEMS(tested_double_value); ++i) {
+                                float written = tested_double_value[i];
+
+                                TEST_ANNOTATE(GBS_global_string("type=%i FLOAT written=%f", int(type), written));
+                                TEST_EXPECT_NO_ERROR(GB_write_lossless_float(gb_field, written));
+
+                                double EPSILON = written*0.000001; // choose epsilon value depending on written value
+                                if (EPSILON<=0.0) { // avoid zero epsilon value
+                                    EPSILON = 0.000000001;
+                                }
+
+                                GB_ERROR error = NULL;
+                                float    read  = GB_read_lossless_float(gb_field, error);
+
+                                TEST_EXPECT_NO_ERROR(error);
+                                TEST_EXPECT_SIMILAR(written, read, EPSILON);
+                            }
+                            break;
+                        }
+                        default: it_assert(0); break; // missing test for filter
+                    }
+                }
+                else {
+                    // test that GB_write_lossless_... does fail for other types:
+                    switch (filter) {
+                        case FIELD_FILTER_INT_WRITEABLE:
+                            TEST_EXPECT_ERROR_CONTAINS(GB_write_lossless_int(gb_field, 4711), "Cannot use");
+                            break;
+                        case FIELD_FILTER_BYTE_WRITEABLE:
+                            TEST_EXPECT_ERROR_CONTAINS(GB_write_lossless_byte(gb_field, 128), "Cannot use");
+                            break;
+                        case FIELD_FILTER_FLOAT_WRITEABLE:
+                            TEST_EXPECT_ERROR_CONTAINS(GB_write_lossless_float(gb_field, M_PI), "Cannot use");
+                            break;
+                        default: it_assert(0); break; // missing test for filter
+                    }
+                }
+            }
+        }
+    }
+
+    GB_close(gb_main);
+}
+
+#endif // UNIT_TESTS
+
+// --------------------------------------------------------------------------------
