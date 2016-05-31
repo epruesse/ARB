6a1fe32006d40687c6f1597255819414ce3f3be3
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2016-05-31 11:02:41 +0000

    * full update from 'trunk' into 'gtk_only'   * adds: log:trunk@14887,14889,14891,14893:14894

--- branches/gtk_only/WINDOW/AW_preset.cxx
+++ branches/gtk_only/WINDOW/AW_preset.cxx
@@ -392,7 +392,7 @@ void AW_gc_manager::add_gc(const char* gc_description, int& gc, bool is_color_gr
 
         for (gc_container::iterator g = GCs.begin(); g != GCs.end(); ++g) {
             if (strcmp(g->colorlabel.c_str(), referenced_colorlabel) == 0) {
-                default_color = AW_root::SINGLETON->awar(color_awarname(gc_base_name, g->key))->read_char_pntr();
+                default_color = AW_root::SINGLETON->awar(color_awarname(gc_base_name, g->key))->read_char_pntr(); // @@@ should use default value (not current value)
                 found         = true;
                 break;
             }
@@ -602,16 +602,10 @@ char *AW_get_color_group_name(AW_root *awr, int color_group) {
 static const int STD_LABEL_LEN = 15;
 
 void AW_gc_manager::create_gc_buttons(AW_window *aws, bool for_colorgroups) {
-    int idx = 0;
+    for (int idx = for_colorgroups ? first_colorgroup_idx : 0; idx<int(GCs.size()); ++idx) {
+        const gc_desc& gcd = GCs[idx];
 
-    gc_container::iterator gcd = GCs.begin();
-    if (for_colorgroups) {
-        advance(gcd, first_colorgroup_idx);
-        idx += first_colorgroup_idx;
-    }
-
-    for (; gcd != GCs.end(); ++gcd, ++idx) { // @@@ loop over idx
-        if (gcd->is_color_group() != for_colorgroups) continue;
+        if (gcd.is_color_group() != for_colorgroups) continue;
 
         if (for_colorgroups) {
             int color_group_no = idx-first_colorgroup_idx+1;
@@ -623,16 +617,16 @@ void AW_gc_manager::create_gc_buttons(AW_window *aws, bool for_colorgroups) {
         }
         else {
             aws->label_length(STD_LABEL_LEN);
-            aws->label(gcd->colorlabel.c_str());
+            aws->label(gcd.colorlabel.c_str());
         }
 
         // length of color button cannot be modified (diff to motif version)
-        aws->create_color_button(color_awarname(gc_base_name, gcd->key), NULL);
-        if (gcd->has_font)   {
+        aws->create_color_button(color_awarname(gc_base_name, gcd.key), NULL);
+        if (gcd.has_font)   {
             // length of font button cannot be modified (diff to motif version)
-            aws->create_font_button(font_awarname(gc_base_name, gcd->key), NULL);
+            aws->create_font_button(font_awarname(gc_base_name, gcd.key), NULL);
         }
-        if (!gcd->same_line) aws->at_newline();
+        if (!gcd.same_line) aws->at_newline();
     }
 }
 
