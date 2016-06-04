9f846b1eb9324ac757ac22336bcb99bab9269dd1
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2016-06-04 14:32:00 +0000

    * color conversion     - native code for conversion between       * 16bit color,       * ascii colorstring and       * normalized color (float)     - WM-dependent code for xcolor-name -> rgb conversion     - unittests (does not test xcolor-name conversion in motif version; would need AW_root)

--- branches/color/WINDOW/AW_rgb.cxx
+++ branches/color/WINDOW/AW_rgb.cxx
@@ -10,35 +10,168 @@
 
 #include "aw_rgb.hxx"
 
+static AW_rgb16 CRAZY_PINK("#C0C");
+static AW_rgb16 convert_xcolorname(const char *xcolorname, bool& failed);
+
+void AW_rgb16::rescale(uint16_t my_max) {
+    float scale = MAX/my_max;
+
+    R = R*scale + 0.5;
+    G = G*scale + 0.5;
+    B = B*scale + 0.5;
+}
+
+AW_rgb16::AW_rgb16(const char *colorname) {
+    /*! converts colorname
+     * @param colorname  either color hexstring (allowed for orange: '#F80', '#ff8800', '#fFF888000' or '#ffff88880000') or X color name (e.g. "green")
+     */
+
+    bool failed = (colorname[0] != '#');
+    if (!failed) {
+        const char    *end;
+        unsigned long  num    = strtoul(colorname+1, const_cast<char**>(&end), 16);
+        int            length = end-colorname-1;
+
+        switch (length) {
+            case  3: R = num >>  8; G = (num >>  4) & 0xf;    B = num & 0xf;    rescale(0xf);   break;
+            case  6: R = num >> 16; G = (num >>  8) & 0xff;   B = num & 0xff;   rescale(0xff);  break;
+            case  9: R = num >> 24; G = (num >> 12) & 0xfff;  B = num & 0xfff;  rescale(0xfff); break;
+            case 12: R = num >> 32; G = (num >> 16) & 0xffff; B = num & 0xffff; break;
+
+            default:
+                aw_assert(0); // hex color spec has invalid length
+                failed = true;
+                break;
+        }
+    }
+
+    if (failed) *this = convert_xcolorname(colorname, failed);
+
+    if (failed) {
+        fprintf(stderr, "Warning: Failed to interpret color '%s' - fallback to crazy pink\n", colorname);
+        *this = CRAZY_PINK;
+    }
+}
+
+const char *AW_rgb16::ascii() const {
+    const int   FIXEDLEN = 1+3*4;
+    static char buffer[FIXEDLEN+1];
+
+#if defined(ASSERTION_USED)
+    int printed =
+#endif
+        sprintf(buffer, "#%04x%04x%04x", R, G, B);
+
+    aw_assert(printed == FIXEDLEN);
+
+    return buffer;
+}
+
+// --------------------------------------------------------------------------------
+
+#ifdef UNIT_TESTS
+#ifndef TEST_UNIT_H
+#include <test_unit.h>
+#endif
+
+#define TEST_ASCII_COLOR_CONVERT(i,o) TEST_EXPECT_EQUAL(AW_rgb16(i).ascii(),o)
+#define TEST_ASCII_COLOR_IDENT(c)     TEST_ASCII_COLOR_CONVERT(c,c);
+
+#define TEST_NORMALIZED_CONVERSION(c) TEST_EXPECT_EQUAL(AW_rgb16(AW_rgb_normalized(AW_rgb16(c))).ascii(), c);
+
+void TEST_rgb() {
+    // check construction from diff.-length color specifications
+    AW_rgb16 orange4("#f80");
+    AW_rgb16 orange8("#ff8800");
+    AW_rgb16 orange12("#fff888000");
+    AW_rgb16 orange16("#ffff88880000");
+
+    TEST_EXPECT(orange16 == AW_rgb16(65535, 34952, 0));
+    TEST_EXPECT(orange12 == orange16);
+    TEST_EXPECT(orange8  == orange12);
+    TEST_EXPECT(orange4  == orange8);
+
+    AW_rgb16 lblue4("#004");
+    AW_rgb16 lblue8("#000044");
+    AW_rgb16 lblue12("#000000444");
+    AW_rgb16 lblue16("#000000004444");
+
+    TEST_EXPECT(lblue16 == AW_rgb16(0, 0, 17476));
+    TEST_EXPECT(lblue12 == lblue16);
+    TEST_EXPECT(lblue8  == lblue12);
+    TEST_EXPECT(lblue4  == lblue8);
+
+    TEST_EXPECT_EQUAL(lblue8.ascii(), "#000000004444");
+
+    TEST_ASCII_COLOR_CONVERT("#1"   "2"   "3",   "#1111" "2222" "3333");
+    TEST_ASCII_COLOR_CONVERT("#45"  "67"  "89",  "#4545" "6767" "8989");
+    TEST_ASCII_COLOR_CONVERT("#678" "9ab" "cde", "#6786" "9ab9" "cdec");
+    TEST_ASCII_COLOR_CONVERT("#001" "010" "100", "#0010" "0100" "1001");
+
+    TEST_ASCII_COLOR_IDENT("#0123456789ab");
+    TEST_ASCII_COLOR_IDENT("#fedcba987654");
+
+    // conversion normalized -> 16bit for special values
+    TEST_EXPECT_EQUAL(AW_rgb16(AW_rgb_normalized(0.25,  0.5,    0.75  )).ascii(), "#3fff" "7fff" "bfff");
+    TEST_EXPECT_EQUAL(AW_rgb16(AW_rgb_normalized(1./3,  1./5,   1./7  )).ascii(), "#5555" "3333" "2492");
+    TEST_EXPECT_EQUAL(AW_rgb16(AW_rgb_normalized(.1,    .01,    .001  )).ascii(), "#1999" "028f" "0041");
+    TEST_EXPECT_EQUAL(AW_rgb16(AW_rgb_normalized(.0001, .00005, .00002)).ascii(), "#0006" "0003" "0001");
+
+    // test conversion 16bit -> normalized -> 16bit is stable
+    TEST_NORMALIZED_CONVERSION("#000000000000");
+    TEST_NORMALIZED_CONVERSION("#0123456789ab");
+    TEST_NORMALIZED_CONVERSION("#ffffffffffff");
+
+    // invalid colors
+    TEST_EXPECT(AW_rgb16("kashdkjasdh") == CRAZY_PINK);
+    TEST_EXPECT(AW_rgb16("")            == CRAZY_PINK);
+
+#ifdef ARB_GTK
+    // x-colors (doesn't work w/o GUI in motif version)
+    TEST_EXPECT_EQUAL(AW_rgb16("white").ascii(), "#ffffffffffff");
+    TEST_EXPECT_EQUAL(AW_rgb16("black").ascii(), "#000000000000");
+#endif
+}
+
+#endif // UNIT_TESTS
+
+// --------------------------------------------------------------------------------
+//      WM-dependent code
+
 #if defined(ARB_GTK)
-// -------------
-//      gtk
 
-#include <gdk/gdk.h>
+# include <gdk/gdk.h>
 
-AW_rgb16::AW_rgb16(const char* name) {
-    GdkColor col;
-    gdk_color_parse(name, &col);
-    R = col.red;
-    G = col.green;
-    B = col.blue;
-}
+#else // ARB_MOTIF
 
-#else
-// ---------------
-//      motif
-
- // @@@ need gtk/motif independent AW_rgb16::AW_rgb16(const char* name); 
+#include "aw_root.hxx"
+#include "aw_window.hxx"
+#include "aw_window_Xm.hxx"
 
 #endif
 
-#if 0 // unused atm
-char* AW_rgb::ascii() const {
+static AW_rgb16 convert_xcolorname(const char *xcolorname, bool& failed) {
+#if defined(ARB_GTK)
     GdkColor col;
-    col.red   = red;
-    col.green = green;
-    col.blue  = blue;
-    return gdk_color_to_string(&col);
-}
+    failed = !gdk_color_parse(xcolorname, &col);
+#else // ARB_MOTIF
+    XColor   col, unused;
+    if (AW_root::SINGLETON) { // only works if window was set (by caller)
+        AW_root_Motif *mroot = AW_root::SINGLETON->prvt;
+        failed = !XLookupColor(mroot->display,
+                               mroot->colormap,
+                               xcolorname,
+                               &col,     // exact color
+                               &unused); // screen-color
+    }
+    else {
+        failed = true;
+    }
 #endif
+
+    return
+        failed
+        ? AW_rgb16()
+        : AW_rgb16(col.red, col.green, col.blue);
+}
 
--- branches/color/WINDOW/aw_rgb.hxx
+++ branches/color/WINDOW/aw_rgb.hxx
@@ -11,23 +11,37 @@
 #ifndef AW_RGB_HXX
 #define AW_RGB_HXX
 
+#ifndef _STDINT_H
 #include <stdint.h>
+#endif
 #ifndef CXXFORWARD_H
 #include <cxxforward.h>
+#endif
+#ifndef ARB_ASSERT_H
+#include <arb_assert.h>
+#endif
+
+#ifndef aw_assert
+#define aw_assert(bed) arb_assert(bed)
 #endif
 
 #if defined(ARB_GTK)
 #define AW_WINDOW_FG AW_rgb()
 #endif
 
+class AW_rgb_normalized;
+
 class AW_rgb16 {
     uint16_t R, G, B;
 
+    void rescale(uint16_t my_max);
 public:
     static CONSTEXPR float MAX = 65535.0;
 
     AW_rgb16() : R(0), G(0), B(0) {}
-    explicit AW_rgb16(const char* name);
+    AW_rgb16(uint16_t red, uint16_t green, uint16_t blue) : R(red), G(green), B(blue) {}
+    explicit AW_rgb16(const char *colorname);
+    explicit inline AW_rgb16(const AW_rgb_normalized& o);
 
     bool operator == (const AW_rgb16& o) const { return R==o.R && G==o.G && B == o.B; }
     bool operator != (const AW_rgb16& o) const { return !operator==(o); }
@@ -35,11 +49,14 @@ public:
     uint16_t r() const { return R; }
     uint16_t g() const { return G; }
     uint16_t b() const { return B; }
+
+    const char *ascii() const;
 };
 
 class AW_rgb_normalized {
     float R, G, B;
 public:
+    AW_rgb_normalized(float red, float green, float blue) : R(red), G(green), B(blue) {}
     explicit AW_rgb_normalized(const AW_rgb16& o) :
         R(o.r()/AW_rgb16::MAX),
         G(o.g()/AW_rgb16::MAX),
@@ -50,6 +67,8 @@ public:
     float g() const { return G; }
     float b() const { return B; }
 };
+
+inline AW_rgb16::AW_rgb16(const AW_rgb_normalized& o) : R(o.r()*MAX), G(o.g()*MAX), B(o.b()*MAX) {}
 
 #if defined(ARB_MOTIF)
 typedef unsigned long AW_rgb; // =XColor.pixel
