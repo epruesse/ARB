ab397bec62bf898c92fb25e57737494dc5be753f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2016-08-07 10:17:54 +0000

    * crash of r15167 in NDEBUG versions (eg. with gcc 4.4.3 and 4.4.7) is caused by [http://bugs.arb-home.de/wiki/CommonWarnings#dereferencingtype-punnedpointerwillbreakstrict-aliasingrules strict-aliasing] optimization     - fixing the warnings solves the problem

--- branches/fixres/CORE/arb_mem.h
+++ branches/fixres/CORE/arb_mem.h
@@ -32,24 +32,22 @@ namespace arb_mem {
         if (error) failed_to_allocate(strerror(error));
     }
 
-    inline void re_alloc(void **tgt, size_t nelem, size_t elsize) {
+    template<class TYPE>
+    inline void re_alloc(TYPE*& tgt, size_t nelem, size_t elsize) {
         size_t nsize = nelem*elsize;
 
-        void *mem = realloc(*tgt, nsize);
-        if (!mem) arb_mem::failed_to_allocate(nelem, elsize);
-
-        *tgt = mem;
+        tgt = (TYPE*)realloc(tgt, nsize);
+        if (!tgt) arb_mem::failed_to_allocate(nelem, elsize);
     }
-    inline void re_calloc(void **tgt, size_t oelem, size_t nelem, size_t elsize) {
+    template<class TYPE>
+    inline void re_calloc(TYPE*& tgt, size_t oelem, size_t nelem, size_t elsize) {
         size_t nsize = nelem*elsize;
 
-        void *mem = realloc(*tgt, nsize);
-        if (!mem) arb_mem::failed_to_allocate(nelem, elsize);
+        tgt = (TYPE*)realloc(tgt, nsize);
+        if (!tgt) arb_mem::failed_to_allocate(nelem, elsize);
 
         size_t osize = oelem*elsize;
-        if (nsize>osize) memset(((char*)mem)+osize, 0, nsize-osize);
-
-        *tgt = mem;
+        if (nsize>osize) memset(((char*)tgt)+osize, 0, nsize-osize);
     }
 };
 
@@ -62,13 +60,13 @@ inline void ARB_alloc_aligned(TYPE*& tgt, size_t nelems) {
 template<class TYPE>
 inline void ARB_realloc(TYPE*& tgt, size_t nelem) {
     /*! reallocate memoryblock to fit 'nelem' entries (terminate on failure) */
-    arb_mem::re_alloc((void**)&tgt, nelem, sizeof(TYPE));
+    arb_mem::re_alloc(tgt, nelem, sizeof(TYPE));
 }
 template<class TYPE>
 inline void ARB_recalloc(TYPE*& tgt, size_t oelem, size_t nelem) {
     /*! reallocate memoryblock to fit 'nelem' entries (partially cleared if 'oelem<nelem'; terminate on failure) */
     if (oelem != nelem) {
-        arb_mem::re_calloc((void**)&tgt, oelem, nelem, sizeof(TYPE));
+        arb_mem::re_calloc(tgt, oelem, nelem, sizeof(TYPE));
     }
 }
 
