09fcf2f7edf3dfe8d63d4d922fd1a72d04f79f6f
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2016-08-17 18:57:45 +0000

    * same as [15198] for gtk-code

--- WINDOW/aw_common_impl.hxx
+++ WINDOW/aw_common_impl.hxx
@@ -27,7 +27,7 @@ public:
                   AW_area     area);
     ~AW_common_gtk();
 
-    virtual AW_GC   *create_gc() OVERRIDE;
+    AW_GC   *create_gc() OVERRIDE;
 
     const AW_GC_gtk *map_gc(int gc) const { return DOWNCAST(const AW_GC_gtk*, AW_common::map_gc(gc)); }
     AW_GC_gtk       *map_mod_gc(int gc) { return DOWNCAST(AW_GC_gtk*, AW_common::map_mod_gc(gc)); }
@@ -42,8 +42,8 @@ class AW_GC_gtk : public AW_GC { // derived from Noncopyable
     struct Pimpl;
     Pimpl *prvt;
 
-    virtual void wm_set_font(const char* fontname, bool force_monospace=false) OVERRIDE;
-    virtual int get_actual_string_size(const char *str) const OVERRIDE;
+    void wm_set_font(const char* fontname, bool force_monospace=false) OVERRIDE;
+    int get_actual_string_size(const char *str) const OVERRIDE;
 public:
     AW_GC_gtk(AW_common *common);
     ~AW_GC_gtk() OVERRIDE;
