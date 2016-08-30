81af281bc0b7d428afbc1d12ed905f1ddc8a000c
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2016-08-30 13:01:53 +0000

    * {{{AW_font_limits}}}     - adds {{{was_notified()}}}; use as assertion     - default ctor: initializer list       * used by {{{reset()}}} (was: vice versa)     - (assert) combining ctor works for default-constructed instances     - bugfix: {{{min_width}}} was invalid for {{{AW_GC::get_font_limits(char c)}}}   * {{{AW_font_group}}}     - use another instance of {{{AW_font_limits}}} (instead of single variables) => default ctor works correct     - {{{unregisterAll()}}} just reinitializes this     - renames

--- trunk/WINDOW/aw_font_limits.hxx
+++ trunk/WINDOW/aw_font_limits.hxx
@@ -21,10 +21,9 @@ struct AW_font_limits {
     short width;
     short min_width;
 
-    void reset() {
-        ascent    = descent = width = 0;
-        min_width = SHRT_MAX;
-    }
+    void reset() { *this = AW_font_limits(); }
+
+    bool was_notified() const { return min_width != SHRT_MAX; } // true when notify... called
 
     void notify(short ascent_, short descent_, short width_) {
         ascent    = std::max(ascent_, ascent);
@@ -35,15 +34,22 @@ struct AW_font_limits {
 
     short get_height() const { return ascent+descent+1; }
 
-    bool is_monospaced() const { return width == min_width; }
+    bool is_monospaced() const { aw_assert(was_notified()); return width == min_width; }
 
-    AW_font_limits() { reset(); }
-    AW_font_limits(const AW_font_limits& lim1, const AW_font_limits& lim2) :
+    AW_font_limits() :
+        ascent(0),
+        descent(0),
+        width(0),
+        min_width(SHRT_MAX)
+    {}
+    AW_font_limits(const AW_font_limits& lim1, const AW_font_limits& lim2) : // works with notified and initial AW_font_limits
         ascent(std::max(lim1.ascent, lim2.ascent)),
         descent(std::max(lim1.descent, lim2.descent)),
         width(std::max(lim1.width, lim2.width)),
         min_width(std::min(lim1.min_width, lim2.min_width))
-    {}
+    {
+        aw_assert(correlated(lim1.was_notified() || lim2.was_notified(), was_notified()));
+    }
 };
 
 
