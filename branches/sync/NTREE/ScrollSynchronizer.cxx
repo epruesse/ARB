cc2c22baed7c441d090213e1695c8a370b8bc302
westram <westram@129cd134-2152-0410-ab65-f3f895bafe68>
2016-10-11 15:20:42 +0000

    * do not track outside clip-area (eliminates unwanted tracks)

--- branches/sync/NTREE/ScrollSynchronizer.cxx
+++ branches/sync/NTREE/ScrollSynchronizer.cxx
@@ -13,44 +13,50 @@
 #include <TreeDisplay.hxx>
 #include <awt_canvas.hxx>
 
+using namespace AW;
+
 class AW_trackSpecies_device: public AW_simple_device {
     SpeciesSet& species;
 
-    bool track(AW_bitset filteri) {
-        bool drawflag = false;
-        if (filteri & filter) {
-            const AW_click_cd *clickable = get_click_cd();
-            if (clickable) {
-                ClickedType  clicked    = (ClickedType)clickable->get_cd2();
-                GBDATA      *gb_species = NULL;
+    void track() {
+        const AW_click_cd *clickable = get_click_cd();
+        if (clickable) {
+            ClickedType  clicked    = (ClickedType)clickable->get_cd2();
+            GBDATA      *gb_species = NULL;
 
-                if (clicked == CL_SPECIES) {
+            switch (clicked) {
+                case CL_SPECIES: // NDS list
                     gb_species = (GBDATA*)clickable->get_cd1();
-                }
-                else if (clicked == CL_NODE) {
+                    break;
+
+                case CL_NODE:
+                case CL_BRANCH: {
                     TreeNode *node = (TreeNode*)clickable->get_cd1();
                     if (node->is_leaf) {
                         gb_species = node->gb_node;
                     }
+                    break;
                 }
+                default:
+                    nt_assert(0); // needs to be handled?
+                    break;
+            }
 
-                if (gb_species) {
+            if (gb_species) {
 #if defined(DEBUG)
-                    bool do_insert = species.find(gb_species) == species.end();
+                bool do_insert = species.find(gb_species) == species.end();
 #else // NDEBUG
-                    bool do_insert = true;
+                bool do_insert = true;
 #endif
-                    if (do_insert) {
+                if (do_insert) {
 #if defined(DUMP_SYNC) && 1
-                        const char *name = GBT_get_name(gb_species);
-                        fprintf(stderr, " - adding species #%zu '%s'\n", species.size(), name);
+                    const char *name = GBT_get_name(gb_species);
+                    fprintf(stderr, " - adding species #%zu '%s'\n", species.size(), name);
 #endif
-                        species.insert(gb_species);
-                    }
+                    species.insert(gb_species);
                 }
             }
         }
-        return drawflag;
     }
 
 public:
@@ -62,9 +68,24 @@ public:
     AW_DEVICE_TYPE type() OVERRIDE { return AW_DEVICE_CLICK; }
     void specific_reset() OVERRIDE {}
 
-    bool line_impl(int, const AW::LineVector&, AW_bitset filteri) OVERRIDE { return track(filteri); }
-    bool text_impl(int, const char*, const AW::Position&, AW_pos, AW_bitset filteri, long int) OVERRIDE { return track(filteri); }
-    bool invisible_impl(const AW::Position&, AW_bitset) OVERRIDE { return false; }
+    bool line_impl(int, const LineVector& Line, AW_bitset filteri) OVERRIDE {
+        bool drawflag = false;
+        if (filteri & filter) {
+            LineVector transLine = transform(Line);
+            LineVector clippedLine;
+            drawflag = clip(transLine, clippedLine);
+            if (drawflag) track();
+        }
+        return drawflag;
+    }
+    bool text_impl(int, const char*, const Position& pos, AW_pos, AW_bitset filteri, long int) OVERRIDE {
+        bool drawflag = false;
+        if (filteri & filter) {
+            if (!is_outside_clip(transform(pos))) track();
+        }
+        return drawflag;
+    }
+    bool invisible_impl(const Position&, AW_bitset) OVERRIDE { return false; }
 };
 
 
@@ -84,11 +105,6 @@ SpeciesSetPtr MasterCanvas::track_displayed_species() {
     device.reset();
 
     const AW_screen_area& rect = ntw->rect;
-
-    // @@@ too many species get tracked (some above, some below viewport borders of dendrogram view)
-    // possible reasons:
-    // - viewport has wrong size
-    // - need to test clipping in tracker
 
     device.set_top_clip_border(rect.t);
     device.set_bottom_clip_border(rect.b);
